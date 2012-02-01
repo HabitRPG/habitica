ad20410a0e28b0c945d510ed9b5c932674769604
Tyler Renelle <tylerrenelle@gmail.com>
2012-01-31 22:57:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HabitTracker::Application.config.secret_token = '30a31d09d78a5b71f1540b9f5c09a58aae42771273308f76f82efa9921a21ed47fd4c4e49076c052f0974a389820234601bdb0554ced2cbed78c8db3efe0dec4'
