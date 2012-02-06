6a7fa90cc4b63edde150c4a30e7ca9c114f6e50d
Tyler Renelle <tylerrenelle@gmail.com>
2012-02-06 12:50:19 -0500

    update_all zero out user levels and experience

--- db/migrate/20120206170948_add_lvl_experience_to_user.rb
+++ db/migrate/20120206170948_add_lvl_experience_to_user.rb
@@ -2,6 +2,8 @@ class AddLvlExperienceToUser < ActiveRecord::Migration
   def up
     add_column :users, :exp, :float, :default=>0.0
     add_column :users, :lvl, :integer, :default=>1
+    User.update_all ["lvl = ?", 1]
+    User.update_all ["exp = ?", 0.0]
   end
   
   def down
