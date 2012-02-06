ee4a9e376d235977cc5804bb78cd52235ca4c170
Tyler Renelle <tylerrenelle@gmail.com>
2012-02-06 13:14:21 -0500

    Set default reward.value to 20, not 1

--- db/migrate/20120204214727_create_rewards.rb
+++ db/migrate/20120204214727_create_rewards.rb
@@ -2,7 +2,7 @@ class CreateRewards < ActiveRecord::Migration
   def change
     create_table :rewards do |t|
       t.string :name
-      t.integer :value, :default=>1
+      t.integer :value, :default=>20
       t.integer :position, :default=>0
       t.references :user
 
