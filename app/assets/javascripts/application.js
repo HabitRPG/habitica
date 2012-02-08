7d7d7de36d18ecfc92cc7fc7fc518b7c90625421
Tyler Renelle <tylerrenelle@gmail.com>
2012-02-07 19:05:23 -0500

    after rails g backbone:install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/habit_tracker
 //= require_tree .
---
+++ app/assets/javascripts/backbone/habit_tracker.js.coffee
@@ -0,0 +1,11 @@
+#= require_self
+#= require_tree ./templates
+#= require_tree ./models
+#= require_tree ./views
+#= require_tree ./routers
+
+window.HabitTracker =
+  Models: {}
+  Collections: {}
+  Routers: {}
+  Views: {}
