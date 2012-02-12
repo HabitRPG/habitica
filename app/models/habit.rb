daef5d276c163b3552933682b37f4b9f0b592440
Tyler Renelle <tylerrenelle@gmail.com>
2012-02-11 21:03:58 -0500

    Clean up clear_done cron method

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -11,39 +11,20 @@ class Habit < ActiveRecord::Base
   # TODO set cron for this
   def self.clear_done
     Habit.where(:habit_type => Habit::DAILY).collect do |h|
-      h.vote('down', true) unless h.done
+      unless h.done
+        value = 0
+        if h.score < 0
+          value = ( ( -0.1 * h.score + 1 ) * -1 )
+        else
+          value = ( ( 0.9 ** h.score ) * -1 )
+        end
+        h.score += value
+        h.user.exp += value
+        h.user.save
+      end
       h.done = false
       h.save
     end
   end
   
-  def vote(direction, clear=false)
-    # For negative values, use a line: something like y=-.1x+1
-    # For positibe values, taper off with inverse log: y=.9^x
-    # Would love to use inverse log for the whole thing, but after 13 fails it hits infinity
-    sign = ( direction=='up' ? 1 : -1 )
-    value = 0
-    if self.score < 0
-      value = ( ( -0.1 * self.score + 1 ) * sign )
-    else
-      value = ( ( 0.9 ** self.score ) * sign )
-    end
-
-    self.score += value
-    self.user.exp += value
-    
-    # also add money. Only take away money if it was a mistake (aka, a checkbox)
-    if (direction=='up') || (direction=='down' && clear==false && self.habit_type!=Habit::ALWAYS)
-      self.user.money += value
-      self.user.save
-    end
-    
-    # up/down -voting as checkbox & assigning as done, 2 birds one stone
-    if(self.habit_type==Habit::DAILY || self.habit_type==Habit::ONE_TIME)
-      self.done = true if direction=='up'
-      self.done = false if direction=='down'
-    end
-    
-    save
-  end
 end
