e5c5e8c10be3df9a38c4b178a24e82ac90c390d5
Tyler Renelle <tylerrenelle@gmail.com>
2012-03-25 21:57:23 -0400

    Setting up boilerplate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require plugins
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,7 @@ HabitTracker::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+ config.assets.precompile += %w( modernizr.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
