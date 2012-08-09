0bfdc907fcf97aa99b7c247d771c57674a483ad2
Tyler Renelle <tylerrenelle@gmail.com>
2012-08-09 11:10:44 -0400

    don't use "up" on production or debug, only development

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-require('derby').run(__dirname + '/lib/server')
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'debug') {
+  // "up" module interferes with node debug & node-inspector
+  // also, nodejitsu (aka, 'production') doesn't like up
+  console.log("Using require('./lib/server').listen(3000) without Up");
+  require('./lib/server').listen(3000);
+} else {
+  require('derby').run(__dirname + '/lib/server');
+}
