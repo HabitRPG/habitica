8f9e0313a3b812921cfbb4000b583fa38ebe9d9e
Tyler Renelle <tylerrenelle@gmail.com>
2012-08-24 12:07:35 -0400

    small tweaks to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'debug') {
-  // "up" module interferes with node debug & node-inspector
-  // also, nodejitsu (aka, 'production') doesn't like up
-  port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === 'debug' || process.env.NODE_ENV === 'production') {
+  // "up" module interferes with node debug & node-inspector. also, nodejitsu (aka, 'production') doesn't like up
+  console.log('Running in ' + process.env.NODE_ENV + ': require("./lib/server").listen(' + port + ');');
   require('./lib/server').listen(port);
 } else {
-  require('derby').run(__dirname + '/lib/server');
+  require('derby').run(__dirname + '/lib/server', port);
 }
