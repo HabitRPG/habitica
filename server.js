6c2c5c55d80a69446a7d8f830f51b8581129c2d6
Tyler Renelle <tylerrenelle@gmail.com>
2012-11-15 10:49:51 -0500

    remove lib/ and coffee-script compilation step, also remove "up" for development. Now only src/, coffee-script compiles on run-time, and will need to restart server each change

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-var port = process.env.PORT || 3000;
-if (process.env.NODE_ENV === 'debug' || process.env.NODE_ENV === 'production') {
-  // "up" module interferes with node debug & node-inspector. also, nodejitsu (aka, 'production') doesn't like up
-  console.log('Running in ' + process.env.NODE_ENV + ': require("./lib/server").listen(' + port + ');');
-  require('./lib/server').listen(port);
-} else {
-  require('derby').run(__dirname + '/lib/server', port);
-}
+require('coffee-script') // remove intermediate compilation requirement
+require('./src/server').listen(process.env.PORT || 3000);
+
+// Note: removed "up" module, which is default for development (but interferes with and production + PaaS)
+// Restore to 5310bb0 if I want it back (see https://github.com/codeparty/derby/issues/165#issuecomment-10405693)
