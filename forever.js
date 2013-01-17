7bafc4f8e1cb90bcf2bc2326f2a9317d1ee3133b
Tyler Renelle <tylerrenelle@gmail.com>
2013-01-17 15:43:03 -0500

    remove forever-monitor, we're going to be using forever

--- forever.js
+++
@@ -1,5 +0,0 @@
-require('coffee-script') // remove intermediate compilation requirement
-require('./src/server').listen(process.env.PORT || 3000);
-
-// Note: removed "up" module, which is default for development (but interferes with and production + PaaS)
-// Restore to 5310bb0 if I want it back (see https://github.com/codeparty/derby/issues/165#issuecomment-10405693)
--- server.js
+++ server.js
@@ -1,49 +1,5 @@
-/*process.on('uncaughtException', function(exception) {
-    notifyAdmin(exception)
-    console.error(exception)
-});
-
 require('coffee-script') // remove intermediate compilation requirement
-require('./src/server').listen(process.env.PORT || 3000);*/
+require('./src/server').listen(process.env.PORT || 3000);
 
-var forever = require('forever-monitor');
-var child = new (forever.Monitor)('forever.js');
-
- // FIXME on('error') and on('stderr') aren't working
- child.on('restart', function(){
-    notifyAdmin('Server has restarted.')
- });
-
- child.on('exit', function() {
-     var err = 'server.js has exited after 10 restarts';
-     console.log(err);
-     notifyAdmin(err);
- });
- child.start();
-
-function notifyAdmin(err){
-    var nodemailer = require("derby-auth/node_modules/nodemailer");
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-        service: process.env.SMTP_SERVICE || 'Gmail',
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    });
-    var mailData = {
-        from: "HabitRPG <admin@habitrpg.com>",
-        to: 'tylerrenelle@gmail.com',
-        subject: "HabitRPG Error",
-        text: err
-    }
-
-    // send mail with defined transport object
-    smtpTransport.sendMail(mailData, function(error, response){
-        if(error){
-            console.log(error);
-        }else{
-            console.log("Message sent: " + response.message);
-        }
-        smtpTransport.close(); // shut down the connection pool, no more messages
-    });
-}
+// Note: removed "up" module, which is default for development (but interferes with and production + PaaS)
+// Restore to 5310bb0 if I want it back (see https://github.com/codeparty/derby/issues/165#issuecomment-10405693)
