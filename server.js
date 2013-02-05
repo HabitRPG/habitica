96338f3169ee2f6b29e6cfe8f600bce2f15174d0
Tyler Renelle <tylerrenelle@gmail.com>
2013-02-04 21:13:04 -0500

    allow crash server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-process.on('uncaughtException', function (exception) {
-   console.error(exception);
-    // don't crash for now
-});
+/*process.on('uncaughtException', function (error) {
+    console.log(error.stack);
+});*/
 
 require('coffee-script') // remove intermediate compilation requirement
-require('./lib/server').listen(process.env.PORT || 3000);
+require('./src/server').listen(process.env.PORT || 3000);
 
 // Note: removed "up" module, which is default for development (but interferes with and production + PaaS)
 // Restore to 5310bb0 if I want it back (see https://github.com/codeparty/derby/issues/165#issuecomment-10405693)
