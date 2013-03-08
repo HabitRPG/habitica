24c767776495ae991a15df1894e8185330a4057b
Philip How <philip.j.how@gmail.com>
2013-03-08 00:20:46 +0000

    Migration script for removing old dailies histories

---
+++ migrations/20130307_remove_duff_histories.js
@@ -0,0 +1,29 @@
+// mongo habitrpg ./node_modules/underscore/underscore.js ./migrations/20130307_normalize_algo_values.js
+
+/**
+ * Remove duff histories for dailies
+ */
+db.users.find().forEach(function(user){
+
+
+    _.each(user.tasks, function(task, key){
+        // remove task history 
+        if (task.type === "daily") {
+            task.history = []
+        }
+    });
+
+    try {
+        db.users.update(
+            {_id:user._id},
+            {$set:
+                {
+                'tasks' : user.tasks
+                }
+            },
+            {multi:true}
+        );
+    } catch(e) {
+        print(e);
+    }
+})
