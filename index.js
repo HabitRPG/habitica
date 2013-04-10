58f6689a3307e70c9041841fc6f83495f8cc25c3
Tyler Renelle <tylerrenelle@gmail.com>
2013-04-10 15:00:18 -0400

    hack fix for window.exports

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ exports.helpers = require('./helpers')
 
 // This is how we're exporting this module to the browser. A preferable way would be http://requirejs.org/docs/api.html#packages
 // but I couldn't get that working
-if (!!window) window.habitrpgShared = exports;
+try {
+    window;
+    window.habitrpgShared = exports;
+} catch(e) {}
