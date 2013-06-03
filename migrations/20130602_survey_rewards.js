c1cda305824770a73bc19cec8a0534fcb2423af3
Tyler Renelle <tylerrenelle@gmail.com>
2013-06-03 19:15:48 -0400

    achievements: add migration for survey achievements

---
+++ migrations/20130602_survey_rewards.js
@@ -0,0 +1,20 @@
+//mongo habitrpg ./node_modules/lodash/lodash.js migrations/20130602_survey_rewards.js
+
+var members = [];
+members = _.uniq(members);
+print(members.length)
+
+db.users.update({
+    _id: {$exists:1},
+    $or:[
+        {_id: {$in: members}},
+//        {'profile.name': {$in: members}},
+        {'auth.facebook.name': {$in: members}},
+        {'auth.local.username': {$in: members}},
+        {'auth.local.email': {$in: members}}
+    ]
+},
+{
+    $set: { 'achievements.helpedHabit': true },
+    $inc: { balance: (2.5) }
+})
