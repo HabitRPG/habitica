a5a57a244bcbddb5bcded9fb9c6c73f69cc3de13
Tyler Renelle <tylerrenelle@gmail.com>
2013-06-04 10:32:33 -0400

    fix survey migration

--- migrations/20130602_survey_rewards.js
+++ migrations/20130602_survey_rewards.js
@@ -1,20 +1,25 @@
 //mongo habitrpg ./node_modules/lodash/lodash.js migrations/20130602_survey_rewards.js
 
-var members = [];
+var members = []
 members = _.uniq(members);
-print(members.length)
 
-db.users.update({
+var query = {
     _id: {$exists:1},
     $or:[
         {_id: {$in: members}},
-//        {'profile.name': {$in: members}},
+        //{'profile.name': {$in: members}},
         {'auth.facebook.name': {$in: members}},
         {'auth.local.username': {$in: members}},
         {'auth.local.email': {$in: members}}
     ]
-},
-{
-    $set: { 'achievements.helpedHabit': true },
-    $inc: { balance: 2.5 }
-})
+};
+
+print(db.users.count(query));
+
+db.users.update(query,
+    {
+        $set: { 'achievements.helpedHabit': true },
+        $inc: { balance: 2.5 }
+    },
+    {multi:true}
+)
