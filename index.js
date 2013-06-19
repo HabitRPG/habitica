6bf0305ea423a16be213bb8ad712c27c35299191
Tyler Renelle <tylerrenelle@gmail.com>
2013-06-18 21:40:43 -0400

    fixed the empty helpers issue. it was circular dependencies - algos dep'd helpers, helpers dep'd algos. Since one came first, it didn't have the other - so it was an empty object. maybe this top-level index.js combinging ./script/* files isn't such a good idea, because I think manual requires from within a file will resolve circular dependencies @yangit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports.items = require('./script/items.coffee');
-module.exports.algos = require('./script/algos.coffee');
 module.exports.helpers = require('./script/helpers.coffee');
+module.exports.algos = require('./script/algos.coffee');
 
 try {
     window;
