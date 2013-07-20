89568bd8691b2f55daaa536ae14d4b6dc7f50954
Tomaž <litenull@gmail.com>
2013-07-20 02:15:29 +0200

    task editing

--- app/scripts/controllers/tasksCtrl.js
+++ app/scripts/controllers/tasksCtrl.js
@@ -169,22 +169,113 @@ habitrpg.controller('TasksCtrl', function TasksCtrl($scope, $rootScope, $locatio
     $scope.editTask = function(task) {
         if (!task.editing) {
             task.editing = true
+                if (typeof task.tags == 'undefined') {
+                    task.tags = {}
+                }
         }else{
             task.editing = false
         }
     }
 
     $scope.save = function(task) {
-        
-        User.log([
+
+        var ops = ([
             {op: 'set', path: "tasks." + task.id + ".text", value: task.text},
             {op: 'set', path: "tasks." + task.id + ".notes", value: task.notes},
             {op: 'set', path: "tasks." + task.id + ".up", value: task.up},
-            {op: 'set', path: "tasks." + task.id + ".down", value: task.down}
-        ]);
+            {op: 'set', path: "tasks." + task.id + ".down", value: task.down},
+            {op: 'set', path: "tasks." + task.id + ".priority", value: task.priority},
+            {op: 'set', path: "tasks." + task.id + ".date", value: task.date},
+            {op: 'set', path: "tasks." + task.id + ".price", value: task.price},
+        ])
+
+        _.each(task.tags, function(el, key, list) {
+            ops.push({op: 'set', path: "tasks." + task.id + ".tags." + key, value: el})
+        })
+
+        _.each(task.repeat, function(el, key, list) {
+            ops.push({op: 'set', path: "tasks." + task.id + ".repeat." + key, value: el})
+        })
+
+        User.log(ops)
        
        task.editing = false
 
+    }
+
+    $scope.showAdvancedOptions = function(task) {
+        if (!task.advanced) {
+            task.advanced = true
+        }else{
+            task.advanced = false
+        }
+    }
+
+    $scope.setPriority = function(task, priority) {
+        task.priority = priority
+    }
+
+    $scope.getPriority = function(task, priority) {
+        if (task.priority == priority) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    $scope.getRepeat = function(task, repeat) {
+        var enabled = '';
+        _.each(task.repeat, function(el, key, list) {
+            if (key == repeat && el == true) {
+                enabled = true
+            }
+        })
+
+        if (enabled) {
+            return true
+        }else{
+            return false
+        }
+    }
+
+    $scope.setRepeat = function(task, repeat) {
+
+        if (typeof task.repeat == 'undefined') {
+            var obj = {}
+            obj[repeat] = true;
+
+            task.repeat = {}
+            task.repeat = obj
+            return true
+        }
+
+
+        if (!task.repeat[repeat]) {
+            task.repeat[repeat] = true
+        }else{
+            task.repeat[repeat] = false
+        }
+
+    }
+
+    $scope.addTag = function(task, tag) {
+        var obj = {}
+        obj[tag] = true
+        task.tags.push(obj)
+    }
+
+    $scope.toggleTag = function(task, tag) {
+        var obj = {}
+        if (typeof task.tags != 'undefined') {
+            if (!task.tags[tag]) {
+                task.tags[tag] = false;
+            }else{
+                task.tags[tag] = true
+            }
+        }else{
+             task.tags = {}
+             task.tags[tag] = true
+        }
     }
 
 
