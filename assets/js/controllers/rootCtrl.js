aedb90176d813f1098069214f65b9317602fae69
Tyler Renelle <tylerrenelle@gmail.com>
2013-08-27 19:59:53 -0400

    rewrite2: convert rootCtrl to js, add $rootScope.set() method.

---
+++ assets/js/controllers/rootCtrl.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/* Make user and settings available for everyone through root scope.
+ */
+
+habitrpg.controller("RootCtrl", function($scope, $rootScope, $location, User) {
+  $rootScope.modals = {};
+  $rootScope.User = User;
+  $rootScope.user = User.user;
+  $rootScope.settings = User.settings;
+
+  /*
+   FIXME this is dangerous, organize helpers.coffee better, so we can group them by which controller needs them,
+   and then simply _.defaults($scope, Helpers.user) kinda thing
+   */
+  _.defaults($rootScope, window.habitrpgShared.algos);
+  _.defaults($rootScope, window.habitrpgShared.helpers);
+
+  /*
+   Very simple path-set. `set('preferences.gender','m')` for example. We'll deprecate this once we have a complete API
+   */
+  $rootScope.set = function(k, v) {
+    var log = { op: 'set' };
+    window.helpers.dotSet(k, v, User.user);
+    log[k] = v;
+    User.log(log);
+  };
+
+  $rootScope.authenticated = function() {
+    User.settings.auth.apiId !== "";
+  };
+
+});
