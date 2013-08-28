fce194275ebee9559f9f03e2c983840b9484bc8f
Tyler Renelle <tylerrenelle@gmail.com>
2013-08-28 19:20:47 -0400

    rewrite: fix up some settings page stuff

--- assets/js/controllers/settingsCtrl.js
+++ assets/js/controllers/settingsCtrl.js
@@ -4,17 +4,23 @@
 habitrpg.controller('SettingsCtrl',
   ['$scope', 'User', '$location',
   function($scope, User, $location) {
-    $scope.resetApp = function () {
-        localStorage.clear();
-        location.reload();
-    };
-    $scope.auth = function (id, token) {
-        User.authenticate(id, token, function (err) {
-            if (!err) {
-                alert('Login successful!');
-                $location.path("/habit");
-            }
-        });
+
+    // FIXME we have this re-declared everywhere, figure which is the canonical version and delete the rest
+//    $scope.auth = function (id, token) {
+//        User.authenticate(id, token, function (err) {
+//            if (!err) {
+//                alert('Login successful!');
+//                $location.path("/habit");
+//            }
+//        });
+//    }
+
+    $scope.saveDayStart = function(){
+      var dayStart = +User.user.preferences.dayStart;
+      if (dayStart < 0 || dayStart > 24) {
+        dayStart = 0;
+      }
+      User.log({'op':'set', data:{'preferences.dayStart': dayStart}});
     }
 
   }
--- assets/js/directives/directives.js
+++ assets/js/directives/directives.js
@@ -28,17 +28,6 @@ habitrpg.directive('habitrpgAdsense', function() {
   }
 })
 
-/**
- * Directive that executes an expression when the element it is applied to loses focus.
- */
-habitrpg.directive('taskBlur', function() {
-  return function(scope, elem, attrs) {
-    elem.bind('blur', function() {
-      scope.$apply(attrs.taskBlur);
-    });
-  };
-});
-
 habitrpg.directive('whenScrolled', function() {
     return function(scope, elm, attr) {
         var raw = elm[0];
