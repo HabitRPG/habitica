fb0ffd075e6b71bedb3b7e6f12016a1068861c8a
Tyler Renelle <tylerrenelle@gmail.com>
2013-09-01 12:56:43 -0400

    rewrite: converted static pages, removed old unused static pages

--- src/middleware.js
+++ src/middleware.js
@@ -2,30 +2,25 @@ var nconf = require('nconf');
 var _ = require('lodash');
 
 module.exports = function(req, res, next) {
-  /*
-  //splash = (req, res, next) ->
-  //  isStatic = req.url.split('/')[1] is 'static'
-  //  unless req.query?.play? or req.getModel().get('_userId') or isStatic
-  //    res.redirect('/static/front')
-  //  else
-  //    next()
-  */
 
-  /* Set _mobileDevice to true or false so view can exclude portions from mobile device*/
+  // show them the splash page if they're not logged in
+  var isStatic = req.url.split('/')[1] == 'static';
+//  if (!isStatic && !req.headers['x-api-user'] && !req.headers['x-api-key'] && req.headers.accept != 'application/json')
+//    return res.redirect('/static/front')
 
-  var _base;
-  _.defaults(((_base = res.locals).habitrpg != null ? (_base = res.locals).habitrpg : _base.habitrpg = {}), {
-    NODE_ENV: nconf.get('NODE_ENV'),
+  res.locals.habitrpg  = res.locals.habitrpg || {}
+  _.defaults(res.locals.habitrpg, {
+    NODE_ENV: isStatic ? 'production' : nconf.get('NODE_ENV'), // Don't show debugging options on static pages
     IS_MOBILE: /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(req.header('User-Agent')),
     STRIPE_PUB_KEY: nconf.get('STRIPE_PUB_KEY')
   });
-  /*CORS middleware*/
 
+  //CORS middleware
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,HEAD,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type,Accept,Content-Encoding,X-Requested-With,x-api-user,x-api-key");
-  /* wtf is this for?*/
 
+  //wtf is this for?
   if (req.method === 'OPTIONS') {
     return res.send(200);
   }
--- src/routes/pages.js
+++ src/routes/pages.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = new express.Router();
 var _ = require('lodash');
 
+// -------- App --------
 router.get('/', function(req, res) {
   return res.render('index', {
     title: 'HabitRPG | Your Life, The Role Playing Game',
@@ -11,11 +12,46 @@ router.get('/', function(req, res) {
 });
 
 router.get('/partials/tasks', function(req, res) {
-  return res.render('tasks/index');
+  res.render('tasks/index');
 });
 
 router.get('/partials/options', function(req, res) {
-  return res.render('options');
+  res.render('options');
 });
+
+// -------- Marketing --------
+
+router.get('/splash.html', function(req, res) {
+  res.redirect('/static/front');
+});
+
+router.get('/static/front', function(req, res) {
+  res.render('static/front', {env: res.locals.habitrpg});
+});
+
+router.get('/static/about', function(req, res) {
+  res.redirect('http://community.habitrpg.com/node/97');
+});
+
+router.get('/static/team', function(req, res) {
+  res.redirect('http://community.habitrpg.com/node/96');
+});
+
+router.get('/static/extensions', function(req, res) {
+  res.redirect('http://community.habitrpg.com/extensions');
+});
+
+router.get('/static/faq', function(req, res) {
+  res.redirect('http://community.habitrpg.com/faq-page');
+});
+
+router.get('/static/privacy', function(req, res) {
+  res.render('static/privacy', {env: res.locals.habitrpg});
+});
+
+router.get('/static/terms', function(req, res) {
+  res.render('static/terms', {env: res.locals.habitrpg});
+});
+
 
 module.exports = router;
