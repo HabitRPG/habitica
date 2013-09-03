e41efa98e45ddb47766b8ba26d9b62d1b6105bfe
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2013-09-03 12:05:54 +0200

    working stable

--- assets/js/controllers/petsCtrl.js
+++ assets/js/controllers/petsCtrl.js
@@ -1,10 +1,8 @@
-habitrpg.controller("PetsCtrl", ['$scope', '$rootScope', 'User',
-  function($scope, $rootScope, User) {
+habitrpg.controller("PetsCtrl", ['$scope', 'User',
+  function($scope, User) {
 
-    var user = $rootScope.user;
-
-    $scope.userPets = user.items.pets;
-    $scope.userCurrentPet = user.items.currentPet;
+    $scope.userPets = User.user.items.pets;
+    $scope.userCurrentPet = User.user.items.currentPet;
     $scope.pets = window.habitrpgShared.items.items.pets;
     $scope.hatchingPotions = window.habitrpgShared.items.items.hatchingPotions;
 
@@ -19,21 +17,15 @@ habitrpg.controller("PetsCtrl", ['$scope', '$rootScope', 'User',
     }
 
     $scope.choosePet = function(name, potion){
-      console.log(user.items.currentPet);
       if($scope.userCurrentPet && $scope.userCurrentPet.str === (name + '-' + potion)){
         $scope.userCurrentPet = null;
-        User.log({op:'set', data:{'items.currentPet': $scope.userCurrentPet}});
       }else{
         var pet = _.find($scope.pets, {name: name});
-        console.log(pet, {name: $scope.pets});
         pet.modifier = potion;
         pet.str = name + '-' + potion;
         $scope.userCurrentPet = pet;
-        User.log({op:'set', data:{'items.currentPet': $scope.userCurrentPet}});
       }
-      // Changes doesn't propagate!!! here the console log for 'user.items.currentPet' the old value and
-      // for '$scope.userCurrentPet' the new one
-      console.log(user.items.currentPet, $scope.userCurrentPet);
+      User.set('items.currentPet', $scope.userCurrentPet);
     }
 
   }]);
