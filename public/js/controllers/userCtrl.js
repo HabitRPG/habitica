68426c7ccbe46c3be6ca439d6918ce0bd1a4c400
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2013-10-13 20:47:59 +0200

    profile edits saved only on click

--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -9,14 +9,27 @@ habitrpg.controller("UserCtrl", ['$scope', '$location', 'User',
   $scope.toggleHelm = function(val){
     User.log({op:'set', data:{'preferences.showHelm':val}});
   }
+
+  $scope.$watch('_editing.profile', function(value){
+    if(value === true) $scope.editingProfile = angular.copy(User.user.profile);
+  });  
+
+  $scope.save = function(){
+    var values = {};
+    _.each($scope.editingProfile, function(value, key){
+      // Using toString because we need to compare two arrays (websites)
+      if($scope.editingProfile[key].toString() !== $scope.profile.profile[key].toString()) values['profile.' + key] = value;
+    });
+    User.setMultiple(values);
+    $scope._editing.profile = false;
+  }
+
   $scope.addWebsite = function(){
-    if (!User.user.profile.websites) User.user.profile.websites = [];
-    User.user.profile.websites.push($scope._newWebsite);
-    User.set('profile.websites', User.user.profile.websites);
+    if (!$scope.editingProfile.websites) $scope.editingProfile.websites = [];
+    $scope.editingProfile.websites.push($scope._newWebsite);
     $scope._newWebsite = '';
   }
   $scope.removeWebsite = function($index){
-    User.user.profile.websites.splice($index,1);
-    User.set('profile.websites', User.user.profile.websites);
+    $scope.editingProfile.websites.splice($index,1);
   }
 }]);
