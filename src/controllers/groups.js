567b69a0f1877abe0fe220c2b8cd180aee43b618
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2013-11-02 15:54:10 +0100

    fix #1669

--- src/controllers/groups.js
+++ src/controllers/groups.js
@@ -64,7 +64,8 @@ api.list = function(req, res) {
       Group.findOne({type: 'party', members: {'$in': [user._id]}})
         .select(groupFields).exec(function(err, party){
           if (err) return cb(err);
-          cb(null, [party]); // return as an array for consistent ngResource use
+
+          cb(null, (party === null ? [] : [party])); // return as an array for consistent ngResource use
         });
     },
 
