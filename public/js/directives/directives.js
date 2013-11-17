a81e77f0ff4427258ee7859c06520cbe622d37bf
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2013-11-17 20:52:39 +0100

    update chat messages timestamp

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -153,3 +153,16 @@ habitrpg
     }
   }]);
 
+habitrpg.directive('fromNow', ['$interval', function($interval){
+  return function(scope, element, attr){
+    var updateText = function(){ element.text(moment(scope.message.timestamp).fromNow()) };
+    updateText();
+    // Update the counter every 60secs if was sent less than one hour ago otherwise every hour
+    // OPTIMIZATION, every time the interval is run, update the interval time
+    var intervalTime = moment().diff(scope.message.timestamp, 'minute') < 60 ? 60000 : 3600000;
+    var interval = $interval(function(){ updateText() }, intervalTime, false);
+    scope.$on('$destroy', function() {
+      $interval.cancel(interval);
+    });
+  }
+}]);
