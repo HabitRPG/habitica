be4691cf5811037000228c0e26b219b4163d2ebd
Adrien Lemaire <lemaire.adrien@gmail.com>
2013-11-23 16:21:43 +1100

    sort by level

--- public/js/controllers/headerCtrl.js
+++ public/js/controllers/headerCtrl.js
@@ -3,9 +3,14 @@
 habitrpg.controller("HeaderCtrl", ['$scope', 'Groups', 'User',
   function($scope, Groups, User) {
     $scope.party = Groups.party(function(){
-      $scope.partyMinusSelf = _.filter($scope.party.members, function(member){
-        return member._id !== User.user._id;
-      });
+      $scope.partyMinusSelf = _.sortBy(
+        _.filter($scope.party.members, function(member){
+          return member._id !== User.user._id;
+        }),
+        function (member) {
+          return member.stats.lvl
+        }
+      ).reverse()
     });
   }
 ]);
