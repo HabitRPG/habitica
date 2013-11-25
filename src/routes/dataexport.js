c0a02eeef87e99d08ecad849ca004cb6fd802d7c
Nick Gordon <ngordon779@gmail.com>
2013-11-25 08:06:27 -0800

    support for xml and json export of all user data

--- src/routes/dataexport.js
+++ src/routes/dataexport.js
@@ -6,5 +6,7 @@ var nconf = require('nconf');
 
 /* Data export */
 router.get('/history.csv',auth.authWithSession,dataexport.history); //[todo] encode data output options in the data controller and use these to build routes
+router.get('/userdata.xml',auth.authWithSession,dataexport.leanuser,dataexport.userdata.xml);
+router.get('/userdata.json',auth.authWithSession,dataexport.leanuser,dataexport.userdata.json);
 
 module.exports = router;
