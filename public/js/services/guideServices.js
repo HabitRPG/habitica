a28327410e882d014cea1485cc13d22176e927a6
Xatres <jdrice12@gmail.com>
2013-12-02 19:01:17 -0500

    Updated Beginner's Tour
    As requested by SabreCat, I've done some editing on the Beginner's Tour. Mostly I just flesh out the guide to how the game works and try to contextualize everything on the page with the user avatar. If we implement these changes, there may need to be a few edits to the starting task descriptions to prevent overlap.
    
    Definitely check to make sure I didn't mess up any of the coding. I'm a writer, not a programmer.

--- public/js/services/guideServices.js
+++ public/js/services/guideServices.js
@@ -19,35 +19,52 @@ angular.module('guideServices', []).
         {
           element: ".main-herobox",
           title: "Welcome to HabitRPG",
-          content: "Welcome to HabitRPG, a habit-tracker which treats your goals like a Role Playing Game. I'm <a href='http://www.kickstarter.com/profile/1823740484' target='_blank'>Justin</a>, your guide! "
+          content: "Welcome to HabitRPG, a habit-tracker which treats your goals like a Role Playing Game. I'm <a href='http://www.kickstarter.com/profile/1823740484' target='_blank'>Justin</a>, your guide!",
+        }, {
+          element: ".main-herobox",
+          title: "Your Avatar",
+          content: "This is your avatar. He represents you in the world of Habitica. As you accomplish goals, this little guy will gain levels, earn gold, and equip himself for further challenges ahead.",
+        }, {
+          element: ".main-herobox",
+          title: "Avatar Customization",
+          content: "You can customize your avatar by clicking on him when the tour is complete. Change your sex, hair color, skin color, and more from this menu. You can also find a number of HabitRPG's exciting social features by clicking through tabs on the customization page.",
         }, {
           element: "#bars",
-          title: "Achieve goals and level up",
-          content: "As you accomplish goals, you level up. If you fail your goals, you lose hit points. Lose all your HP and you die."
+          title: "Hit Points",
+          content: "The red bar tracks your avatar's hit points. Whenever you fail to meet a goal, you take damage and lose HP. If your HP bar reaches zero, you die. Dying results in the loss of one level, all your gold, and a piece of equipment.",
+        }, {
+          element: "#bars",
+          title: "Experience Points",
+          content: "The yellow bar tracks your avatar's experience points. Whenever you succeed in achieving a goal, you gain both gold and EXP. When your EXP bar maxes out, you gain a level. Gaining levels is how you unlock new and exciting features on HabitRPG.",
+        }, {
+          element: "ul.habits",
+          title: "Types of Goals",
+          content: "HabitRPG allows you to track your goals in three different ways. These goals are categorized in columns as Habits, Dailies, or To-Dos.",
+          placement: "bottom"
         }, {
           element: "ul.habits",
           title: "Habits",
-          content: "Habits are goals that you constantly track.",
+          content: "Habits are goals that you constantly track. They can be given plus or minus values, allowing you to gain EXP and gold for good habits or lose HP for bad ones.",
           placement: "bottom"
         }, {
           element: "ul.dailys",
           title: "Dailies",
-          content: "Dailies are goals that you want to complete once a day.",
+          content: "Dailies are goals that you want to complete once a day. Checking off a daily reaps EXP and gold. Failing to check off your daily by midnight results in a loss of HP.",
           placement: "bottom"
         }, {
           element: "ul.todos",
           title: "Todos",
-          content: "Todos are one-off goals which need to be completed eventually.",
+          content: "Todos are one-off goals which have no set deadline. They make for a quick and easy way to gain experience.",
           placement: "bottom"
         }, {
           element: "ul.rewards",
           title: "Rewards",
-          content: "As you complete goals, you earn gold to buy rewards. Buy them liberally - rewards are integral in forming good habits.",
+          content: "All that gold you earned will allow you to reward yourself with either custom or in-game prizes. Buy them liberally – rewarding yourself is integral in forming good habits.",
           placement: "bottom"
         }, {
           element: "ul.habits li:first-child",
           title: "Hover over comments",
-          content: "Different task-types have special properties. Hover over each task's comment for more information. When you're ready to get started, delete the existing tasks and add your own.",
+          content: "You can add comments to your tasks by clicking the edit icon. Hover over each task's comment for more details about how HabitRPG works. When you're ready to get started, you can delete the existing tasks and add your own.",
           placement: "right"
         }
       ];
