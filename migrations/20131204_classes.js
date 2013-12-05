decaa2867becc7a365c8118d6897b9dbe87aab6f
Tyler Renelle <tylerrenelle@gmail.com>
2013-12-04 17:08:55 -0700

    classes: misc fixes. start a running migration file (add to, or add notes as we go along)

---
+++ migrations/20131204_classes.js
@@ -0,0 +1,22 @@
+/**
+ * Probably the biggest migration of all! This adds the following features:
+ *  - Customization Redo: https://trello.com/c/YKXmHNjY/306-customization-redo
+ *  - Armory: https://trello.com/c/83M5RqQB/299-armory
+ *  - Classes
+ */
+
+db.users.find().forEach(function(user){
+
+  // grant backer/contrib gear, rather than using js logic
+
+  // migrate current owned items
+
+  // gender => size
+  user.preferences.size = (user.preferences.gender == 'f') ? 'slim' : 'broad';
+  delete user.preferences.gender;
+
+  // Delete armorSet
+  delete user.preferences.armorSet;
+
+  db.users.update({_id:user._id}, user);
+});
