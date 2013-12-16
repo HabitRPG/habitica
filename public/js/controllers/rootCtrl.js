a7100b6606a25e8579d0993f1df0793de369a3a9
Tyler Renelle <tylerrenelle@gmail.com>
2013-12-16 00:18:50 -0700

    classes: cast URL fix

--- public/js/controllers/rootCtrl.js
+++ public/js/controllers/rootCtrl.js
@@ -158,7 +158,7 @@ habitrpg.controller("RootCtrl", ['$scope', '$rootScope', '$location', 'User', '$
     $scope.castEnd = function(target, type, $event){
       if ($scope.spell.target != type) return Notification.text("Invalid target");
       $scope.spell.cast(User.user, target);
-      $http.post('/api/v2/user/cast/' + $scope.spell.name, {target:target, type:type}).success(function(){
+      $http.post('/api/v2/user/class/cast/' + $scope.spell.name, {target:target, type:type}).success(function(){
         var msg = "You cast " + $scope.spell.text;
         switch (type) {
           case 'task': msg += ' on ' + target.text;break;
