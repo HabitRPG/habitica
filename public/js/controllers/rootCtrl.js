a4ba5b036fa24967db4e94c225d8f8ad6839fca9
Tyler Renelle <tylerrenelle@gmail.com>
2013-12-20 18:52:14 -0700

    save user on spell-cast (so we don't get second mp loss notif)

--- public/js/controllers/rootCtrl.js
+++ public/js/controllers/rootCtrl.js
@@ -165,6 +165,7 @@ habitrpg.controller("RootCtrl", ['$scope', '$rootScope', '$location', 'User', '$
     $scope.castEnd = function(target, type, $event){
       if ($scope.spell.target != type) return Notification.text("Invalid target");
       $scope.spell.cast(User.user, target);
+      User.save();
       $http.post('/api/v2/user/class/cast/' + $scope.spell.name, {target:target, type:type}).success(function(){
         var msg = "You cast " + $scope.spell.text;
         switch (type) {
