91b0f3fb9607d940161f3113777763b8d88b9030
Dušan Juretić <dusan.juretic@gmail.com>
2013-12-25 20:09:18 -0300

    E2e testing: added tests for failed login and register new user

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,22 +1,43 @@
 'use strict';
 
-describe('index page', function() {
+describe('front page', function() {
   beforeEach(function(){
     browser.ignoreSynchronization = true;
-    browser.get('http://localhost:3000/');
+    browser.get('/');
   });
 
   it('shows the front page', function(){
-    // browser.sleep(3000);
     var button = element(by.className('btn'));
     expect(button.getText()).toEqual('Play');
   });
 
-  it('shows the login form', function(){
+  it("don't login when using wrong credentials", function(){
     var button = element(by.className('btn'));
     button.click();
-    
+    element(by.model('loginUsername')).sendKeys('username');
+    element(by.model('loginPassword')).sendKeys('pass');
+    var login = element(by.css("#login-tab input[value='Login']"));
+    login.click();
+    var alertDialog = browser.switchTo().alert();
+    expect(alertDialog.getText()).toMatch(/Username 'username' not found/);
+    alertDialog.accept();
   });
 
-
+  it('registers a new user', function(){
+    var button = element(by.className('btn'));
+    button.click();
+    browser.sleep(1000);
+    var registerTab = element(by.linkText('Register'));
+    registerTab.click();
+    element(by.model('registerVals.username')).sendKeys('user');
+    element(by.model('registerVals.email')).sendKeys('user@example.com');
+    element(by.model('registerVals.password')).sendKeys('pass');
+    element(by.model('registerVals.confirmPassword')).sendKeys('pass');
+    var register = element(by.css("#register-tab input[value='Register']"));
+    register.click();
+    browser.getCurrentUrl().then(function(url){
+      browser.sleep(1000);
+      expect(url).toMatch(/#\/tasks$/);
+    });
+  });
 });
