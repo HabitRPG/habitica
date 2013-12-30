1ace999cb7a38b9dc990f8051f95200796d18e62
Tyler Renelle <tylerrenelle@gmail.com>
2013-12-29 23:49:19 -0700

    Tasks: add double-click to edit, and date-picker-popup for due-dates. Show red dates when overdue (basic)

--- public/js/controllers/tasksCtrl.js
+++ public/js/controllers/tasksCtrl.js
@@ -70,6 +70,13 @@ habitrpg.controller("TasksCtrl", ['$scope', '$rootScope', '$location', 'User','N
 
     /*
      ------------------------
+     To-Dos
+     ------------------------
+     */
+    $scope._today = moment().add('days',1);
+
+    /*
+     ------------------------
      Checklists
      ------------------------
      */
