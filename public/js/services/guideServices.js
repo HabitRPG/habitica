53aa0bba59328072b01548e9f28ca05e2a166752
Tyler Renelle <tylerrenelle@gmail.com>
2014-01-06 21:26:55 -0700

    #2245 fix tour wording from 1.5% => 50% class stat bonus

--- public/js/services/guideServices.js
+++ public/js/services/guideServices.js
@@ -159,7 +159,7 @@ angular.module('guideServices', []).
           }),
           element: '.equipment-tab',
           title: "Class Gear",
-          content: "First: don't panic! Your old gear is in your inventory, and you're now wearing your apprentice <strong>" + User.user.stats.class + "</strong> equipment. Wearing your class's gear grants you a 1.5% bonus to stats. However, feel free to switch back to your old gear."
+          content: "First: don't panic! Your old gear is in your inventory, and you're now wearing your apprentice <strong>" + User.user.stats.class + "</strong> equipment. Wearing your class's gear grants you a 50% bonus to stats. However, feel free to switch back to your old gear."
         },
         {
           path: '/#/options/profile/stats',
