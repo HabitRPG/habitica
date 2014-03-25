92c8dbba3eed17a8db987958ab4dca2485733884
Tyler Renelle <tylerrenelle@gmail.com>
2014-03-24 23:36:54 -0600

    feat(mystery): bailey + migration for march mystery items

--- migrations/20140218_mysteryitem.js
+++
@@ -1,6 +0,0 @@
-db.users.update(
-  {'purchased.plan.customerId':{$ne:null}},
-//  {_id:'9'},
-  {$push: {'purchased.plan.mysteryItems':{$each:['armor_mystery_201402','head_mystery_201402','back_mystery_201402']}}},
-  {multi:true}
-)
