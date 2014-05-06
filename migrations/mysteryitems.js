4990a6d0e15e3b0032a0d8b4df3a70ef68fcc2e6
Tyler Renelle <tylerrenelle@gmail.com>
2014-05-06 10:13:33 -0600

    feat(subscriptions): add free month script for users who canceled in old system

--- migrations/mysteryitems.js
+++ migrations/mysteryitems.js
@@ -1,5 +1,17 @@
-db.users.update(
-  {'purchased.plan.customerId':{$ne:null}},
-  {$push: {'purchased.plan.mysteryItems':{$each:['back_mystery_201404','headAccessory_mystery_201404']}}},
-  {multi:true}
-)
+var _id = '';
+var update = {
+  $push: {
+    'purchased.plan.mysteryItems':{
+      $each:['back_mystery_201404','headAccessory_mystery_201404']
+    }
+  }
+};
+
+if (_id) {
+  // singular (missing items)
+  db.users.update({_id:_id}, update);
+} else {
+  // multiple (once @ start of event)
+  db.users.update({'purchased.plan.customerId':{$ne:null}}, update, {multi:true});
+}
+
