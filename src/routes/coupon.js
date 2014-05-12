c2aed860dbb68c537f9a3c16866d2a741b53345b
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2014-05-12 17:22:55 +0200

    fix(modules): load i18n

--- src/routes/coupon.js
+++ src/routes/coupon.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = new express.Router();
 var auth = require('../controllers/auth');
 var coupon = require('../controllers/coupon');
+var i18n = require('../i18n');
 
 router.get('/api/v2/coupons', auth.authWithUrl, i18n.getUserLanguage, coupon.ensureAdmin, coupon.getCoupons);
 router.post('/api/v2/coupons/generate/:event', auth.auth, i18n.getUserLanguage, coupon.ensureAdmin, coupon.generateCoupons);
