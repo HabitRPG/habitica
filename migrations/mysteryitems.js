b5bf54a81523b27629ff290ac1cbc786fbd393a1
Tyler Renelle <tylerrenelle@gmail.com>
2014-06-12 12:32:20 -0600

    chore(subscribers): some busted transaction migration code

--- migrations/mysteryitems.js
+++ migrations/mysteryitems.js
@@ -7,6 +7,21 @@ var update = {
   }
 };
 
+/*var update = {
+  $set:{
+    'purchased.plan':{
+      customerId: "",
+      dateCreated: new Date(),
+      dateTerminated: null,
+      dateUpdated:new Date(),
+      gemsBought: 0,
+      mysteryItems: [],
+      paymentMethod: "Paypal",
+      planId : "basic_earned"
+    }
+  }
+};*/
+
 if (_id) {
   // singular (missing items)
   db.users.update({_id:_id}, update);
