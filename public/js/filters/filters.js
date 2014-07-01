9c55bfb498e7c34b730bc9bfbd00f07a7018e478
Tyler Renelle <tylerrenelle@gmail.com>
2014-06-30 21:28:09 -0600

    fix(dilatory): round boss hp/rage to bil/mil/thou, fixes #3667

--- public/js/filters/filters.js
+++ public/js/filters/filters.js
@@ -16,6 +16,8 @@ angular.module('habitrpg')
   })
   .filter('goldRoundThousandsToK', function(){
     return function (gp) {
-      return (gp > 999) ? (Math.floor(gp / 100) / 10.0 + 'k') : gp;
+      return (gp > 999999999) ? (gp / Math.pow(10, 9)).toFixed(1) + "b" :
+        (gp > 999999) ? (gp / Math.pow(10, 6)).toFixed(1) + "m" :
+        (gp > 999) ? (gp / Math.pow(10, 3)).toFixed(1) + "k" : gp;
     }
   })
