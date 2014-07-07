973732421a048f701443cfc3b1ecc8a16ec4b3ca
Tyler Renelle <tylerrenelle@gmail.com>
2014-07-07 13:37:16 -0600

    fix(delete account): allow deleting account after they've canceled subscription, before it terminates

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -258,7 +258,8 @@ api.cron = function(req, res, next) {
 // api.reset // Shared.ops
 
 api['delete'] = function(req, res, next) {
-  if (res.locals.user.purchased.plan.customerId)
+  var plan = res.locals.user.purchased.plan;
+  if (plan && plan.customerId && !plan.dateTerminated)
     return res.json(400,{err:"You have an active subscription, cancel your plan before deleting your account."});
   res.locals.user.remove(function(err){
     if (err) return next(err);
