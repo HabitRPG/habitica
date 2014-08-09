46809f1f3e439107abaa04828446cb02acdb0937
Adrien Lemaire <dori@ikkyotech.com>
2014-08-09 20:01:24 +0900

    remove migration script

--- migrations/20140809_soundTheme.js
+++
@@ -1,9 +0,0 @@
-//Add defaults to show gears in all users
-db.users.update(
-    {},
-    {$set:{
-        'preferences.soundTheme': 'danielTheBard',
-        'preferences.sound': 1
-    }},
-    {multi:true}
-)
