e6a99f33a9fe423689e36280cae3a0b18a696623
Tyler Renelle <tylerrenelle@gmail.com>
2014-11-07 17:04:41 -0700

    feat(sharing): add ability to share avatar. adds static page with avatar screen, and generates image from it

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -28,6 +28,50 @@ _.each(pages, function(name){
   });
 })
 
+var mongoose = require('mongoose');
+router.get('/static/avatar-:uuid.html', i18n.getUserLanguage, middleware.locals, function(req, res) {
+  mongoose.model('User').findById(req.params.uuid).select('stats profile items achievements preferences backer contributor').exec(function(err, user){
+    res.render('avatar-static', {
+      title: user.profile.name,
+      env: _.defaults({user:user},res.locals.habitrpg)
+    });
+  })
+});
+var Pageres = require('pageres'); //https://github.com/sindresorhus/pageres
+var s3 = require('s3'); //https://github.com/andrewrk/node-s3-client
+var bucket = 'habitrpg-dev';
+var client = s3.createClient({
+  s3Options: {
+    accessKeyId: nconf.get("S3").accessKeyId,
+    secretAccessKey: nconf.get("S3").secretAccessKey
+  }
+});
+router.get('/static/avatar-:uuid.png', i18n.getUserLanguage, middleware.locals, function(req, res, next) {
+  var filename = 'avatar-'+req.params.uuid+'.png';
+  new Pageres({delay: 1})
+    .src(nconf.get('BASE_URL')+'/static/avatar-'+req.params.uuid+'.html', ['140x147'], {crop: true, filename: filename.replace('.png','')})
+    .dest(__dirname)//TODO Delete this aftewards, or stream directly to s3 instead
+    .run(function (err, file) {
+      if (err) return next(err);
+      var params = {
+        localFile: __dirname + "/" + filename,
+        s3Params: { Bucket: bucket, Key: filename }
+      };
+      var uploader = client.uploadFile(params);
+      uploader.on('error', function(err) {
+        console.error("unable to upload:", err.stack);
+      });
+      //uploader.on('progress', function() {
+      //  console.log("progress", uploader.progressMd5Amount,
+      //    uploader.progressAmount, uploader.progressTotal);
+      //});
+      uploader.on('end', function() {
+        res.json(200,{file: s3.getPublicUrlHttp(bucket,filename)});
+        console.log("done uploading");
+      });
+    });
+});
+
 // --------- Redirects --------
 
 router.get('/static/extensions', function(req, res) {
