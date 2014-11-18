db61a623c43b4321a0220835cbf3a588889ac3cc
steelstyle <steelstyle@free.fr>
2014-11-17 23:50:51 -0500

    Begining the implementation of pay and subsciption using bitpay

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -15,6 +15,8 @@ var moment = require('moment');
 var api = module.exports;
 var isProduction = nconf.get("NODE_ENV") === "production";
 var stripe = require("stripe")(nconf.get('STRIPE_API_KEY'));
+var bitauth = require('bitauth');
+var bitpay = require('bitpay');
 
 var PaypalRecurring = require('paypal-recurring');
 var paypalRecurring = new PaypalRecurring({
@@ -281,4 +283,47 @@ api.paypalIPN = function(req, res, next) {
         break;
     }
   });
+};
+
+
+//---------------------------------
+// BitPay Module
+
+api.bitpayCheckout = function(req, res, next) {
+  async.waterfall([
+    function (cb) {
+      var privkey = bitauth.decrypt(nconf.get('BITPAY_ENCRYPT_PASSWORD'),nconf.get('BITPAY_KEY'));
+      var client = bitpay.createClient(privkey, {
+        config : {
+          apiHost : nconf.get('BITPAY_API_HOST'),
+          apiPort : nconf.get('BITPAY_API_PORT'),
+          forceSSL :nconf.get('BITPAY_FORCE_SSL')
+        }
+      });
+
+      client.on('error', function(err){
+        console.log(err);
+      });
+
+      client.on('ready', function(){
+        var data = {
+          price: 5,
+          currency: 'USD',
+          posData : JSON.stringify({uuid: res.locals.user._id}),
+          redirectURL : nconf.get('BASE_URL') + '/bitpay/checkout/success?uuid=' + res.locals.user._id
+        };
+
+        client.post('invoices',data,function(err, invoice){
+            if (!err){
+              res.send(200,invoice.url);
+            }
+        })
+      });
+    }
+  ]);
+
+};
+
+api.bitpayCheckoutSuccess = function(req,res,next) {
+  // TODO implement the case of sucess
 };
