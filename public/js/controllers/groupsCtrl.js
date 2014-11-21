e88b797e4abe1bf24a98c8126661014fcc9e008a
Tyler Renelle <tylerrenelle@gmail.com>
2014-11-21 08:15:19 -0700

    fix(members): add a callback so members modal doesn't popup until after member is fetched

--- public/js/controllers/groupsCtrl.js
+++ public/js/controllers/groupsCtrl.js
@@ -45,8 +45,9 @@ habitrpg.controller("GroupsCtrl", ['$scope', '$rootScope', 'Shared', 'Groups', '
         } else {
           // We need the member information up top here, but then we pass it down to the modal controller
           // down below. Better way of handling this?
-          Members.selectMember(uid);
-          $rootScope.openModal('member', {controller:'MemberModalCtrl'});
+          Members.selectMember(uid, function(){
+            $rootScope.openModal('member', {controller:'MemberModalCtrl'});
+          });
         }
       }
 
