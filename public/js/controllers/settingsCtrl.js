756fa83418e470578b0b43975a7c4e3d2e244284
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2014-12-29 22:26:12 +0100

    fix(date): typo

--- public/js/controllers/settingsCtrl.js
+++ public/js/controllers/settingsCtrl.js
@@ -65,7 +65,7 @@ habitrpg.controller('SettingsCtrl',
       User.set({'preferences.language': $scope.language.code});
     }
 
-    $scope.availableFormats = ['MM/dd/yyyy','dd/MM/yyyy', 'YYYY/MM/DD'];
+    $scope.availableFormats = ['MM/dd/yyyy','dd/MM/yyyy', 'yyyy/MM/dd'];
 
     $scope.reroll = function(){
       User.user.ops.reroll({});
