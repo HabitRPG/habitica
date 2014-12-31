51acc477fe9ad58aba4c7aeede788d496441c3c6
Sabe Jones <sabrecat@gmail.com>
2014-12-30 19:57:24 -0600

    feat(NYE): New Year's cards

--- public/js/controllers/rootCtrl.js
+++ public/js/controllers/rootCtrl.js
@@ -195,6 +195,9 @@ habitrpg.controller("RootCtrl", ['$scope', '$rootScope', '$location', 'User', '$
     $scope.castStart = function(spell) {
       if (User.user.stats.mp < spell.mana) return Notification.text(window.env.t('notEnoughMana'));
 
+      if (spell.key == 'nye' && User.user.stats.gp < spell.value)
+        return Notification.text('Not enough gold.');
+
       $rootScope.applyingAction = true;
       $scope.spell = spell;
       if (spell.target == 'self') {
