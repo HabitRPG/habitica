774bb7bc3a4613073a45378e6c47345c86fa6664
Alice Harris <alice.harris@oldgods.net>
2014-12-31 15:42:46 +1000

    add 'items.' to 'gear.owned'

--- migrations/20141230_new_years_hats.js
+++ migrations/20141230_new_years_hats.js
@@ -1,11 +1,11 @@
 db.users.update(
-    {'gear.owned.head_special_nye':{$ne:null}},
-    {$set:{'gear.owned.head_special_nye2014':false}},
+    {'items.gear.owned.head_special_nye':{$ne:null}},
+    {$set:{'items.gear.owned.head_special_nye2014':false}},
     {multi:1}
 )
 
 db.users.update(
-    {'gear.owned.head_special_nye':null},
-    {$set:{'gear.owned.head_special_nye':false}},
+    {'items.gear.owned.head_special_nye':null},
+    {$set:{'items.gear.owned.head_special_nye':false}},
     {multi:1}
 )
