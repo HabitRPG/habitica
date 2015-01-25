af4f51857b96937f2df301490a6cb38a4d614f00
Blade Barringer <blade@crookedneighbor.com>
2015-01-25 15:10:15 -0600

    Changed models to use common instead of shared

--- src/models/challenge.js
+++ src/models/challenge.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var shared = require('habitrpg-shared');
+var shared = require('../../common');
 var _ = require('lodash');
 var TaskSchemas = require('./task');
 
