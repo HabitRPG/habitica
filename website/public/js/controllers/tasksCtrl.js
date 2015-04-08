9bb2f462640f31e3a17c63682104e668c6328384
Kevin Gisi <kevin@kevingisi.com>
2015-04-07 22:37:32 -0400

    Modify removeTask to accept a task object. Update task view to reflect new method signature.

--- website/public/js/controllers/tasksCtrl.js
+++ website/public/js/controllers/tasksCtrl.js
@@ -82,9 +82,9 @@ habitrpg.controller("TasksCtrl", ['$scope', '$rootScope', '$location', 'User','N
       }
     };
 
-    $scope.removeTask = function(list, $index) {
+    $scope.removeTask = function(task) {
       if (!confirm(window.env.t('sureDelete'))) return;
-      User.user.ops.deleteTask({params:{id:list[$index].id}})
+      User.user.ops.deleteTask({params:{id:task.id}})
     };
 
     $scope.saveTask = function(task, stayOpen, isSaveAndClose) {
@@ -166,7 +166,7 @@ habitrpg.controller("TasksCtrl", ['$scope', '$rootScope', '$location', 'User','N
           focusChecklist(task,$index-1);
         // Don't allow the backspace key to navigate back now that the field is gone
         $event.preventDefault();
-      } 
+      }
     }
     $scope.swapChecklistItems = function(task, oldIndex, newIndex) {
       var toSwap = task.checklist.splice(oldIndex, 1)[0];
@@ -186,7 +186,7 @@ habitrpg.controller("TasksCtrl", ['$scope', '$rootScope', '$location', 'User','N
 
     /*
      ------------------------
-     Items 
+     Items
      ------------------------
      */
 
