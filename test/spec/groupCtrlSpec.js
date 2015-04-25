3458815f28291711a4ad268dab6856b9b0161bc1
Joy Clark <joyclark2004@msn.com>
2015-04-25 23:08:28 +0200

    #5085 added some unit tests.

--- test/spec/groupCtrlSpec.js
+++ test/spec/groupCtrlSpec.js
@@ -65,3 +65,42 @@ describe('Groups Controller', function() {
     expect(myGuilds).to.be.called
   });
 });
+
+describe("Autocomplete controller", function() {
+  beforeEach(module('habitrpg'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  describe("AutocompleteCtrl", function() {
+    var $scope, controller;
+
+    beforeEach(function() {
+      $scope = {query: undefined, $watch: function(task,fn) {}}; // mock watch function
+      controller = $controller('AutocompleteCtrl', {$scope: $scope});
+    })
+
+    it('filtering with undefined query (not loaded yet) defaults to true', function() {
+      expect($scope.filterUser({user: "boo"})).to.be.ok
+    })
+
+    it('filtering with null query (no typing yet) defaults to true', function() {
+      $scope.query = null
+      expect($scope.filterUser({user: "boo"})).to.be.ok
+    })
+
+    it('filtering with prefix element will return true', function() {
+      $scope.query = {text: "pre"}
+      expect($scope.filterUser({user: "prefix"})).to.be.ok
+    })
+
+    it('filtering with nonprefix element will return false', function() {
+      $scope.query = {text: "noprefix"}
+      expect($scope.filterUser({user: "prefix"})).to.not.be.ok
+    })
+
+  });
+});
