f95eaa736a51b67d5935c0c3d79494fc0e1717cc
Kevin Gisi <kevin@kevingisi.com>
2015-06-08 08:35:10 -0400

    Add first directive spec

---
+++ test/spec/directives/focus-me.directive.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('focusMe Directive', function() {
+  var element, scope;
+
+  beforeEach(module('habitrpg'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+    scope = $rootScope.$new();
+
+    element = "<input focus-me='model'></input>";
+
+    element = $compile(element)(scope);
+    scope.$digest();
+  }));
+
+  it('focuses the element when appended to the DOM', function(done) {
+    inject(function($timeout) {
+      element.appendTo(document.body);
+
+      element.on('focus', function() { done() });
+
+      $timeout.flush();
+    });
+  });
+});
