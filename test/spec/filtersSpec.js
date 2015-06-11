2c52829256b12f66df3334181f0072754f2df70a
Jiri Chara <jirik.chara@gmail.com>
2015-06-11 20:39:29 +0200

    Implement filter tasks by text & notes
    In order to be able to filter tasks by it's text or notes - we need an
    extra filter to be implemented, because angular does not support OR
    condition for it's default filter directive.

--- test/spec/filtersSpec.js
+++ test/spec/filtersSpec.js
@@ -27,4 +27,28 @@ describe('Custom Filters', function() {
       });
     });
   });
+
+  describe('filterByTextAndNotes', function () {
+    it('returns undefined when no input given', function () {
+      expect(filter('filterByTextAndNotes')()).to.eql(undefined);
+    });
+
+    it('returns input if term is not a string', function () {
+      var input  = [1, 2, 3];
+      expect(filter('filterByTextAndNotes')(input, '')).to.eql(input);
+      expect(filter('filterByTextAndNotes')(input, undefined)).to.eql(input);
+      expect(filter('filterByTextAndNotes')(input, [])).to.eql(input);
+      expect(filter('filterByTextAndNotes')(input, new Date())).to.eql(input);
+    });
+
+    it('filters items by notes and text', function () {
+      var tasks = [
+        { text: 'foo' },
+        { text: 'foo', notes: 'bar' }
+      ];
+
+      expect(filter('filterByTextAndNotes')(tasks, 'bar')).to.eql([tasks[1]]);
+      expect(filter('filterByTextAndNotes')(tasks, 'foo')).to.eql([tasks[0], tasks[1]]);
+    });
+  });
 });
