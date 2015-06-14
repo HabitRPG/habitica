c3307dd6841cd0efb641e8c460f5d0325fecc44d
TheHollidayInn <krh121791@gmail.com>
2015-06-13 20:30:52 -0500

    Added clone challenge test

--- test/spec/controllers/challengesCtrlSpec.js
+++ test/spec/controllers/challengesCtrlSpec.js
@@ -182,4 +182,33 @@ describe('Challenges Controller', function() {
       });
     });
   });
+
+  it('Clones a challenge', function() {
+    var copyChallenge = new challenges.Challenge({
+      name: 'copyChallenge',
+      description: 'copyChallenge',
+      habits: [],
+      dailys: [],
+      todos: [],
+      rewards: [],
+      leader: user._id,
+      group: "copyGroup",
+      timestamp: +(new Date),
+      members: [user],
+      official: false,
+      _isMember: true
+    });
+    scope.clone(copyChallenge)
+
+    expect( scope.obj.name ).to.eql(copyChallenge.name );
+    expect( scope.obj.description ).to.eql(copyChallenge.description );
+    expect( scope.obj.habits ).to.eql(copyChallenge.habits );
+    expect( scope.obj.dailys ).to.eql(copyChallenge.dailys );
+    expect( scope.obj.todos ).to.eql(copyChallenge.todos );
+    expect( scope.obj.rewards ).to.eql(copyChallenge.rewards );
+    expect( scope.obj.leader ).to.eql(copyChallenge.leader );
+    expect( scope.obj.official ).to.eql(copyChallenge.official );
+
+  });
+
 });
