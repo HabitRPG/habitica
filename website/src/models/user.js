14983cb99615b058d4338c5bd131f8f3a1aabac2
Alys <alice.harris@oldgods.net>
2015-06-16 13:10:49 +1000

    replace minus Habit and To-Do sounds, rename NomindTR's theme, rename keys for theme since they aren't live yet

--- website/src/models/user.js
+++ website/src/models/user.js
@@ -310,7 +310,7 @@ var UserSchema = new Schema({
     skin: {type:String, 'default':'915533'},
     shirt: {type: String, 'default': 'blue'},
     timezoneOffset: Number,
-    sound: {type:String, 'default':'off', enum: ['off','danielTheBard', 'wattsTheme', 'gSalvadi']},
+    sound: {type:String, 'default':'off', enum: ['off','danielTheBard', 'wattsTheme', 'gokulTheme']},
     language: String,
     automaticAllocation: Boolean,
     allocationMode: {type:String, enum: ['flat','classbased','taskbased'], 'default': 'flat'},
