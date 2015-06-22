f8e3120b3a9bb014d827e65ea4fe066d0337e5ff
Sabe Jones <sabrecat@gmail.com>
2015-06-22 18:49:15 -0500

    fix(analytics): Move landing page tracking
    ...to AuthCtrl

--- website/public/js/controllers/authCtrl.js
+++ website/public/js/controllers/authCtrl.js
@@ -8,6 +8,8 @@ angular.module('habitrpg')
   .controller("AuthCtrl", ['$scope', '$rootScope', 'User', '$http', '$location', '$window','ApiUrl', '$modal', 'Analytics',
     function($scope, $rootScope, User, $http, $location, $window, ApiUrl, $modal, Analytics) {
 
+      Analytics.track({'hitType':'pageview','eventCategory':'page','eventAction':'landing page','page':'/static/front'});
+
       $scope.logout = function() {
         localStorage.clear();
         window.location.href = '/logout';
