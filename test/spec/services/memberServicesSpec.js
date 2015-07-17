a9757c9dbb9c318f9af05c26da70df534d32d241
Blade Barringer <blade@crookedneighbor.com>
2015-07-17 08:43:37 -0500

    Refactor member service; Remove logic in functions that were never used

--- test/spec/services/memberServicesSpec.js
+++ test/spec/services/memberServicesSpec.js
@@ -8,30 +8,59 @@ describe('memberServices', function() {
     members = Members;
   }));
 
+   afterEach(function() {
+     $httpBackend.verifyNoOutstandingExpectation();
+     $httpBackend.verifyNoOutstandingRequest();
+   });
+
+
   it('has no members at the beginning', function() {
     expect(members.members).to.be.an('object');
     expect(members.members).to.eql({});
     expect(members.selectedMember).to.be.undefined;
   });
 
-  it('populates members', function(){
-    var uid = 'abc';
-    members.populate({
-      members: [{ _id: uid }]
-    });
-    expect(members.members).to.eql({
-      abc: { _id: uid }
+  describe('addToMembersList', function() {
+    it('adds member to members object', function() {
+      var member = { _id: 'user_id' };
+      members.addToMembersList(member);
+      expect(members.members).to.eql({
+        user_id: { _id: 'user_id' }
+      });
     });
   });
 
-  it('selects a member', function(){
-    var uid = 'abc';
-    $httpBackend.expectGET('/api/v2/members/' + uid).respond({ _id: uid });
-    members.selectMember(uid, function(){});
-    $httpBackend.flush();
+  describe('selectMember', function() {
+    it('fetches member if not already in cache', function() {
+      var uid = 'abc';
+      $httpBackend.expectGET('/api/v2/members/' + uid).respond({ _id: uid });
+      members.selectMember(uid, function(){});
+      $httpBackend.flush();
 
-    expect(members.selectedMember._id).to.eql(uid);
-    expect(members.members).to.have.property(uid);
+      expect(members.selectedMember._id).to.eql(uid);
+      expect(members.members).to.have.property(uid);
+    });
+
+    it('fetches member if member data in cache is incomplete', function() {
+      var uid = 'abc';
+      members.members = {
+        abc: { _id: 'abc', items: {} }
+      }
+      $httpBackend.expectGET('/api/v2/members/' + uid).respond({ _id: uid });
+      members.selectMember(uid, function(){});
+      $httpBackend.flush();
+
+      expect(members.selectedMember._id).to.eql(uid);
+      expect(members.members).to.have.property(uid);
+    });
+
+    it('gets member from cache if member has a weapons object', function() {
+      var uid = 'abc';
+      members.members[uid] = { _id: uid, items: { weapon: {} } };
+      members.selectMember(uid, function(){
+        expect(members.selectedMember._id).to.eql(uid);
+        expect(members.members).to.have.property(uid);
+      });
+    });
   });
-
 });
