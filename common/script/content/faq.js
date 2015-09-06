245c7d48c8d9c5faaff0317d84a3083a14d4d26a
Blade Barringer <blade@crookedneighbor.com>
2015-09-06 16:11:53 -0500

    Simplify faq logic

--- common/script/content/faq.js
+++ common/script/content/faq.js
@@ -6,56 +6,16 @@ var t = require('./translation.coffee');
 
 var faq = {};
 
-faq.questions = [
-  {
-    question: t('mobileFaqQuestion1'),
-    ios: t('iosFaqAnswer1')
-  },
-  {
-    question: t('mobileFaqQuestion2'),
-    ios: t('iosFaqAnswer2')
-  },
-  {
-    question: t('mobileFaqQuestion3'),
-    ios: t('iosFaqAnswer3')
-  },
-  {
-    question: t('mobileFaqQuestion4'),
-    ios: t('iosFaqAnswer4')
-  },
-  {
-    question: t('mobileFaqQuestion5'),
-    ios: t('iosFaqAnswer5')
-  },
-  {
-    question: t('mobileFaqQuestion6'),
-    ios: t('iosFaqAnswer6')
-  },
-  {
-    question: t('mobileFaqQuestion7'),
-    ios: t('iosFaqAnswer7')
-  },
-  {
-    question: t('mobileFaqQuestion8'),
-    ios: t('iosFaqAnswer8')
-  },
-  {
-    question: t('mobileFaqQuestion9'),
-    ios: t('iosFaqAnswer9')
-  },
-  {
-    question: t('mobileFaqQuestion10'),
-    ios: t('iosFaqAnswer10')
-  },
-  {
-    question: t('mobileFaqQuestion11'),
-    ios: t('iosFaqAnswer11')
-  },
-  {
-    question: t('mobileFaqQuestion12'),
-    ios: t('iosFaqAnswer12')
-  }
-];
+faq.questions = [];
+
+for (var i = 1; i <= 12; i++) {
+  var question = {
+    question: t('mobileFaqQuestion' + i),
+    ios: t('iosFaqAnswer' + i)
+  };
+
+  faq.questions.push(question);
+}
 
 faq.stillNeedHelp = {
   ios: t('iosStillNeedHelp')
