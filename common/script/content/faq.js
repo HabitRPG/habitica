6fd2b9e5370a9530787378ea998b24250696ffae
Blade Barringer <blade@crookedneighbor.com>
2015-09-06 17:13:04 -0500

    Update content with web faq

--- common/script/content/faq.js
+++ common/script/content/faq.js
@@ -12,14 +12,16 @@ faq.questions = [];
 for (var i = 1; i <= NUMBER_OF_QUESTIONS; i++) {
   var question = {
     question: t('faqQuestion' + i),
-    ios: t('iosFaqAnswer' + i)
+    ios: t('iosFaqAnswer' + i),
+    web: t('webFaqAnswer' + i)
   };
 
   faq.questions.push(question);
 }
 
 faq.stillNeedHelp = {
-  ios: t('faqStillNeedHelp')
+  ios: t('iosFaqStillNeedHelp'),
+  web: t('webFaqStillNeedHelp')
 };
 
 module.exports = faq;
