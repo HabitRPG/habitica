df5a434d710b0ee51c305d8e1756db15ff609d6f
Blade Barringer <blade@crookedneighbor.com>
2015-09-17 08:45:35 -0500

    Move drop eggs into separate module

---
+++ common/script/content/eggs/drops.js
@@ -0,0 +1,46 @@
+let t = require('../helpers/translator');
+
+let eggs = {
+  Wolf: {
+    text: t('dropEggWolfText'),
+    adjective: t('dropEggWolfAdjective')
+  },
+  TigerCub: {
+    text: t('dropEggTigerCubText'),
+    mountText: t('dropEggTigerCubMountText'),
+    adjective: t('dropEggTigerCubAdjective')
+  },
+  PandaCub: {
+    text: t('dropEggPandaCubText'),
+    mountText: t('dropEggPandaCubMountText'),
+    adjective: t('dropEggPandaCubAdjective')
+  },
+  LionCub: {
+    text: t('dropEggLionCubText'),
+    mountText: t('dropEggLionCubMountText'),
+    adjective: t('dropEggLionCubAdjective')
+  },
+  Fox: {
+    text: t('dropEggFoxText'),
+    adjective: t('dropEggFoxAdjective')
+  },
+  FlyingPig: {
+    text: t('dropEggFlyingPigText'),
+    adjective: t('dropEggFlyingPigAdjective')
+  },
+  Dragon: {
+    text: t('dropEggDragonText'),
+    adjective: t('dropEggDragonAdjective')
+  },
+  Cactus: {
+    text: t('dropEggCactusText'),
+    adjective: t('dropEggCactusAdjective')
+  },
+  BearCub: {
+    text: t('dropEggBearCubText'),
+    mountText: t('dropEggBearCubMountText'),
+    adjective: t('dropEggBearCubAdjective')
+  },
+};
+
+module.exports = eggs;
--- common/script/content/eggs/index.js
+++ common/script/content/eggs/index.js
@@ -3,48 +3,7 @@ let each = require('lodash').each;
 let defaults = require('lodash').defaults;
 let t = require('../helpers/translator');
 
-let dropEggs = {
-  Wolf: {
-    text: t('dropEggWolfText'),
-    adjective: t('dropEggWolfAdjective')
-  },
-  TigerCub: {
-    text: t('dropEggTigerCubText'),
-    mountText: t('dropEggTigerCubMountText'),
-    adjective: t('dropEggTigerCubAdjective')
-  },
-  PandaCub: {
-    text: t('dropEggPandaCubText'),
-    mountText: t('dropEggPandaCubMountText'),
-    adjective: t('dropEggPandaCubAdjective')
-  },
-  LionCub: {
-    text: t('dropEggLionCubText'),
-    mountText: t('dropEggLionCubMountText'),
-    adjective: t('dropEggLionCubAdjective')
-  },
-  Fox: {
-    text: t('dropEggFoxText'),
-    adjective: t('dropEggFoxAdjective')
-  },
-  FlyingPig: {
-    text: t('dropEggFlyingPigText'),
-    adjective: t('dropEggFlyingPigAdjective')
-  },
-  Dragon: {
-    text: t('dropEggDragonText'),
-    adjective: t('dropEggDragonAdjective')
-  },
-  Cactus: {
-    text: t('dropEggCactusText'),
-    adjective: t('dropEggCactusAdjective')
-  },
-  BearCub: {
-    text: t('dropEggBearCubText'),
-    mountText: t('dropEggBearCubMountText'),
-    adjective: t('dropEggBearCubAdjective')
-  }
-};
+let dropEggs = require('./drops');
 
 each(dropEggs, (egg, key) => {
   return defaults(egg, {
