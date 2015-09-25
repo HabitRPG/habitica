3bb48063c901f4bfc835d8b977ac38cc17e55c0c
Blade Barringer <blade@crookedneighbor.com>
2015-09-25 08:08:44 -0500

    Correct saddle

--- common/script/src/content/food/saddle.js
+++ common/script/src/content/food/saddle.js
@@ -7,7 +7,11 @@ const CAN_BUY = true;
 const CAN_DROP = false;
 
 let saddle = {
-  Saddle: { value: 5 },
+  Saddle: {
+    value: 5,
+    text: t('foodSaddleText'),
+    notes: t('foodSaddleNotes'),
+  },
 };
 
 setFoodDefaults(saddle, {canBuy: CAN_BUY, canDrop: CAN_DROP});
