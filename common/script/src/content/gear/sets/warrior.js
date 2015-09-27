40fc03263d8dbca35c90ceff4442c72f01285d80
Blade Barringer <blade@crookedneighbor.com>
2015-09-27 09:11:40 -0500

    Reformate warrior content file

--- common/script/src/content/gear/sets/warrior.js
+++ common/script/src/content/gear/sets/warrior.js
@@ -2,106 +2,44 @@ import {generateGearSet} from '../../helpers';
 import events from '../../events';
 
 let armor = {
-  1: {
-    con: 3,
-    value: 30
-  },
-  2: {
-    con: 5,
-    value: 45
-  },
-  3: {
-    con: 7,
-    value: 65
-  },
-  4: {
-    con: 9,
-    value: 90
-  },
-  5: {
-    con: 11,
-    value: 120,
-    last: true
-  }
+  1: { con: 3, value: 30 },
+  2: { con: 5, value: 45 },
+  3: { con: 7, value: 65 },
+  4: { con: 9, value: 90 },
+  5: { con: 11, value: 120, last: true },
 };
 
 let head = {
-  1: {
-    str: 2,
-    value: 15
-  },
-  2: {
-    str: 4,
-    value: 25
-  },
-  3: {
-    str: 6,
-    value: 40
-  },
-  4: {
-    str: 9,
-    value: 60
-  },
-  5: {
-    str: 12,
-    value: 80,
-    last: true
-  }
+  1: { str: 2, value: 15 },
+  2: { str: 4, value: 25 },
+  3: { str: 6, value: 40 },
+  4: { str: 9, value: 60 },
+  5: { str: 12, value: 80, last: true },
 };
 
 let shield = {
-  1: {
-    con: 2,
-    value: 20
-  },
-  2: {
-    con: 3,
-    value: 35
-  },
-  3: {
-    con: 5,
-    value: 50
-  },
-  4: {
-    con: 7,
-    value: 70
-  },
-  5: {
-    con: 9,
-    value: 90,
-    last: true
-  }
+  1: { con: 2, value: 20 },
+  2: { con: 3, value: 35 },
+  3: { con: 5, value: 50 },
+  4: { con: 7, value: 70 },
+  5: { con: 9, value: 90, last: true },
 };
 
 let weapon = {
-  0: {
-    value: 1
-  },
-  1: {
-    str: 3,
-    value: 20
-  },
-  2: {
-    str: 6,
-    value: 30
-  },
-  3: {
-    str: 9,
-    value: 45
-  },
-  4: {
-    str: 12,
-    value: 65
-  },
-  5: {
-    str: 15,
-    value: 90
-  },
-  6: {
-    str: 18,
-    value: 120,
-    last: true
-  }
+  0: { value: 1 },
+  1: { str: 3, value: 20 },
+  2: { str: 6, value: 30 },
+  3: { str: 9, value: 45 },
+  4: { str: 12, value: 65 },
+  5: { str: 15, value: 90 },
+  6: { str: 18, value: 120, last: true },
+};
+
+let warriorSet = {
+  armor: armor,
+  head: head,
+  shield: shield,
+  weapon: weapon,
 };
 
 generateGearSet(armor, {setName: 'warrior', gearType: 'armor'});
@@ -109,9 +47,4 @@ generateGearSet(head, {setName: 'warrior', gearType: 'head'});
 generateGearSet(shield, {setName: 'warrior', gearType: 'shield'});
 generateGearSet(weapon, {setName: 'warrior', gearType: 'weapon'});
 
-export default {
-  armor: armor,
-  head: head,
-  shield: shield,
-  weapon: weapon,
-};
+export default warriorSet;
