98e6252e959098bc159f2b9146020c0e5ecf2a83
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-09-27 17:19:42 +0200

    wip: mongoose 4 changes

--- website/src/models/user.js
+++ website/src/models/user.js
@@ -543,7 +543,7 @@ UserSchema.pre('save', function(next) {
                   .concat(shared.content.userDefaults.todos)
                   .concat(shared.content.userDefaults.rewards);
 
-    async.each(defaultTasks, function(task, cb) {
+    Task.create(defaultTasks.map(function(task) {
       var newTask = _.cloneDeep(task);
       newTask.userId = self._id;
 
@@ -561,8 +561,8 @@ UserSchema.pre('save', function(next) {
         });
       }
 
-      Task.create(newTask, cb);
-    }, next);
+      return newTask;
+    }), next);
   }
 });
 
