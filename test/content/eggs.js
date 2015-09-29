54e910d67ab9dced798cadd1aeb14622b14f5ceb
Blade Barringer <blade@crookedneighbor.com>
2015-09-29 08:07:07 -0500

    Add egg content test

---
+++ test/content/eggs.js
@@ -0,0 +1,21 @@
+import {allEggs} from '../../common/script/src/content/eggs';
+import {each} from 'lodash';
+
+describe('Egg Locales', () => {
+  each(allEggs, (egg, key) => {
+    describe(`${key} Egg`, () => {
+      it('has a valid text attribute', () => {
+        expectValidTranslationString(egg.text);
+      });
+
+      it('has a valid notes attribute', () => {
+        expectValidTranslationString(egg.notes);
+      });
+
+      it('has a valid ajective attribute', () => {
+        expectValidTranslationString(egg.adjective);
+      });
+    });
+  });
+});
+
