e27751c1be7cb763f189b2a6ca2af2d0809fb69f
Blade Barringer <blade@crookedneighbor.com>
2015-09-29 08:06:48 -0500

    Organize quest tests by key

--- test/content/quests.js
+++ test/content/quests.js
@@ -3,18 +3,20 @@ import {each} from 'lodash';
 
 describe('Quest Locales', () => {
   each(allQuests, (quest, key) => {
-    it(`${key} has a valid text attribute`, () => {
-      expectValidTranslationString(quest.text);
-    });
-
-    it(`${key} has a valid notes attribute`, () => {
-      expectValidTranslationString(quest.notes);
-    });
-
-    if (quest.completion) {
-      it(`${key} has a valid completion attribute`, () => {
-        expectValidTranslationString(quest.completion);
+    describe(`${key} Quest`, () => {
+      it('has a valid text attribute', () => {
+        expectValidTranslationString(quest.text);
       });
-    }
+
+      it('has a valid notes attribute', () => {
+        expectValidTranslationString(quest.notes);
+      });
+
+      if (quest.completion) {
+        it('has a valid completion attribute', () => {
+          expectValidTranslationString(quest.completion);
+        });
+      }
+    });
   });
 });
