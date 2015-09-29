9de2ee473d39992e4de67135a37cfd2d027126ce
Blade Barringer <blade@crookedneighbor.com>
2015-09-29 12:54:45 -0500

    Correct gear test

--- test/content/gear.js
+++ test/content/gear.js
@@ -1,8 +1,3 @@
-import armor from '../../common/script/src/content/gear/armor';
-import {each} from 'lodash';
-
-let allGear = [armor];
-
 describe('Gear Locales', () => {
   runTestsInDirectory('gear');
 });
