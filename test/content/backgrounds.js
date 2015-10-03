62b15c4f48b147a8657fed6a5a250e79d3712155
Blade Barringer <blade@crookedneighbor.com>
2015-10-03 11:54:58 -0500

    Simplify background tests

--- test/content/backgrounds.js
+++ test/content/backgrounds.js
@@ -1,23 +1,20 @@
 import {
-  expectValidTranslationString
+  expectValidTranslationString,
+  describeEachItem
 } from '../helpers/content.helper';
-
-import backgroundSets from '../../common/script/src/content/backgrounds';
 import {each} from 'lodash';
 
-describe('Backgrounds Locales', () => {
-  each(backgroundSets, (set, key) => {
-    describe(`${key} Set`, () => {
-      each(set, (bg, name) => {
-        describe(`${name} Background`, () => {
-          it('has a valid text attribute', () => {
-            expectValidTranslationString(bg.text);
-          });
+import backgroundSets from '../../common/script/src/content/backgrounds';
 
-          it('has a valid notes attribute', () => {
-            expectValidTranslationString(bg.notes);
-          });
-        });
+describe('Backgrounds', () => {
+  each(backgroundSets, (set, name) => {
+    describeEachItem(name, set, (bg, key) => {
+      it('has a valid text attribute', () => {
+        expectValidTranslationString(bg.text);
+      });
+
+      it('has a valid notes attribute', () => {
+        expectValidTranslationString(bg.notes);
       });
     });
   });
