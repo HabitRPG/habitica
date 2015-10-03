cc579b97a3918dc3943ce37389e7490c9ceb99b7
Blade Barringer <blade@crookedneighbor.com>
2015-10-03 11:42:23 -0500

    Increase converage around egg content

--- test/content/eggs.js
+++ test/content/eggs.js
@@ -1,25 +1,36 @@
 import {
-  expectValidTranslationString
+  expectValidTranslationString,
+  describeEachItem
 } from '../helpers/content.helper';
 
 import {allEggs} from '../../common/script/src/content/eggs';
-import {each} from 'lodash';
 
-describe('Egg Locales', () => {
-  each(allEggs, (egg, key) => {
-    describe(`${key} Egg`, () => {
-      it('has a valid text attribute', () => {
-        expectValidTranslationString(egg.text);
-      });
+describeEachItem('Eggs', allEggs, (egg, key) => {
+  it('has a key attribute', () => {
+    expect(egg.key).to.eql(key);
+  });
 
-      it('has a valid notes attribute', () => {
-        expectValidTranslationString(egg.notes);
-      });
+  it('has a valid text attribute', () => {
+    expectValidTranslationString(egg.text);
+  });
 
-      it('has a valid ajective attribute', () => {
-        expectValidTranslationString(egg.adjective);
-      });
-    });
+  it('has a valid mountText attribute', () => {
+    expectValidTranslationString(egg.mountText);
+  });
+
+  it('has a valid notes attribute', () => {
+    expectValidTranslationString(egg.notes);
+  });
+
+  it('has a valid ajective attribute', () => {
+    expectValidTranslationString(egg.adjective);
+  });
+
+  it('has a canBuy function', () => {
+    expect(egg.canBuy).to.be.a('function');
+  });
+
+  it('has a value attribute', () => {
+    expect(egg.value).to.be.greaterThan(0);
   });
 });
-
