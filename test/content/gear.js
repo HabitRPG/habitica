a492649f717edfa93d775cf163a77dd7d65d1b45
Blade Barringer <blade@crookedneighbor.com>
2015-10-03 09:59:10 -0500

    Simplify gear tests and test more items

--- test/content/gear.js
+++ test/content/gear.js
@@ -1,3 +1,49 @@
+import {tree as allGear} from '../../common/script/src/content/gear';
+import {each} from 'lodash';
+
 describe('Gear', () => {
-  runTestsInDirectory('gear');
+  each(allGear, (piece, type) => {
+    describeEachItem(type, piece, (set, key) => {
+      checkGearAttributes(set);
+    });
+  });
 });
+
+function checkGearAttributes(set) {
+  each(set, (gear, key) => {
+    describe(`${key}`, () => {
+
+      it('has a value attribute', () => {
+        expect(gear.value).to.be.at.least(0);
+      });
+
+      it('has a valid con attribute', () => {
+        expect(gear.con).to.be.at.least(0);
+      });
+
+      it('has a valid int attribute', () => {
+        expect(gear.int).to.be.at.least(0);
+      });
+
+      it('has a valid per attribute', () => {
+        expect(gear.per).to.be.at.least(0);
+      });
+
+      it('has a valid str attribute', () => {
+        expect(gear.str).to.be.at.least(0);
+      });
+
+      it('has a canBy function', () => {
+        expect(gear.canBuy).to.be.a('function');
+      });
+
+      it('has a valid text attribute', () => {
+        expectValidTranslationString(gear.text);
+      });
+
+      it('has a valid notes attribute', () => {
+        expectValidTranslationString(gear.notes);
+      });
+    });
+  });
+}
