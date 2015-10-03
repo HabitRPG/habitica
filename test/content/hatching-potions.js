7e080d5423f05bb5542190689df1a28992bdb61e
Blade Barringer <blade@crookedneighbor.com>
2015-10-03 11:08:03 -0500

    Expand hatching potions test

--- test/content/hatching-potions.js
+++ test/content/hatching-potions.js
@@ -1,20 +1,29 @@
 import {
-  expectValidTranslationString
+  expectValidTranslationString,
+  describeEachItem
 } from '../helpers/content.helper';
-
-import { all as potions } from '../../common/script/src/content/hatching-potions';
 import {each} from 'lodash';
 
-describe('Hatching Potion Locales', () => {
-  each(potions, (potion, key) => {
-    describe(`${key} Potion`, () => {
-      it('has a valid text attribute', () => {
-        expectValidTranslationString(potion.text);
-      });
+import { all as potions } from '../../common/script/src/content/hatching-potions';
 
-      it('has a valid notes attribute', () => {
-        expectValidTranslationString(potion.notes);
-      });
-    });
+describeEachItem('Hatching Potions', potions, (potion, key) => {
+  it('has a valid key', () => {
+    expect(potion.key).to.eql(key);
+  });
+
+  it('has a canBuy function', () => {
+    expect(potion.canBuy).to.be.a('function');
+  });
+
+  it('has a valid text attribute', () => {
+    expectValidTranslationString(potion.text);
+  });
+
+  it('has a valid notes attribute', () => {
+    expectValidTranslationString(potion.notes);
+  });
+
+  it('has a valid value', () => {
+    expect(potion.value).to.be.greaterThan(0);
   });
 });
