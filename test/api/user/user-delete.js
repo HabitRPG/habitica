7f5c556654d481a3e70cb6aaabba50d570eda31f
Blade Barringer <blade@crookedneighbor.com>
2015-10-17 20:23:29 -0500

    Add pending tests

--- test/api/user/user-delete.js
+++ test/api/user/user-delete.js
@@ -26,4 +26,20 @@ describe('DELETE /user', () => {
         done();
       });
   });
+
+  context('user in solo group', () => {
+
+    it('deletes party when user is the only member');
+
+    it('deletes private guild when user is the only member');
+  });
+
+  context('user in group with members', () => {
+
+    it('removes user from all groups user was a part of');
+
+    it('chooses new group leader for any group user was the leader of');
+
+    it('removes invitations from groups');
+  });
 });
