a3def0eca57f8b6a0a1905631cbafe4635781f74
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-10-24 11:53:23 -0400

    Add gulp-stylus

--- tasks/gulp-build.js
+++ tasks/gulp-build.js
@@ -4,6 +4,8 @@ import browserify from 'browserify';
 let source = require('vinyl-source-stream');
 let rename = require("gulp-rename");
 let minifyCss = require('gulp-minify-css');
+let stylus = require('gulp-stylus');
+let nib = require('nib');
 
 require('gulp-grunt')(gulp);
 
@@ -31,6 +33,21 @@ gulp.task('build:dev', ['babel:common', 'prepare:staticNewStuff'], (done) => {
 	.pipe(minifyCss({compatibility: 'ie8'}))
 	.pipe(rename('habitrpg-shared.css'))
 	.pipe(gulp.dest('common/dist/sprites'));
+
+	let stylusOptions = {
+		compress: false,
+		use: nib(),
+		'include css': true
+
+	};
+	gulp.src('website/public/css/index.styl')
+		.pipe(stylus(stylusOptions))
+		.pipe(rename('app.css'))
+		.pipe(gulp.dest('website/build'))
+
+	gulp.src('website/public/css/static.styl')
+		.pipe(stylus(stylusOptions))
+		.pipe(gulp.dest('website/build'))
 });
 
 gulp.task('build:dev:watch', ['build:dev'], () => {
