f98c6b7192634b745ba5d1065c7a69501f52caa6
Blade Barringer <blade@crookedneighbor.com>
2015-10-25 09:40:23 -0500

    Add chai-shallow-deep-equal plugin

--- test/helpers/globals.helper.js
+++ test/helpers/globals.helper.js
@@ -5,5 +5,6 @@
 global._ = require("lodash")
 global.chai = require("chai")
 chai.use(require("sinon-chai"))
+chai.use(require("chai-shallow-deep-equal"));
 chai.use(require("chai-as-promised"));
 global.expect = chai.expect
