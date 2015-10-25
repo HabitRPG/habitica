177c81da81165f5ab6df9acd369027eb464c2035
Blade Barringer <blade@crookedneighbor.com>
2015-10-25 10:43:34 -0500

    Use promise syntax for Get groups id route test.

--- test/api/groups/GET-groups_id.test.js
+++ test/api/groups/GET-groups_id.test.js
@@ -19,8 +19,8 @@ describe('GET /groups/:id', () => {
     context(`Member of a ${groupType}`, () => {
       let leader, member, createdGroup;
 
-      before((done) => {
-        createAndPopulateGroup({
+      before(() => {
+        return createAndPopulateGroup({
           members: 30,
           groupDetails: {
             name: 'test guild',
@@ -31,59 +31,53 @@ describe('GET /groups/:id', () => {
           leader = res.leader;
           member = res.members[0];
           createdGroup = res.group;
-          done();
-        }).catch(done);
+        });
       });
 
-      it('returns the group object', (done) => {
+      it('returns the group object', () => {
         let api = requester(member);
-        api.get(`/groups/${createdGroup._id}`)
-          .then((group) => {
-            expect(group._id).to.eql(createdGroup._id);
-            expect(group.name).to.eql(createdGroup.name);
-            expect(group.type).to.eql(createdGroup.type);
-            expect(group.privacy).to.eql(createdGroup.privacy);
-            done();
-          }).catch(done);
+        return expect(api.get(`/groups/${createdGroup._id}`))
+          .to.eventually.shallowDeepEqual({
+            _id: createdGroup._id,
+            name: createdGroup.name,
+            type: createdGroup.type,
+            privacy: createdGroup.privacy,
+          });
       });
 
-      it('transforms members array to an array of user objects', (done) => {
+      it('transforms members array to an array of user objects', () => {
         let api = requester(member);
-        api.get(`/groups/${createdGroup._id}`).then((group) => {
-          let foundMember = group.members[0];
-          expect(foundMember._id).to.exist;
-          expect(foundMember.profile.name).to.exist;
-          expect(foundMember.items).to.exist;
-          expect(foundMember.stats).to.exist;
-          expect(foundMember.achievements).to.exist;
-          expect(foundMember.contributor).to.exist;
-          done();
-        }).catch(done);
+        return api.get(`/groups/${createdGroup._id}`).then((group) => {
+          let member = group.members[0];
+          expect(member._id).to.exist;
+          expect(member.profile.name).to.exist;
+          expect(member.contributor).to.exist;
+          expect(member.achievements).to.exist;
+          expect(member.items).to.exist;
+        });
       });
 
-      it('transforms leader id to leader object', (done) => {
+      it('transforms leader id to leader object', () => {
         let api = requester(member);
-        api.get(`/groups/${createdGroup._id}`).then((group) => {
+        return api.get(`/groups/${createdGroup._id}`).then((group) => {
           expect(group.leader._id).to.eql(leader._id);
           expect(group.leader.profile.name).to.eql(leader.profile.name);
           expect(group.leader.items).to.exist;
           expect(group.leader.stats).to.exist;
           expect(group.leader.achievements).to.exist;
           expect(group.leader.contributor).to.exist;
-          done();
-        }).catch(done);
+        });
       });
 
-      it('includes the user in the members list', (done) => {
+      it('includes the user in the members list', () => {
         let api = requester(member);
-        api.get(`/groups/${createdGroup._id}`).then((group) => {
+        return api.get(`/groups/${createdGroup._id}`).then((group) => {
           let members = group.members;
           let userInGroup = find(members, (user) => {
             return member._id === user._id;
           });
           expect(userInGroup).to.be.ok;
-          done();
-        }).catch(done);
+        });
       });
     });
   });
@@ -91,8 +85,8 @@ describe('GET /groups/:id', () => {
   context('Non-member of a public guild', () => {
     let leader, nonMember, createdGroup;
 
-    before((done) => {
-      createAndPopulateGroup({
+    before(() => {
+      return createAndPopulateGroup({
         members: 1,
         groupDetails: {
           name: 'test guild',
@@ -105,40 +99,36 @@ describe('GET /groups/:id', () => {
         return generateUser();
       }).then((user) => {
         nonMember = user;
-        done();
-      }).catch(done);
+      });
     });
 
-    it('returns the group object for a non-member', (done) => {
+    it('returns the group object for a non-member', () => {
       let api = requester(nonMember);
-      api.get(`/groups/${createdGroup._id}`)
+      return api.get(`/groups/${createdGroup._id}`)
         .then((group) => {
           expect(group._id).to.eql(createdGroup._id);
           expect(group.name).to.eql(createdGroup.name);
           expect(group.type).to.eql(createdGroup.type);
           expect(group.privacy).to.eql(createdGroup.privacy);
-          done();
-        }).catch(done);
+        });
     });
 
-    it('does not include user in members list', (done) => {
+    it('does not include user in members list', () => {
       let api = requester(nonMember);
-      api.get(`/groups/${createdGroup._id}`)
-        .then((group) => {
-          let userInGroup = find(group.members, (user) => {
-            return nonMember._id === user._id;
-          });
-          expect(userInGroup).to.not.be.ok;
-          done();
-        }).catch(done);
+      return api.get(`/groups/${createdGroup._id}`).then((group) => {
+        let userInGroup = find(group.members, (user) => {
+          return nonMember._id === user._id;
+        });
+        expect(userInGroup).to.not.be.ok;
+      });
     });
   });
 
   context('Private Guilds', () => {
     let leader, nonMember, createdGroup;
 
-    before((done) => {
-      createAndPopulateGroup({
+    before(() => {
+      return createAndPopulateGroup({
         members: 1,
         groupDetails: {
           name: 'test guild',
@@ -151,25 +141,21 @@ describe('GET /groups/:id', () => {
         return generateUser();
       }).then((user) => {
         nonMember = user;
-        done();
-      }).catch(done);
+      });
     });
 
-    it('does not return the group object for a non-member', (done) => {
+    it('does not return the group object for a non-member', () => {
       let api = requester(nonMember);
-      api.get(`/groups/${createdGroup._id}`)
-        .then((done)).catch((err) => {
-          expect(err).to.eql('Group not found or you don\'t have access.');
-          done()
-      });
+      return expect(api.get(`/groups/${createdGroup._id}`))
+        .to.be.rejectedWith('Group not found or you don\'t have access.');
     });
   });
 
   context('Non-member of a party', () => {
     let leader, nonMember, createdGroup;
 
-    before((done) => {
-      createAndPopulateGroup({
+    before(() => {
+      return createAndPopulateGroup({
         members: 1,
         groupDetails: {
           name: 'test party',
@@ -182,25 +168,21 @@ describe('GET /groups/:id', () => {
         return generateUser();
       }).then((user) => {
         nonMember = user;
-        done();
-      }).catch(done);
+      });
     });
 
-    it('does not return the group object for a non-member', (done) => {
+    it('does not return the group object for a non-member', () => {
       let api = requester(nonMember);
-      api.get(`/groups/${createdGroup._id}`)
-        .then((done)).catch((err) => {
-          expect(err).to.eql('Group not found or you don\'t have access.');
-          done()
-        });
+      return expect(api.get(`/groups/${createdGroup._id}`))
+        .to.be.rejectedWith('Group not found or you don\'t have access.');
     });
   });
 
   context('Member of a party', () => {
     let leader, member, createdGroup;
 
-    before((done) => {
-      createAndPopulateGroup({
+    before(() => {
+      return createAndPopulateGroup({
         members: 1,
         groupDetails: {
           name: 'test party',
@@ -211,40 +193,34 @@ describe('GET /groups/:id', () => {
         leader = res.leader;
         createdGroup = res.group;
         member = res.members[0];
-        done();
-      }).catch(done);
+      });
     });
 
-    it('returns the user\'s party if an id of "party" is passed in', (done) => {
+    it('returns the user\'s party if an id of "party" is passed in', () => {
       let api = requester(member);
-      api.get('/groups/party')
+      return api.get('/groups/party')
         .then((group) => {
           expect(group._id).to.eql(createdGroup._id);
           expect(group.name).to.eql(createdGroup.name);
           expect(group.type).to.eql(createdGroup.type);
           expect(group.privacy).to.eql(createdGroup.privacy);
-          done();
-        }).catch(done);
+        });
     });
   });
 
   context('Non-existent group', () => {
     let user;
 
-    beforeEach((done) => {
-      generateUser().then((_user) => {
+    beforeEach(() => {
+      return generateUser().then((_user) => {
         user = _user;
-        done();
-      }).catch(done);
+      });
     });
 
-    it('returns error if group does not exist', (done) => {
+    it('returns error if group does not exist', () => {
       let api = requester(user);
-      api.get('/groups/group-that-does-not-exist')
-        .then((done)).catch((err) => {
-          expect(err).to.eql('Group not found or you don\'t have access.');
-          done()
-        });
+      return expect(api.get('/groups/group-that-does-not-exist'))
+        .to.be.rejectedWith('Group not found or you don\'t have access.');
     });
   });
 });
