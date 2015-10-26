0eac95317075e8004804ece74d96d2a3296fd936
Blade Barringer <blade@crookedneighbor.com>
2015-10-26 08:04:30 -0500

    Adjust stying of put test

--- test/api/user/PUT-user.test.js
+++ test/api/user/PUT-user.test.js
@@ -5,23 +5,19 @@ import {
 
 describe('PUT /user', () => {
   let api, user;
-  
-  beforeEach((done) => {
-    generateUser().then((usr) => {
+
+  beforeEach(() => {
+    return generateUser().then((usr) => {
       user = usr;
-      api = requester(usr);
-      done();
-    }).catch(done);
+      api = requester(user);
+    });
   });
-  
-  it("updates the user", () => {
-    let api = requester(user);
-    return api.get('/user')
-    .then((fetchedUser) => {  
-      return api.put("/user", {"profile.name" : "Frodo"});
-    })
-    .then((updatedUser) => {
-      expect(updatedUser.profile.name).to.eql("Frodo");
+
+  it('updates the user', () => {
+    return api.put('/user', {
+      'profile.name' : 'Frodo',
+    }).then((updatedUser) => {
+      expect(updatedUser.profile.name).to.eql('Frodo');
     });
   });
 });
