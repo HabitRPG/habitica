eb524f585ab6d22c3ae3332396e865dc8afe5c45
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-10-28 12:10:08 -0400

    Convert tab to spaces again with search and replace

--- tasks/gulp-copy.js
+++ tasks/gulp-copy.js
@@ -1,11 +1,11 @@
 import gulp from 'gulp';
 
 gulp.task('copy', (cb) => {
-	gulp.src('website/public/favicon.ico').pipe(gulp.dest('website/build'));
-	gulp.src(['common/dist/sprites/spritesmith*.png', 'common/img/sprites/backer-only/*.gif', 'common/img/sprites/npc_ian.gif', 'common/img/sprites/quest_burnout.gif'], {
-		base: './'
-	}).pipe(gulp.dest('website/build'));	
-	gulp.src(['website/public/bower_components/bootstrap/dist/fonts/*'], {
-		base: './website/public'
-	}).pipe(gulp.dest('website/build'));
+  gulp.src('website/public/favicon.ico').pipe(gulp.dest('website/build'));
+  gulp.src(['common/dist/sprites/spritesmith*.png', 'common/img/sprites/backer-only/*.gif', 'common/img/sprites/npc_ian.gif', 'common/img/sprites/quest_burnout.gif'], {
+    base: './'
+  }).pipe(gulp.dest('website/build'));
+  gulp.src(['website/public/bower_components/bootstrap/dist/fonts/*'], {
+    base: './website/public'
+  }).pipe(gulp.dest('website/build'));
 })
