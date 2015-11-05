aaa4a7da12f984dd8b9979167279f0e881b97820
Blade Barringer <blade@crookedneighbor.com>
2015-11-04 20:45:51 -0600

    Add sinon to helpers

--- test/helpers/globals.helper.js
+++ test/helpers/globals.helper.js
@@ -4,6 +4,7 @@
 
 global._ = require("lodash")
 global.chai = require("chai")
+global.sinon = require("sinon");
 chai.use(require("sinon-chai"))
 chai.use(require("chai-as-promised"));
 global.expect = chai.expect
