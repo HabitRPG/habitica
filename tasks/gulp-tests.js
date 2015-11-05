af7b660e18ee0b88569c07de7e7cc72c8f08d94c
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-11-05 18:40:39 -0500

    Call callback to finish the task

--- tasks/gulp-tests.js
+++ tasks/gulp-tests.js
@@ -56,7 +56,8 @@ gulp.task('test:prepare:server', ['test:prepare:mongo'], () => {
   }
 });
 
-gulp.task('test:prepare:build', ['test:prepare:translations', 'build:dev'], () => {
+gulp.task('test:prepare:build', ['test:prepare:translations', 'build:dev'], (cb) => {
+  cb();
 });
 
 gulp.task('test:prepare:webdriver', (cb) => {
