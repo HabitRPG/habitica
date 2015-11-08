9451e7239b72e8b6348bac04c9d648debad8cf23
Blade Barringer <blade@crookedneighbor.com>
2015-11-08 09:00:07 -0600

    Add nconf setup to unit test helper

--- test/helpers/globals.helper.js
+++ test/helpers/globals.helper.js
@@ -8,5 +8,9 @@ global.sinon = require("sinon");
 chai.use(require("sinon-chai"))
 chai.use(require("chai-as-promised"));
 global.expect = chai.expect
-
 global.sandbox = sinon.sandbox.create();
+
+//------------------------------
+// Load nconf for unit tests
+//------------------------------
+require('../../website/src/libs/api-v3/setupNconf')();
