e518e2795f389f960ece8ee82b27731be6b69420
Blade Barringer <blade@crookedneighbor.com>
2015-11-09 19:11:25 -0600

    Correct setupConfig to use proper linting.

--- website/src/libs/api-v3/setupNconf.js
+++ website/src/libs/api-v3/setupNconf.js
@@ -4,12 +4,12 @@ import { join, resolve } from 'path';
 const PATH_TO_CONFIG = join(resolve(__dirname, '../../../../config.json'));
 
 export default function setupNconf (file) {
-  file = file || PATH_TO_CONFIG;
+  let configFile = file || PATH_TO_CONFIG;
 
   nconf
     .argv()
     .env()
-    .file('user', file);
+    .file('user', configFile);
 
   nconf.set('IS_PROD', nconf.get('NODE_ENV') === 'production');
   nconf.set('IS_DEV', nconf.get('NODE_ENV') === 'development');
