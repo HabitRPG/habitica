ac4f26a94f06d837f0a6b68f7c3dd26c68275522
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-11-11 16:05:35 +0100

    add tests for password utilities

--- website/src/libs/api-v3/password.js
+++ website/src/libs/api-v3/password.js
@@ -1,5 +1,7 @@
+// Utilities for working with passwords
 import crypto from 'crypto';
 
+// Return the encrypted version of a password (using sha1) given a salt
 export function encrypt (password, salt) {
   return crypto
           .createHmac('sha1', salt)
@@ -7,6 +9,7 @@ export function encrypt (password, salt) {
           .digest('hex');
 }
 
+// Create a salt, default length is 10
 export function makeSalt (len = 10) {
   return crypto
           .randomBytes(Math.ceil(len / 2))
