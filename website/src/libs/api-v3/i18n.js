f672ac8c59ff2d037a46a68d0c4f21265493cfa1
Blade Barringer <blade@crookedneighbor.com>
2015-11-13 18:05:28 -0600

    Port over change to v2 lib

--- website/src/libs/api-v3/i18n.js
+++ website/src/libs/api-v3/i18n.js
@@ -79,10 +79,30 @@ export let defaultLangCodes = _.without(langCodes, 'en_GB');
 
 // A map of languages that have different versions and the relative versions
 export let multipleVersionsLanguages = {
-  es: ['es-419', 'es-mx', 'es-gt', 'es-cr', 'es-pa', 'es-do', 'es-ve', 'es-co', 'es-pe',
-        'es-ar', 'es-ec', 'es-cl', 'es-uy', 'es-py', 'es-bo', 'es-sv', 'es-hn',
-        'es-ni', 'es-pr'],
-  zh: ['zh-tw'],
+  es: {
+    'es-419': 'es_419',
+    'es-mx': 'es_419',
+    'es-gt': 'es_419',
+    'es-cr': 'es_419',
+    'es-pa': 'es_419',
+    'es-do': 'es_419',
+    'es-ve': 'es_419',
+    'es-co': 'es_419',
+    'es-pe': 'es_419',
+    'es-ar': 'es_419',
+    'es-ec': 'es_419',
+    'es-cl': 'es_419',
+    'es-uy': 'es_419',
+    'es-py': 'es_419',
+    'es-bo': 'es_419',
+    'es-sv': 'es_419',
+    'es-hn': 'es_419',
+    'es-ni': 'es_419',
+    'es-pr': 'es_419',
+  },
+  zh: {
+    'zh-tw': 'zh_TW',
+  }
 };
 
 // Export en strings only, temporary solution for mobile
