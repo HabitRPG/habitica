466797cc6cf616849d582726f6dfdc917f4565fc
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-11-14 15:47:45 +0100

    add some basic tests

---
+++ test/api/v3/unit/libs/buildManifest.test.js
@@ -0,0 +1,18 @@
+import {
+  getManifestFiles,
+} from '../../../../../website/src/libs/api-v3/buildManifest';
+
+describe('Build Manifest', () => {
+  describe('getManifestFiles', () => {
+    it('returns an html string', () => {
+      let htmlCode = getManifestFiles('app');
+
+      expect(htmlCode).to.be.a.String;
+    });
+
+    it('throws an error in case the page does not exist', () => {
+      let getManifestFilesFn = () => { getManifestFiles('strange name here') };
+      expect(getManifestFilesFn).to.throw(Error);
+    });
+  });
+});
