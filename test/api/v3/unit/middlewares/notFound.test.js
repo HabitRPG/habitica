3b633a87b94904cfbe196089fb2ced76f9a8a94f
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-11-18 12:44:52 +0100

    try fixing the notFound test

--- test/api/v3/unit/middlewares/notFound.test.js
+++ test/api/v3/unit/middlewares/notFound.test.js
@@ -4,10 +4,11 @@ describe('notFound Middleware', () => {
   it('returns a 404 error when the resource is not found', () => {
     let request = requester().get('/api/v3/dummy-url');
 
-    return expect(request)
-      .to.eventually.be.rejected.and.eql({
-        error: "NotFound",
-        message: "Not found.",
-      });
-  }); 
+    return request.then((errBody) => {
+      expect(errBody.error).to.equal('NotFound');
+      expect(errBody.message).to.equal('Not found.');
+    }).to.eventually.be.rejected.and.eql({
+      code: 404,
+    });
+  });
 });
