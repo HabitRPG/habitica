8742679e4928ed6d3d5846bbb8656520cac0fb69
Sabe Jones <sabrecat@gmail.com>
2015-11-20 15:27:00 -0500

    test(refPush): Push existing ID

--- test/common/helpers.test.js
+++ test/common/helpers.test.js
@@ -22,6 +22,7 @@ describe('convenience functions', () => {
     let testObject = {};
     const UUID = 'identity';
     const ITEM_WITH_UUID = {id: UUID, data: 'abcdef'};
+    const UUID_2 = 'uniqueness';
 
     it('adds an item to the given object', () => {
       refPush(testObject, ITEM_WITH_UUID);
@@ -55,13 +56,26 @@ describe('convenience functions', () => {
     });
 
     it('increments sort position by 1 for each added item', () => {
-      const UUID_2 = 'uniqueness';
       const ITEM_WITH_UUID_2 = {id: UUID_2};
       const GENERATED_UUID = keys(testObject)[1];
 
       refPush(testObject, ITEM_WITH_UUID_2);
+      expect(size(testObject)).to.eql(3);
       expect(testObject[GENERATED_UUID].sort).to.eql(1);
       expect(testObject[UUID_2].sort).to.eql(2);
+    });
+
+    it('creates a new item and UUID if existing UUID is supplied', () => {
+      const ITEM_WITH_UUID_2 = {id: UUID_2, data: 'information'};
+
+      refPush(testObject, ITEM_WITH_UUID_2);
+      expect(size(testObject)).to.eql(4);
+      expect(testObject[UUID_2].data).to.not.exist;
+
+      const GENERATED_UUID = keys(testObject)[3];
+
+      expect(GENERATED_UUID).to.not.eql(UUID_2);
+      expect(testObject[GENERATED_UUID].data).to.eql(ITEM_WITH_UUID_2.data);
     });
   });
 });
