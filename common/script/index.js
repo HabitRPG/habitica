69553260d322c68975eddcc25161c05ec392c41b
Alys <alice.harris@oldgods.net>
2015-11-24 08:10:56 +1000

    revert refactor of preenHistory

--- common/script/index.js
+++ common/script/index.js
@@ -1,11 +1,10 @@
 import {
   daysSince,
   shouldDo,
-  preenHistory, // temporary pending further refactoring
 } from '../../common/script/cron';
 import * as statHelpers from './statHelpers';
 
-var $w, _, api, content, i18n, moment, sortOrder,
+var $w, _, api, content, i18n, moment, preenHistory, sortOrder,
   indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 moment = require('moment');
@@ -74,6 +73,48 @@ api.planGemLimits = {
   convRate: 20,
   convCap: 25
 };
+
+/*
+Preen history for users with > 7 history entries
+This takes an infinite array of single day entries [day day day day day...], and turns it into a condensed array
+of averages, condensing more the further back in time we go. Eg, 7 entries each for last 7 days; 1 entry each week
+of this month; 1 entry for each month of this year; 1 entry per previous year: [day*7 week*4 month*12 year*infinite]
+ */
+
+preenHistory = function(history) {
+  var newHistory, preen, thisMonth;
+  history = _.filter(history, function(h) {
+    return !!h;
+  });
+  newHistory = [];
+  preen = function(amount, groupBy) {
+    var groups;
+    groups = _.chain(history).groupBy(function(h) {
+      return moment(h.date).format(groupBy);
+    }).sortBy(function(h, k) {
+      return k;
+    }).value();
+    groups = groups.slice(-amount);
+    groups.pop();
+    return _.each(groups, function(group) {
+      newHistory.push({
+        date: moment(group[0].date).toDate(),
+        value: _.reduce(group, (function(m, obj) {
+          return m + obj.value;
+        }), 0) / group.length
+      });
+      return true;
+    });
+  };
+  preen(50, "YYYY");
+  preen(moment().format('MM'), "YYYYMM");
+  thisMonth = moment().format('YYYYMM');
+  newHistory = newHistory.concat(_.filter(history, function(h) {
+    return moment(h.date).format('YYYYMM') === thisMonth;
+  }));
+  return newHistory;
+};
+
 
 /*
   Preen 3-day past-completed To-Dos from Angular & mobile app
