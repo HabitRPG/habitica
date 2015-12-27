e39605dfbdea79d127b27c51b39a8f33ce573a36
Blade Barringer <blade@crookedneighbor.com>
2015-12-27 09:48:06 -0600

    refactor(common): Make countExists helper readable

--- common/script/helpers.js
+++ common/script/helpers.js
@@ -25,8 +25,16 @@ export function refPush (reflist, item) {
   return reflist[item.id];
 }
 
+/*
+  Counts truthy values in an object.
+  If truthy (true, 8, -3, 'string', { foo: 'bar'}, ['array'], {}, []), 1 gets added to the total.
+  If not truthy (false, 0, '', null), 0 gets added.
+ */
+
 export function countExists (items) {
-  return reduce(items, function countEach (m, v) {
-    return m + (v ? 1 : 0);
+  return reduce(items, (total, value) => {
+    let count = value ? 1 : 0;
+
+    return total + count;
   }, 0);
 }
