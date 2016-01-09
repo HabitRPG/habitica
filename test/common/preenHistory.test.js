2fff34cff47756e610a3748ad57c1a3deec36eff
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-01-09 19:47:18 +0100

    finalize tests

--- test/common/preenHistory.test.js
+++ test/common/preenHistory.test.js
@@ -30,29 +30,41 @@ describe('preenHistory', () => {
 
   it('does not modify history if all entries are more recent than cutoff (free users)', () => {
     let h = generateHistory(60);
-    expect(preenHistory(_.cloneDeep(h))).to.eql(h);
+    expect(preenHistory(_.cloneDeep(h), false, 0)).to.eql(h);
   });
 
   it('does not modify history if all entries are more recent than cutoff (subscribers)', () => {
     let h = generateHistory(365);
-    expect(preenHistory(_.cloneDeep(h), true)).to.eql(h);
+    expect(preenHistory(_.cloneDeep(h), true, 0)).to.eql(h);
   });
 
   it('does aggregate data in monthly entries before cutoff (free users)', () => {
     let h = generateHistory(81); // Jumps to July
-    let preened = preenHistory(_.cloneDeep(h));
+    let preened = preenHistory(_.cloneDeep(h), false, 0);
     expect(preened.length).to.eql(62); // Keeps 60 days + 2 entries per august and july
   });
 
   it('does aggregate data in monthly entries before cutoff (subscribers)', () => {
-    let h = generateHistory(386); // Jumps to July
-    let preened = preenHistory(_.cloneDeep(h), true);
-    expect(preened.length).to.eql(367); // Keeps 367 days + 2 entries per august and july
+    let h = generateHistory(396); // Jumps to September 2012
+    let preened = preenHistory(_.cloneDeep(h), true, 0);
+    expect(preened.length).to.eql(367); // Keeps 365 days + 2 entries per october and september
   });
 
-  xit('does aggregate data in monthly and yearly entries before cutoff (free users)', () => {
+  it('does aggregate data in monthly and yearly entries before cutoff (free users)', () => {
     let h = generateHistory(731); // Jumps to October 21 2012
-    let preened = preenHistory(_.cloneDeep(h), true);
-    expect(preened.length).to.eql(368); // Keeps 60 days + 10 montly entries and 1 yearly entry for Oct 21 - Oct 31 2012
+    let preened = preenHistory(_.cloneDeep(h), false, 0);
+    expect(preened.length).to.eql(73); // Keeps 60 days + 11 montly entries and 2 yearly entry for 2011 and 2012
+  });
+
+  it('does aggregate data in monthly and yearly entries before cutoff (subscribers)', () => {
+    let h = generateHistory(1031); // Jumps to October 21 2012
+    let preened = preenHistory(_.cloneDeep(h), true, 0);
+    expect(preened.length).to.eql(380); // Keeps 365 days + 13 montly entries and 2 yearly entries for 2011 and 2010
+  });
+
+  it('correctly aggregates values', () => {
+    let h = generateHistory(63); // Compress last 3 days
+    let preened = preenHistory(_.cloneDeep(h), false, 0);
+    expect(preened[0].value).to.eql((61 + 62 + 63) / 3);
   });
 });
