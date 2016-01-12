36cd9e94306e35339c06be07e433e24a77dabf5d
Blade Barringer <blade@crookedneighbor.com>
2016-01-12 08:14:50 -0600

    tests(api): Increase test coverage for group post tests

--- test/api/v3/integration/groups/POST-groups.test.js
+++ test/api/v3/integration/groups/POST-groups.test.js
@@ -21,6 +21,26 @@ describe('POST /group', () => {
       });
     });
 
+    it('it returns validation error when type is not supported', async () => {
+      await expect(
+        user.post('/groups', { name: 'Group with unsupported type', type: 'foo' })
+      ).to.eventually.be.rejected.and.eql({
+        code: 400,
+        error: 'BadRequest',
+        message: 'Group validation failed',
+      });
+    });
+
+    it('sets the group leader to the user who created the group', async () => {
+      await expect(
+        user.post('/groups', {
+          name: 'Test Public Guild',
+          type: 'guild',
+        })
+      ).to.eventually.have.property('leader', user._id);
+    });
+  });
+
   context('Guilds', () => {
     it('returns an error when a user with insufficient funds attempts to create a guild', async () => {
       await user.update({ balance: 0 });
@@ -35,6 +55,18 @@ describe('POST /group', () => {
         error: 'NotAuthorized',
         message: t('messageInsufficientGems'),
       });
+    });
+
+    it('adds guild to user\'s list of guilds', async () => {
+      let guild = await user.post('/groups', {
+        name: 'some guild',
+        type: 'guild',
+        privacy: 'public',
+      });
+
+      let updatedUser = await user.get('/user');
+
+      expect(updatedUser.guilds).to.include(guild._id);
     });
 
     context('public guild', () => {
@@ -106,6 +138,32 @@ describe('POST /group', () => {
       expect(party.name).to.equal(partyName);
       expect(party.type).to.equal(partyType);
       expect(party.memberCount).to.equal(1);
+    });
+
+    it('does not require gems to create a party', async () => {
+      await user.update({ balance: 0 });
+
+      let party = await user.post('/groups', {
+        name: partyName,
+        type: partyType,
+      });
+
+      expect(party._id).to.exist;
+
+      let updatedUser = await user.get('/user');
+
+      expect(updatedUser.balance).to.eql(user.balance);
+    });
+
+    it('sets party id on user object', async () => {
+      let party = await user.post('/groups', {
+        name: partyName,
+        type: partyType,
+      });
+
+      let updatedUser = await user.get('/user');
+
+      expect(updatedUser.party._id).to.eql(party._id);
     });
 
     it('prevents user in a party from creating another party', async () => {
