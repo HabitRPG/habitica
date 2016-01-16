ca3d2594f2fa8dcac46cb752debd4fd22c33f4cf
Toby Fee <tobyfee@gmail.com>
2016-01-15 17:44:35 -0800

    added code comments

--- website/views/options/inventory/popoverToAriaLabel.js
+++ website/views/options/inventory/popoverToAriaLabel.js
@@ -2,11 +2,12 @@
 $( document ).ready(function() {
   $("button[popover]").each(function(){
     var text = $(this).attr('popover');
-    //not everything has a popover-title. If it does we want that for the aria label
+    //not everything has a popover-title. If it does we want that title for the aria-label
     if ($(this).attr('popover-title') !== undefined){
       var title = $(this).attr('popover-title');
       $(this).attr('aria-label', title);
     } else {
+      //fall back to just using the popover text if no title is present
       $(this).attr('aria-label', text);
     }
   });
