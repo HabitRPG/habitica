b1848da8ded07a5b3dbc8f985e034360739066b1
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-01-29 14:43:14 +0100

    do not output server logs when testing, ability to  run tests and server separately

--- website/src/libs/api-v3/logger.js
+++ website/src/libs/api-v3/logger.js
@@ -4,12 +4,15 @@ import winston from 'winston';
 import nconf from 'nconf';
 
 const IS_PROD = nconf.get('IS_PROD');
+const IS_TEST = nconf.get('IS_TEST');
 
 let logger = new winston.Logger();
 
 if (IS_PROD) {
   // TODO production logging, use loggly
   // log errors to console too
+} else if (IS_TEST) {
+  // Do not log anything when testing
 } else {
   logger
     .add(winston.transports.Console, {
