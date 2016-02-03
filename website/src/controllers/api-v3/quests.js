0684e307909406b41bf11737dc9a0a4471a5b902
Keith Holliday <krh121791@gmail.com>
2016-02-03 17:04:57 -0600

    Added quest cancel route and initial tests

--- website/src/controllers/api-v3/quests.js
+++ website/src/controllers/api-v3/quests.js
@@ -4,6 +4,9 @@ import {
   model as Group,
 } from '../../models/group';
 import {
+  model as User,
+} from '../../models/user';
+import {
   NotFound,
   NotAuthorized,
 } from '../../libs/api-v3/errors';
@@ -64,5 +67,51 @@ api.inviteToQuest = {
     res.respond(200, {});
   },
 };
+
+/**
+ * @api {post} /groups/:groupId/quests/cancel Cancels a quest
+ * @apiVersion 3.0.0
+ * @apiName CancelQuest
+ * @apiGroup Group
+ *
+ * @apiParam {string} groupId The group _id (or 'party')
+ *
+ * @apiSuccess {Object} Group Object
+ */
+api.cancelQuest = {
+  method: 'POST',
+  url: '/groups/:groupId/quests/cancel',
+  middlewares: [authWithHeaders(), cron],
+  async handler (req, res) {
+    // Cancel a quest BEFORE it has begun (i.e., in the invitation stage)
+    // Quest scroll has not yet left quest owner's inventory so no need to return it.
+    // Do not wipe quest progress for members because they'll want it to be applied to the next quest that's started.
+    let user = res.locals.user;
+    let groupId = req.params.groupId;
+
+    req.checkParams('groupId', res.t('groupIdRequired')).notEmpty();
+
+    let validationErrors = req.validationErrors();
+    if (validationErrors) throw validationErrors;
+
+    let group = await Group.getGroup({user, groupId, fields: 'type quest'});
+    if (!group) throw new NotFound(res.t('groupNotFound'));
+
+    if (group.quest.active) throw new NotAuthorized(res.t('cantCancelActiveQuest'));
+
+    group.quest = {key: null, progress: {}, leader: null, members: {}};
+    group.markModified('quest');
+    await group.save();
+
+    await User.update(
+      {'party._id': groupId},
+      {$set: {'party.quest.RSVPNeeded': false, 'party.quest.key': null}},
+      {multi: true}
+    );
+
+    res.respond(200, group);
+  },
+};
+
 
 export default api;
