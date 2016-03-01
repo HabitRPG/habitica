531d3969f44185526e6740929edde9f8754a0cc4
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-03-01 09:15:13 +0100

    fix cluster.fork

--- website/src/server.js
+++ website/src/server.js
@@ -13,7 +13,9 @@ var cores = +nconf.get("WEB_CONCURRENCY") || 0;
 
 if (cores!==0 && cluster.isMaster && (isDev || isProd)) {
   // Fork workers. If config.json has CORES=x, use that - otherwise, use all cpus-1 (production)
-  _.times(cores, cluster.fork);
+  _.times(cores, function () {
+    cluster.fork();
+  });
 
   cluster.on('disconnect', function(worker, code, signal) {
     var w = cluster.fork(); // replace the dead worker
