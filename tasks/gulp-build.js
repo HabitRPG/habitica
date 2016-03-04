9447a6ce1070126d580e302b33dbce80e8cf46f8
Blade Barringer <blade@crookedneighbor.com>
2016-03-03 18:25:01 -0600

    feat: pre-compile babel when used in production

--- tasks/gulp-build.js
+++ tasks/gulp-build.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import babel from 'gulp-babel';
 require('gulp-grunt')(gulp);
 
 gulp.task('build', () => {
@@ -9,6 +10,20 @@ gulp.task('build', () => {
   }
 });
 
+gulp.task('build:src', () => {
+  return gulp.src('website/src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('website/transpiled-babel/'));
+});
+
+gulp.task('build:common', () => {
+  return gulp.src('common/script/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('common/transpiled-babel/'));
+});
+
+gulp.task('build:server', ['build:src', 'build:common']);
+
 gulp.task('build:dev', ['browserify', 'prepare:staticNewStuff'], (done) => {
   gulp.start('grunt-build:dev', done);
 });
@@ -17,6 +32,6 @@ gulp.task('build:dev:watch', ['build:dev'], () => {
   gulp.watch(['website/public/**/*.styl', 'common/script/*']);
 });
 
-gulp.task('build:prod', ['browserify', 'prepare:staticNewStuff'], (done) => {
+gulp.task('build:prod', ['browserify', 'build:server', 'prepare:staticNewStuff'], (done) => {
   gulp.start('grunt-build:prod', done);
 });
