68b7d05683a31d4fa3bc6c422869380f68fceb05
kogelnikp <philipp.kogelnik@hotmail.com>
2016-03-12 17:29:50 +0100

    improved code layout

--- test/e2e/tasks.test.js
+++ test/e2e/tasks.test.js
@@ -2,33 +2,31 @@
 
 // State: public welcome page
 var HabiticaPublic = (function () {
-    
   function generateUserid(length) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for( var i=0; i < length; i++ ) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for ( var i=0; i < length; i++ ) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
   }
-    
+
   return {
-    
     get: function () {
       return browser.get('/');
     },
     selfCheck: function () {
       expect(browser.getLocationAbsUrl()).toBe('');
     },
-    register: function() {
-      var userId = generateUserid(10);    
+    register: function () {
+      var userId = generateUserid(10);
       element(by.id('play-btn')).click();
       element(by.linkText('Register')).click();
       element(by.model('registerVals.username')).sendKeys(userId);
       element(by.model('registerVals.email')).sendKeys(userId + '@example.com');
       element(by.model('registerVals.password')).sendKeys('pass');
       element(by.model('registerVals.confirmPassword')).sendKeys('pass');
-      element(by.css('#registrationForm input[type="submit"]')).click();     
+      element(by.css('#registrationForm input[type="submit"]')).click();
       browser.sleep(3000);
     }
   };
@@ -40,19 +38,19 @@ var WelcomeTour = (function () {
     selfCheck: function () {
       expect(browser.getLocationAbsUrl()).toBe('/tasks');
     },
-    startTour: function() {
-      return element(by.linkText('Enter Habitica')).click();      
+    startTour: function () {
+      return element(by.linkText('Enter Habitica')).click();
     },
-    continueTour: function() {
+    continueTour: function () {
       return element(by.css('button[data-role="next"]')).click();
     },
-    endTour: function() {
+    endTour: function () {
       return element(by.css('button[data-role="end"]')).click();
     },
-    checkFirstTask: function() {
+    checkFirstTask: function () {
       return element(by.css('ul.todos.main-list div.task-controls label')).click();
     },
-    toLvl2: function() {
+    toLvl2: function () {
       return element(by.css('div.modal-content button.btn-primary')).click();
     }
   };
@@ -61,9 +59,8 @@ var WelcomeTour = (function () {
 // State: tasks page
 var Tasks = (function () {
   var todoList = element.all(by.css('ul.todos.main-list li.uncompleted'));
-  
   var sampleTaskName = 'This is my task';
-  
+
   return {
     selfCheck: function () {
       expect(browser.getLocationAbsUrl()).toBe('/tasks');
@@ -71,35 +68,35 @@ var Tasks = (function () {
     get: function () {
       browser.get('/#/tasks');
     },
-    countTodos: function() {
+    countTodos: function () {
       return todoList.count();
-    },    
-    addTodo: function() {
+    },
+    addTodo: function () {
       element(by.css('form[name="newtodoform"] input[type="text"]')).sendKeys(sampleTaskName);
       element(by.css('form[name="newtodoform"] button[type="submit"]')).click();
     },
-    checkTodo: function() {
+    checkTodo: function () {
       element(by.css('ul.todos.main-list li:nth-of-type(1) label')).click();
     },
-    logout: function() {
+    logout: function () {
       element(by.css('a[menu="settings"]')).click()
-      element(by.css('a[ng-click="logout()"]')).click();    
-            
+      element(by.css('a[ng-click="logout()"]')).click();
+
       browser.sleep(1000);
     }
   };
 }());
 
 // Test suites
-describe('habitica app', function() {
+describe('habitica app', function () {
 
-  it('should redirect index.html to /static/front', function(done) {
+  it('should redirect index.html to /static/front', function (done) {
     HabiticaPublic.get();
     HabiticaPublic.selfCheck();
-    
+
     done();
   });
-  
+
   it('should register a new user and complete the tour', function (done) {
     HabiticaPublic.register();
     WelcomeTour.selfCheck();
@@ -113,34 +110,34 @@ describe('habitica app', function() {
     WelcomeTour.checkFirstTask();
     WelcomeTour.toLvl2();
     Tasks.selfCheck();
-    
+
     done();
   });
 
   // test suite for tasks view
-  describe('task view', function() {
+  describe('task view', function () {
 
     it('should show the tasks page', function (done) {
       Tasks.get();
-      Tasks.selfCheck();   
-      
+      Tasks.selfCheck();
+
       done();
     });
-    
+
     it('should create and close todo', function (done) {
-      
-      var count = 0; 
-      Tasks.countTodos().then(function(countnew) {count = countnew;});      
+
+      var count = 0;
+      Tasks.countTodos().then(function(countnew) { count = countnew; });
       Tasks.addTodo();
       browser.sleep(100);
-      Tasks.countTodos().then(function(countnew) {expect(countnew).toEqual(count + 1);});
+      Tasks.countTodos().then(function(countnew) { expect(countnew).toEqual(count + 1); });
       browser.sleep(100);
       Tasks.checkTodo();
-      Tasks.countTodos().then(function(countnew) {expect(countnew).toEqual(count);});
-      
+      Tasks.countTodos().then(function(countnew) { expect(countnew).toEqual(count); });
+
       done();
     });
-    
+
     it('should log out the user', function () {
       Tasks.logout();
       HabiticaPublic.selfCheck();
