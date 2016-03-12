12116fd6767a23422acbbb3b21347c2bb549ce14
kogelnikp <philipp.kogelnik@hotmail.com>
2016-03-12 19:01:18 +0100

    rewrote objects to shorthand form

--- test/e2e/tasks.test.js
+++ test/e2e/tasks.test.js
@@ -2,23 +2,23 @@
 
 // State: public welcome page
 var HabiticaPublic = (function () {
-  function generateUserid(length) {
+  function generateUserid (length) {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for ( var i=0; i < length; i++ ) {
+    for (var i=0; i < length; i ++) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
   }
 
   return {
-    get: function () {
+    get() {
       return browser.get('/');
     },
-    selfCheck: function () {
+    selfCheck() {
       expect(browser.getLocationAbsUrl()).toBe('');
     },
-    register: function () {
+    register() {
       var userId = generateUserid(10);
       element(by.id('play-btn')).click();
       element(by.linkText('Register')).click();
@@ -28,31 +28,31 @@ var HabiticaPublic = (function () {
       element(by.model('registerVals.confirmPassword')).sendKeys('pass');
       element(by.css('#registrationForm input[type="submit"]')).click();
       browser.sleep(3000);
-    }
+    },
   };
 }());
 
 // State: welcome tour after registering
 var WelcomeTour = (function () {
   return {
-    selfCheck: function () {
+    selfCheck() {
       expect(browser.getLocationAbsUrl()).toBe('/tasks');
     },
-    startTour: function () {
+    startTour() {
       return element(by.linkText('Enter Habitica')).click();
     },
-    continueTour: function () {
+    continueTour() {
       return element(by.css('button[data-role="next"]')).click();
     },
-    endTour: function () {
+    endTour() {
       return element(by.css('button[data-role="end"]')).click();
     },
-    checkFirstTask: function () {
+    checkFirstTask() {
       return element(by.css('ul.todos.main-list div.task-controls label')).click();
     },
-    toLvl2: function () {
+    toLvl2() {
       return element(by.css('div.modal-content button.btn-primary')).click();
-    }
+    },
   };
 }());
 
@@ -62,34 +62,33 @@ var Tasks = (function () {
   var sampleTaskName = 'This is my task';
 
   return {
-    selfCheck: function () {
+    selfCheck() {
       expect(browser.getLocationAbsUrl()).toBe('/tasks');
     },
-    get: function () {
+    get() {
       browser.get('/#/tasks');
     },
-    countTodos: function () {
+    countTodos() {
       return todoList.count();
     },
-    addTodo: function () {
+    addTodo() {
       element(by.css('form[name="newtodoform"] input[type="text"]')).sendKeys(sampleTaskName);
       element(by.css('form[name="newtodoform"] button[type="submit"]')).click();
     },
-    checkTodo: function () {
+    checkTodo() {
       element(by.css('ul.todos.main-list li:nth-of-type(1) label')).click();
     },
-    logout: function () {
-      element(by.css('a[menu="settings"]')).click()
+    logout() {
+      element(by.css('a[menu="settings"]')).click();
       element(by.css('a[ng-click="logout()"]')).click();
 
       browser.sleep(1000);
-    }
+    },
   };
 }());
 
 // Test suites
 describe('habitica app', function () {
-
   it('should redirect index.html to /static/front', function (done) {
     HabiticaPublic.get();
     HabiticaPublic.selfCheck();
@@ -116,7 +115,6 @@ describe('habitica app', function () {
 
   // test suite for tasks view
   describe('task view', function () {
-
     it('should show the tasks page', function (done) {
       Tasks.get();
       Tasks.selfCheck();
@@ -125,15 +123,20 @@ describe('habitica app', function () {
     });
 
     it('should create and close todo', function (done) {
-
       var count = 0;
-      Tasks.countTodos().then(function(countnew) { count = countnew; });
+      Tasks.countTodos().then(function (countnew) {
+        count = countnew;
+      });
       Tasks.addTodo();
       browser.sleep(100);
-      Tasks.countTodos().then(function(countnew) { expect(countnew).toEqual(count + 1); });
+      Tasks.countTodos().then(function (countnew) {
+        expect(countnew).toEqual(count + 1);
+      });
       browser.sleep(100);
       Tasks.checkTodo();
-      Tasks.countTodos().then(function(countnew) { expect(countnew).toEqual(count); });
+      Tasks.countTodos().then(function (countnew) {
+        expect(countnew).toEqual(count);
+      });
 
       done();
     });
