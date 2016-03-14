3f6be927f3b0bf86f78c79c71d82529c3602bcb0
kogelnikp <philipp.kogelnik@hotmail.com>
2016-03-14 16:06:25 +0100

    removed generateUserId
    This function is not used any more

--- test/e2e/tasks.test.js
+++ test/e2e/tasks.test.js
@@ -2,15 +2,6 @@
 
 // State: public welcome page
 let HabiticaPublic = (function () {
-  function generateUserid (length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  }
-
   return {
     get () {
       return browser.get('/');
