06f0bad5da0c096f228ec28e3d827fa8a5cdc3cb
Blade Barringer <blade@crookedneighbor.com>
2016-03-14 22:20:22 -0500

    refactor: Move order by equipmentment button to top of view

--- website/public/js/controllers/sortableInventoryCtrl.js
+++ website/public/js/controllers/sortableInventoryCtrl.js
@@ -1,17 +1,19 @@
 habitrpg.controller('SortableInventoryController', ['$scope',
   function ($scope) {
-
-    // doing this in a function causes 10 $digest iteration error
-    attributeSort = {
-      con: ['-con', '-(con+int+per+str)'],
-      int: ['-int', '-(con+int+per+str)'],
-      per: ['-per', '-(con+int+per+str)'],
-      str: ['-str', '-(con+int+per+str)'],
+    var attributeSort = {
+      constitution: ['-con', '-(con+int+per+str)'],
+      intelligence: ['-int', '-(con+int+per+str)'],
+      perception: ['-per', '-(con+int+per+str)'],
+      strength: ['-str', '-(con+int+per+str)'],
       name: 'text()',
       set: 'set'
     }
 
-    $scope.setOrder = function(order) {
+    $scope.order = attributeSort.name;
+    $scope.orderChoice = 'name';
+
+    $scope.setOrder = function (order) {
+      $scope.orderChoice = order;
       if (order in attributeSort) {
         $scope.order = attributeSort[order];
       }
