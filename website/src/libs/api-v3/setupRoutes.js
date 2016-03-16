6921c1694a1e2c84265344f228b122b2d899a5d8
Blade Barringer <blade@crookedneighbor.com>
2016-03-15 21:53:11 -0500

    feat: Allow routes to not need an async handler

--- website/src/libs/api-v3/setupRoutes.js
+++ website/src/libs/api-v3/setupRoutes.js
@@ -9,6 +9,7 @@ let router = express.Router(); // eslint-disable-line babel/new-cap
 // Wrapper function to handler `async` route handlers that return promises
 // It takes the async function, execute it and pass any error to next (args[2])
 let _wrapAsyncFn = fn => (...args) => fn(...args).catch(args[2]);
+let noop = (req, res, next) =>  next();
 
 function walkControllers (filePath) {
   fs
@@ -23,7 +24,9 @@ function walkControllers (filePath) {
           let {method, url, middlewares = [], handler} = action;
 
           method = method.toLowerCase();
-          router[method](url, ...middlewares, _wrapAsyncFn(handler));
+          let fn = handler ? _wrapAsyncFn(handler) : noop;
+
+          router[method](url, ...middlewares, fn);
         });
       }
     });
