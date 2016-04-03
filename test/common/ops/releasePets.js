487a26ec435a8f772c65ac3c79569de3ca58397e
Keith Holliday <krh121791@gmail.com>
2016-04-03 14:37:20 -0500

    Ported release pets, added unit tests, added route with integration tests

---
+++ test/common/ops/releasePets.js
@@ -0,0 +1,57 @@
+import releasePets from '../../../common/script/ops/releasePets';
+import i18n from '../../../common/script/i18n';
+import {
+  generateUser,
+} from '../../helpers/common.helper';
+import {
+  NotAuthorized,
+} from '../../../common/script/libs/errors';
+
+describe('shared.ops.releasePets', () => {
+  let user;
+  let animal = 'Wolf-Base';
+
+  beforeEach(() => {
+    user = generateUser();
+    user.items.currentPet = animal;
+    user.items.pets[animal] = 5;
+    user.balance = 1;
+  });
+
+  it('returns an error when user balance is too low', (done) => {
+    user.balance = 0;
+
+    try {
+      releasePets(user);
+    } catch (err) {
+      expect(err).to.be.an.instanceof(NotAuthorized);
+      expect(err.message).to.equal(i18n.t('notEnoughGems'));
+      done();
+    }
+  });
+
+  it('releases pets', () => {
+    let response = releasePets(user);
+
+    expect(response.message).to.equal(i18n.t('petsReleased'));
+    expect(user.items.pets[animal]).to.equal(0);
+  });
+
+  it('removes currentPet', () => {
+    releasePets(user);
+
+    expect(user.items.currentPet).to.be.empty;
+  });
+
+  it('decreases user\'s balance', () => {
+    releasePets(user);
+
+    expect(user.balance).to.equal(0);
+  });
+
+  it('incremenets beastMasterCount', () => {
+    releasePets(user);
+
+    expect(user.achievements.beastMasterCount).to.equal(1);
+  });
+});
