c916c747752a7db92facb024e47cb1c98ad7ee62
Keith Holliday <krh121791@gmail.com>
2016-04-03 14:43:16 -0500

    Ported release both, added unit tests, add release both route with integration tests

--- common/script/index.js
+++ common/script/index.js
@@ -118,6 +118,7 @@ import purchase from './ops/purchase';
 import purchaseHourglass from './ops/hourglassPurchase';
 import readCard from './ops/readCard';
 import openMysteryItem from './ops/openMysteryItem';
+import releaseBoth from './ops/releaseBoth';
 
 api.ops = {
   scoreTask,
@@ -137,6 +138,7 @@ api.ops = {
   purchaseHourglass,
   readCard,
   openMysteryItem,
+  releaseBoth,
 };
 
 import handleTwoHanded from './fns/handleTwoHanded';
