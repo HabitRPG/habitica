61419711daf757793a44ccb30fcb87dccbe7671c
Nick Bunn <thrain123@gmail.com>
2016-04-04 20:33:52 -0500

    removed chatbox controller. created new copytodo function in inbox's new Ctrl

--- website/public/js/controllers/inboxCtrl.js
+++ website/public/js/controllers/inboxCtrl.js
@@ -1,7 +1,28 @@
 'use strict';
 
-habitrpg.controller("InboxCtrl", ['$scope', 'Groups', 'User',
-    function($scope, Groups, User) {
-      $scope.group = {name:'inbox'};
+habitrpg.controller("InboxCtrl", ['$scope', '$rootScope',
+    function($scope, $rootScope) {
+      $scope.copyToDo = function(message) {
+        var taskNotes = env.t("taskFromInbox",  {
+          from: message.uuid == 'system'
+            ? 'system'
+            : '[' + message.user + ']',
+          message: '[' + message.text + ']'
+        });
+        var taskText = env.t("taskTextFromInbox", {
+          from: message.uuid == 'system'
+            ? 'system'
+            : '[' + message.user + ']'
+        });
+
+        var newScope = $scope.$new();
+        newScope.text = taskText;
+        newScope.notes = taskNotes;
+
+        $rootScope.openModal('copyChatToDo',{
+          controller:'CopyMessageModalCtrl',
+          scope: newScope
+        });
+      };
     }
   ]);
