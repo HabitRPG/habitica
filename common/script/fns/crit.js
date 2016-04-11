2d69285080d8a230fc1f6400f42679e69cd3d412
Victor Piousbox <victor@wasya.co>
2016-04-11 00:28:33 +0000

    shared-code-crit

--- common/script/fns/crit.js
+++ common/script/fns/crit.js
@@ -1,12 +1,6 @@
-module.exports = function(user, stat, chance) {
-  var s;
-  if (stat == null) {
-    stat = 'str';
-  }
-  if (chance == null) {
-    chance = .03;
-  }
-  s = user._statsComputed[stat];
+
+module.exports = function crit (user, stat = 'str', chance = 0.03) {
+  let s = user._statsComputed[stat];
   if (user.fns.predictableRandom() <= chance * (1 + s / 100)) {
     return 1.5 + 4 * s / (s + 200);
   } else {
--- tasks/gulp-eslint.js
+++ tasks/gulp-eslint.js
@@ -11,7 +11,6 @@ const COMMON_FILES = [
   // @TODO remove these negations as the files are converted over.
   '!./common/script/content/index.js',
   '!./common/script/ops/reset.js',
-  '!./common/script/fns/crit.js',
   '!./common/script/fns/randomDrop.js',
   '!./common/script/libs/appliedTags.js',
   '!./common/script/libs/countExists.js',
