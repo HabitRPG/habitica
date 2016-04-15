b00f691e963ce07cbf5d2df8b55a274f92e2b24f
Victor Piousbox <victor@wasya.co>
2016-04-15 17:02:02 +0000

    chat.js controller no longer errors out

--- website/src/controllers/api-v3/chat.js
+++ website/src/controllers/api-v3/chat.js
@@ -15,8 +15,7 @@ import nconf from 'nconf';
 import setupNconf from '../../libs/api-v3/setupNconf';
 setupNconf();
 
-console.log('+++ +++ this:', nconf.get('FLAG_REPORT_EMAIL'));
-const FLAG_REPORT_EMAILS = nconf.get('FLAG_REPORT_EMAIL').split(',').map((email) => {
+const FLAG_REPORT_EMAILS = nconf.get('FLAG_REPORT_EMAIL').map((email) => {
   return { email, canSend: true };
 });
 
