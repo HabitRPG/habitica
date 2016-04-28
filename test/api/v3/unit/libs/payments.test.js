a3a63ab031e035a4f6d97285c561d7827bac121c
Victor Piousbox <victor@wasya.co>
2016-04-28 01:37:34 +0000

    cleanup of sinon spy for fakeSend

--- test/api/v3/unit/libs/payments.test.js
+++ test/api/v3/unit/libs/payments.test.js
@@ -23,10 +23,12 @@ describe('payments/index', () => {
 
   describe('#cancelSubscription', () => {
     beforeEach(() => {
+      fakeSend = sinon.spy(sender, 'sendTxn');
       data = { user: new User() };
     });
 
     afterEach(() => {
+      fakeSend.restore();
     });
 
     it('plan.extraMonths is defined', () => {
@@ -63,15 +65,8 @@ describe('payments/index', () => {
     });
 
     it('sends a text', async () => {
-      fakeSend = sinon.spy(sender, 'sendTxn');
-
       await api.cancelSubscription(data);
-      sinon.assert.calledOnce(fakeSend);
-
-      fakeSend.restore();
+      sinon.assert.called(fakeSend);
     });
-  });
-
-  describe('#buyGems', async () => {
   });
 });
