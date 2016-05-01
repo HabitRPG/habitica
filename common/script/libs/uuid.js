a0e63c0eb28896b4183cde1b55849c8439977ca2
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-05-01 15:01:00 +0200

    use uuid module for uuids

--- common/script/libs/uuid.js
+++ common/script/libs/uuid.js
@@ -1,9 +1,1 @@
-// TODO use node-uuid module
-module.exports = function() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    var r, v;
-    r = Math.random() * 16 | 0;
-    v = (c === "x" ? r : r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-};
+module.exports = require('uuid').v4;
