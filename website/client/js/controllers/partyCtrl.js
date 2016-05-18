d888fc758871973fb9c59b02f33534aa54a1e7a6
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-05-18 11:23:11 +0200

    v3 client: remove console.log statement

--- website/client/js/controllers/partyCtrl.js
+++ website/client/js/controllers/partyCtrl.js
@@ -186,7 +186,6 @@ habitrpg.controller("PartyCtrl", ['$rootScope','$scope','Groups','Chat','User','
       $scope.questForceStart = function(){
         Quests.sendAction('quests/force-start')
           .then(function(quest) {
-            console.log(quest)
             $scope.group.quest = quest;
           });
       };
