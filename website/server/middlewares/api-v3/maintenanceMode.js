ddefb1c583e9b748f541fa15c12b539417e21a1a
Sabe Jones <sabrecat@gmail.com>
2016-05-19 17:59:33 +0000

    WIP(maintenance): display end time

--- website/server/middlewares/api-v3/maintenanceMode.js
+++ website/server/middlewares/api-v3/maintenanceMode.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import nconf from 'nconf';
 
 const MAINTENANCE_MODE = nconf.get('MAINTENANCE_MODE');
@@ -5,9 +6,19 @@ const MAINTENANCE_MODE = nconf.get('MAINTENANCE_MODE');
 module.exports = function maintenanceMode (req, res, next) {
   if (MAINTENANCE_MODE !== 'true') return next();
 
+  const MAINTENANCE_END = new Date('Sat May 21 2016 12:45 UTC');
+
   if (req.headers && req.headers.accept && req.headers.accept.indexOf('text/html') !== -1) {
-    return res.status(503).render('../../../views/static/maintenance');
+    return res.status(503).render('../../../views/static/maintenance', {
+      env: res.locals.habitrpg,
+      t: res.t,
+      maintenanceEndDate: MAINTENANCE_END.toLocaleDateString(),
+      maintenanceEndTime: MAINTENANCE_END.toLocaleTimeString(),
+    });
   } else {
-    return res.status(503).send('Server offline for maintenance.');
+    return res.status(503).send({
+      error: 'Maintenance', 
+      message: 'Server offline for maintenance.',
+    });
   }
 };
