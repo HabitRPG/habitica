1165a39c490b90cf7017835f52ea3b2701253a2c
Sabe Jones <sabrecat@gmail.com>
2016-05-19 20:08:00 +0000

    WIP(maintenance): start adding info page

--- website/server/middlewares/api-v3/maintenanceMode.js
+++ website/server/middlewares/api-v3/maintenanceMode.js
@@ -9,14 +9,24 @@ module.exports = function maintenanceMode (req, res, next) {
   getUserLanguage(req, res, function (err) {
     if (err) return next(err);
 
+    const MAINTENANCE_START = nconf.get('MAINTENANCE_START');
     const MAINTENANCE_END = nconf.get('MAINTENANCE_END');
 
     if (req.headers && req.headers.accept && req.headers.accept.indexOf('text/html') !== -1) {
-      return res.status(503).render('../../../views/static/maintenance', {
-        env: res.locals.habitrpg,
-        t: res.t,
-        maintenanceEnd: new Date(MAINTENANCE_END),
-      });
+      if (req.path === '/views/static/maintenance-info') {
+        return res.status(503).render('../../../views/static/maintenance-info', {
+          env: res.locals.habitrpg,
+          t: res.t,
+          maintenanceStart: new Date(MAINTENANCE_START),
+          maintenanceEnd: new Date(MAINTENANCE_END),
+        });
+      } else {
+        return res.status(503).render('../../../views/static/maintenance', {
+          env: res.locals.habitrpg,
+          t: res.t,
+          maintenanceEnd: new Date(MAINTENANCE_END),
+        });
+      }
     } else {
       return res.status(503).send({
         error: 'Maintenance', 
