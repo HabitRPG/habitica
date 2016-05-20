d221bcc84c69ae01ab6579ce412e7e4ecaf67a9a
Sabe Jones <sabrecat@gmail.com>
2016-05-19 22:28:13 -0500

    Maintenance announcement (#7372)
    * chore(news): announce downtime
    
    * fix(news): tweaks and corrections
    
    * fix: Update maintenance page to use dynamic date

--- website/src/routes/pages.js
+++ website/src/routes/pages.js
@@ -23,7 +23,7 @@ router.get('/', i18n.getUserLanguage, locals, function(req, res) {
 
 // -------- Static Pages --------
 
-var pages = ['front', 'privacy', 'terms', 'api', 'features', 'videos', 'contact', 'plans', 'new-stuff', 'community-guidelines', 'old-news', 'press-kit', 'faq', 'overview', 'apps', 'clear-browser-data', 'merch'];
+var pages = ['front', 'privacy', 'terms', 'api', 'features', 'videos', 'contact', 'plans', 'new-stuff', 'community-guidelines', 'old-news', 'press-kit', 'faq', 'overview', 'apps', 'clear-browser-data', 'merch', 'maintenance-info'];
 
 _.each(pages, function(name){
   router.get('/static/' + name, i18n.getUserLanguage, locals, function(req, res) {
