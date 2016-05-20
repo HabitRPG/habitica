7ea9476bd585b52f6c7808653e83e4bbe248b81d
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-05-20 14:48:38 +0200

    maintenance mode: do not connect to mongodb

--- website/server/libs/api-v3/setupMongoose.js
+++ website/server/libs/api-v3/setupMongoose.js
@@ -5,19 +5,24 @@ import mongoose from 'mongoose';
 import Bluebird from 'bluebird';
 
 const IS_PROD = nconf.get('IS_PROD');
+const MAINTENANCE_MODE = nconf.get('MAINTENANCE_MODE');
 
 // Use Q promises instead of mpromise in mongoose
 mongoose.Promise = Bluebird;
 
-let mongooseOptions = !IS_PROD ? {} : {
-  replset: { socketOptions: { keepAlive: 120, connectTimeoutMS: 30000 } },
-  server: { socketOptions: { keepAlive: 120, connectTimeoutMS: 30000 } },
-};
+// Do not connect to MongoDB when in maintenance mode
+if (MAINTENANCE_MODE !== 'true') {
+  let mongooseOptions = !IS_PROD ? {} : {
+    replset: { socketOptions: { keepAlive: 120, connectTimeoutMS: 30000 } },
+    server: { socketOptions: { keepAlive: 120, connectTimeoutMS: 30000 } },
+  };
 
-const NODE_DB_URI = nconf.get('IS_TEST') ? nconf.get('TEST_DB_URI') : nconf.get('NODE_DB_URI');
-let db = mongoose.connect(NODE_DB_URI, mongooseOptions, (err) => {
-  if (err) throw err;
-  logger.info('Connected with Mongoose.');
-});
+  const NODE_DB_URI = nconf.get('IS_TEST') ? nconf.get('TEST_DB_URI') : nconf.get('NODE_DB_URI');
 
-autoinc.init(db);
+  let db = mongoose.connect(NODE_DB_URI, mongooseOptions, (err) => {
+    if (err) throw err;
+    logger.info('Connected with Mongoose.');
+  });
+
+  autoinc.init(db);
+}
