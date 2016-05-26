dff44f36103806fd6a962e725271cbc7a8609b45
Sabe Jones <sabrecat@gmail.com>
2016-05-26 21:49:43 +0000

    WIP(drops): get drops out of _tmp

--- website/client/js/services/userServices.js
+++ website/client/js/services/userServices.js
@@ -14,8 +14,8 @@ angular.module('habitrpg')
 /**
  * Services that persists and retrieves user from localStorage.
  */
-  .factory('User', ['$rootScope', '$http', '$location', '$window', 'STORAGE_USER_ID', 'STORAGE_SETTINGS_ID', 'Notification', 'ApiUrl', 'Tasks', 'Tags',
-    function($rootScope, $http, $location, $window, STORAGE_USER_ID, STORAGE_SETTINGS_ID, Notification, ApiUrl, Tasks, Tags) {
+  .factory('User', ['$rootScope', '$http', '$location', '$window', 'STORAGE_USER_ID', 'STORAGE_SETTINGS_ID', 'Notification', 'ApiUrl', 'Tasks', 'Tags', 'Content',
+    function($rootScope, $http, $location, $window, STORAGE_USER_ID, STORAGE_SETTINGS_ID, Notification, ApiUrl, Tasks, Tags, Content) {
       var authenticated = false;
       var defaultSettings = {
         auth: { apiId: '', apiToken: ''},
@@ -231,7 +231,52 @@ angular.module('habitrpg')
               var critBonus = crit * 100 - 100;
               Notification.crit(critBonus);
             }
-            if (drop) user._tmp.drop = drop;
+            if (drop) {
+              var text, notes, type;
+              $rootScope.playSound('Item_Drop');
+
+              // Note: For Mystery Item gear, drop.type will be 'head', 'armor', etc
+              // so we use drop.notificationType below.
+
+              if (drop.type !== 'gear' && drop.type !== 'Quest' && drop.notificationType !== 'Mystery') {
+                if (drop.type === 'Food') {
+                  type = 'food';
+                } else if (drop.type === 'HatchingPotion') {
+                  type = 'hatchingPotions';
+                } else {
+                  type = drop.type.toLowerCase() + 's';
+                }
+                if(!user.items[type][drop.key]){
+                  user.items[type][drop.key] = 0;
+                }
+                user.items[type][drop.key]++;
+              }
+
+              if (drop.type === 'HatchingPotion'){
+                text = Content.hatchingPotions[drop.key].text();
+                notes = Content.hatchingPotions[drop.key].notes();
+                Notification.drop(env.t('messageDropPotion', {dropText: text, dropNotes: notes}), drop);
+              } else if (drop.type === 'Egg'){
+                text = Content.eggs[drop.key].text();
+                notes = Content.eggs[drop.key].notes();
+                Notification.drop(env.t('messageDropEgg', {dropText: text, dropNotes: notes}), drop);
+              } else if (drop.type === 'Food'){
+                text = Content.food[drop.key].text();
+                notes = Content.food[drop.key].notes();
+                Notification.drop(env.t('messageDropFood', {dropArticle: drop.article, dropText: text, dropNotes: notes}), drop);
+              } else if (drop.type === 'Quest') {
+                $rootScope.selectedQuest = Content.quests[drop.key];
+                $rootScope.openModal('questDrop', {controller:'PartyCtrl', size:'sm'});
+              } else if (drop.notificationType === 'Mystery') {
+                text = Content.gear.flat[drop.key].text();
+                Notification.drop(env.t('messageDropMysteryItem', {dropText: text}), drop);
+              } else {
+                // Keep support for another type of drops that might be added
+                Notification.drop(drop.dialog);
+              }
+
+              // Analytics.track({'hitType':'event','eventCategory':'behavior','eventAction':'acquire item','itemName':after.key,'acquireMethod':'Drop'});
+            }
           });
         },
 
