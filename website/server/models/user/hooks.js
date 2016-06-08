a69481e81248c09cefd2c4b632cf6503f0f30c3a
MathWhiz <raymond.arndorfer@gmail.com>
2016-06-08 11:46:42 -0400

    Re-fix default tasks?

--- website/server/models/user/hooks.js
+++ website/server/models/user/hooks.js
@@ -53,8 +53,8 @@ function _populateDefaultTasks (user, taskTypes) {
       if (newTask.notes) newTask.notes = taskDefaults.notes(user.preferences.language);
       if (taskDefaults.checklist) {
         newTask.checklist = _.map(taskDefaults.checklist, (checklistItem) => {
-          checklistItem.text = checklistItem.text(user.preferences.language);
-          return checklistItem;
+          let text = checklistItem.text(user.preferences.language);
+          return {text: text, enabled: checklistItem.enabled};
         });
       }
 
