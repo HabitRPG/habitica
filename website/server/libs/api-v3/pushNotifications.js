edd99617ad49eb790c58d46a912995c2f59a333e
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-06-17 18:12:53 +0200

    feat(push-notifications): improve logging

--- website/server/libs/api-v3/pushNotifications.js
+++ website/server/libs/api-v3/pushNotifications.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import nconf from 'nconf';
 import pushNotify from 'push-notify';
 import apnLib from 'apn';
+import logger from './logger';
 
 const GCM_API_KEY = nconf.get('PUSH_CONFIGS:GCM_SERVER_API_KEY');
 
@@ -10,25 +11,37 @@ let gcm = GCM_API_KEY ? pushNotify.gcm({
   retries: 3,
 }) : undefined;
 
+if (gcm) {
+  gcm.on('transmissionError', (err, message, registrationId) => {
+    logger.error('GCM Error', err, message, registrationId);
+  });
+}
+
 const APN_CERT = nconf.get('PUSH_CONFIGS:APN_PEM_FILES:CERT');
+const APN_KEY = nconf.get('PUSH_CONFIGS:APN_PEM_FILES:KEY_PART_1') + nconf.get('PUSH_CONFIGS:APN_PEM_FILES:KEY_PARTY_2');
 
 // Split key into two parts, because we have to stay below 4096 bytes for env
 // Variables.
 let apn = APN_CERT ? pushNotify.apn({
-  key: nconf.get('PUSH_CONFIGS:APN_PEM_FILES:KEY_PART_1') + nconf.get('PUSH_CONFIGS:APN_PEM_FILES:KEY_PARTY_2'),
+  key: APN_KEY,
   cert: APN_CERT,
 }) : undefined;
 
 if (apn) {
+  apn.on('error', err => logger.error('APN error', err));
+  apn.on('transmissionError', (errorCode, notification, device) => {
+    logger.error('APN transmissionError', errorCode, notification, device);
+  });
+
   let feedback = new apnLib.Feedback({
-    key: nconf.get('PUSH_CONFIGS:APN_PEM_FILES:KEY_PART_1') + nconf.get('PUSH_CONFIGS:APN_PEM_FILES:KEY_PARTY_2'),
+    key: APN_KEY,
     cert: APN_CERT,
     batchFeedback: true,
     interval: 3600, // Check for feedback once an hour
   });
-  feedback.on('feedback', () => {
-    // console.log("GOT FEEDBACK");
-    // console.log(devices);
+
+  feedback.on('feedback', (devices) => {
+    logger.info('Delivery of push notifications failed for some Apple devices.', devices);
   });
 }
 
@@ -43,6 +56,7 @@ module.exports = function sendNotification (user, details = {}) {
 
   let payload = details.payload ? details.payload : {};
   payload.identifier = details.identifier;
+
   _.each(pushDevices, pushDevice => {
     switch (pushDevice.type) {
       case 'android':
