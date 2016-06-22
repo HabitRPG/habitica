e53d1bfc9c6d52e4ed94970fee28b3c20501b18f
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-06-22 19:31:14 +0200

    download creds from S3 and create AWS lib

--- website/server/controllers/top-level/dataexport.js
+++ website/server/controllers/top-level/dataexport.js
@@ -9,16 +9,14 @@ import csvStringify from '../../libs/api-v3/csvStringify';
 import moment from 'moment';
 import js2xml from 'js2xmlparser';
 import Pageres from 'pageres';
-import AWS from 'aws-sdk';
 import nconf from 'nconf';
 import got from 'got';
 import Bluebird from 'bluebird';
 import locals from '../../middlewares/api-v3/locals';
+import {
+  S3,
+} from '../../libs/api-v3/aws';
 
-let S3 = new AWS.S3({
-  accessKeyId: nconf.get('S3:accessKeyId'),
-  secretAccessKey: nconf.get('S3:secretAccessKey'),
-});
 const S3_BUCKET = nconf.get('S3:bucket');
 
 const BASE_URL = nconf.get('BASE_URL');
--- website/server/libs/api-v3/pushNotifications.js
+++ website/server/libs/api-v3/pushNotifications.js
@@ -3,6 +3,10 @@ import nconf from 'nconf';
 import pushNotify from 'push-notify';
 import apnLib from 'apn';
 import logger from './logger';
+import Bluebird from 'bluebird';
+import {
+  S3,
+} from './aws';
 
 const GCM_API_KEY = nconf.get('PUSH_CONFIGS:GCM_SERVER_API_KEY');
 
@@ -17,33 +21,47 @@ if (gcm) {
   });
 }
 
-const APN_ENABLED = nconf.get('PUSH_CONFIGS:APN:ENABLED') === 'true';
-const APN_FILES_PATH = nconf.get('PUSH_CONFIGS:APN:FILES_PATH');
-const APN_KEY_PATH = `${APN_FILES_PATH}/key.pem`;
-const APN_CERT_PATH = `${APN_FILES_PATH}/cert.pem`;
+let apn;
 
-let apn = APN_ENABLED ? pushNotify.apn({
-  key: APN_KEY_PATH,
-  cert: APN_CERT_PATH,
-}) : undefined;
+// Load APN certificate and key from S3
+const APN_ENABLED = nconf.get('PUSH_CONFIGS:APN_ENABLED') === 'true';
+const S3_BUCKET = nconf.get('S3:bucket');
 
-if (apn) {
-  apn.on('error', err => logger.error('APN error', err));
-  apn.on('transmissionError', (errorCode, notification, device) => {
-    logger.error('APN transmissionError', errorCode, notification, device);
-  });
+if (APN_ENABLED) {
+  Bluebird.all([
+    S3.getObject({
+      Bucket: S3_BUCKET,
+      Key: 'apple_apn/cert.pem',
+    }).promise(),
+    S3.getObject({
+      Bucket: S3_BUCKET,
+      Key: 'apple_apn/cert.pem',
+    }).promise(),
+  ])
+  .then(([cert, key]) => {
+    console.log(cert, key);
+    apn = pushNotify.apn({
+      key: cert,
+      cert: key,
+    });
 
-  let feedback = new apnLib.Feedback({
-    key: APN_KEY_PATH,
-    cert: APN_CERT_PATH,
-    batchFeedback: true,
-    interval: 3600, // Check for feedback once an hour
-  });
+    apn.on('error', err => logger.error('APN error', err));
+    apn.on('transmissionError', (errorCode, notification, device) => {
+      logger.error('APN transmissionError', errorCode, notification, device);
+    });
 
-  feedback.on('feedback', (devices) => {
-    if (devices && devices.length > 0) {
-      logger.info('Delivery of push notifications failed for some Apple devices.', devices);
-    }
+    let feedback = new apnLib.Feedback({
+      key: cert,
+      cert: key,
+      batchFeedback: true,
+      interval: 3600, // Check for feedback once an hour
+    });
+
+    feedback.on('feedback', (devices) => {
+      if (devices && devices.length > 0) {
+        logger.info('Delivery of push notifications failed for some Apple devices.', devices);
+      }
+    });
   });
 }
 
