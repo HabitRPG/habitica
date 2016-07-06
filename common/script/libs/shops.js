d86175e034dba1d0828c292ee5f317fdcc89efd7
Phillip Thelen <viirus@pherth.net>
2016-07-06 20:01:17 +0200

    changes to shop api calls

--- common/script/libs/shops.js
+++ common/script/libs/shops.js
@@ -4,55 +4,126 @@ import t from '../content/translation';
 
 let shops = {};
 
-shops.getMarketCategories = _.memoize(function getMarket (user) {
+shops.getMarketCategories = function getMarket (language, user) {
   let categories = [];
   let eggsCategory = {
     identifier: 'eggs',
-    text: t('eggs'),
-    notes: t('dropsExplanation'),
+    text: t('eggs')(language),
+    notes: t('dropsExplanation')(language),
   };
-  let eggs = _.values(_.cloneDeep(content.questEggs)).filter(egg => egg.canBuy(user));
-  eggs = eggs.concat(_.values(_.cloneDeep(content.dropEggs)));
+  let eggs = _.map(_.values(content.questEggs).filter(egg => egg.canBuy(user)), egg => {
+    return {
+      key: egg.key,
+      text: t('egg', {eggType: egg.text()})(language),
+      notes: egg.notes(language),
+      value: egg.value,
+    }
+  });
+  eggs = eggs.concat(_.map(_.values(content.dropEggs), egg => {
+    return {
+      key: egg.key,
+      text: t('egg', {eggType: egg.text()})(language),
+      notes: egg.notes(language),
+      value: egg.value,
+    }
+  }));
   _.each(eggs, egg => {
     egg.class = 'Pet_Egg_'+egg.key;
-    egg.text = t('egg', {eggType: egg.text()});
+    egg.locked = false;
+    egg.currency = 'gems';
   });
   eggsCategory.items = _.sortBy(eggs, 'key');
   categories.push(eggsCategory);
 
   let hatchingPotionsCategory = {
     identifier: 'hatchingPotions',
-    text: t('hatchingPotions'),
-    notes: t('dropsExplanation'),
+    text: t('hatchingPotions')(language),
+    notes: t('dropsExplanation')(language),
   };
-  let hatchingPotions = _.values(_.cloneDeep(content.hatchingPotions)).filter(hp => !hp.limited);
-  _.each(hatchingPotions, hatchingPotion => {
-    hatchingPotion.class = 'Pet_HatchingPotion_'+hatchingPotion.key;
+  let hatchingPotions = _.map(_.values(content.hatchingPotions).filter(hp => !hp.limited), hatchingPotion => {
+    return {
+      key: hatchingPotion.key,
+      text: hatchingPotion.text(language),
+      notes: hatchingPotion.notes(language),
+      class: 'Pet_HatchingPotion_' + hatchingPotion.key,
+      value: hatchingPotion.value,
+      locked: false,
+      currency: 'gems',
+    }
   });
   hatchingPotionsCategory.items = _.sortBy(hatchingPotions, 'key');
   categories.push(hatchingPotionsCategory);
 
   let foodCategory = {
     identifier: 'food',
-    text: t('food'),
-    notes: t('dropsExplanation'),
+    text: t('food')(language),
+    notes: t('dropsExplanation')(language),
   };
-  let food = _.values(_.cloneDeep(content.food)).filter(food => food.canDrop || food.key === 'Saddle');
-  _.each(food, foodItem => {
-    foodItem.class = 'Pet_Food_'+foodItem.key;
+  let food = _.map(_.values(content.food).filter(food => food.canDrop || food.key === 'Saddle'), foodItem => {
+    return {
+      key: foodItem.key,
+      text: foodItem.text(language),
+      notes: foodItem.notes(language),
+      class: 'Pet_Food_'+foodItem.key,
+      value: foodItem.value,
+      locked: false,
+      currency: 'gems',
+  }
   });
   foodCategory.items = _.sortBy(food, 'key');
   categories.push(foodCategory);
 
-  _.each(categories, category => {
-    _.each(category.items, item => {
-      item.locked = false;
-      item.currency = 'gems';
+  return categories;
+};
+
+shops.getMemoizedMarketCategories = _.memoize(shops.getMarketCategories);
+
+shops.getTimeTravelerCategories = function getTimeTravelerCategories (language, user) {
+
+};
+
+
+shops.getSeasonalShopCategories = function getSeasonalShopCategories (language) {
+  let availableSets = {
+    summerWarrior:t("daringSwashbucklerSet")(language),
+    summerMage:t("emeraldMermageSet")(language),
+    summerHealer:t("reefSeahealerSet")(language),
+    summerRogue:t("roguishPirateSet")(language),
+    summer2015Warrior:t("sunfishWarriorSet")(language),
+    summer2015Mage:t("shipSoothsayerSet")(language),
+    summer2015Healer:t("strappingSailorSet")(language),
+    summer2015Rogue:t("reefRenegadeSet")(language)
+  };
+
+  let categories = [];
+
+  for (let key in availableSets) {
+    let category = {
+      identifier: key,
+      text: availableSets[key]
+    };
+
+    var flatGearArray = _.toArray(content.gear.flat);
+
+    category.items = _.map(_.where(flatGearArray, {index: key}), gear => {
+      return {
+        key: gear.key,
+        text: gear.text(language),
+        notes: gear.notes(language),
+        value: gear.value,
+        type: gear.type,
+        specialClass: gear.specialClass,
+        locked: false,
+        currency: 'gems'
+      };
     });
-  });
+    categories.push(category);
+  }
 
   return categories;
-});
+};
+
+shops.getMemoizedSeasonalShopCategories = _.memoize(shops.getSeasonalShopCategories);
 
 
 module.exports = shops;
--- website/server/controllers/api-v3/shops.js
+++ website/server/controllers/api-v3/shops.js
@@ -1,6 +1,5 @@
 import { authWithHeaders } from '../../middlewares/api-v3/auth';
 import shops from '../../../../common/script/libs/shops';
-import _ from 'lodash';
 
 let api = {};
 
@@ -20,28 +19,39 @@ api.getMarketItems = {
   middlewares: [authWithHeaders()],
   async handler (req, res) {
     let user = res.locals.user;
-    console.log(shops);
 
     let resObject = {
       identifier: 'market',
       text: res.t('market'),
-      categories: shops.getMarketCategories(user)
+      categories: shops.getMarketCategories(req.language, user)
     };
 
-    let propKeys = ['key', 'text', 'notes', 'value', 'currency', 'locked'];
-    _.each(categories, category => {
-      category.text = category.text(req.language);
-      category.notes = category.notes(req.language);
-      _.each(category.items, item => {
-        _.each(item, (itemPropVal, itemPropKey) => {
-          if (propKeys.indexOf(itemPropKey) === -1) {
-            delete item[itemPropKey];
-          } else {
-            if (_.isFunction(itemPropVal) && itemPropVal.i18nLangFunc) item[itemPropKey] = itemPropVal(req.language);
-          }
-        });
-      });
-    });
+    res.respond(200, resObject);
+  },
+};
+
+/**
+ * @apiIgnore
+ * @api {get} /api/v3/shops/seasonal-shop get the available items for the seasonal shop
+ * @apiVersion 3.0.0
+ * @apiName GetSeasonalShopItems
+ * @apiGroup Shops
+ *
+ * @apiSuccess {Object} data List of push devices
+ * @apiSuccess {string} message Success message
+ */
+api.getSeasonalShopItems = {
+  method: 'GET',
+  url: '/shops/seasonal-shop',
+  middlewares: [authWithHeaders()],
+  async handler (req, res) {
+    let user = res.locals.user;
+
+    let resObject = {
+      identifier: 'seasonalShop',
+      text: res.t('seasonalShop'),
+      categories: shops.getSeasonalShopCategories(req.language, user)
+    };
 
     res.respond(200, resObject);
   },
