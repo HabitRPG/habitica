24d25026cfbd537b46c80a07167d02e807a40ab3
Phillip Thelen <viirus@pherth.net>
2016-07-26 21:36:55 +0200

    Add API calls for shop inventories (#7810)
    * Add API call for market inventory
    
    * changes to shop api calls
    
    * optimize shop categories
    
    * add API call for quests
    
    * add api call for time travelers shop
    
    * fic buying items in shops
    
    * fix linting errors
    
    * shop adjustments for iOS app
    
    * add tests to shops
    
    * fix syntax issues
    
    * Code formatting
    
    * correct indentation
    
    * add tests for api routes
    
    * fix time travelers and seasonal
    
    * Increase test coverage for shop routes
    
    * refactor: Pull out trinket count in time traveler route
    
    * refactor: Clarify instructions for seasonal shop script
    
    * lint: Remove extra new line
    
    * Adjust shops common test

---
+++ test/common/libs/shops.js
@@ -0,0 +1,84 @@
+import shared from '../../../common';
+import {
+  generateUser,
+} from '../../helpers/common.helper';
+
+describe('shops', () => {
+  let user = generateUser();
+
+  describe('market', () => {
+    let shopCategories = shared.shops.getMarketCategories(user);
+
+    it('contains at least the 3 default categories', () => {
+      expect(shopCategories.length).to.be.greaterThan(2);
+    });
+
+    it('does not contain an empty category', () => {
+      _.each(shopCategories, (category) => {
+        expect(category.items.length).to.be.greaterThan(0);
+      });
+    });
+
+    it('items contain required fields', () => {
+      _.each(shopCategories, (category) => {
+        _.each(category.items, (item) => {
+          expect(item).to.have.all.keys(['key', 'text', 'notes', 'value', 'currency', 'locked', 'purchaseType', 'class']);
+        });
+      });
+    });
+  });
+
+  describe('questShop', () => {
+    let shopCategories = shared.shops.getQuestShopCategories(user);
+
+    it('does not contain an empty category', () => {
+      _.each(shopCategories, (category) => {
+        expect(category.items.length).to.be.greaterThan(0);
+      });
+    });
+
+    it('items contain required fields', () => {
+      _.each(shopCategories, (category) => {
+        _.each(category.items, (item) => {
+          expect(item).to.have.all.keys('key', 'text', 'notes', 'value', 'currency', 'locked', 'purchaseType', 'boss', 'class', 'collect', 'drop', 'unlockCondition', 'lvl');
+        });
+      });
+    });
+  });
+
+  describe('timeTravelers', () => {
+    let shopCategories = shared.shops.getTimeTravelersCategories(user);
+
+    it('does not contain an empty category', () => {
+      _.each(shopCategories, (category) => {
+        expect(category.items.length).to.be.greaterThan(0);
+      });
+    });
+
+    it('items contain required fields', () => {
+      _.each(shopCategories, (category) => {
+        _.each(category.items, (item) => {
+          expect(item).to.have.all.keys('key', 'text', 'value', 'currency', 'locked', 'purchaseType', 'class', 'notes', 'class');
+        });
+      });
+    });
+  });
+
+  describe('seasonalShop', () => {
+    let shopCategories = shared.shops.getSeasonalShopCategories(user);
+
+    it('does not contain an empty category', () => {
+      _.each(shopCategories, (category) => {
+        expect(category.items.length).to.be.greaterThan(0);
+      });
+    });
+
+    it('items contain required fields', () => {
+      _.each(shopCategories, (category) => {
+        _.each(category.items, (item) => {
+          expect(item).to.have.all.keys('key', 'text', 'notes', 'value', 'currency', 'locked', 'purchaseType', 'specialClass', 'type');
+        });
+      });
+    });
+  });
+});
