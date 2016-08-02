4244c7519eac09bf1984493a87f0c65b0cc9f06c
Oziris <ironmaiden.o@gmail.com>
2016-08-01 21:03:01 -0300

    Add Dewin's new Theme (#7795)
    * Add files via upload
    
    * Add string for new theme
    
    * Added 'dewinTheme'
    
    * Added 'dewinTheme'
    
    * Delete Boss Battles Background Music.mp3
    
    * Delete Boss Battles Background Music.ogg
    
    * Delete Settings Page Background Music.mp3
    
    * Delete Settings Page Background Music.ogg
    
    * Add audio files with correct names
    
    * Delete Boss Battles_Background_Music.mp3
    
    * Delete Boss Battles_Background_Music.ogg
    
    * Add files via upload

--- website/server/models/user/schema.js
+++ website/server/models/user/schema.js
@@ -391,7 +391,7 @@ let schema = new Schema({
     skin: {type: String, default: '915533'},
     shirt: {type: String, default: 'blue'},
     timezoneOffset: {type: Number, default: 0},
-    sound: {type: String, default: 'off', enum: ['off', 'danielTheBard', 'gokulTheme', 'luneFoxTheme', 'wattsTheme', 'rosstavoTheme']},
+    sound: {type: String, default: 'off', enum: ['off', 'danielTheBard', 'gokulTheme', 'luneFoxTheme', 'wattsTheme', 'rosstavoTheme', 'dewinTheme']},
     chair: {type: String, default: 'none'},
     timezoneOffsetAtLastCron: Number,
     language: String,
