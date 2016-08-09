ed8e8f3a6729cdd9a481cfe24f450132d3c26908
Alyssa Batula <batulaa@drexel.edu>
2016-08-08 21:54:06 -0400

    Updated collection quest tests for no found items.
    Single-item quests state [user] found no [items]. instead of [user] found nothing. If a
    quest has two items, the chat message is [user] found no [item1] and no [item2].

--- test/api/v3/unit/models/group.test.js
+++ test/api/v3/unit/models/group.test.js
@@ -350,7 +350,23 @@ describe('Group Model', () => {
           party = await Group.findOne({_id: party._id});
 
           expect(Group.prototype.sendChat).to.be.calledOnce;
-          expect(Group.prototype.sendChat).to.be.calledWith('`Participating Member found nothing.`');
+          expect(Group.prototype.sendChat).to.be.calledWith('`Participating Member found no Bars of Soap.`');
+        });
+
+        it('sends a chat message if no progress is made on quest with multiple items', async () => {
+          progress.collectedItems = 0;
+          party.quest.key = 'dilatoryDistress1';
+          party.quest.active = false;
+
+          await party.startQuest(questLeader);
+          await party.save();
+
+          await Group.processQuestProgress(participatingMember, progress);
+
+          party = await Group.findOne({_id: party._id});
+
+          expect(Group.prototype.sendChat).to.be.calledOnce;
+          expect(Group.prototype.sendChat).to.be.calledWith('`Participating Member found no Blue Fins and no Fire Coral.`');
         });
 
         it('handles collection quests with multiple items', async () => {
