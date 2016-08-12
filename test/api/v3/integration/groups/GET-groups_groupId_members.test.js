730f5466ac1164d9f51bc3baba778f258eda2b8e
Jaka Kranjc <lynxlynxlynx@sourcemage.org>
2016-08-12 22:28:51 +0200

    api: expose user's inbox.optOut for `api/v3/members/:memberId` #7798

--- test/api/v3/integration/groups/GET-groups_groupId_members.test.js
+++ test/api/v3/integration/groups/GET-groups_groupId_members.test.js
@@ -79,7 +79,7 @@ describe('GET /groups/:groupId/members', () => {
 
     expect(memberRes).to.have.all.keys([ // works as: object has all and only these keys
       '_id', 'id', 'preferences', 'profile', 'stats', 'achievements', 'party',
-      'backer', 'contributor', 'auth', 'items',
+      'backer', 'contributor', 'auth', 'items', 'inbox',
     ]);
     expect(Object.keys(memberRes.auth)).to.eql(['timestamps']);
     expect(Object.keys(memberRes.preferences).sort()).to.eql(['size', 'hair', 'skin', 'shirt',
@@ -88,6 +88,8 @@ describe('GET /groups/:groupId/members', () => {
     expect(memberRes.stats.maxMP).to.exists;
     expect(memberRes.stats.maxHealth).to.equal(common.maxHealth);
     expect(memberRes.stats.toNextLevel).to.equal(common.tnl(memberRes.stats.lvl));
+    expect(memberRes.inbox.optOut).to.exist;
+    expect(memberRes.inbox.messages).to.not.exist;
   });
 
   it('returns only first 30 members', async () => {
