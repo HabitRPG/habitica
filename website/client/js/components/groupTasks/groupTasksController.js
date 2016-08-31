91d7119bd29b2acf337a0bdb0666f1fd0e212452
Keith Holliday <krh121791@gmail.com>
2016-08-31 16:59:09 -0500

    Added checklist support to ui

--- website/client/js/components/groupTasks/groupTasksController.js
+++ website/client/js/components/groupTasks/groupTasksController.js
@@ -69,4 +69,67 @@ habitrpg.controller('GroupTasksCtrl', ['$scope', 'Shared', 'Tasks', function ($s
         task.tags.splice(tagIndex, 1);
       }
     };
+
+    /*
+     ------------------------
+     Checklists
+     ------------------------
+     */
+    function focusChecklist(task, index) {
+      window.setTimeout(function() {
+        $('#task-'+ task._id +' .checklist-form input[type="text"]')[index].focus();
+      });
+    }
+
+    $scope.addChecklist = function(task) {
+      task.checklist = [{completed:false, text:""}];
+      focusChecklist(task, 0);
+    }
+
+    $scope.addChecklistItem = function(task, $event, $index) {
+      if (task.checklist[$index].text) {
+        $scope.saveTask(task, true);
+        if ($index === task.checklist.length - 1)
+          task.checklist.push({ completed: false, text: '' });
+        focusChecklist(task, $index + 1);
+      } else {
+        // TODO Provide UI feedback that this item is still blank
+      }
+    }
+
+    $scope.removeChecklistItem = function(task, $event, $index, force) {
+      // Remove item if clicked on trash icon
+      if (force) {
+        if (task.checklist[$index].id) Tasks.removeChecklistItem(task._id, task.checklist[$index].id);
+        task.checklist.splice($index, 1);
+      } else if (!task.checklist[$index].text) {
+        // User deleted all the text and is now wishing to delete the item
+        // saveTask will prune the empty item
+        if (task.checklist[$index].id) Tasks.removeChecklistItem(task._id, task.checklist[$index].id);
+        // Move focus if the list is still non-empty
+        if ($index > 0)
+          focusChecklist(task, $index-1);
+        // Don't allow the backspace key to navigate back now that the field is gone
+        $event.preventDefault();
+      }
+    }
+
+    $scope.swapChecklistItems = function(task, oldIndex, newIndex) {
+      var toSwap = task.checklist.splice(oldIndex, 1)[0];
+      task.checklist.splice(newIndex, 0, toSwap);
+      $scope.saveTask(task, true);
+    }
+
+    $scope.navigateChecklist = function(task,$index,$event){
+      focusChecklist(task, $event.keyCode == '40' ? $index+1 : $index-1);
+    }
+
+    $scope.checklistCompletion = function(checklist){
+      return _.reduce(checklist,function(m,i){return m+(i.completed ? 1 : 0);},0)
+    }
+
+    $scope.collapseChecklist = function(task) {
+      task.collapseChecklist = !task.collapseChecklist;
+      $scope.saveTask(task,true);
+    }
   }]);
