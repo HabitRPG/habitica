68dbcde90c0e0dfbbffed0f1e3444e46e58cf9e8
Keith Holliday <krh121791@gmail.com>
2016-08-31 17:33:30 -0500

    Fixed delete tags route

--- website/server/controllers/api-v3/tasks.js
+++ website/server/controllers/api-v3/tasks.js
@@ -633,9 +633,18 @@ api.addTagToTask = {
     if (validationErrors) throw validationErrors;
 
     let taskId = req.params.taskId;
-    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id, { userId: user._id });
+    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id);
 
-    if (!task) throw new NotFound(res.t('taskNotFound'));
+    if (!task) {
+      throw new NotFound(res.t('taskNotFound'));
+    } else if (task.group.id) {
+      let group = await Group.getGroup({user, groupId: task.group.id, fields: requiredGroupFields});
+      if (!group) throw new NotFound(res.t('groupNotFound'));
+      if (group.leader !== user._id) throw new NotAuthorized(res.t('onlyGroupLeaderCanEditTasks'));
+    } else if (task.userId !== user._id) { // If the task is owned by a user make it's the current one
+      throw new NotFound(res.t('taskNotFound'));
+    }
+
     let tagId = req.params.tagId;
 
     let alreadyTagged = task.tags.indexOf(tagId) !== -1;
@@ -673,9 +682,17 @@ api.removeTagFromTask = {
     if (validationErrors) throw validationErrors;
 
     let taskId = req.params.taskId;
-    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id, { userId: user._id });
+    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id);
 
-    if (!task) throw new NotFound(res.t('taskNotFound'));
+    if (!task) {
+      throw new NotFound(res.t('taskNotFound'));
+    } else if (task.group.id) {
+      let group = await Group.getGroup({user, groupId: task.group.id, fields: requiredGroupFields});
+      if (!group) throw new NotFound(res.t('groupNotFound'));
+      if (group.leader !== user._id) throw new NotAuthorized(res.t('onlyGroupLeaderCanEditTasks'));
+    } else if (task.userId !== user._id) { // If the task is owned by a user make it's the current one
+      throw new NotFound(res.t('taskNotFound'));
+    }
 
     let hasTag = removeFromArray(task.tags, req.params.tagId);
     if (!hasTag) throw new NotFound(res.t('tagNotFound'));
