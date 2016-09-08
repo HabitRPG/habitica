4bc25fdad59281c43878531ad0e0e7b6b5943390
Keith Holliday <krh121791@gmail.com>
2016-09-07 19:06:29 -0500

    Added assign user tag input

--- website/client/js/components/groupTaskActions/groupTaskActionsController.js
+++ website/client/js/components/groupTaskActions/groupTaskActionsController.js
@@ -3,7 +3,22 @@ habitrpg.controller('GroupTaskActionsCtrl', ['$scope', 'Shared', 'Tasks',
     //This is a dependency from the route. There is probably a better way to inject this through the directive
     $scope.group = $scope.obj;
 
+<<<<<<< HEAD
     $scope.assignedMembers = [];
 
 
+=======
+    $scope.tags = [
+      { text: 'just' },
+      { text: 'some' },
+      { text: 'cool' },
+      { text: 'tags' }
+    ];
+
+    $scope.loadTags = function(query) {
+      // _.pluck(stooges, 'name');
+      console.log($scope.group.members);
+        return [ { text: 'Tag1' }, { text: 'Tag2' } , { text: 'Tag3' }];// $scope.group.members;
+    };
+>>>>>>> Added assign user tag input
   }]);
