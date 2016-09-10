b2ab48861e77c2d057e7fea7affb6fdcd598ceb2
Thomas Gamble <thomasgamble2@gmail.com>
2016-09-10 10:37:15 -0700

    drops are randomly selected, not based on user values fixes #7929

--- common/script/fns/randomVal.js
+++ common/script/fns/randomVal.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
-import predictableRandom from './predictableRandom';
 
 // Get a random property from an object
 // returns random property (the value)
 
-module.exports = function randomVal (user, obj, options = {}) {
+function randomGenerator (user, seed, providedRandom) {
+  return providedRandom ? providedRandom(user, seed) : Math.random();
+}
+
+module.exports = function randomVal (user, obj, options = {}, randomFunc) {
   let array = options.key ? _.keys(obj) : _.values(obj);
-  let rand = predictableRandom(user, options.seed);
+  let rand = randomGenerator(user, options.seed, randomFunc);
   array.sort();
   return array[Math.floor(rand * array.length)];
 };
