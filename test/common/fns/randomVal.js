e8bd0e0d8fb3a99691330bd3c3be9c59dbd434a3
Thomas Gamble <thomasgamble2@gmail.com>
2016-09-10 10:37:15 -0700

    drops are randomly selected, not based on user values fixes #7929

--- test/common/fns/randomVal.js
+++ test/common/fns/randomVal.js
@@ -1,10 +1,14 @@
 import randomVal from '../../../website/common/script/fns/randomVal';
+
 import {
   generateUser,
 } from '../../helpers/common.helper';
 
 describe('shared.fns.randomVal', () => {
   let user;
+  let predictableRandom = () => {
+    return 0.5;
+  };
   let obj = {
     a: 1,
     b: 2,
@@ -17,103 +21,69 @@ describe('shared.fns.randomVal', () => {
   });
 
   describe('returns a random property value from an object', () => {
-    it('returns the same value when the seed is the same', () => {
+    it('returns the same value when the seed is the same and using a predictable random', () => {
       let val1 = randomVal(user, obj, {
         seed: 222,
+        randomFunc: predictableRandom,
       });
 
       let val2 = randomVal(user, obj, {
         seed: 222,
+        randomFunc: predictableRandom,
       });
 
       expect(val2).to.equal(val1);
     });
 
-    it('returns the same value when user.stats is the same', () => {
+    it('returns the same value when user.stats is the same and using a predictable random', () => {
       user.stats.gp = 34;
-      let val1 = randomVal(user, obj);
-      let val2 = randomVal(user, obj);
+      let val1 = randomVal(user, obj, { randomFunc: predictableRandom });
+      let val2 = randomVal(user, obj, { randomFunc: predictableRandom });
 
       expect(val2).to.equal(val1);
     });
 
-    it('returns a different value when the seed is different', () => {
-      let val1 = randomVal(user, obj, {
-        seed: 222,
-      });
-
-      let val2 = randomVal(user, obj, {
-        seed: 333,
-      });
-
-      expect(val2).to.not.equal(val1);
-    });
-
-    it('returns a different value when user.stats is different', () => {
-      user.stats.gp = 34;
-      let val1 = randomVal(user, obj);
-      user.stats.gp = 343;
-      let val2 = randomVal(user, obj);
-
-      expect(val2).to.not.equal(val1);
+    it('returns a value when no predictable random is provided', () => {
+      let val = randomVal(user, obj);
+      expect(_.contains(obj, val));
     });
   });
 
   describe('returns a random key from an object', () => {
-    it('returns the same key when the seed is the same', () => {
+    it('returns the same key when the seed is the same and using a predictable random', () => {
       let key1 = randomVal(user, obj, {
         key: true,
         seed: 222,
+        randomFunc: predictableRandom,
       });
 
       let key2 = randomVal(user, obj, {
         key: true,
         seed: 222,
+        randomFunc: predictableRandom,
       });
 
       expect(key2).to.equal(key1);
     });
 
-    it('returns the same key when user.stats is the same', () => {
+    it('returns the same key when user.stats is the same and using a predictable random', () => {
       user.stats.gp = 45;
       let key1 = randomVal(user, obj, {
         key: true,
+        randomFunc: predictableRandom,
       });
 
       let key2 = randomVal(user, obj, {
         key: true,
+        randomFunc: predictableRandom,
       });
 
       expect(key2).to.equal(key1);
     });
 
-    it('returns a different key when the seed is different', () => {
-      let key1 = randomVal(user, obj, {
-        key: true,
-        seed: 222,
-      });
-
-      let key2 = randomVal(user, obj, {
-        key: true,
-        seed: 333,
-      });
-
-      expect(key2).to.not.equal(key1);
-    });
-
-    it('returns a different key when user.stats is different', () => {
-      user.stats.gp = 45;
-      let key1 = randomVal(user, obj, {
-        key: true,
-      });
-
-      user.stats.gp = 43;
-
-      let key2 = randomVal(user, obj, {
-        key: true,
-      });
-
-      expect(key2).to.not.equal(key1);
+    it('returns a key when no predictable random is provided', () => {
+      let key = randomVal(user, obj);
+      expect(obj[key] !== undefined);
     });
   });
 });
