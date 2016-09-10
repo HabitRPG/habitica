a9f01e6f22569e9fd854c1bf2e23c7cfa72867a9
Thomas Gamble <thomasgamble2@gmail.com>
2016-09-10 10:37:15 -0700

    drops are randomly selected, not based on user values fixes #7929

--- test/common/fns/randomVal.js
+++ test/common/fns/randomVal.js
@@ -4,116 +4,52 @@ import {
 } from '../../helpers/common.helper';
 
 describe('shared.fns.randomVal', () => {
-  let user;
-  let obj = {
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 4,
-  };
+  let user, obj;
 
   beforeEach(() => {
     user = generateUser();
+    obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+    };
   });
 
-  describe('returns a random property value from an object', () => {
-    it('returns the same value when the seed is the same', () => {
-      let val1 = randomVal(user, obj, {
-        seed: 222,
-      });
-
-      let val2 = randomVal(user, obj, {
-        seed: 222,
-      });
-
-      expect(val2).to.equal(val1);
-    });
-
-    it('returns the same value when user.stats is the same', () => {
-      user.stats.gp = 34;
-      let val1 = randomVal(user, obj);
-      let val2 = randomVal(user, obj);
-
-      expect(val2).to.equal(val1);
-    });
-
-    it('returns a different value when the seed is different', () => {
-      let val1 = randomVal(user, obj, {
-        seed: 222,
-      });
-
-      let val2 = randomVal(user, obj, {
-        seed: 333,
-      });
-
-      expect(val2).to.not.equal(val1);
-    });
-
-    it('returns a different value when user.stats is different', () => {
-      user.stats.gp = 34;
-      let val1 = randomVal(user, obj);
-      user.stats.gp = 343;
-      let val2 = randomVal(user, obj);
-
-      expect(val2).to.not.equal(val1);
-    });
+  afterEach(() => {
+    sandbox.restore();
   });
 
-  describe('returns a random key from an object', () => {
-    it('returns the same key when the seed is the same', () => {
-      let key1 = randomVal(user, obj, {
-        key: true,
-        seed: 222,
-      });
+  it('returns a random value from an object', () => {
+    let result = randomVal(user, obj);
+    expect(result).to.be.oneOf([1, 2, 3, 4]);
+  });
 
-      let key2 = randomVal(user, obj, {
-        key: true,
-        seed: 222,
-      });
+  it('uses Math.random to determine the property', () => {
+    sandbox.spy(Math, 'random');
 
-      expect(key2).to.equal(key1);
+    randomVal(user, obj);
+
+    expect(Math.random).to.be.calledOnce;
+  });
+
+  it('can pass in a custom random function that takes in the user and a seed argument', () => {
+    let randomSpy = sandbox.stub().returns(0.3);
+    sandbox.spy(Math, 'random');
+
+    let result = randomVal(user, obj, {
+      seed: 100,
+      randomFunc: randomSpy,
     });
 
-    it('returns the same key when user.stats is the same', () => {
-      user.stats.gp = 45;
-      let key1 = randomVal(user, obj, {
-        key: true,
-      });
+    expect(Math.random).to.not.be.called;
+    expect(randomSpy).to.be.calledOnce;
+    expect(randomSpy).to.be.calledWith(user, 100);
+    expect(result).to.equal(2);
+  });
 
-      let key2 = randomVal(user, obj, {
-        key: true,
-      });
-
-      expect(key2).to.equal(key1);
-    });
-
-    it('returns a different key when the seed is different', () => {
-      let key1 = randomVal(user, obj, {
-        key: true,
-        seed: 222,
-      });
-
-      let key2 = randomVal(user, obj, {
-        key: true,
-        seed: 333,
-      });
-
-      expect(key2).to.not.equal(key1);
-    });
-
-    it('returns a different key when user.stats is different', () => {
-      user.stats.gp = 45;
-      let key1 = randomVal(user, obj, {
-        key: true,
-      });
-
-      user.stats.gp = 43;
-
-      let key2 = randomVal(user, obj, {
-        key: true,
-      });
-
-      expect(key2).to.not.equal(key1);
-    });
+  it('returns a random key when the key option is passed in', () => {
+    let result = randomVal(user, obj, { key: true });
+    expect(result).to.be.oneOf(['a', 'b', 'c', 'd']);
   });
 });
