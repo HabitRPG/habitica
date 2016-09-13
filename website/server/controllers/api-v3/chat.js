302da8de580cb403b2686971c3593140dd6fab33
Andre LaFleur <andre.lafleur@utah.edu>
2016-09-13 10:15:46 -0600

    Remove a comment and a locales message

--- website/server/controllers/api-v3/chat.js
+++ website/server/controllers/api-v3/chat.js
@@ -222,9 +222,6 @@ api.flagChat = {
 
     if (!message) throw new NotFound(res.t('messageGroupChatNotFound'));
 
-    // Your own message should be flaggable
-    // if (message.uuid === user._id) throw new NotFound(res.t('messageGroupChatFlagOwnMessage'));
-
     let update = {$set: {}};
 
     // Log user ids that have flagged the message
