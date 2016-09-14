6307968db4769f3048e78ede909aec4490f0a703
Blade Barringer <blade@crookedneighbor.com>
2016-09-14 12:32:31 -0500

    chore: Move audio folder to assets

--- website/server/middlewares/static.js
+++ website/server/middlewares/static.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 const IS_PROD = nconf.get('IS_PROD');
 const MAX_AGE = IS_PROD ? 31536000000 : 0;
+const ASSETS_DIR = path.join(__dirname, '/../../assets');
 const PUBLIC_DIR = path.join(__dirname, '/../../client-old');
 const BUILD_DIR = path.join(__dirname, '/../../build');
 
@@ -11,7 +12,7 @@ module.exports = function staticMiddleware (expressApp) {
   // TODO move all static files to a single location (one for public and one for build)
   expressApp.use(express.static(BUILD_DIR, { maxAge: MAX_AGE }));
   expressApp.use('/common/dist', express.static(`${PUBLIC_DIR}/../../common/dist`, { maxAge: MAX_AGE }));
-  expressApp.use('/common/audio', express.static(`${PUBLIC_DIR}/../../common/audio`, { maxAge: MAX_AGE }));
+  expressApp.use('/assets/audio', express.static(`${ASSETS_DIR}/audio`, { maxAge: MAX_AGE }));
   expressApp.use('/common/img', express.static(`${PUBLIC_DIR}/../../common/img`, { maxAge: MAX_AGE }));
   expressApp.use(express.static(PUBLIC_DIR));
 
