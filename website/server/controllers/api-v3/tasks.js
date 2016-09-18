1f7344da7cc8c09226d5f42127f14a5acddd3a52
Keith Holliday <krh121791@gmail.com>
2016-09-18 16:22:48 -0500

    Fixed failing task tests

--- website/server/controllers/api-v3/tasks.js
+++ website/server/controllers/api-v3/tasks.js
@@ -680,7 +680,7 @@ api.addTagToTask = {
     if (validationErrors) throw validationErrors;
 
     let taskId = req.params.taskId;
-    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id, { userId: user._id });
+    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id);
 
     if (!task) throw new NotFound(res.t('taskNotFound'));
     let tagId = req.params.tagId;
@@ -722,7 +722,7 @@ api.removeTagFromTask = {
     if (validationErrors) throw validationErrors;
 
     let taskId = req.params.taskId;
-    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id, { userId: user._id });
+    let task = await Tasks.Task.findByIdOrAlias(taskId, user._id);
 
     if (!task) throw new NotFound(res.t('taskNotFound'));
 
