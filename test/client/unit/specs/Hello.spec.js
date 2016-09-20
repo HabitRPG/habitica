de78918b6fdd14b1c5e9d95fef45611480c18c02
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-09-20 17:50:38 +0200

    disable import for non existing file

--- test/client/unit/specs/Hello.spec.js
+++ test/client/unit/specs/Hello.spec.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Hello from 'src/components/Hello';
+// import Hello from 'src/components/Hello';
 
 describe('Hello.vue', () => {
   xit('should render correct contents', () => {
