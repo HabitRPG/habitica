b6eea1dd139be7534161ebc6a997e4f7249cdc75
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-09-20 18:31:25 +0200

    add line wrongly removed, update regexp

--- test/client/unit/index.js
+++ test/client/unit/index.js
@@ -7,4 +7,5 @@ var testsContext = require.context('./specs', true, /\.spec$/);
 testsContext.keys().forEach(testsContext);
 
 // require all src files except main.js/ README.md / index.html for coverage.
-var srcContext = require.context('../../../website/client', true, /^\.\/(?!(main(\.js)|README(\.md)|index(\.html))?$)/);
+var srcContext = require.context('../../../website/client', true, /^\.\/(?!(main(\.js)?)|(index(\.html)?)$)/);
+srcContext.keys().forEach(srcContext);
