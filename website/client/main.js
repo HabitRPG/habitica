6f4b147e572ea496b7b031daf7dad44b09df71fa
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-09-22 17:37:45 +0200

    wip: vuex structure

--- website/client/main.js
+++ website/client/main.js
@@ -4,16 +4,38 @@ require('babel-polyfill');
 
 import Vue from 'vue';
 import VuexRouterSync from 'vuex-router-sync';
+import VueResource from 'vue-resource';
 import App from './components/app';
 import router from './router';
 import store from './vuex/store';
 
+Vue.use(VueResource);
+Vue.http.headers.common['x-api-user'] = '';
+Vue.http.headers.common['x-api-key'] = '';
+
 // Sync Vuex and Router
 VuexRouterSync.sync(store, router);
 
-new Vue({ // eslint-disable-line no-new
-  router,
-  store,
-  el: '#app',
-  render: h => h(App),
+// Setup listener for title that is outside Vue's scope
+store.watch(state => state.title, (newTitle) => {
+  document.title = newTitle;
 });
+
+// Load the user and then render the app
+store.dispatch('fetchUser').then(() => {
+  new Vue({ // eslint-disable-line no-new
+    router,
+    store,
+    el: '#app',
+    render: h => h(App),
+    mounted () {
+      // Remove loading screen
+      let loadingScreen = document.getElementById('loading-screen');
+      if (loadingScreen) document.body.removeChild(loadingScreen);
+    },
+  });
+}).catch(() => {
+  // TODO redirect to logout
+});
+
+
