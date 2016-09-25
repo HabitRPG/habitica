b266dcde428a2c9c020a3761b0d28135e8ce1061
Blade Barringer <blade@crookedneighbor.com>
2016-09-24 21:25:14 -0500

    refactor: remove seeding from randomVal

--- website/common/script/fns/randomVal.js
+++ website/common/script/fns/randomVal.js
@@ -3,17 +3,17 @@ import _ from 'lodash';
 // Get a random property from an object
 // returns random property (the value)
 
-function randomGenerator (user, seed, providedRandom) {
-  return providedRandom ? providedRandom(user, seed) : Math.random();
+function trueRandom () {
+  return Math.random();
 }
 
 module.exports = function randomVal (obj, options = {}) {
   let array = options.key ? _.keys(obj) : _.values(obj);
-  let rand = randomGenerator(options.user, options.seed, options.randomFunc);
+  let random = (options.predictableRandom || trueRandom)();
 
   array.sort();
 
-  let randomIndex = Math.floor(rand * array.length);
+  let randomIndex = Math.floor(random * array.length);
 
   return array[randomIndex];
 };
