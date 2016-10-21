2dd78483bd7213a282ab6a9d63fe91fdd8101b08
Keith Holliday <krh121791@gmail.com>
2016-10-21 04:57:09 -0500

    Abstracted subscription schema

--- website/server/libs/payments.js
+++ website/server/libs/payments.js
@@ -14,7 +14,7 @@ import {
 import {
   NotAuthorized,
   NotFound,
-} from './libs/errors';
+} from './errors';
 
 let api = {};
 
--- website/server/models/group.js
+++ website/server/models/group.js
@@ -23,6 +23,9 @@ import pusher from '../libs/pusher';
 import {
   syncableAttrs,
 } from '../libs/taskManager';
+import {
+  schema as SubscriptionPlanSchema,
+} from './subscriptionPlan';
 
 const questScrolls = shared.content.quests;
 const Schema = mongoose.Schema;
@@ -92,24 +95,9 @@ export let schema = new Schema({
   },
   purchased: {
     active: {type: Boolean, default: false},
-    plan: {
-      planId: String,
-      paymentMethod: String, // enum: ['Paypal','Stripe', 'Gift', 'Amazon Payments', '']}
-      customerId: String, // Billing Agreement Id in case of Amazon Payments
-      dateCreated: Date,
-      dateTerminated: Date,
-      dateUpdated: Date,
-      extraMonths: {type: Number, default: 0},
-      gemsBought: {type: Number, default: 0},
-      mysteryItems: {type: Array, default: () => []},
-      lastBillingDate: Date, // Used only for Amazon Payments to keep track of billing date
-      consecutive: {
-        count: {type: Number, default: 0},
-        offset: {type: Number, default: 0}, // when gifted subs, offset++ for each month. offset-- each new-month (cron). count doesn't ++ until offset==0
-        gemCapExtra: {type: Number, default: 0},
-        trinkets: {type: Number, default: 0},
-      },
-    },
+    plan: {type: SubscriptionPlanSchema, default: () => {
+      return {};
+    }},
   },
 }, {
   strict: true,
