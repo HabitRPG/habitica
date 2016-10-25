841a46171a6194b8346f89cf5b6828fdb2f7605a
AccioBooks <MathWhiz@users.noreply.github.com>
2016-10-25 13:03:27 -0500

    Update modelsPaths.js

--- website/server/controllers/api-v3/modelsPaths.js
+++ website/server/controllers/api-v3/modelsPaths.js
@@ -11,7 +11,7 @@ let allModels = ['user', 'tag', 'challenge', 'group'].concat(tasksModels);
  * @apiName GetUserModelPaths
  * @apiGroup Meta
  *
- * @apiParam (Body) {String="user","group","challenge","tag","habit","daily","todo","reward"} model The name of the model
+ * @apiParam (Path) {String="user","group","challenge","tag","habit","daily","todo","reward"} model The name of the model
  *
  * @apiExample {curl} Tag
  * curl https://habitica.com/api/v3/models/tag/paths
