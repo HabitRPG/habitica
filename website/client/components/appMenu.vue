37bf06f8f48bfc5f18c90b5c9d60874dbcd9c9ed
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-10-26 20:20:12 +0200

    initial styling for top menu

--- website/client/components/appMenu.vue
+++ website/client/components/appMenu.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-.ui.top.fixed.menu
+#app-menu.ui.top.fixed.menu
   .header.item Habitica
-  router-link.item(to="/") Tasks
+  router-link.item(to="/", exact) Tasks
   // .simple makes it possible to have a dropdown without JS
   .ui.simple.dropdown
     router-link.item(to="/inventory") Inventory
@@ -19,11 +19,11 @@
       router-link.item(to="/social/party") Party
       router-link.item(to="/social/guilds") Guilds
   .ui.simple.dropdown
-    router-link.item(to="/") Help
+    router-link.item(to="/help") Help
     .menu
-      router-link.item(to="/") Faq
-      router-link.item(to="/") Report a bug
-      router-link.item(to="/") Request a feature
+      router-link.item(to="/help/faq") Faq
+      router-link.item(to="/help/report-bug") Report a bug
+      router-link.item(to="/help/request-feature") Request a feature
   .right.menu
     .item
       i.icon.diamond
@@ -43,14 +43,64 @@
         .divider
         router-link.item(to="/user/settings") Settings
         .divider
-        router-link.item(to="/") Logout
+        router-link.item(to="/logout") Logout
 </template>
 
 <style>
 .ui.menu .right.menu .ui.simple.dropdown > .item::before {
-    right: auto;
-    left: 0;
+  right: auto;
+  left: 0;
 }
+
+#app-menu {
+  background-color: #432874;
+  border-bottom: 0px;
+}
+
+#app-menu .header.item {
+  width: 256px;
+}
+
+#app-menu > .item, #app-menu .dropdown > .item {
+  height: 56px;
+  font-size: 16px;
+  line-height: 1.5;
+  font-weight: bold;
+  text-align: center;
+  padding: 16px 20px;
+  color: #fff;
+}
+
+#app-menu > .item::before, #app-menu .dropdown > .item::before {
+  width: 0px;
+}
+
+#app-menu > .item:hover, #app-menu .dropdown:hover {
+  background-color: #6133b4;
+}
+
+#app-menu > .item.router-link-active {
+  box-shadow: 0px -4px 0px #6133b4 inset;
+}
+
+#app-menu > .dropdown > .menu {
+  border: none;
+}
+
+#app-menu .dropdown .menu > .item {
+  /* !important to override Semantic UI's !important */
+  width: 217px;
+  background: #6133b4 !important;
+  color: #fff !important;
+  font-size: 16px !important;
+  line-height: 1.5;
+  padding: 6px 0px 6px 20px !important;
+}
+
+#app-menu .dropdown .menu > .item:hover {
+  background: #4f2a93 !important; /* to override Semantic UI's !important */
+}
+
 </style>
 
 <script>
