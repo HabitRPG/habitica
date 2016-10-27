3c7ed4864e71d5e2b64d8d904b29049e86549818
shalott <shalott@gmail.com>
2016-10-27 17:49:53 -0400

    adding counter to habits
    First pass at adding counter to habits. Open issue: the cron test will fail when it runs on a Monday or the first day of a month. Also to be resolved, how to display the +/- counter.

--- test/api/v3/unit/libs/cron.test.js
+++ test/api/v3/unit/libs/cron.test.js
@@ -412,6 +412,55 @@ describe('cron', () => {
 
       expect(tasksByType.habits[0].value).to.equal(1);
     });
+    
+    it('should reset a daily habit counter each day', () => {
+      tasksByType.habits[0].counterUp = 1;
+      tasksByType.habits[0].counterDown = 1;
+
+      cron({user, tasksByType, daysMissed, analytics});
+
+      expect(tasksByType.habits[0].counterUp).to.equal(0); 
+      expect(tasksByType.habits[0].counterDown).to.equal(0);
+    });
+    
+    it ('should reset a weekly habit counter each Monday', () => {
+      tasksByType.habits[0].frequency = 'weekly';
+      tasksByType.habits[0].counterUp = 1;
+      tasksByType.habits[0].counterDown = 1;
+
+      // should not reset (unless we're testing on Monday?)
+      cron({user, tasksByType, daysMissed, analytics});
+      
+      expect(tasksByType.habits[0].counterUp).to.equal(1); 
+      expect(tasksByType.habits[0].counterDown).to.equal(1);
+
+      // should reset
+      daysMissed = 8;
+      cron({user, tasksByType, daysMissed, analytics});
+      
+      expect(tasksByType.habits[0].counterUp).to.equal(0); 
+      expect(tasksByType.habits[0].counterDown).to.equal(0);
+    });
+
+    it ('should reset a monthly habit counter the first day of each month', () => {
+      tasksByType.habits[0].frequency = 'monthly';
+      tasksByType.habits[0].counterUp = 1;
+      tasksByType.habits[0].counterDown = 1;
+
+      // should not reset (unless we're testing on the first day of the month?)
+      cron({user, tasksByType, daysMissed, analytics});
+      
+      expect(tasksByType.habits[0].counterUp).to.equal(1); 
+      expect(tasksByType.habits[0].counterDown).to.equal(1);
+
+      // should reset
+      daysMissed = 32;
+      cron({user, tasksByType, daysMissed, analytics});
+      
+      expect(tasksByType.habits[0].counterUp).to.equal(0); 
+      expect(tasksByType.habits[0].counterDown).to.equal(0);
+    });
+    
   });
 
   describe('perfect day', () => {
