006bd2a46682709d9d406308829cb14520b59c12
Keith Holliday <krh121791@gmail.com>
2016-11-02 12:45:54 -0500

    Added group ui items back and initial group approval directive

---
+++ website/client-old/js/components/groupApprovals/groupApprovalsDirective.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function(){
+  angular
+    .module('habitrpg')
+    .directive('groupApprovals', groupApprovals);
+
+  groupApprovals.$inject = [
+  ];
+
+  function groupApprovals() {
+
+    return {
+      scope: {
+        group: '=',
+      },
+      templateUrl: 'partials/groups.tasks.approvals.html',
+      // controller: 'GroupApprovalsCtrl',
+    };
+  }
+}());
