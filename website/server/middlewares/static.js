9e405b4a93a63e72d0c1ce34dccb2b4f60aa516b
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-11-18 12:04:15 +0100

    fix sprites path

--- website/server/middlewares/static.js
+++ website/server/middlewares/static.js
@@ -6,6 +6,7 @@ const IS_PROD = nconf.get('IS_PROD');
 const IS_NEW_CLIENT_ENABLED = nconf.get('NEW_CLIENT_ENABLED') === 'true';
 const MAX_AGE = IS_PROD ? 31536000000 : 0;
 const ASSETS_DIR = path.join(__dirname, '/../../assets');
+const STATIC_DIR = path.join(__dirname, '/../../static');
 const PUBLIC_DIR = path.join(__dirname, '/../../client-old');
 const BUILD_DIR = path.join(__dirname, '/../../build');
 
@@ -18,10 +19,9 @@ module.exports = function staticMiddleware (expressApp) {
   // TODO move all static files to a single location (one for public and one for build)
   expressApp.use(express.static(BUILD_DIR, { maxAge: MAX_AGE }));
   // TODO figure out better way to set up sprites assets
-  expressApp.use('/static/sprites', express.static(`${ASSETS_DIR}/sprites/dist`, { maxAge: MAX_AGE }));
+  expressApp.use('/static/sprites', express.static(`${STATIC_DIR}/sprites`, { maxAge: MAX_AGE }));
   // so we have access to the gif sprites
-  expressApp.use(express.static(`${ASSETS_DIR}/sprites/backer-only/`, { maxAge: MAX_AGE }));
-  expressApp.use(express.static(`${ASSETS_DIR}/sprites/`, { maxAge: MAX_AGE }));
+  expressApp.use(express.static(`${STATIC_DIR}/sprites/gif/backer_only`, { maxAge: MAX_AGE }));
 
   expressApp.use('/assets/audio', express.static(`${ASSETS_DIR}/audio`, { maxAge: MAX_AGE }));
   expressApp.use('/assets/img', express.static(`${PUBLIC_DIR}/../../website/assets/img`, { maxAge: MAX_AGE }));
