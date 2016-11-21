eb3ee2500cb9069733f891268368b45af0492338
Keith Holliday <krh121791@gmail.com>
2016-11-20 23:24:06 -0600

    Added ability for old leader to delete subscription the created

--- website/server/models/subscriptionPlan.js
+++ website/server/models/subscriptionPlan.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 import baseModel from '../libs/baseModel';
+import validator from 'validator';
 
 export let schema = new mongoose.Schema({
   planId: String,
   subscriptionId: String,
+  owner: {type: String, ref: 'User', validate: [validator.isUUID, 'Invalid uuid.']},
   quantity: {type: Number, default: 1},
   paymentMethod: String, // enum: ['Paypal','Stripe', 'Gift', 'Amazon Payments', '']}
   customerId: String, // Billing Agreement Id in case of Amazon Payments
