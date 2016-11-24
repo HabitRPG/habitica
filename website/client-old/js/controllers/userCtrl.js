bf9d9c74842c223cf079e73037531dd73b2cf939
Sabe Jones <sabrecat@gmail.com>
2016-11-24 00:08:41 +0000

    fix(backgrounds): don't gem-buy checkin bgs

--- website/client-old/js/controllers/userCtrl.js
+++ website/client-old/js/controllers/userCtrl.js
@@ -65,12 +65,14 @@ habitrpg.controller("UserCtrl", ['$rootScope', '$scope', '$location', 'User', '$
           (fullSet ? 3.75 : 1.75) : // (Backgrounds) 15G per set, 7G per individual
           (fullSet ? 1.25 : 0.5); // (Hair, skin, etc) 5G per set, 2G per individual
 
-      if (fullSet) {
-        if (confirm(window.env.t('purchaseFor',{cost:cost*4})) !== true) return;
-        if (User.user.balance < cost) return $rootScope.openModal('buyGems');
-      } else if (!_.get(User.user, 'purchased.' + path)) {
-        if (confirm(window.env.t('purchaseFor',{cost:cost*4})) !== true) return;
-        if (User.user.balance < cost) return $rootScope.openModal('buyGems');
+      if (path.indexOf('background.blue') === -1 && path.indexOf('background.green') === -1 && path.indexOf('background.red') === -1 && path.indexOf('background.purple') === -1 && path.indexOf('background.yellow') === -1) {
+        if (fullSet) {
+          if (confirm(window.env.t('purchaseFor',{cost:cost*4})) !== true) return;
+          if (User.user.balance < cost) return $rootScope.openModal('buyGems');
+        } else if (!_.get(User.user, 'purchased.' + path)) {
+          if (confirm(window.env.t('purchaseFor',{cost:cost*4})) !== true) return;
+          if (User.user.balance < cost) return $rootScope.openModal('buyGems');
+        }
       }
       User.unlock({query:{path:path}})
     }
