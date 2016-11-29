bab41647f5807aeeafb7b958b75c923e55af6fee
Keith Holliday <krh121791@gmail.com>
2016-11-29 09:18:07 -0600

    Fixed lint issues

--- test/api/v3/unit/libs/cron.test.js
+++ test/api/v3/unit/libs/cron.test.js
@@ -6,7 +6,7 @@ import requireAgain from 'require-again';
 import { recoverCron, cron } from '../../../../../website/server/libs/cron';
 import { model as User } from '../../../../../website/server/models/user';
 import * as Tasks from '../../../../../website/server/models/task';
-import { clone, filter } from 'lodash';
+import { clone } from 'lodash';
 import common from '../../../../../website/common';
 import analytics from '../../../../../website/server/libs/analyticsService';
 
