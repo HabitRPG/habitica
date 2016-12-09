105af982384a1c3d4ae50e8f93940cc178672151
Keith Holliday <krh121791@gmail.com>
2016-12-09 10:32:09 -0600

    Fixed obj transfer on scope

--- website/client-old/js/services/taskServices.js
+++ website/client-old/js/services/taskServices.js
@@ -264,6 +264,7 @@ angular.module('habitrpg')
         modalScope.navigateChecklist = scopeInc.navigateChecklist;
         modalScope.checklistCompletion = scopeInc.checklistCompletion;
         modalScope.canEdit = scopeInc.canEdit;
+        modalScope.obj = scopeInc.obj;
       }
       modalScope.cancelTaskEdit = cancelTaskEdit;
 
