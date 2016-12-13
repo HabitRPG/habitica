32846263e7827ce9f06d3da791589e5c5a48eb72
Negue <eugen.bolz@gmail.com>
2016-12-13 19:29:10 +0100

    fix syntax after merge

--- website/client-old/js/controllers/userCtrl.js
+++ website/client-old/js/controllers/userCtrl.js
@@ -107,5 +107,6 @@ habitrpg.controller("UserCtrl", ['$rootScope', '$scope', '$location', 'User', '$
 
     $scope.achievements = Shared.achievements.getAchievementsForProfile($scope.profile);
     $scope.achievPopoverPlacement = 'right';
-    $scope.achievAppendToBody = 'true'; // append-to-body breaks popovers in modal windows  }
+    $scope.achievAppendToBody = 'true'; // append-to-body breaks popovers in modal windows  
+  }
 ]);
