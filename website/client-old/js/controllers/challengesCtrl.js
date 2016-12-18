de6816be4cdae953004f9032d889548b459cc846
Travis Husman <thusman13@gmail.com>
2016-12-18 15:03:56 -0800

    Fixing client side parameter updates.

--- website/client-old/js/controllers/challengesCtrl.js
+++ website/client-old/js/controllers/challengesCtrl.js
@@ -320,7 +320,7 @@ habitrpg.controller("ChallengesCtrl", ['$rootScope','$scope', 'Shared', 'User', 
     */
 
     $scope.join = function (challenge) {
-      challenge._joiningInProgress = false;
+      challenge._joiningInProgress = true;
       Challenges.joinChallenge(challenge._id)
         .then(function (response) {
           User.user.challenges.push(challenge._id);
@@ -330,8 +330,8 @@ habitrpg.controller("ChallengesCtrl", ['$rootScope','$scope', 'Shared', 'User', 
         .then(function (response) {
           var tasks = response.data.data;
           User.syncUserTasks(tasks);
+          challenge._joiningInProgress = false;
         });
-      challenge._joiningInProgress = true;
     }
 
     $scope.leave = function(keep, challenge) {
