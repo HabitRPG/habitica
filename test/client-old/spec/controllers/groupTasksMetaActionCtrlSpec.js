0153d607555533950a6b5f920971857b3a2cb691
Keith Holliday <krh121791@gmail.com>
2017-01-09 09:50:48 -0700

    Added test for group task meta actions. Added sync when user claims

---
+++ test/client-old/spec/controllers/groupTasksMetaActionCtrlSpec.js
@@ -0,0 +1,42 @@
+describe.only('Group Tasks Meta Actions Controller', () => {
+  let scope, user, userSerivce;
+
+  beforeEach(() => {
+    module(function($provide) {
+      $provide.value('User', {});
+    });
+
+    inject(($rootScope, $controller) => {
+      user = specHelper.newUser();
+      user._id = "unique-user-id";
+      userSerivce = {user: user};
+      userSerivce.sync = sandbox.stub();
+
+      scope = $rootScope.$new();
+
+      $controller('GroupTaskMetaActionsCtrl', {$scope: scope, User: userSerivce});
+
+      scope.task = {
+        group: {
+          assignedUsers: [],
+        },
+      };
+    });
+  });
+
+  describe('claim', () => {
+    beforeEach(() => {
+      sandbox.stub(window, 'confirm').returns(true);
+    });
+
+    it('adds user to assigned users of scope task ', () => {
+      scope.claim();
+      expect(scope.task.group.assignedUsers).to.contain(user._id);
+    });
+
+    it('syncs user tasks ', () => {
+      scope.claim();
+      expect(userSerivce.sync).to.be.calledOnce;
+    });
+  });
+});
