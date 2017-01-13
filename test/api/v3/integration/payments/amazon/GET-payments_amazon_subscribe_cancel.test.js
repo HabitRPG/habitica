410b8e1e52f7533c2ccd56adbc4038e2e5d1e71c
Keith Holliday <krh121791@gmail.com>
2017-01-12 17:39:59 -0700

    Added integration tests for amazon subscribe cancel

--- test/api/v3/integration/payments/amazon/GET-payments_amazon_subscribe_cancel.test.js
+++ test/api/v3/integration/payments/amazon/GET-payments_amazon_subscribe_cancel.test.js
@@ -1,21 +1,92 @@
 import {
   generateUser,
+  generateGroup,
   translate as t,
 } from '../../../../../helpers/api-integration/v3';
+import amzLib from '../../../../../../website/server/libs/amazonPayments';
 
 describe('payments : amazon #subscribeCancel', () => {
   let endpoint = '/amazon/subscribe/cancel';
-  let user;
+  let user, group, amazonSubscribeCancelStub;
 
   beforeEach(async () => {
     user = await generateUser();
   });
 
-  it('verifies subscription', async () => {
+  it('throws error when there users has no subscription', async () => {
     await expect(user.get(endpoint)).to.eventually.be.rejected.and.eql({
       code: 401,
       error: 'NotAuthorized',
       message: t('missingSubscription'),
+    });
+  });
+
+  describe('sucess', () => {
+    beforeEach(async () => {
+      amazonSubscribeCancelStub = sinon.stub(amzLib, 'cancelSubscription').returnsPromise().resolves({});
+    });
+
+    afterEach(() => {
+      amzLib.cancelSubscription.restore();
+    });
+
+    it('cancels a user subscription', async () => {
+      user = await generateUser({
+        'profile.name': 'sender',
+        'purchased.plan.customerId': 'customer-id',
+        'purchased.plan.planId': 'basic_3mo',
+        'purchased.plan.lastBillingDate': new Date(),
+        balance: 2,
+      });
+
+      await user.get(endpoint);
+
+      expect(amazonSubscribeCancelStub).to.be.calledOnce;
+      expect(amazonSubscribeCancelStub.args[0][0].user._id).to.eql(user._id);
+      expect(amazonSubscribeCancelStub.args[0][0].groupId).to.eql(group._id);
+      expect(amazonSubscribeCancelStub.args[0][0].headers).to.eql({
+        accept: 'application/json',
+        'accept-encoding': 'gzip, deflate',
+        connection: 'close',
+        host: 'localhost:3000',
+        'user-agent': 'node-superagent/1.8.4',
+        'x-api-key': user.apiToken,
+        'x-api-user': user._id,
+      });
+    });
+
+    it.only('cancels a group subscription', async () => {
+      user = await generateUser({
+        'profile.name': 'sender',
+        'purchased.plan.customerId': 'customer-id',
+        'purchased.plan.planId': 'basic_3mo',
+        'purchased.plan.lastBillingDate': new Date(),
+        balance: 2,
+      });
+
+      group = await generateGroup(user, {
+        name: 'test group',
+        type: 'guild',
+        privacy: 'public',
+        'purchased.plan.customerId': 'customer-id',
+        'purchased.plan.planId': 'basic_3mo',
+        'purchased.plan.lastBillingDate': new Date(),
+      });
+
+      await user.get(endpoint + '?groupId=' + group._id);
+
+      expect(amazonSubscribeCancelStub).to.be.calledOnce;
+      expect(amazonSubscribeCancelStub.args[0][0].user._id).to.eql(user._id);
+      expect(amazonSubscribeCancelStub.args[0][0].groupId).to.eql(group._id);
+      expect(amazonSubscribeCancelStub.args[0][0].headers).to.eql({
+        accept: 'application/json',
+        'accept-encoding': 'gzip, deflate',
+        connection: 'close',
+        host: 'localhost:3000',
+        'user-agent': 'node-superagent/1.8.4',
+        'x-api-key': user.apiToken,
+        'x-api-user': user._id,
+      });
     });
   });
 });
