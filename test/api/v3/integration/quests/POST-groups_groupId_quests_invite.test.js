faa251bd65267ecdec32410be3c96bfcee62ad9f
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-01-18 19:41:53 +0100

    fix typo

--- test/api/v3/integration/quests/POST-groups_groupId_quests_invite.test.js
+++ test/api/v3/integration/quests/POST-groups_groupId_quests_invite.test.js
@@ -205,7 +205,7 @@ describe('POST /groups/:groupId/quests/invite/:questKey', () => {
       expect(group.chat[0]._meta).to.exist;
       expect(group.chat[0]._meta).to.have.all.keys(['participatingMembers']);
 
-      let returnedGroup = await leader.get(`/groups/${group._id}`);
+      let returnedGroup = await groupLeader.get(`/groups/${group._id}`);
       expect(returnedGroup.chat[0]._meta).to.be.undefined;
     });
   });
