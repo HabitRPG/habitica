01ed6a834adc1ba7a600dd69a15f8259d12f684b
Gerardo Saca <gsacavdm@hotmail.com>
2017-01-18 19:01:41 -0800

    Remove bad nextRewardAt check

--- website/client-old/js/controllers/userCtrl.js
+++ website/client-old/js/controllers/userCtrl.js
@@ -93,7 +93,7 @@ habitrpg.controller("UserCtrl", ['$rootScope', '$scope', '$location', 'User', '$
 
     $scope.getProgressDisplay = function () {
       var currentLoginDay = Content.loginIncentives[$scope.profile.loginIncentives];
-      if (!currentLoginDay || !nextRewardAt) return env.t('moreIncentivesComingSoon');
+      if (!currentLoginDay) return env.t('moreIncentivesComingSoon');
       var nextRewardAt = currentLoginDay.nextRewardAt;
       if (!currentLoginDay.prevRewardKey) currentLoginDay.prevRewardKey = 0;
       return env.t('checkinProgressTitle') + ' ' + ($scope.profile.loginIncentives - currentLoginDay.prevRewardKey) + '/' + (nextRewardAt - currentLoginDay.prevRewardKey);
