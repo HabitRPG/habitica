b6122564cf0e271529f76094d6e067bb0c9b9c9f
Gerardo Saca <gsacavdm@hotmail.com>
2017-01-28 22:21:51 -0800

    Fix faqs string and test

--- website/common/script/content/faq.js
+++ website/common/script/content/faq.js
@@ -1,4 +1,5 @@
 import t from './translation';
+import nconf from 'nconf';
 
 const NUMBER_OF_QUESTIONS = 12;
 
@@ -14,7 +15,10 @@ for (let i = 0; i <= NUMBER_OF_QUESTIONS; i++) {
   let question = {
     question: t(`faqQuestion${i}`),
     ios: t(`iosFaqAnswer${i}`),
-    web: t(`webFaqAnswer${i}`),
+    web: t(`webFaqAnswer${i}`, {
+      techAssistanceEmail: nconf.get('EMAILS:TECH_ASSISTANCE_EMAIL'),
+      wikiTechAssistanceEmail: `mailto:${nconf.get('EMAILS:TECH_ASSISTANCE_EMAIL')}`,
+    }),
   };
 
   faq.questions.push(question);
