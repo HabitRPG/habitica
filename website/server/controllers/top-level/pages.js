e7f3c0bc64d7df45d8a1af725092b1c0d6c202a7
Alys <alice.harris@oldgods.net>
2017-01-31 20:54:33 +1000

    update landing page's number of user accounts to 2 million
    YES! TWO MILLION!

--- website/server/controllers/top-level/pages.js
+++ website/server/controllers/top-level/pages.js
@@ -6,7 +6,7 @@ import nconf from 'nconf';
 let api = {};
 
 const IS_PROD = nconf.get('IS_PROD');
-const TOTAL_USER_COUNT = '1,500,000';
+const TOTAL_USER_COUNT = '2,000,000';
 const LOADING_SCREEN_TIPS = 33;
 const IS_NEW_CLIENT_ENABLED = nconf.get('NEW_CLIENT_ENABLED') === 'true';
 
