27bcacf1f02540734a8b01df453903934e9dee18
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-02-01 19:36:46 +0100

    render error if password reset code is missing or invalid

--- website/server/controllers/top-level/auth.js
+++ website/server/controllers/top-level/auth.js
@@ -15,32 +15,31 @@ api.resetPasswordSetNewOne  = {
   runCron: false,
   async handler (req, res) {
     // TODO
-    // - validate user being local
     // - convert to bcrypt
     // - tests
     // - set new one or set a automatically generated password?
-
-    req.checkQuery('code', res.t('passwordResetCodeRequired')).notEmpty();
-    let validationErrors = req.validationErrors();
-    if (validationErrors) throw validationErrors;
+    let isCodeValid = true;
 
     let userId;
+
     // wrapping the code in a try to be able to handle the error here
     try {
-      let decryptedPasswordResetCode = JSON.parse(decrypt(req.query.code));
+      let decryptedPasswordResetCode = JSON.parse(decrypt(req.query.code || 'invalid')); // also catches missing code
       userId = decryptedPasswordResetCode.userId;
       let expiresAt = decryptedPasswordResetCode.expiresAt;
 
-      if (moment(expiresAt).isAfter(moment())) throw new Error('EXPIRED_PASSWORD_RESET_CODE');
+      if (moment(expiresAt).isAfter(moment())) throw new Error();
     } catch (err) {
-      if (err.message === 'EXPIRED_PASSWORD_RESET_CODE') return; // TODO render expiration page
-      // TODO render generic error page
+      isCodeValid = false;
     }
 
     let user = await User.findById(userId).exec();
+    // check if user is found and if it's an email & password account
+    if (!user || !user.auth || !user.auth.local || !user.auth.local.email) isCodeValid = false;
 
-    return res.render('static/reset-password-set-new-one.jade', {
+    return res.status(isCodeValid ? 200 : 401).render('static/reset-password-set-new-one.jade', {
       env: res.locals.habitrpg,
+      isCodeValid,
     });
   },
 };
