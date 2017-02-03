ca5161b48ac15f315bdfbede0eaa45deb1ba102e
Phillip Thelen <viirus@pherth.net>
2017-02-03 10:58:48 +0100

    fix gem purchase validation (#8470)

--- website/server/libs/inAppPurchases.js
+++ website/server/libs/inAppPurchases.js
@@ -7,6 +7,8 @@ import Bluebird from 'bluebird';
 // const CONNECTION_FAILED = 6778002;
 // const PURCHASE_EXPIRED = 6778003;
 
+// We are using this PR instead of the main release: https://github.com/voltrue2/in-app-purchase/pull/74
+// Reason: Without the PR, the library attempts to validate IAP products as subscriptions and fails.
 iap.config({
   // This is the path to the directory containing iap-sanbox/iap-live files
   googlePublicKeyPath: nconf.get('IAP_GOOGLE_KEYDIR'),
