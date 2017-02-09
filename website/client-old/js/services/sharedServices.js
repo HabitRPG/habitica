9addd9e6558a3a8c7c07413c1e9ad444676fb068
Keith Holliday <krh121791@gmail.com>
2017-02-09 11:49:25 -0700

    Added content api call to front end

--- website/client-old/js/services/sharedServices.js
+++ website/client-old/js/services/sharedServices.js
@@ -8,6 +8,19 @@ angular.module('habitrpg')
 .factory('Shared', [function () {
   return window.habitrpgShared;
 }])
-.factory('Content', ['Shared', function (Shared) {
+.factory('Content', ['Shared', '$http', function (Shared, $http) {
+
+  function loadContent () {
+    $http({
+      method: 'GET',
+      url: '/api/v3/content',
+    })
+    .then(function (response) {
+      Shared.content = response.data.data;
+    });
+  }
+
+  loadContent();
+
   return Shared.content;
 }]);
