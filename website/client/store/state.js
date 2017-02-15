2ace7631f53f465d223673c54c37615a2517b1d4
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-02-15 11:46:56 +0100

    update comment about deepFreeze

--- website/client/store/state.js
+++ website/client/store/state.js
@@ -5,11 +5,9 @@ const state = {
   title: 'Habitica',
   user: null,
   tasks: null, // user tasks
-  // content data, frozen to prevent Vue from modifying it
+  // content data, frozen to prevent Vue from modifying it since it's static data that never changes
   // TODO apply freezing to the entire codebase (the server) and not only to the client side?
-  // NOTE this is quite slow, taking 60-80ms on a fast computer, let's find an alternative
-  // that being said, not freezing it is slow too because Vue's making the object reactive takes
-  // about 70ms
+  // NOTE this takes about 10-15ms on a fast computer
   content: deepFreeze(content),
 };
 
