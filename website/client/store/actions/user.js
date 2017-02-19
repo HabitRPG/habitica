573d40409a4985e546d4bebe882ec1f3f8d6faaf
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-02-19 15:17:51 +0100

    wip work to show loading status of resources

--- website/client/store/actions/user.js
+++ website/client/store/actions/user.js
@@ -1,6 +1,19 @@
 import axios from 'axios';
 
-export async function fetch (store) { // eslint-disable-line no-shadow
-  let response = await axios.get('/api/v3/user');
-  store.state.user = response.data.data;
+export async function fetch (store, forceReload) { // eslint-disable-line no-shadow
+  let loadingStatus = forceReload ? 'NOT_LOADED' : store.state.loadingStatus.user;
+
+  switch (loadingStatus) {
+    case 'NOT_LOADED': {
+      let response = await axios.get('/api/v3/user');
+      store.state.loadingStatus.user = 'LOADED';
+      return store.state.user = response.data.data;
+    }
+    case 'LOADING': {
+      break;
+    }
+    case 'LOADED': {
+      return store.state.user;
+    }
+  }
 }
--- website/client/store/state.js
+++ website/client/store/state.js
@@ -6,7 +6,15 @@ const state = {
   user: null,
   tasks: null, // user tasks
   party: null, // user party
-  userGuilds: null,
+  // keep track of which resources are LOADED, LOADING, NOT_LOADED
+  // to avoid multiple HTTP requests when a resource is requested at the same
+  // time in two places
+  loadingStatus: {
+    user: 'NOT_LOADED',
+    tasks: 'NOT_LOADED',
+    party: 'NOT_LOADED',
+    // TODO guilds, challeges: array of {id, status}?
+  },
   // content data, frozen to prevent Vue from modifying it since it's static and never changes
   // TODO apply freezing to the entire codebase (the server) and not only to the client side?
   // NOTE this takes about 10-15ms on a fast computer
