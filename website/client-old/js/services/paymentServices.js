b0ed9013096e82fa615dd22b0196bb4427f3bbf4
Keith Holliday <krh121791@gmail.com>
2017-02-22 10:27:19 -0700

    Prevented party tasks from being displayed twice

--- website/client-old/js/services/paymentServices.js
+++ website/client-old/js/services/paymentServices.js
@@ -25,8 +25,7 @@ function($rootScope, User, $http, Content) {
     var amount = 500;// 500 = $5
     if (sub) amount = sub.price * 100;
     if (data.gift && data.gift.type=='gems') amount = data.gift.gems.amount / 4 * 100;
-    if (data.group) amount = (sub.price + 3 * (data.group.memberCount - 1)) * 100
-    console.log(data)
+    if (data.group) amount = (sub.price + 3 * (data.group.memberCount - 1)) * 100;
 
     StripeCheckout.open({
       key: window.env.STRIPE_PUB_KEY,
