560e041ed9465d899a2b8d3f7d603906511d8229
Gerardo Saca <gsacavdm@hotmail.com>
2017-02-22 21:12:18 -0800

    Fix faq.jade and add workaround for faq.js

--- website/common/script/content/faq.js
+++ website/common/script/content/faq.js
@@ -1,27 +1,30 @@
 import t from './translation';
-import nconf from 'nconf';
 
-const NUMBER_OF_QUESTIONS = 12;
+let faqFunction = function(replacements)
+{
+  const NUMBER_OF_QUESTIONS = 12;
 
-let faq = {
-  questions: [],
-  stillNeedHelp: {
-    ios: t('iosFaqStillNeedHelp'),
-    web: t('webFaqStillNeedHelp'),
-  },
-};
-
-for (let i = 0; i <= NUMBER_OF_QUESTIONS; i++) {
-  let question = {
-    question: t(`faqQuestion${i}`),
-    ios: t(`iosFaqAnswer${i}`),
-    web: t(`webFaqAnswer${i}`, {
-      techAssistanceEmail: nconf.get('EMAILS:TECH_ASSISTANCE_EMAIL'),
-      wikiTechAssistanceEmail: `mailto:${nconf.get('EMAILS:TECH_ASSISTANCE_EMAIL')}`,
-    }),
+  let faq = {
+    questions: [],
+    stillNeedHelp: {
+      ios: t('iosFaqStillNeedHelp'),
+      web: t('webFaqStillNeedHelp'),
+    },
   };
 
-  faq.questions.push(question);
+  for (let i = 0; i <= NUMBER_OF_QUESTIONS; i++) {
+    let question = {
+      question: t(`faqQuestion${i}`),
+      ios: t(`iosFaqAnswer${i}`),
+      web: t(`webFaqAnswer${i}`, { 
+        //TODO: Need to pull these values from nconf
+        techAssistanceEmail: 'techassistance@habitica.com', 
+        wikiTechAssistanceEmail: 'mailto:techassistance@habitica.com',
+      }),
+    };
+
+    faq.questions.push(question);
+  }
 }
 
-module.exports = faq;
+module.exports = faqFunction;
