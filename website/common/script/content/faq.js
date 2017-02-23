927d173074863f92353a2ee495d647e926432473
Gerardo Saca <gsacavdm@hotmail.com>
2017-02-22 23:30:09 -0800

    Fixing accidental checking for faq.js

--- website/common/script/content/faq.js
+++ website/common/script/content/faq.js
@@ -1,30 +1,27 @@
 import t from './translation';
 
-let faqFunction = function(replacements)
-{
-  const NUMBER_OF_QUESTIONS = 12;
+const NUMBER_OF_QUESTIONS = 12;
 
-  let faq = {
-    questions: [],
-    stillNeedHelp: {
-      ios: t('iosFaqStillNeedHelp'),
-      web: t('webFaqStillNeedHelp'),
-    },
+let faq = {
+  questions: [],
+  stillNeedHelp: {
+    ios: t('iosFaqStillNeedHelp'),
+    web: t('webFaqStillNeedHelp'),
+  },
+};
+
+for (let i = 0; i <= NUMBER_OF_QUESTIONS; i++) {
+  let question = {
+    question: t(`faqQuestion${i}`),
+    ios: t(`iosFaqAnswer${i}`),
+    web: t(`webFaqAnswer${i}`, {
+      //  TODO: Need to pull these values from nconf
+      techAssistanceEmail: 'techassistance@habitica.com',
+      wikiTechAssistanceEmail: 'mailto:techassistance@habitica.com',
+    }),
   };
 
-  for (let i = 0; i <= NUMBER_OF_QUESTIONS; i++) {
-    let question = {
-      question: t(`faqQuestion${i}`),
-      ios: t(`iosFaqAnswer${i}`),
-      web: t(`webFaqAnswer${i}`, { 
-        //TODO: Need to pull these values from nconf
-        techAssistanceEmail: 'techassistance@habitica.com', 
-        wikiTechAssistanceEmail: 'mailto:techassistance@habitica.com',
-      }),
-    };
-
-    faq.questions.push(question);
-  }
+  faq.questions.push(question);
 }
 
-module.exports = faqFunction;
+module.exports = faq;
