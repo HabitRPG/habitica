3841573e09e54c8d41e02527d53f5ef8f9c65e0d
MathWhiz <raymond.arndorfer@gmail.com>
2017-02-23 20:32:10 -0500

    fix lint

--- test/api/v3/integration/user/auth/POST-register_local.test.js
+++ test/api/v3/integration/user/auth/POST-register_local.test.js
@@ -5,7 +5,7 @@ import {
   createAndPopulateGroup,
   getProperty,
 } from '../../../../../helpers/api-integration/v3';
-import { ApiUser } from '../../../../../helpers/api-integration/api-classes'
+import { ApiUser } from '../../../../../helpers/api-integration/api-classes';
 import { v4 as generateRandomUserName } from 'uuid';
 import { each } from 'lodash';
 import { encrypt } from '../../../../../../website/server/libs/encryption';
