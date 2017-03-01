204e6a8b7c4afae8ed38ff144f626b5929af0521
Keith Holliday <krh121791@gmail.com>
2017-02-28 22:49:13 -0700

    Show task notes modal on click

--- website/client-old/js/directives/task.directive.js
+++ website/client-old/js/directives/task.directive.js
@@ -7,9 +7,10 @@
 
   task.$inject = [
     'Shared',
+    '$modal',
   ];
 
-  function task(Shared) {
+  function task(Shared, $modal) {
     return {
       restrict: 'E',
       templateUrl: 'templates/task.html',
@@ -18,6 +19,22 @@
         $scope.getClasses = function (task, user, list, main) {
           return Shared.taskClasses(task, user.filters, user.preferences.dayStart, user.lastCron, list.showCompleted, main);
         }
+
+        $scope.showNoteDetails = function (task) {
+          task.popoverOpen = false;
+
+          $modal.open({
+            templateUrl: 'modals/task-extra-notes.html',
+            controller: function ($scope, task) {
+              $scope.task = task;
+            },
+            resolve: {
+              task: function() {
+                return task;
+              }
+            }
+          })
+        };
       }
     }
   }
