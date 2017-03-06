b0de7b98102117701afd7ea0ca74372dbbec1e25
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-03-06 19:05:39 +0100

    typo

--- website/client/components/social/tavern.vue
+++ website/client/components/social/tavern.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
   data () {
-    // @TODO: Abstract to Store
+    // TODO: Abstract to Store
     let messages = [
       {
         from: 'Paglias',
@@ -26,7 +26,7 @@ export default {
 
   methods: {
     sendChat: function sendChat () {
-      // @TODO: This will be default values based on the conversation and current user
+      // TODO: This will be default values based on the conversation and current user
       let messageToSend = {
         from: 'TheHollidayInn',
         fromUserId: 3211,
