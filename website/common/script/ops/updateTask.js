1082359f2c46095b1cfddcd3a1ab0dfbbaf3035d
Keith Holliday <krh121791@yahoo.com>
2017-03-08 16:48:30 -0700

    Repeatables fixes (#8538)
    * Prevented watch functions from being called when task._edit is removed
    
    * Added start date support on the UI task summary
    
    * Fixed setting of monthly and calculations
    
    * Fixed linting issues
    
    * Added check for existence
    
    * Added existence check
    
    * Ensured correct start date is used on update
    
    * Hid repeat options from anything not a daily
    
    * Added missing locales
    
    * Moved repeatables out of advance options

--- website/common/script/ops/updateTask.js
+++ website/common/script/ops/updateTask.js
@@ -22,5 +22,14 @@ module.exports = function updateTask (task, req = {}) {
 
   merge(task, omit(body, ['_id', 'id', 'type', 'reminders', 'checklist', 'tags']));
 
+  // Ensure arrays are emptied
+  if (body.daysOfMonth && body.daysOfMonth.length === 0) {
+    task.daysOfMonth = [];
+  }
+
+  if (body.weeksOfMonth && body.weeksOfMonth.length === 0) {
+    task.weeksOfMonth = [];
+  }
+
   return [task];
 };
