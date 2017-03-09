9521c10640dc04251a95a645cbf7d28f827d6c86
Keith Holliday <krh121791@gmail.com>
2017-03-09 14:20:09 -0700

    Added comments

--- migrations/groups/add-unlimited-subscription.js
+++ migrations/groups/add-unlimited-subscription.js
@@ -1,3 +1,11 @@
+var migrationName = 'AddUnlimitedSubscription';
+var authorName = 'TheHollidayInn'; // in case script author needs to know when their ...
+var authorUuid = ''; //... own data is done
+
+/*
+ * This migrations will add a free subscription to a specified group
+ */
+
 import { model as Group } from '../../website/server/models/group';
 
 // @TODO: this should probably be a GroupManager library method
@@ -16,7 +24,7 @@ async function addUnlimitedSubscription (groupId) {
     return group.save();
 };
 
-module.exports = async function addUnlimitedSubscriptionCretor () {
+module.exports = async function addUnlimitedSubscriptionCreator () {
     let groupId = process.argv[2];
 
     if (!groupId) throw Error('Group ID is required');
--- migrations/groups/update-groups-with-group-plans.js
+++ migrations/groups/update-groups-with-group-plans.js
@@ -1,3 +1,12 @@
+var migrationName = 'ResyncGroupPlanMembers';
+var authorName = 'TheHollidayInn'; // in case script author needs to know when their ...
+var authorUuid = ''; //... own data is done
+
+/*
+ * This migrations will iterate through all groups with a group plan a subscription and resync the free
+ * subscription to all members
+ */
+
 import Bluebird from 'bluebird';
 
 import { model as Group } from '../../website/server/models/group';
