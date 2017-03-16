cc9bca5f636c7cafcce1eb849442eaa876520047
Lulock <lulock@users.noreply.github.com>
2017-03-16 22:38:24 +0400

    invalid class change throws error (#8496) (#8531)
    * invalid class-change throws error
    
    minor fixes
    
    indentation fixes
    
    indentation fixes
    
    indentation fixes
    
    * minor fixes

--- website/common/script/ops/changeClass.js
+++ website/common/script/ops/changeClass.js
@@ -7,15 +7,39 @@ import splitWhitespace from '../libs/splitWhitespace';
 import { capByLevel } from '../statHelpers';
 import {
   NotAuthorized,
+  BadRequest,
 } from '../libs/errors';
+
+function resetClass (user, req = {}) {
+  if (user.preferences.disableClasses) {
+    user.preferences.disableClasses = false;
+    user.preferences.autoAllocate = false;
+  } else {
+    if (user.balance < 0.75) throw new NotAuthorized(i18n.t('notEnoughGems', req.language));
+    user.balance -= 0.75;
+  }
+
+  user.stats.str = 0;
+  user.stats.con = 0;
+  user.stats.per = 0;
+  user.stats.int = 0;
+  user.stats.points = capByLevel(user.stats.lvl);
+  user.flags.classSelected = false;
+}
 
 module.exports = function changeClass (user, req = {}, analytics) {
   let klass = get(req, 'query.class');
-
   // user.flags.classSelected is set to false after the user paid the 3 gems
   if (user.stats.lvl < 10) {
     throw new NotAuthorized(i18n.t('lvl10ChangeClass', req.language));
-  } else if (!user.flags.classSelected && (klass === 'warrior' || klass === 'rogue' || klass === 'wizard' || klass === 'healer')) {
+  } else if (!klass) {
+    // if no class is specified, reset points and set user.flags.classSelected to false. User will have paid 3 gems and will be prompted to select class.
+    resetClass(user, req);
+  } else if (klass === 'warrior' || klass === 'rogue' || klass === 'wizard' || klass === 'healer') {
+    if (user.flags.classSelected) {
+      resetClass(user, req);
+    }
+
     user.stats.class = klass;
     user.flags.classSelected = true;
 
@@ -56,22 +80,9 @@ module.exports = function changeClass (user, req = {}, analytics) {
       });
     }
   } else {
-    if (user.preferences.disableClasses) {
-      user.preferences.disableClasses = false;
-      user.preferences.autoAllocate = false;
-    } else {
-      if (user.balance < 0.75) throw new NotAuthorized(i18n.t('notEnoughGems', req.language));
-      user.balance -= 0.75;
-    }
-
-    user.stats.str = 0;
-    user.stats.con = 0;
-    user.stats.per = 0;
-    user.stats.int = 0;
-    user.stats.points = capByLevel(user.stats.lvl);
-    user.flags.classSelected = false;
+    // if invalid class is specified, throw an error.
+    throw new BadRequest(i18n.t('invalidClass', req.language));
   }
-
   return [
     pick(user, splitWhitespace('stats flags items preferences')),
   ];
