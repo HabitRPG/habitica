41e367b9d40441c85dbbf75b9faa939c3837d30c
Sabe Jones <sabrecat@gmail.com>
2017-03-18 12:01:41 -0500

    Spring Fling 2017 (#8579)
    * feat(event): Spring Fling 2017
    
    * fix(sprites): adjustments
    Also enables pastel hair/skin purchases

--- website/server/controllers/api-v3/shops.js
+++ website/server/controllers/api-v3/shops.js
@@ -107,8 +107,8 @@ api.getSeasonalShopItems = {
     let resObject = {
       identifier: 'seasonalShop',
       text: res.t('seasonalShop'),
-      notes: res.t('seasonalShopClosedText'),
-      imageName: 'seasonalshop_closed',
+      notes: res.t('seasonalShopText'),
+      imageName: 'seasonalshop_open',
       categories: shops.getSeasonalShopCategories(user, req.language),
     };
 
