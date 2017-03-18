12cefe4e9f0a930d76900a11e76d2396122136d0
Sabe Jones <sabrecat@gmail.com>
2017-03-18 12:01:41 -0500

    Spring Fling 2017 (#8579)
    * feat(event): Spring Fling 2017
    
    * fix(sprites): adjustments
    Also enables pastel hair/skin purchases

--- website/common/script/libs/shops.js
+++ website/common/script/libs/shops.js
@@ -220,6 +220,18 @@ shops.getTimeTravelersCategories = function getTimeTravelersCategories (user, la
 // };
 shops.getSeasonalShopCategories = function getSeasonalShopCategories (user, language) {
   const AVAILABLE_SETS = {
+    springHealer: i18n.t('lovingPupSet', language),
+    springMage: i18n.t('magicMouseSet', language),
+    springRogue: i18n.t('stealthyKittySet', language),
+    springWarrior: i18n.t('mightyBunnySet', language),
+    spring2015Healer: i18n.t('comfortingKittySet', language),
+    spring2015Mage: i18n.t('magicianBunnySet', language),
+    spring2015Rogue: i18n.t('sneakySqueakerSet', language),
+    spring2015Warrior: i18n.t('bewareDogSet', language),
+    spring2016Healer: i18n.t('springingBunnySet', language),
+    spring2016Mage: i18n.t('grandMalkinSet', language),
+    spring2016Rogue: i18n.t('cleverDogSet', language),
+    spring2016Warrior: i18n.t('braveMouseSet', language),
   };
 
   const AVAILABLE_SPELLS = [
