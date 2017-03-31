1629dc7979878b27e729d8a26d64011de9b71660
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-03-31 19:44:11 +0200

    add secondary menu component and style it

--- website/client/components/inventory/index.vue
+++ website/client/components/inventory/index.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
 .row
   .col-12
-    nav.nav
+    secondary-menu
       router-link.nav-link(:to="{name: 'inventory'}", exact) {{ $t('inventory') }}
       router-link.nav-link(:to="{name: 'equipment'}") {{ $t('equipment') }}
       router-link.nav-link(:to="{name: 'stable'}") {{ $t('stable') }}
   .col-12
     router-view
 </template>
+
+<script>
+import SecondaryMenu from 'client/components/secondaryMenu';
+
+export default {
+  components: {
+    SecondaryMenu,
+  },
+};
+</script>
---
+++ website/client/components/secondaryMenu.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+nav.nav.d-flex.justify-content-center.secondary-menu
+  slot
+</template>
+
+<style lang="scss" scoped>
+@import '~client/assets/scss/colors.scss';
+
+.secondary-menu {
+  background: $gray-600;
+  margin-left: -15px;
+  margin-right: -15px;
+}
+
+.nav-link {
+  font-size: 16px;
+  line-height: 1.5;
+  padding: 16px 24px;
+  font-weight: bold;
+  color: $gray-50;
+
+  &.active {
+    color: $purple-200;
+    box-shadow: 0px -4px 0px $purple-300 inset;
+  }
+
+  &:hover {
+    background: $gray-500;
+  }
+}
+</style>
