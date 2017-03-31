2a58d30df5e31825bc26b82b410e971dd2723e91
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-03-31 21:52:01 +0200

    misc css, fixes for secondary menu

--- website/client/components/inventory/index.vue
+++ website/client/components/inventory/index.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
 .row
-  .col-12
-    secondary-menu
-      router-link.nav-link(:to="{name: 'inventory'}", exact) {{ $t('inventory') }}
-      router-link.nav-link(:to="{name: 'equipment'}") {{ $t('equipment') }}
-      router-link.nav-link(:to="{name: 'stable'}") {{ $t('stable') }}
+  secondary-menu.col-12
+    router-link.nav-link(:to="{name: 'inventory'}", exact) {{ $t('inventory') }}
+    router-link.nav-link(:to="{name: 'equipment'}") {{ $t('equipment') }}
+    router-link.nav-link(:to="{name: 'stable'}") {{ $t('stable') }}
   .col-12
     router-view
 </template>
--- website/client/components/social/index.vue
+++ website/client/components/social/index.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
 .row
-  .col-12
-    secondary-menu
-      router-link.nav-link(:to="{name: 'tavern'}", exact) {{ $t('tavern') }}
-      router-link.nav-link(:to="{name: 'inbox'}") {{ $t('inbox') }}
-      router-link.nav-link(:to="{name: 'guildsDiscovery'}") {{ $t('guilds') }}
+  secondary-menu.col-12
+    router-link.nav-link(:to="{name: 'tavern'}", exact) {{ $t('tavern') }}
+    router-link.nav-link(:to="{name: 'inbox'}") {{ $t('inbox') }}
+    router-link.nav-link(:to="{name: 'guildsDiscovery'}") {{ $t('guilds') }}
   .col-12
     router-view
 </template>
