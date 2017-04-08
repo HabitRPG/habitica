8589f106094208a2e83b1d01f4656318d740618d
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-04-08 11:30:13 +0200

    equipment: fix vue binding. version 2

--- website/client/store/actions/common.js
+++ website/client/store/actions/common.js
@@ -2,5 +2,4 @@ import equipOp from 'common/script/ops/equip';
 
 export function equip (store, params) {
   equipOp(store.state.user.data, {params});
-  store.state.user.data.items.gear.equipped = Object.assign({}, store.state.user.data.items.gear.equipped);
 }
