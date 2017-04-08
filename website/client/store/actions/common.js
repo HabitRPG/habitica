23f4e84ab83c2a7ee4aef551a48a7763f35fdaa7
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-04-08 11:25:02 +0200

    equipment: correctly bind new properties on items.gear.equipped

--- website/client/store/actions/common.js
+++ website/client/store/actions/common.js
@@ -2,4 +2,5 @@ import equipOp from 'common/script/ops/equip';
 
 export function equip (store, params) {
   equipOp(store.state.user.data, {params});
+  store.state.user.data.items.gear.equipped = Object.assign({}, store.state.user.data.items.gear.equipped);
 }
