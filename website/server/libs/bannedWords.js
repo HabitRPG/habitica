674da92ea448c7e89112e5454bc060d6ecc89768
Keith Holliday <krh121791@gmail.com>
2017-04-19 11:40:38 -0600

    Added warning to banned words

--- website/server/libs/bannedWords.js
+++ website/server/libs/bannedWords.js
@@ -1,3 +1,28 @@
+// CONTENT WARNING:
+// This file contains slurs, swear words, religious oaths, and words related to addictive substance and adult topics.
+// Do not read this file if you do not want to be exposed to those words.
+// The words are stored in an array called `bannedWords` which is then exported with `module.exports = bannedWords;`
+// This file does not contain any other code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let bannedWords = ['TEST_PLACEHOLDER_SWEAR_WORD_HERE'];
 
 module.exports = bannedWords;
