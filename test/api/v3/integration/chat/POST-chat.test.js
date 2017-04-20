e21e47d5cadf8dd287ca97dafe93e8f3df946aeb
TheHollidayInn <krh121791@gmail.com>
2017-04-20 08:30:02 -0600

    Fixed linting issues

--- test/api/v3/integration/chat/POST-chat.test.js
+++ test/api/v3/integration/chat/POST-chat.test.js
@@ -180,27 +180,29 @@ describe('POST /chat', () => {
     expect(memberWithNotification.newMessages[`${group._id}`]).to.exist;
   });
 
-  it('Returns an error when the user has been posting too many messages', async () => {
-    // Post as many messages are needed to reach the spam limit
-    for (let i = 0; i < SPAM_MESSAGE_LIMIT; i++) {
-      let result = await additionalMember.post(`/groups/${TAVERN_ID}/chat`, { message: testMessage }); // eslint-disable-line babel/no-await-in-loop
-      expect(result.message.id).to.exist;
-    }
+  context('Spam prevention', () => {
+    it('Returns an error when the user has been posting too many messages', async () => {
+      // Post as many messages are needed to reach the spam limit
+      for (let i = 0; i < SPAM_MESSAGE_LIMIT; i++) {
+        let result = await additionalMember.post(`/groups/${TAVERN_ID}/chat`, { message: testMessage }); // eslint-disable-line no-await-in-loop
+        expect(result.message.id).to.exist;
+      }
 
-    await expect(additionalMember.post(`/groups/${TAVERN_ID}/chat`, { message: testMessage })).to.eventually.be.rejected.and.eql({
-      code: 401,
-      error: 'NotAuthorized',
-      message: t('messageGroupChatSpam'),
+      await expect(additionalMember.post(`/groups/${TAVERN_ID}/chat`, { message: testMessage })).to.eventually.be.rejected.and.eql({
+        code: 401,
+        error: 'NotAuthorized',
+        message: t('messageGroupChatSpam'),
+      });
     });
-  });
 
-  it('contributor should not receive spam alert', async () => {
-    let userSocialite = await member.update({'contributor.level': SPAM_MIN_EXEMPT_CONTRIB_LEVEL, 'flags.chatRevoked': false});
+    it('contributor should not receive spam alert', async () => {
+      let userSocialite = await member.update({'contributor.level': SPAM_MIN_EXEMPT_CONTRIB_LEVEL, 'flags.chatRevoked': false});
 
-    // Post 1 more message than the spam limit to ensure they do not reach the limit
-    for (let i = 0; i < SPAM_MESSAGE_LIMIT + 1; i++) {
-      let result = await userSocialite.post(`/groups/${TAVERN_ID}/chat`, { message: testMessage }); // eslint-disable-line babel/no-await-in-loop
-      expect(result.message.id).to.exist;
-    }
+      // Post 1 more message than the spam limit to ensure they do not reach the limit
+      for (let i = 0; i < SPAM_MESSAGE_LIMIT + 1; i++) {
+        let result = await userSocialite.post(`/groups/${TAVERN_ID}/chat`, { message: testMessage }); // eslint-disable-line no-await-in-loop
+        expect(result.message.id).to.exist;
+      }
+    });
   });
 });
