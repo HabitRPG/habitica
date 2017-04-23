ad61fc4882ea23073b6642ff9365b92b8c624d31
Alyssa Batula <batulaa@drexel.edu>
2017-04-23 19:38:08 -0400

    Added warning to bannedSlurs file

--- website/server/bannedSlurs.js
+++ website/server/bannedSlurs.js
@@ -1,3 +1,56 @@
+/* eslint-disable no-multiple-empty-lines */
+
+// CONTENT WARNING:
+// This file contains slurs, swear words, religious oaths, and words related to addictive substance and adult topics.
+// Do not read this file if you do not want to be exposed to those words.
+// The words are stored in an array called `bannedSlurs` which is then exported with `module.exports = bannedSlurs;`
+// This file does not contain any other code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let bannedSlurs = ['TEST_PLACEHOLDER_SLUR_WORD_HERE'];
 
 module.exports = bannedSlurs;
