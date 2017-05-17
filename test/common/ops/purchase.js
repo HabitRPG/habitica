8555e4bea807bfff3ca033d73e1bc239e66f0390
SabreCat <sabrecat@gmail.com>
2017-05-17 19:37:19 +0000

    test(bundles): shop and purchase tests

--- test/common/ops/purchase.js
+++ test/common/ops/purchase.js
@@ -9,6 +9,8 @@ import i18n from '../../../website/common/script/i18n';
 import {
   generateUser,
 } from '../../helpers/common.helper';
+import forEach from 'lodash/forEach';
+import moment from 'moment';
 
 describe('shared.ops.purchase', () => {
   const SEASONAL_FOOD = 'Meat';
@@ -199,6 +201,25 @@ describe('shared.ops.purchase', () => {
       purchase(user, {params: {type, key}});
 
       expect(user.items.gear.owned[key]).to.be.true;
+    });
+
+    it.only('purchases quest bundles', () => {
+      let clock = sandbox.useFakeTimers(+moment('2017-05-20'));
+      let type = 'bundles';
+      let key = 'featheredFriends';
+      let questList = [
+        'falcon',
+        'harpy',
+        'owl',
+      ];
+
+      purchase(user, {params: {type, key}});
+
+      forEach(questList, (bundledKey) => {
+        expect(user.items.quests[bundledKey]).to.equal(1);
+      });
+
+      clock.restore();
     });
   });
 });
