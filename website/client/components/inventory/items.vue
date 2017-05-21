f512bb696eceaa107dbef895c102acfd235fd3da
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-05-21 19:12:30 +0200

    show food, eggs and potions

--- website/client/components/inventory/items.vue
+++ website/client/components/inventory/items.vue
@@ -9,13 +9,13 @@
       h3(v-once) {{ $t('equipmentType') }}
       .form-group
         .form-check(
-          v-for="group in itemsGroups",
-          :key="group",
+          v-for="group in groups",
+          :key="group.key",
         )
           label.custom-control.custom-checkbox
-            input.custom-control-input(type="checkbox", v-model="viewOptions[group].selected")
+            input.custom-control-input(type="checkbox", v-model="group.selected")
             span.custom-control-indicator
-            span.custom-control-description(v-once) {{ $t(group) }}
+            span.custom-control-description(v-once) {{ $t(group.key) }}
   .col-10.standard-page
     .clearfix
       h1.float-left.mb-0.page-header(v-once) {{ $t('items') }}
@@ -24,28 +24,33 @@
           b-dropdown-item(@click="sortBy = 'quantity'", :active="sortBy === 'quantity'") {{ $t('quantity') }}
           b-dropdown-item(@click="sortBy = 'AZ'", :active="sortBy === 'AZ'") {{ $t('sortAZ') }}
     div(
-      v-for="group in itemsGroups",
-      v-if="viewOptions[group].selected",
-      :key="group",
+      v-for="group in groups",
+      v-if="group.selected",
+      :key="group.key",
     )
       h2
-       | {{ $t(group) }}
+       | {{ $t(group.key) }}
        |
-       span.badge.badge-pill.badge-default {{items[group].length}}
+       span.badge.badge-pill.badge-default {{group.quantity}}
 
       .items
         item(
-          v-for="({data: item, quantity}, index) in items[group]",
-          v-if="viewOptions[group].open || index < itemsPerLine",
+          v-for="({data: item, quantity}, index) in items[group.key]",
+          v-if="group.open || index < itemsPerLine",
           :item="item",
           :key="item.key",
+          :itemContentClass="`Pet_${group.classPrefix}_${item.key}`"
+          :starVisible="true",
+          :selected="true",
         )
-      div(v-if="items[group].length === 0")
-        p(v-once) {{ $t('noGearItemsOfType', { type: $t(group) }) }}
+          template(slot="popoverContent", scope="ctx") {{ ctx.item.text() }}
+
+      div(v-if="items[group.key].length === 0")
+        p(v-once) {{ $t('noGearItemsOfType', { type: $t(group.key) }) }}
       a.btn.btn-show-more(
-        v-if="items[group].length > itemsPerLine",
-        @click="viewOptions[group].open = !viewOptions[group].open"
-      ) {{ viewOptions[group].open ? $t('showLessGearItems', { type: $t(group) }) : $t('showAllGearItems', { type: $t(group), items: items[group].length }) }}
+        v-if="items[group.key].length > itemsPerLine",
+        @click="group.open = !group.open"
+      ) {{ group.open ? $t('showLessGearItems', { type: $t(group.key) }) : $t('showAllGearItems', { type: $t(group.key), items: items[group.key].length }) }}
 
 </template>
 
@@ -61,6 +66,20 @@ import bDropdown from 'bootstrap-vue/lib/components/dropdown';
 import bDropdownItem from 'bootstrap-vue/lib/components/dropdown-item';
 import Item from 'client/components/inventory/item';
 
+const groups = [
+  ['eggs', 'Egg'],
+  ['hatchingPotions', 'HatchingPotion'],
+  ['food', 'Food'],
+].map(([group, classPrefix]) => {
+  return {
+    key: group,
+    quantity: 0,
+    selected: true,
+    open: false,
+    classPrefix,
+  };
+});
+
 export default {
   components: {
     Item,
@@ -72,8 +91,7 @@ export default {
       itemsPerLine: 9,
       searchText: null,
       searchTextThrottled: null,
-      viewOptions: {},
-      groups: ['eggs', 'hatchingPotions', 'food'],
+      groups,
       sortBy: 'quantity', // or 'AZ'
     };
   },
@@ -87,43 +105,34 @@ export default {
       content: 'content',
       user: 'user.data',
     }),
-    // TODO copied from Equipment, not used now but will in future once
-    // not all items groups will be shown for all users (the special ones)
-    itemsGroups () {
-      return this.groups.map((group) => {
-        this.$set(this.viewOptions, group, {
-          selected: true,
-          open: false,
-        });
-
-        return group;
-      });
-    },
     items () {
       const searchText = this.searchTextThrottled;
       const itemsByType = {};
 
-      this.itemsGroups.forEach(group => {
-        let itemsArray = itemsByType[group] = [];
-        const contentItems = this.content[group];
+      this.groups.forEach(group => {
+        const groupKey = group.key;
+        let itemsArray = itemsByType[groupKey] = [];
+        const contentItems = this.content[groupKey];
 
-        each(this.user.items[group], (quantity, itemKey) => {
-          if (quantity > 0) {
+        each(this.user.items[groupKey], (itemQuantity, itemKey) => {
+          if (itemQuantity > 0) {
             const item = contentItems[itemKey];
 
             const isSearched = !searchText || item.text().toLowerCase().indexOf(searchText) !== -1;
             if (isSearched) {
               itemsArray.push({
                 data: item,
-                quantity,
+                quantity: itemQuantity,
               });
+
+              group.quantity += itemQuantity;
             }
           }
         });
 
         itemsArray.sort((a, b) => {
           if (this.sortBy === 'quantity') {
-            return b.quantity - a.quantity;
+            return b.itemQuantity - a.itemQuantity;
           } else { // AZ
             return a.data.text().localeCompare(b.data.text());
           }
