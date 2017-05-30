841f669e4b618a27ac5143593dc9e94962d97a3d
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-05-30 17:38:52 +0200

    fix webpack fonts

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -90,6 +90,14 @@ const baseConfig = {
           name: utils.assetsPath('img/[name].[hash:7].[ext]'),
         },
       },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+        },
+      },
     ],
   },
 };
