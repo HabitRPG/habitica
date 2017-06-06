4921b2f23b2636f8f529ff79d35e19ec7d6825bc
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-06-06 10:48:59 -0700

    implement icons loading without iconmoo

--- website/client/app.vue
+++ website/client/app.vue
@@ -2,7 +2,6 @@
 
 <template lang="pug">
 #app
-  div(v-html="svg", v-once)
   app-menu
   .container-fluid
     app-header
@@ -12,16 +11,12 @@
 <script>
 import AppMenu from './components/appMenu';
 import AppHeader from './components/appHeader';
-import svg from '!raw-loader!assets/svg/sheet.svg';
 
 export default {
   name: 'app',
   components: {
     AppMenu,
     AppHeader,
-  },
-  data () {
-    return {svg};
   },
 };
 </script>
--- website/client/components/ui/icon.vue
+++ website/client/components/ui/icon.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
-svg.svg-icon
-  use(:xlink:href="`#icon-${name}`")
+.svg-icon(v-html="icons[name]")
 </template>
 
 <style lang="scss">
@@ -16,7 +15,25 @@ svg.svg-icon
 </style>
 
 <script>
+// Each icon entry is an array of [name, path (relative to website/client/assets, without .svg)]
+const iconsDefs = [
+  ['gem', 'header/gem'],
+  ['gold', 'header/gold'],
+  ['notifications', 'header/notifications'],
+  ['user', 'header/user'],
+];
+
+const icons = {};
+iconsDefs.map(([icn, path]) => {
+  icons[icn] = require(`!svg-inline-loader!assets/${path}.svg`); // eslint-disable-line global-require
+});
+
 export default {
   props: ['name'],
+  data () {
+    return Object.freeze({
+      icons,
+    });
+  },
 };
 </script>
