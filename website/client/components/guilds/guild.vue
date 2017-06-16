957c9e69d4994079cb42fb8efe95b5fb5312fb0d
Keith Holliday <krh121791@gmail.com>
2017-06-16 12:01:36 -0600

    Began adding toggle

--- website/client/components/guilds/guild.vue
+++ website/client/components/guilds/guild.vue
@@ -76,49 +76,66 @@
           button.btn.btn-primary(v-once, v-if='!isLeader') {{$t('messageGuildLeader')}}
         .button-container
           button.btn.btn-primary(v-once, v-if='isMember && !isParty') {{$t('donateGems')}}
-    div
+
+    .section-header
       .row
         .col-8
           h3(v-once) {{ $t('questDetailsTitle') }}
         .col-4
-      .row.no-quest-section(v-if='isParty && !onQuest')
-        .col-12.text-center
-          .svg-icon(v-html="icons.questIcon")
-          h4(v-once) {{ $t('yourNotOnQuest') }}
-          p(v-once) {{ $t('questDescription') }}
-          button.btn.btn-secondary(v-once) {{ $t('startAQuest') }}
-      .row.quest-active-section
-        .col-12.text-center
-          .svg-icon(v-html="icons.questIcon")
-          p {{questData}}
-          h4(v-once) {{ $t('yourNotOnQuest') }}
-          p(v-once) {{ $t('questDescription') }}
-          <!-- .quest-box(style='background-image: {{this.questBackground}}') -->
-          .quest-box.svg-icon(v-html="icons.questBackground")
-          .boss-info
-            .row
-              .col-6
-                h4.float-left Boss Name
-              .col-6
-                span.float-right Participants
-            .row
-              .col-12.boss-health-bar
-            .row.boss-details
+          .toggle-up(@click="sections.quest = !sections.quest", v-if="sections.quest")
+            .svg-icon(v-html="icons.upIcon")
+          .toggle-down(@click="sections.quest = !sections.quest", v-if="!sections.quest")
+            .svg-icon(v-html="icons.downIcon")
+      .section(v-if="sections.quest")
+        .row.no-quest-section(v-if='isParty && !onQuest')
+          .col-12.text-center
+            .svg-icon(v-html="icons.questIcon")
+            h4(v-once) {{ $t('yourNotOnQuest') }}
+            p(v-once) {{ $t('questDescription') }}
+            button.btn.btn-secondary(v-once) {{ $t('startAQuest') }}
+        .row.quest-active-section
+          .col-12.text-center
+            .svg-icon(v-html="icons.questIcon")
+            p {{questData}}
+            h4(v-once) {{ $t('yourNotOnQuest') }}
+            p(v-once) {{ $t('questDescription') }}
+            <!-- .quest-box(style='background-image: {{this.questBackground}}') -->
+            .quest-box.svg-icon(v-html="icons.questBackground")
+            .boss-info
+              .row
                 .col-6
-                  span.float-left 999/1000
+                  h4.float-left Boss Name
                 .col-6
-                  span.float-right 30 pending damage
+                  span.float-right Participants
+              .row
+                .col-12.boss-health-bar
+              .row.boss-details
+                  .col-6
+                    span.float-left 999/1000
+                  .col-6
+                    span.float-right 30 pending damage
 
+    .section-header
+      .row
+        .col-8
+          h3(v-once) {{ $t('description') }}
+        .col-4
+          button(@click="sections.description = !sections.description") Toggle
+      .section(v-if="sections.description")
+        p(v-once) {{ guild.description }}
+        p Life hacks are tricks, shortcuts, or methods that help increase productivity, efficiency, health, and so on. Generally, they get you to a better state of life. Life hacking is the process of utilizing and implementing these secrets. And, in this guild, we want to help everyone discover these improved ways of doing things.
 
-    div
-      h3(v-once) {{ $t('description') }}
-      p(v-once) {{ guild.description }}
-      p Life hacks are tricks, shortcuts, or methods that help increase productivity, efficiency, health, and so on. Generally, they get you to a better state of life. Life hacking is the process of utilizing and implementing these secrets. And, in this guild, we want to help everyone discover these improved ways of doing things.
-    div
-      h3(v-once) {{$t('guildInformation')}}
-      h4 Welcome
-      p Below are some resources that some members might find useful. Consider checking them out before posting any questions, as they just might help answer some of them! Feel free to share your life hacks in the guild chat, or ask any questions that you might have. Please peruse at your leisure, and remember: this guild is meant to help guide you in the right direction. Only you will know what works best for you.
-    div
+    .section-header
+      .row
+        .col-8
+          h3(v-once) {{ $t('guildInformation') }}
+        .col-4
+          button(@click="sections.information = !sections.information") Toggle
+      .section(v-if="sections.information")
+        h4 Welcome
+        p Below are some resources that some members might find useful. Consider checking them out before posting any questions, as they just might help answer some of them! Feel free to share your life hacks in the guild chat, or ask any questions that you might have. Please peruse at your leisure, and remember: this guild is meant to help guide you in the right direction. Only you will know what works best for you.
+
+    .section-header.challenge
       .row
         .col-8.information-header
           h3(v-once)
@@ -126,12 +143,14 @@
           b-tooltip.icon.tooltip-wrapper(:content="$t('privateDescription')")
             .svg-icon(v-html='icons.information')
         .col-4
-      .row.no-quest-section(v-if='!hasChallenges')
-        .col-12.text-center
-          .svg-icon(v-html="icons.challengeIcon")
-          h4(v-once) {{ $t('haveNoChallenges') }}
-          p(v-once) {{ $t('challengeDescription') }}
-          button.btn.btn-secondary(v-once) {{ $t('createChallenge') }}
+          button(@click="sections.challenges = !sections.challenges") Toggle
+      .section(v-if="sections.challenges")
+        .row.no-quest-section(v-if='!hasChallenges')
+          .col-12.text-center
+            .svg-icon(v-html="icons.challengeIcon")
+            h4(v-once) {{ $t('haveNoChallenges') }}
+            p(v-once) {{ $t('challengeDescription') }}
+            button.btn.btn-secondary(v-once) {{ $t('createChallenge') }}
     div.text-center
       button.btn.btn-primary(class='btn-danger', v-if='isMember') {{ $t('leave') }}
 </template>
@@ -294,6 +313,18 @@
       }
     }
   }
+
+  .section-header {
+    border-top: 1px solid #e1e0e3;
+    margin-top: 1em;
+    padding-top: 1em;
+  }
+
+  .section-header.challenge {
+    border-bottom: 1px solid #e1e0e3;
+    margin-bottom: 1em;
+    padding-bottom: 1em;
+  }
 </style>
 
 <script>
@@ -319,6 +350,8 @@ import questIcon from 'assets/svg/quest.svg';
 import challengeIcon from 'assets/svg/challenge.svg';
 import informationIcon from 'assets/svg/information.svg';
 import questBackground from 'assets/svg/quest-background-border.svg';
+import upIcon from 'assets/svg/up.svg';
+import downIcon from 'assets/svg/down.svg';
 
 export default {
   mixins: [groupUtilities],
@@ -347,8 +380,16 @@ export default {
         challengeIcon,
         information: informationIcon,
         questBackground,
+        upIcon,
+        downIcon,
       }),
       questData: {},
+      sections: {
+        quest: true,
+        description: true,
+        information: true,
+        challenges: true,
+      },
     };
   },
   computed: {
