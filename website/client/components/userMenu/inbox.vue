6c6cbc35b678bbbae29e349f4dce38b33185ae73
Keith Holliday <krh121791@gmail.com>
2017-07-06 15:53:41 -0600

    Added initial inbox

---
+++ website/client/components/userMenu/inbox.vue
@@ -0,0 +1,109 @@
+<template lang="pug">
+  b-modal#inbox-modal(title="", :hide-footer="true", size='lg')
+    .header-wrap.container(slot="modal-header")
+      .row
+        .col-4
+          .row
+            .col-2
+              .svg-icon.envelope(v-html="icons.messageIcon")
+            .col-6
+              h2.text-center Messages
+            .col-2
+              button.btn.btn-secondary +
+        .col-8.to-form
+          strong To:
+          b-form-input
+    .row
+      .col-4.sidebar
+        .search-section
+          b-form-input(placeholder='Search')
+        .empty-messages.text-center
+          .svg-icon.envelope(v-html="icons.messageIcon")
+          h4 You don’t have any messages
+          p Send a message to start a conversation!
+        .messages
+      .col-8.messages
+        .new-message-row
+          b-form-input
+          button.btn.btn-secondary Send
+</template>
+
+<style lang="scss" scoped>
+  @import '~client/assets/scss/colors.scss';
+
+  .envelope {
+    color: #c3c0c7 !important;
+  }
+
+  .sidebar {
+    background-color: #f9f9f9;
+    min-height: 600px;
+
+    .search-section {
+      padding: 1em;
+      box-shadow: 1px 1px solid black;
+    }
+  }
+
+  .messages {
+    position: relative;
+    padding-left: 0;
+  }
+
+  .to-form input {
+    width: 60%;
+    display: inline-block;
+    margin-left: 1em;
+  }
+
+  .empty-messages {
+    margin-top: 10em;
+    color: #c3c0c7;
+
+    h4 {
+      color: #c3c0c7;
+      margin-top: 1em;
+    }
+
+    .envelope {
+      width: 30px;
+      margin: 0 auto;
+    }
+  }
+
+  .new-message-row {
+    background-color: #f9f9f9;
+    position: absolute;
+    bottom: 0;
+    height: 88px;
+    width: 100%;
+    padding: 1em;
+
+    input {
+      display: inline-block;
+      width: 85%;
+    }
+  }
+
+</style>
+
+<script>
+import bModal from 'bootstrap-vue/lib/components/modal';
+import bFormInput from 'bootstrap-vue/lib/components/form-input';
+
+import messageIcon from 'assets/svg/message.svg';
+
+export default {
+  components: {
+    bModal,
+    bFormInput,
+  },
+  data () {
+    return {
+      icons: Object.freeze({
+        messageIcon,
+      }),
+    };
+  },
+};
+</script>
