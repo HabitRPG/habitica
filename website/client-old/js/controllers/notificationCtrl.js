4b90d2055cc0ab859be9d49e9edff0809046d5a7
Keith Holliday <krh121791@gmail.com>
2017-07-07 10:50:11 -0600

    Added check to prevent double cron

--- website/client-old/js/controllers/notificationCtrl.js
+++ website/client-old/js/controllers/notificationCtrl.js
@@ -3,6 +3,8 @@
 habitrpg.controller('NotificationCtrl',
   ['$scope', '$rootScope', 'Shared', 'Content', 'User', 'Guide', 'Notification', 'Analytics', 'Achievement', 'Social', 'Tasks',
   function ($scope, $rootScope, Shared, Content, User, Guide, Notification, Analytics, Achievement, Social, Tasks) {
+    var isRunningYesterdailies = false;
+
     $rootScope.$watch('user', function (after, before) {
       runYesterDailies();
     });
@@ -12,13 +14,17 @@ habitrpg.controller('NotificationCtrl',
     });
 
     function runYesterDailies() {
+      if (isRunningYesterdailies ) return;
+
       var userLastCron = moment(User.user.lastCron).local();
       var userDayStart = moment().startOf('day').add({ hours: User.user.preferences.dayStart });
 
       if (!User.user.needsCron) return;
       var dailys = User.user.dailys;
 
-      if (!Boolean(dailys) || dailys.length === 0) return;
+      if (!Boolean(dailys) || dailys.length === 0) return
+
+      isRunningYesterdailies = true;
 
       var yesterDay = moment().subtract('1', 'day').startOf('day').add({ hours: User.user.preferences.dayStart });
       var yesterDailies = [];
@@ -32,6 +38,7 @@ habitrpg.controller('NotificationCtrl',
 
       if (yesterDailies.length === 0) {
         User.runCron();
+        isRunningYesterdailies = false;
         return;
       };
 
@@ -62,6 +69,7 @@ habitrpg.controller('NotificationCtrl',
 
           $scope.ageDailies = function () {
             User.runCron();
+            isRunningYesterdailies = false;
           };
         }],
       });
