92fdc13adfc221a5b8184fecf1cd09b824bce492
Keith Holliday <krh121791@yahoo.com>
2017-07-10 14:07:14 -0600

    Added assumption that the date passed is in the users timezone (#8859)

--- website/server/libs/taskManager.js
+++ website/server/libs/taskManager.js
@@ -29,12 +29,13 @@ export function setNextDue (task, user, dueDateOption) {
   let now = moment().toDate();
   let dateTaskIsDue = Date.now();
   if (dueDateOption) {
-    dateTaskIsDue = moment(dueDateOption);
+    // @TODO Add required ISO format
+    dateTaskIsDue = moment(dueDateOption).add(user.preferences.timezoneOffset, 'minutes');
+
     // If not time is supplied. Let's assume we want start of Custom Day Start day.
     if (dateTaskIsDue.hour() === 0 && dateTaskIsDue.minute() === 0 && dateTaskIsDue.second() === 0 && dateTaskIsDue.millisecond() === 0) {
       dateTaskIsDue.add(user.preferences.dayStart, 'hours');
     }
-
     now = dateTaskIsDue;
   }
 
