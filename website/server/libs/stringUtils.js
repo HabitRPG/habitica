00d4bc143884322aa92f22a219474f06a31e1f63
boris.abramovich <bojaab3@gmail.com>
2017-07-11 20:37:49 +0300

    added test and did some small refactoring

--- website/server/libs/stringUtils.js
+++ website/server/libs/stringUtils.js
@@ -1,17 +1,17 @@
 
 export function removePunctuationFromString (str) {
-  return str.replace(/[.,\/#!@$%\^&*;:{}=\-_`~()]/g, ' ');
+  return str.replace(/[.,\/#!@$%\^&;:{}=\-_`~()]/g, ' ');
 }
 
 export function getMatchesByWordArray (str, wordsToMatch) {
   let matchedWords = [];
-  str = removePunctuationFromString(str);
   let wordRegexs = wordsToMatch.map((word) => new RegExp(`\\b([^a-z]+)?${word.toLowerCase()}([^a-z]+)?\\b`));
   for (let i = 0; i < wordRegexs.length; i += 1) {
     let regEx = wordRegexs[i];
     let match = str.toLowerCase().match(regEx);
     if (match !== null && match[0] !== null) {
-      matchedWords.push(match[0].trim());
+      let trimmedMatch = removePunctuationFromString(match[0]).trim();
+      matchedWords.push(trimmedMatch);
     }
   }
   return matchedWords;
