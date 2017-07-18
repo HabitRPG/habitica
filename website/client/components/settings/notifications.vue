bb2090bc68e5bc8591ef6f6d509da4a8e04d8af7
Keith Holliday <krh121791@gmail.com>
2017-07-18 12:40:25 -0600

    POrted notifications code

--- website/client/components/settings/notifications.vue
+++ website/client/components/settings/notifications.vue
@@ -1,48 +1,74 @@
 <template lang="pug">
-.row
-  .personal-options.col-md-6
-    .panel.panel-default
-      .panel-heading {{ $t('notifications') }}
-      .panel-body
-        table.table
-          tr
-            td
-            th
-              span {{ $t('email') }}
-            th
-              span {{ $t('push') }}
-          -var unsubscribeFromAllEmails = 'user.preferences.emailNotifications.unsubscribeFromAll'
-          -var unsubscribeFromAllPush = 'user.preferences.pushNotifications.unsubscribeFromAll'
-          each notification in ['newPM', 'wonChallenge', 'giftedGems', 'giftedSubscription', 'invitedParty', 'invitedGuild', 'kickedGroup', 'questStarted', 'invitedQuest', 'importantAnnouncements', 'weeklyRecaps', 'onboarding']
-            tr
-              td
-                span {{ $t('notification') }}
-              td
-                -var preference = 'user.preferences.emailNotifications.' + notification
-                input(type='checkbox', ng-model='#{preference}',
-                  ng-disabled='#{unsubscribeFromAllEmails} === true || #{preference} === undefined',
-                  ng-checked='#{unsubscribeFromAllEmails} === false && #{preference} === true',
-                  ng-change='set({"preferences.emailNotifications.#{notification}": #{preference} ? true: false})')
-              td
-                -var preference = 'user.preferences.pushNotifications.' + notification
-                input(type='checkbox', ng-model='#{preference}',
-                  ng-disabled='#{unsubscribeFromAllPush} === true || #{preference} === undefined',
-                  ng-checked='#{unsubscribeFromAllPush} === false && #{preference} === true',
-                  ng-change='set({"preferences.pushNotifications.#{notification}": #{preference} ? true: false})')
+.row.standard-page
+  .col-12
+    h1 {{ $t('notifications') }}
+  .col-12
+    .checkbox
+      label
+        input(type='checkbox', v-model='user.preferences.pushNotifications.unsubscribeFromAll',
+        @change='set("pushNotifications", "unsubscribeFromAll")')
+        span {{ $t('unsubscribeAllPush') }}
 
+    br
+
+    .checkbox
+      label
+        input(type='checkbox', v-model='user.preferences.emailNotifications.unsubscribeFromAll',
+          @change='set("emailNotifications", "unsubscribeFromAll")')
+        span {{ $t('unsubscribeAllEmails') }}
+    small {{ $t('unsubscribeAllEmailsText') }}
+
+  .col-8
+    table.table
+    tr
+      td
+      th
+        span {{ $t('email') }}
+      th
+        span {{ $t('push') }}
+    tr(v-for='notification in notifications')
+      td
+        span {{ $t(notification) }}
+      td
+        input(type='checkbox', v-model='user.preferences.emailNotifications[notification]',
+          @change='set("emailNotifications", notification)')
+      td
+        input(type='checkbox', v-model='user.preferences.pushNotifications[notification]',
+          @change='set("pushNotifications", notification)')
         hr
-
-        .checkbox
-          label
-            input(type='checkbox', ng-model='user.preferences.pushNotifications.unsubscribeFromAll',
-            ng-change='set({"preferences.pushNotifications.unsubscribeFromAll": user.preferences.pushNotifications.unsubscribeFromAll ? true: false})')
-            span {{ $t('unsubscribeAllPush') }}
-
-        .checkbox
-          label
-            input(type='checkbox', ng-model='user.preferences.emailNotifications.unsubscribeFromAll',
-            ng-change='set({"preferences.emailNotifications.unsubscribeFromAll": user.preferences.emailNotifications.unsubscribeFromAll ? true: false})')
-            span {{ $t('unsubscribeAllEmails') }}
-
-        small {{ $t('unsubscribeAllEmailsText') }}
 </template>
+
+<script>
+import { mapState } from 'client/libs/store';
+
+export default {
+  data () {
+    return {
+      notifications: [
+        'newPM',
+        'wonChallenge',
+        'giftedGems',
+        'giftedSubscription',
+        'invitedParty',
+        'invitedGuild',
+        'kickedGroup',
+        'questStarted',
+        'invitedQuest',
+        'importantAnnouncements',
+        'weeklyRecaps',
+        'onboarding'
+      ],
+    };
+  },
+  computed: {
+    ...mapState({user: 'user.data'}),
+  },
+  methods: {
+    set (preferenceType, notification) {
+      let settings = {};
+      settings[`preferences.${preferenceType}.${notification}`] = this.user.preferences[preferenceType][notification];
+      this.$store.dispatch('user:set', settings);
+    },
+  },
+};
+</script>
