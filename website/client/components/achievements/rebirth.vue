3f38359dcca7d6a0acb14c83f8cac5e4960e6f1e
Keith Holliday <krh121791@gmail.com>
2017-08-09 09:13:22 -0600

    Achievement footer and avatar added

--- website/client/components/achievements/rebirth.vue
+++ website/client/components/achievements/rebirth.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
-  b-modal#testing(:title="$t('guildReminderTitle')", size='lg', :hide-footer="true")
+  b-modal#rebirth(:title="$t('guildReminderTitle')", size='lg', :hide-footer="true")
     .modal-content(style='min-width:28em')
     .modal-body.text-center
       h3(style='margin-bottom:0') {{ $t('modalAchievement') }}
       // @TODO: +achievementAvatar('sun',0)
+      achievement-avatar
       div(ng-if='user.achievements.rebirthLevel < 100')
         | {{ $t('rebirthAchievement', {number: user.achievements.rebirths, level: user.achievements.rebirthLevel}) }}
       div(ng-if='user.achievements.rebirthLevel >= 100')
         | {{ $t('rebirthAchievement100', {number: user.achievements.rebirths}) }}
       br
       button.btn.btn-primary(@click='close()') {{ $t('huzzah') }}
-    // @TODO: +achievementFooter
+    achievement-footer
 </template>
 
 <style scope>
@@ -21,19 +22,23 @@
 
 <script>
 import bModal from 'bootstrap-vue/lib/components/modal';
+import achievementFooter from './achievementFooter';
+import achievementAvatar from './achievementAvatar';
 
 import { mapState } from 'client/libs/store';
 
 export default {
   components: {
     bModal,
+    achievementFooter,
+    achievementAvatar,
   },
   computed: {
     ...mapState({user: 'user.data'}),
   },
   methods: {
     close () {
-      this.$root.$emit('hide::modal', 'testing');
+      this.$root.$emit('hide::modal', 'rebirth');
     },
   },
 };
--- website/client/components/achievements/streak.vue
+++ website/client/components/achievements/streak.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-  b-modal#testing(:title="$t('guildReminderTitle')", size='lg', :hide-footer="true")
+  b-modal#streak(:title="$t('guildReminderTitle')", size='lg', :hide-footer="true")
     .modal-content(style='min-width:28em')
     .modal-body.text-center
       h3(style='margin-bottom: 0') {{ $t('streakAchievement') }}
       // @TODO: +achievementAvatar('thermometer',2.5)
+      achievement-avatar
       h4(ng-if='user.achievements.streak === 1') {{ $t('firstStreakAchievement') }}
       h4(ng-if='user.achievements.streak > 1') {{ $t('streakAchievementCount', {streaks: user.achievements.streak}) }}
       p {{ $t('twentyOneDays') }}
@@ -13,7 +14,7 @@
       .checkbox
         label(style='display:inline-block') {{ $t('dontShowAgain') }}
           input(type='checkbox', ng-model='user.preferences.suppressModals.streak', ng-change='set({"preferences.suppressModals.streak": user.preferences.suppressModals.streak?true: false})')
-    // @TOOD: +achievementFooter
+    achievement-footer
 </template>
 
 <style scope>
@@ -24,19 +25,23 @@
 
 <script>
 import bModal from 'bootstrap-vue/lib/components/modal';
+import achievementFooter from './achievementFooter';
+import achievementAvatar from './achievementAvatar';
 
 import { mapState } from 'client/libs/store';
 
 export default {
   components: {
     bModal,
+    achievementFooter,
+    achievementAvatar,
   },
   computed: {
     ...mapState({user: 'user.data'}),
   },
   methods: {
     close () {
-      this.$root.$emit('hide::modal', 'testing');
+      this.$root.$emit('hide::modal', 'streak');
     },
   },
 };
