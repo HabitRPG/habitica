8614f11a31fe8a550d03965d7486d54f4070cd8a
Pavel Pletenev <cpp.create@gmail.com>
2017-08-16 02:06:19 +0300

    Refactor api description with @apiParam groups to make it more explicit (#8919)
    * Refactor api description for auth.js with @apiParam groups
    
    * Refactor apiDoc toward better consistency
    
    * Fix missing groups for get requests
    
    * Fix missing groups for other request methods

--- website/server/controllers/api-v3/groups.js
+++ website/server/controllers/api-v3/groups.js
@@ -263,9 +263,9 @@ api.createGroupPlan = {
  * @apiName GetGroups
  * @apiGroup Group
  *
- * @apiParam {String} type The type of groups to retrieve. Must be a query string representing a list of values like 'tavern,party'. Possible values are party, guilds, privateGuilds, publicGuilds, tavern
- * @apiParam {String="true","false"} [paginate] Public guilds support pagination. When true guilds are returned in groups of 30
- * @apiParam {Number} [page] When pagination is enabled for public guilds this parameter can be used to specify the page number (the initial page is number 0 and not required)
+ * @apiParam (Query) {String} type The type of groups to retrieve. Must be a query string representing a list of values like 'tavern,party'. Possible values are party, guilds, privateGuilds, publicGuilds, tavern
+ * @apiParam (Query) {String="true","false"} [paginate] Public guilds support pagination. When true guilds are returned in groups of 30
+ * @apiParam (Query) {Number} [page] When pagination is enabled for public guilds this parameter can be used to specify the page number (the initial page is number 0 and not required)
  *
  * @apiParamExample {json} Private Guilds, Tavern:
  *     {
@@ -691,7 +691,7 @@ function _removeMessagesFromMember (member, groupId) {
  * @apiName LeaveGroup
  * @apiGroup Group
  *
- * @apiParam {String} groupId The group _id ('party' for the user party and 'habitrpg' for tavern are accepted)
+ * @apiParam (Path) {String} groupId The group _id ('party' for the user party and 'habitrpg' for tavern are accepted)
  * @apiParam (Query) {String="remove-all","keep-all"} keep=keep-all Whether or not to keep challenge tasks belonging to the group being left.
  * @apiParam (Body) {String="remain-in-challenges","leave-challenges"} [keepChallenges=leave-challenges] Whether or not to remain in the challenges of the group being left.
  *
--- website/server/controllers/api-v3/pushNotifications.js
+++ website/server/controllers/api-v3/pushNotifications.js
@@ -12,8 +12,8 @@ let api = {};
  * @apiName UserAddPushDevice
  * @apiGroup User
  *
- * @apiParam {String} regId The id of the push device
- * @apiParam {String} type The type of push device
+ * @apiParam (Body) {String} regId The id of the push device
+ * @apiParam (Body) {String} type The type of push device
  *
  * @apiSuccess {Object} data List of push devices
  * @apiSuccess {String} message Success message
@@ -52,11 +52,11 @@ api.addPushDevice = {
 
 /**
  * @apiIgnore
- * @api {delete} /api/v3/user/push-devices remove a push device from a user
+ * @api {delete} /api/v3/user/push-devices/:regId remove a push device from a user
  * @apiName UserRemovePushDevice
  * @apiGroup User
  *
- * @apiParam {String} regId The id of the push device
+ * @apiParam (Path) {String} regId The id of the push device
  *
  * @apiSuccess {Object} data List of push devices
  * @apiSuccess {String} message Success message
