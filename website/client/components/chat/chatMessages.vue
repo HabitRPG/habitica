b92f0423a69d86d077516feeb999e1df20ae79dc
Keith Holliday <krh121791@gmail.com>
2017-08-18 09:34:17 -0600

    Removed report own message

--- website/client/components/chat/chatMessages.vue
+++ website/client/components/chat/chatMessages.vue
@@ -27,7 +27,7 @@
           span.action( @click='copyAsTodo(msg)')
             .svg-icon(v-html="icons.copy")
             | {{$t('copyAsTodo')}}
-          span.action(v-if='user.contributor.admin || (!msg.sent && user.flags.communityGuidelinesAccepted)', @click='report(msg)')
+          span.action(v-if='user.contributor.admin || (msg.uuid !== user._id && user.flags.communityGuidelinesAccepted)', @click='report(msg)')
             .svg-icon(v-html="icons.report")
             | {{$t('report')}}
           span.action(v-if='msg.uuid === user._id', @click='remove(msg, index)')
