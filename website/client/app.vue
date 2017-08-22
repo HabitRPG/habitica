46b0c36507bc19be0351958baf33037c97e21b54
thehollidayinn <krh121791@gmail.com>
2017-08-22 17:03:25 -0600

    Fixed overlay color

--- website/client/app.vue
+++ website/client/app.vue
@@ -21,6 +21,13 @@
   }
 </style>
 
+<style>
+  .modal-backdrop.show {
+    opacity: 1;
+    background-color: rgba(67, 40, 116, 0.9) !important;
+  }
+</style>
+
 <script>
 import axios from 'axios';
 import AppMenu from './components/appMenu';
