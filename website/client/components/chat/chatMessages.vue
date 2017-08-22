a398b6aaec515ce913daa10079c847ecedb6c491
thehollidayinn <krh121791@gmail.com>
2017-08-22 14:55:44 -0600

    Hide like count 0

--- website/client/components/chat/chatMessages.vue
+++ website/client/components/chat/chatMessages.vue
@@ -35,7 +35,7 @@
             span.action(v-if='msg.uuid === user._id', @click='remove(msg, index)')
               .svg-icon(v-html="icons.delete")
               | {{$t('delete')}}
-            span.action.float-right
+            span.action.float-right(v-if='likeCount(msg) > 0')
               .svg-icon(v-html="icons.liked")
               | + {{ likeCount(msg) }}
     .row(v-if='user._id === msg.uuid')
@@ -58,7 +58,7 @@
             span.action(v-if='msg.uuid === user._id', @click='remove(msg, index)')
               .svg-icon(v-html="icons.delete")
               | {{$t('delete')}}
-            span.action.float-right
+            span.action.float-right(v-if='likeCount(msg) > 0')
               .svg-icon(v-html="icons.liked")
               | + {{ likeCount(msg) }}
       .col-2
