748ce8a23f74ee173f0876f6ca0ce0eb7d7b842f
negue <negue@users.noreply.github.com>
2017-08-24 18:49:14 +0200

    fix isPinned : check if pinnedItems-array exists

--- website/common/script/libs/isPinned.js
+++ website/common/script/libs/isPinned.js
@@ -2,8 +2,8 @@ module.exports = function isPinned (user, item) {
   if (user === null)
     return false;
 
-  const isUnpinned = user.unpinnedItems.findIndex(unpinned => unpinned.path === item.path) > -1;
-  const pinnedItem = user.pinnedItems.findIndex(pinned => pinned.path === item.path) > -1;
+  const isItemUnpinned = user.unpinnedItems !== undefined && user.unpinnedItems.findIndex(unpinned => unpinned.path === item.path) > -1;
+  const isItemPinned = user.pinnedItems !== undefined && user.pinnedItems.findIndex(pinned => pinned.path === item.path) > -1;
 
-  return pinnedItem && !isUnpinned;
+  return isItemPinned && !isItemUnpinned;
 };
