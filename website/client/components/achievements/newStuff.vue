47df83c5a91c09952a2dbda3e1261ed82723df0e
SabreCat <sabrecat@gmail.com>
2017-08-30 20:29:45 +0000

    feat(modals): Bailey news

--- website/client/components/achievements/newStuff.vue
+++ website/client/components/achievements/newStuff.vue
@@ -1,29 +1,67 @@
 <template lang="pug">
-  b-modal#new-stuff(v-if='user.flags.newStuff', :title="$t('newStuff')", size='lg', :hide-footer="true")
-    .modal-body.new-stuff-modal
-      h3.text-center
-        | {{ this.$t('newStuff') }} by&nbsp;
-        a(target='_blank', href='https://twitter.com/Mihakuu') Bailey
-      div(:class="baileyClass")
-      div(v-html='latestBaileyMessage')
+  b-modal#new-stuff(
+    v-if='user.flags.newStuff',
+    size='lg',
+    :hide-header='true',
+    :hide-footer='true',
+  )
+    .modal-body
+      .media
+        .media-body
+          .media
+            .align-self-center.right-margin(:class='baileyClass')
+            .media-body
+              h1.align-self-center(v-markdown='$t("newStuff")')
+          h2 8/30/2017 - LAST CHANCE FOR LAVA WARRIOR SET AND EMBER HATCHING POTIONS
+          hr
+          .media
+            .promo_mystery_201708.right-margin.align-self-center
+            .media-body
+              h3 Last Chance for Lava Warrior Set
+              p Reminder: you only have three days to <a href='/settings/subscription'>subscribe</a> and receive the Lava Warrior Set! Subscribing also lets you buy gems for gold. The longer your subscription, the more gems you get!
+              p Thanks so much for your support! You help keep Habitica running.
+              p.small.muted by Lemoness
+          h3 Last Chance for Ember Hatching Potions
+          p Reminder: there are three days left to <a href='/shops/market'>buy Ember Hatching Potions</a>! If they come back, it won't be until next year at the earliest, so don't delay!
+          p.small.muted by Balduranne, tricksy.fox, and SabreCat
+        .promo_ember_potions.left-margin.align-self-center
     .modal-footer
       a.btn.btn-info(href='http://habitica.wikia.com/wiki/Whats_New', target='_blank') {{ this.$t('newsArchive') }}
       button.btn.btn-default(@click='close()') {{ this.$t('cool') }}
       button.btn.btn-warning(@click='dismissAlert();') {{ this.$t('dismissAlert') }}
 </template>
 
+<style lang='scss' scoped>
+  @import '~client/assets/scss/static.scss';
+
+  .modal-body {
+    padding-top: 2em;
+  }
+
+  .left-margin {
+    margin-left: 1em;
+  }
+
+  .right-margin {
+    margin-right: 1em;
+  }
+</style>
+
 <script>
-import axios from 'axios';
 import bModal from 'bootstrap-vue/lib/components/modal';
 import { mapState } from 'client/libs/store';
+import markdown from 'client/directives/markdown';
 
 export default {
   components: {
     bModal,
   },
+  computed: {
+    ...mapState({user: 'user.data'}),
+  },
   data () {
     let worldDmg = {
-      bailey: true,
+      bailey: false,
     };
 
     return {
@@ -33,12 +71,8 @@ export default {
       },
     };
   },
-  computed: {
-    ...mapState({user: 'user.data'}),
-    async latestBaileyMessage () {
-      let message = await axios.get('/new-stuff');
-      return message;
-    },
+  directives: {
+    markdown,
   },
   methods: {
     close () {
