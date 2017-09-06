67ad7f9d2960c97fedc2aab4ef5e0f4639bdc369
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-09-06 20:02:21 +0200

    todo

--- website/client/components/notifications.vue
+++ website/client/components/notifications.vue
@@ -312,6 +312,7 @@ export default {
       // Notifications
 
       // Sync
+      // @TODO add a loading spinner somewhere
       await Promise.all([
         this.$store.dispatch('user:fetch', {forceLoad: true}),
         this.$store.dispatch('tasks:fetchUserTasks', {forceLoad: true}),
