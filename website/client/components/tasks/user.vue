3f7ec9e29056db440d1fb6b62425bb4251631459
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2017-09-15 21:53:59 +0100

    client/tasks/user: (create dropdown) change icon color on menuitem hover
    Previously the color was only changing when you hovered over the
    text or icon.
    
    This change changes the icon color when you hover anywhere over
    the `menuitem`

--- website/client/components/tasks/user.vue
+++ website/client/components/tasks/user.vue
@@ -135,7 +135,7 @@
       }
   }
 
-  .dropdown-icon-item:hover .svg-icon, .dropdown-item.active .svg-icon {
+  .dropdown-item:hover .svg-icon, .dropdown-item.active .svg-icon {
     color: #4f2a93;
   }
 
