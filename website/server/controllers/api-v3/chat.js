20ff8ba87aed5156f94540a6fa508fc53460807e
Alys <alice.harris@oldgods.net>
2017-10-22 19:54:12 +1000

    allow banned word / swearword blocker to apply to all public guilds, with specified exceptions

--- website/server/controllers/api-v3/chat.js
+++ website/server/controllers/api-v3/chat.js
@@ -14,6 +14,7 @@ import pusher from '../../libs/pusher';
 import nconf from 'nconf';
 import Bluebird from 'bluebird';
 import bannedWords from '../../libs/bannedWords';
+import guildsAllowingBannedWords from '../../libs/guildsAllowingBannedWords';
 import { getMatchesByWordArray } from '../../libs/stringUtils';
 import { TAVERN_ID } from '../../models/group';
 import bannedSlurs from '../../libs/bannedSlurs';
@@ -171,9 +172,11 @@ api.postChat = {
       throw new NotAuthorized(res.t('chatPrivilegesRevoked'));
     }
 
-    if (group._id === TAVERN_ID) {
+    // prevent banned words being posted, except in private guilds/parties and in certain public guilds with specific topics
+    if (group.privacy !== 'private' && !guildsAllowingBannedWords[group._id]) {
       let matchedBadWords = getBannedWordsFromText(req.body.message);
       if (matchedBadWords.length > 0) {
+        // @TODO replace this split mechanism with something that works properly in translations
         let message = res.t('bannedWordUsed').split('.');
         message[0] += ` (${matchedBadWords.join(', ')})`;
         throw new BadRequest(message.join('.'));
