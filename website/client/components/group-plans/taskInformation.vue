ee9753298229c8726b412b927160748ca9f41d41
thehollidayinn <krh121791@gmail.com>
2017-11-06 21:16:57 -0700

    Removed task from local on delete

--- website/client/components/group-plans/taskInformation.vue
+++ website/client/components/group-plans/taskInformation.vue
@@ -70,6 +70,7 @@
         :groupId="groupId",
         v-on:taskCreated='taskCreated',
         v-on:taskEdited='taskEdited',
+        v-on:taskDestroyed='taskDestroyed'
       )
   .row
     task-column.col-12.col-sm-6.col-3(
@@ -391,6 +392,12 @@ export default {
         return taskItem._id === task._id;
       });
       this.tasksByType[task.type].splice(index, 1, task);
+    },
+    taskDestroyed (task) {
+      let index = findIndex(this.tasksByType[task.type], (taskItem) => {
+        return taskItem._id === task._id;
+      });
+      this.tasksByType[task.type].splice(index, 1);
     },
     cancelTaskModal () {
       this.editingTask = null;
