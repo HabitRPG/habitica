ec179182e7911ca4e771541e70ea1860e97e8eee
Julius Jung <juljun14@gmail.com>
2017-12-05 15:22:59 -0500

    submit initial working solution to disallow selling of Saddle (#9663)

--- website/common/script/content/index.js
+++ website/common/script/content/index.js
@@ -353,6 +353,7 @@ api.food = {
     canBuy () {
       return true;
     },
+    sellWarningNote: t('foodSaddleSellWarningNote'),
     text: t('foodSaddleText'),
     value: 5,
     notes: t('foodSaddleNotes'),
