d6eb17299224b449fcc0bbe4f535f0de09c4fd8e
SabreCat <sabrecat@gmail.com>
2017-12-05 18:18:06 +0000

    chore(news): Bailey

--- website/client/components/static/newStuff.vue
+++ website/client/components/static/newStuff.vue
@@ -4,26 +4,20 @@
       .align-self-center.right-margin(:class='baileyClass')
       .media-body
         h1.align-self-center(v-markdown='$t("newStuff")')
-    h2 11/29/2017 - LAST CHANCE FOR CARPET RIDER SET AND THUNDERSTORM POTIONS, GUILD AND USE CASE SPOTLIGHTS
+    h2 12/5/2017 - ARMOIRE ITEMS, BACKGROUNDS, AND TAKE THIS CHALLENGE
     hr
     .media
-      .promo_potions_thunderstorm.right-margin
       .media-body
-        h3 Last Chance for Carpet Rider Set
-        p(v-markdown='"Reminder: there are only two more days to [subscribe](/user/settings/subscription) and receive the Carpet Rider Set! Subscribing also lets you buy gems for gold. The longer your subscription, the more gems you get!"')
-        p Thanks so much for your support! You help keep Habitica running.
-        .small by Beffymaroo
-        h3 Last Chance for Thunderstorm Hatching Potions
-        p(v-markdown='"Reminder: this is the final day to [buy Thunderstorm Hatching Potions!](/shops/market) If they come back, it won\'t be until next year at the earliest, so don\'t delay!"')
-        .small by Balduranne
-        h3 Generating Interest: Guilds and Use Cases for Money Matters
-        p(v-markdown='"There\'s a new [Guild Spotlight on the blog](https://habitica.wordpress.com/2017/11/28/generating-interest-guilds-for-money-matters/) that highlights the Guilds that can help you as work to improve your budgeting and saving habits! Check it out now to find Habitica\'s best money management communities."')
-    .media
-      .media-body
-        p(v-markdown='"This month\'s [Use Case Spotlight](https://habitica.wordpress.com/2017/11/14/3908/) follows the same theme! It features a number of great suggestions submitted by Habiticans in the [Use Case Spotlights Guild](https://habitica.com/groups/guild/1d3a10bf-60aa-4806-a38b-82d1084a59e6). We hope it helps any of you who might be working on saving and budgeting as the holiday season approaches!"')
-        p Plus, we're collecting user submissions for the next spotlight! How do you use Habitica to stay on top of Holiday Housekeeping? We’ll be featuring player-submitted examples in Use Case Spotlights on the Habitica Blog at the start of next month, so post your suggestions in the Use Case Spotlight Guild now. We look forward to learning more about how you use Habitica to improve your life and get things done!
-        .small by Beffymaroo
-      .scene_money.left-margin
+        h3 December Backgrounds and Armoire Items!
+        p We’ve added three new backgrounds to the Background Shop! Now your avatar can admire a Starry Winter Night, bask in the wonder of a Toymaker's Workshop, and glide along a Cross-Country Ski Trail. You can find them under User Icon > Backgrounds!
+        p Plus, there’s new gold-purchasable equipment in the Enchanted Armoire, including the Lamp-Lighter Set. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)
+        .small by Vampitch, AnnDeLune, CitrineSun, and Hachiseiko
+        h3 Take This Challenge for December
+        p(v-markdown='"The next Take This Challenge has launched, [It\'s Dangerous to Go Alone!](https://habitica.com/challenges/5f70ce5b-2d82-4114-8e44-ca65615aae62), with a focus on maintaining offline relationships. Be sure to check it out to earn additional pieces of the Take This armor set!"')
+        p(v-markdown='"[Take This](http://www.takethis.org/) is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness."')
+      .promo_armoire_backgrounds_201712.left-margin
+    p(v-markdown='"Congratulations to the winners of the last Take This Challenge, \'Notice Me, Senpai!\': grand prize winner M T, and runners-up Trompadour, Eule, mizuki_F, PhiaFox, and Dragon. Plus, all participants in that Challenge have received a piece of the [Take This item set](http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set) if they hadn\'t collected all the pieces already. It is located in your Rewards column. Enjoy!"')
+    .small by Doctor B, the Take This team, Lemoness, and SabreCat
     br
 </template>
 
