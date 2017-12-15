c4ffe39ec9631b7b169c3ae5ece1bbef95b2a900
SabreCat <sabrecat@gmail.com>
2017-12-15 05:34:30 +0000

    chore(news): Bailey

--- website/client/components/static/newStuff.vue
+++ website/client/components/static/newStuff.vue
@@ -4,11 +4,28 @@
       .align-self-center.right-margin(:class='baileyClass')
       .media-body
         h1.align-self-center(v-markdown='$t("newStuff")')
-    h2 12/13/2017 - BLOG POST: THE HABITICA PLANNER
+    h2 12/15/2017 - NEW PET QUEST BUNDLE, GIFT-1-GET-1 SUBSCRIPTIONS, AND BEHIND THE SCENES BLOG
     hr
-    p(v-markdown='"This month\'s [featured Wiki article](https://habitica.wordpress.com/2017/12/13/the-habitica-planner/) is about the Habitica Planner! We hope that it will help you as you find new ways to use Habitica for your task-management needs! Be sure to check it out, and let us know what you think by reaching out on [Twitter](https://twitter.com/habitica), [Tumblr](http://blog.habitrpg.com), and [Facebook](https://facebook.com/habitica)."')
-    .small by Beffymaroo and the Wiki Wizards
-    .scene_calendar.center-block
+    h3 New Discounted Pet Quest Bundle: Winter Quests
+    .media
+      .media-body
+        p(v-markdown='"If you are looking to add some cold weather friends to your Habitica stable, you\'re in luck! From now until December 31, you can purchase the Winter Quest Bundle and receive the Trapper Santa, Find the Cub, and Penguin quests, all for only 7 Gems! That\'s a discount of 5 Gems from the price of purchasing them separately. Check it out in the [Quest Shop](/shops/quests) today!"')
+        .small by Lemoness and SabreCat
+        .small Art by UncommonCriminal, Shaner, Eevachu, Pandoro, melynnrose, Breadstrings, Rattify, and PainterProphet
+        .small Writing by Lefnire, Leephon, and Daniel the Bard
+        .media
+          .promo_winter_subscriptions.left-margin
+          .media-body
+            h3 Gift a Subscription and Get One Free!
+            p In honor of the season of giving--and due to popular demand!--we're bringing back a very special promotion for the next month only. Now when you gift somebody a subscription, you get the same subscription for yourself for free!
+        p Subscribers get tons of perks every month, including exclusive items, the ability to buy gems with gold, a cute exclusive Jackalope Pet, and increased data history. Plus, it helps keep Habitica running :) To gift a subscription to someone, just open their profile and click on the present icon in the upper right.
+      .promo_winter_quests
+    p The special promotion will only run until January 12th, so if you've been curious about trying out a subscription, now's the time! Make a friend happy and use all your new gems to go questing together.
+    p Please note that if you or your gift recipient already have a recurring subscription, the gifted subscription will only start after that subscription is cancelled or has expired. Thanks so much for your support! <3
+    .small by SabreCat and Lemoness
+    h3 Behind the Scenes: Habitica's Winter Staff Retreat
+    p(v-markdown='"There\'s a new [Behind the Scenes post on the Habitica blog](https://habitica.wordpress.com/2017/12/14/behind-the-scenes-habiticas-winter-staff-retreat/) about our recent Staff Retreat. Check out some great photos and anecdotes from the trip, and learn a little more about the Habitica Team!"')
+    .promo_winter_retreat.center-block
     br
 </template>
 
