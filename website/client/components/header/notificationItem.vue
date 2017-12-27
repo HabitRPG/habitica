ece883334e63b76ee36bc9219011b112fa8ab641
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-12-28 00:03:35 +0100

    notification: hover status and margins

--- website/client/components/header/notificationItem.vue
+++ website/client/components/header/notificationItem.vue
@@ -11,23 +11,34 @@
 .notification {
   width: 378px;
   max-width: 100%;
-  padding: 16px 24px;
+  padding: 9px 24px 10px 24px;
   overflow: hidden;
+
+  &:active, &:hover {
+    color: inherit;
+  }
 }
 
 .notification-icon {
-  min-height: 28px;
-  min-width: 28px;
+  min-height: 31px;
+  min-width: 32px;
   border: 1px solid;
   margin-right: 16px;
 }
 
 .notification-content {
+  // total distance from notification top and bottom edges are 15 and 16 pixels
+  margin-top: 6px;
+  margin-bottom: 6px;
+
   white-space: normal;
   border: 1px solid;
 }
 
 .notification-remove {
+  // total distance from the notification top edge is 20 pixels
+  margin-top: 11px;
+
   width: 30px; // the real size is 10px for some reason
   height: 10px;
   margin-left: 16px;
