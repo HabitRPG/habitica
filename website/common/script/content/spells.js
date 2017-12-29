22f6afb9f9db1fe560c31c7aa225c4716edbded7
guyguy2001 <guygeva2311@gmail.com>
2017-12-29 23:41:01 +0200

    Stealth fix - after vue

--- website/common/script/content/spells.js
+++ website/common/script/content/spells.js
@@ -196,9 +196,22 @@ spells.rogue = {
     lvl: 14,
     target: 'self',
     notes: t('spellRogueStealthNotes'),
-    cast (user) {
-      if (!user.stats.buffs.stealth) user.stats.buffs.stealth = 0;
-      user.stats.buffs.stealth += Math.ceil(diminishingReturns(statsComputed(user).per, user.tasksOrder.dailys.length * 0.64, 55));
+    cast(user) {
+      let type = 0;
+      switch (type) {
+        case 0: //current
+          if (!user.stats.buffs.stealth) user.stats.buffs.stealth = 0;
+            user.stats.buffs.stealth += Math.ceil(diminishingReturns(user._statsComputed.per, user.tasksOrder.dailys.length * 0.64, 55));
+          break;
+        case 1: //TNychka's response
+          if (!user.stats.buffs.stealth) user.stats.buffs.stealth = 0;
+            user.stats.buffs.stealth += Math.ceil(diminishingReturns(user._statsComputed.per, (user.tasksOrder.dailys.length * 0.5) - user.stats.buffs.stealth, 55));
+          break;
+        case 2: //my idea- stealth calculates on cron, for the most accurate usage of the number of due dailies
+          if (!user.stats.buffs.stealth) user.stats.buffs.stealth = 0;
+            user.stats.buffs.stealth++;
+          break;
+      }
     },
   },
 };
