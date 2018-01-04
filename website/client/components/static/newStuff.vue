71cb4e85102755a7231e9f58d0f8e832f63899a5
SabreCat <sabrecat@gmail.com>
2018-01-04 21:35:26 +0000

    feat(content): enable Wintery Skins and Hair

--- website/client/components/static/newStuff.vue
+++ website/client/components/static/newStuff.vue
@@ -4,23 +4,19 @@
       .align-self-center.right-margin(:class='baileyClass')
       .media-body
         h1.align-self-center(v-markdown='$t("newStuff")')
-    h2 1/2/2018 - JANUARY BACKGROUNDS AND ARMOIRE ITEMS, AND NEW TAKE THIS CHALLENGE
+    h2 1/4/2018 - WINTERY AVATAR CUSTOMIZATIONS AND NEW AUDIO THEMES
     hr
     .media
       .media-body
-        h3 January Backgrounds and Armoire Items!
-        p We’ve added three new backgrounds to the Background Shop! Now your avatar can drive a Sleigh, bask in the wintery glow of Auroras, and fly over Icy Steppes! Check them out under User Icon > Backgrounds!
-        p Plus, there’s new gold-purchasable equipment in the Enchanted Armoire, including the Coach Driver Set. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)
-        .small by Katy133, weeWitch, denissa, Reesachan, Nummycakes, and Beffymaroo
-        .media
-          .promo_take_this.right-margin
-          .media-body
-            h3 New Take This Challenge!
-            p(v-markdown='"The next Take This Challenge has launched, [Check Your HP!](/challenges/d9722bfb-f8f1-4801-9b40-2aa34ea85105), with a focus on monitoring your mood. Be sure to check it out to earn additional pieces of the Take This armor set and a chance at Gems!"')
-        p(v-markdown='"[Take This](http://www.takethis.org/) is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness."')
-      .promo_armoire_bgs_201801.left-margin
-    p(v-markdown='"Congratulations to the winners of the last Take This Challenge, \'It\'s Dangerous to Go Alone!\': grand prize winner Acoustikarl, and runners-up Ximena18, Sunbeams90, Castrón, Adara McCLain, and Emily Torrey. Plus, all participants in that Challenge have received a piece of the [Take This item set](http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set), if they hadn\'t completed the set already. It is located in your Rewards column. Enjoy!"')
-      .small by Doctor B, the Take This team, Lemoness, and SabreCat
+        h3 Wintery Skins and Hair Colors!
+        p The Seasonal Edition Wintery Hair Colors are now available for purchase via the User Menu under "Edit Avatar"! Now you can dye your avatar's hair Snowy, Peppermint, Holly Green, Aurora, Winter Star, or Festive.
+        p Plus, the Seasonal Edition Wintery Skins are available, too! You can complete your winter avatar look with Aurora, Dapper, Festive, Holly, Polar, Sugar, or Winter Star Skins.
+        p Both of these Seasonal Edition customization sets will only be available to purchase until January 31st, after which they'll be gone until next year, so be sure to swoop them up now!
+        .small by Lemoness and tricksy.fox
+        h3 New Audio Themes!
+        p(v-markdown='"There are two new audio themes available in [site settings](/user/settings/site): Pizilden\'s Theme and the MAFL Theme! Check them out to bring fresh sounds to your Habitica experience."')
+        .small by Pizilden and MAFL
+      .promo_winter_customizations.left-margin
 </template>
 
 <style lang='scss' scoped>
