a9d579d828d5177490c1199b9f921c6a2b5eb787
SabreCat <sabrecat@gmail.com>
2018-01-18 21:30:19 +0000

    fix(buy): correct content constant

--- website/common/script/ops/buyGear.js
+++ website/common/script/ops/buyGear.js
@@ -37,7 +37,7 @@ module.exports = function buyGear (user, req = {}, analytics) {
 
   let itemIndex = Number(item.index);
 
-  if (Number.isInteger(itemIndex) && content.CLASSES.includes(item.klass)) {
+  if (Number.isInteger(itemIndex) && content.classes.includes(item.klass)) {
     let previousLevelGear = key.replace(/[0-9]/, itemIndex - 1);
     let hasPreviousLevelGear = user.items.gear.owned[previousLevelGear];
     let checkIndexToType = itemIndex > (item.type === 'weapon' || item.type === 'shield' && item.klass === 'rogue' ? 0 : 1);
