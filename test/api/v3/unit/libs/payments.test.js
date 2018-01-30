6cc81379cfabdf5a1600bea08dfcf28f7ed24c1a
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2018-01-30 22:16:04 +0100

    remove errors

--- test/api/v3/unit/libs/payments.test.js
+++ test/api/v3/unit/libs/payments.test.js
@@ -413,14 +413,11 @@ describe('payments/index', () => {
       });
     });
 
-    context.only('Mystery Items', () => {
+    context('Mystery Items', () => {
       it('awards mystery items when within the timeframe for a mystery item', async () => {
-        await user.save();
-        console.log('aa')
         let mayMysteryItemTimeframe = 1464725113000; // May 31st 2016
         let fakeClock = sinon.useFakeTimers(mayMysteryItemTimeframe);
-        await user.save();
-        console.log('bb')
+
         data = { paymentMethod: 'PaymentMethod', user, sub: { key: 'basic_3mo' } };
 
         await api.createSubscription(data);
