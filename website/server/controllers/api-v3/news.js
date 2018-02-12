cf4aaf161822a80c72ca103aed8755ca7cc05369
SabreCat <sabrecat@gmail.com>
2018-02-12 21:56:29 +0000

    feat(event): enable Valentines

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'CUPID HATCHING POTIONS AND RESOLUTION PLOT-LINE CONTINUES';
+const LAST_ANNOUNCEMENT_TITLE = 'VALENTINE\'S DAY CELEBRATION AND CHAT IMPROVEMENTS';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -32,22 +32,19 @@ api.getNews = {
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
           </div>
         </div>
-        <h2>2/8/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+        <h2>2/12/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
         <hr/>
-        <h3>Cupid Hatching Potions</h3>
-        <div class="media">
-          <div class="media-body">
-            <p>There's a new pet breed in town! Between now and February 28th, you can buy Cupid Hatching Potions from <a href="/shops/market">the Market</a> and use them to hatch any standard pet egg. (Magic Hatching Potions do not work on Quest Pet eggs.) Cupid Potion Pets aren't picky, so they'll happily eat any kind of food that you feed them!</p>
-            <p>After they're gone, it will be at least a year before the Cupid Hatching Potions are available again, so be sure to get them now!</p>
-            <div class="small mb-3">by Willow the Witty and SabreCat</div>
-            <h3>Resolution Plot-Line: Goo Galore</h3>
-            <p>Remember the mystery of those Habiticans who grew abruptly disheartened with their New Year's Resolutions? There's just been a significant development!</p>
-            <p>One of the investigation teams, led by Viirus, Apollo, and Piyorii, has turned up something very strange in the broken buildings where those Habiticans live...</p>
-            <p>"The cracks in the buildings have started to glow," Viirus says. "And look!" He points, and you see that a strange orange goo is oozing from the shattered stone. Gingerly, you touch it -- it smells sweet and is very, very sticky, almost like caramel.</p>
-            <p>Hmm.... let's keep watch for the cause in the coming days.</p>
-          </div>
-          <div class="promo_cupid_potions ml-3"></div>
-        </div>
+        <h3>Habitica Celebrates Valentine's Day!</h3>
+        <p>In honor of Habitica's holiday celebrating all forms of love, whether it's friendship, familial, or romantic, some of the shopkeepers are dressed up! Take a look around to enjoy their new festive decorations.</p>
+        <div class="small mb-3">By Beffymaroo and Lemoness</div>
+        <div class="promo_valentines center-block"></div>
+        <h3>Send a Valentine</h3>
+        <p>Help motivate all of the lovely people in your life by sending them a caring Valentine. For the next week only, Valentines can be purchased for 10 gold from the <a href="/shops/market">Market</a>. For spreading love and joy throughout the community, both the giver AND the receiver get a coveted "Adoring Friends" badge. Hooray!</p>
+        <p>While you're there, why not check out the other cards that are available to send to your party? Each one gives a special achievement of its own...</p>
+        <div class="small mb-3">by Lemoness and SabreCat</div>
+        <h3>New Chat Performance Improvements</h3>
+        <p>We've deployed some behind-the-scenes improvements to the Tavern chat, which should cause the messages and avatars to load more quickly. Additionally, each chat message that you post in the Tavern and in Guilds will now remember the outfit your avatar was wearing when you post it, even on a refresh! We hope that these changes will make chatting even more enjoyable.</p>
+        <div class="small mb-3">By TheHollidayInn and Alys</div>
 
       </div>
       `,
