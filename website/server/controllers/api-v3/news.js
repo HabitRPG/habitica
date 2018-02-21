84dc1ab546e4a8884820d1c1fe02e849d3809ae2
SabreCat <sabrecat@gmail.com>
2018-02-21 17:35:33 +0000

    chore(news): Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'WORLD BOSS: THE DYSHEARTENER IS UNLEASHED';
+const LAST_ANNOUNCEMENT_TITLE = 'WORLD BOSS RAGE STRIKE, GUILD SPOTLIGHT, AND WIKI WEDNESDAY';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -32,23 +32,28 @@ api.getNews = {
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
           </div>
         </div>
-        <h2>2/14/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+        <h2>2/21/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
         <hr/>
-        <p>Oh no, a World Boss is attacking Habitica! Head to the Tavern to see it now. If you're on mobile, make sure that you have the latest versions downloaded to get the full experience!</p>
-        <div class="promo_dysheartener center-block"></div>
-        <p class="text-center">~*~</p>
-        <p>The sun is rising on Valentine's Day when a shocking crash splinters the air. A blaze of sickly pink light lances through all the buildings, and bricks crumble as a deep crack rips through Habit City's main street. An unearthly shrieking rises through the air, shattering windows as a hulking form slithers forth from the oozing earth.</p>
-        <p>Mandibles snap and a carapace glitters; legs upon legs unfurl in the air. The crowd begins to scream as the insectoid creature rears up, revealing itself to be none other than that cruelest of creatures: the fearsome Dysheartener itself. It howls in anticipation and lunges forward, hungering to gnaw on the hopes of hard-working Habiticans. With each rasping scrape of its spiny forelegs, you feel a vise of despair tightening in your chest.</p>
-        <p>"Take heart, everyone!" Lemoness shouts. "It probably thinks that we're easy targets because so many of us have daunting New Year's Resolutions, but it's about to discover that Habiticans know how to stick to their goals!"</p>
-        <p>AnnDeLune raises her staff. "Let's tackle our tasks and take this monster down!"</p>
-        <p class="text-center">~*~</p>
-        <p>Complete Habits, Dailies and To-Dos to damage the World Boss! Incomplete Dailies fill the Rage Strike Bar. When the Rage Strike bar is full, the World Boss will attack one of Habitica's shopkeepers. A World Boss will never damage individual players or accounts in any way. Only active accounts who are not resting in the Inn will have their incomplete Dailies tallied.</p>
-        <p>*If you’d prefer not to see the World Boss due to a phobia, check out the <a href="http://habitica.wikia.com/wiki/Phobia_Protection_Extension" target="_blank">Phobia Protection Extension</a> (and set it to hide “Beetles”) :)</p>
+        <h3>World Boss: Dysheartener attacks the Seasonal Sorceress!</h3>
+        <p>Oh, no! After feasting on our undone Dailies, the Dysheartener has gained the strength to unleash its Shattering Heartbreak attack. With a shrill shriek, it brings its spiny forelegs down upon the pavilion that houses <a href='/shops/seasonal'>the Seasonal Shop</a>! The concussive blast of magic shreds the wood, and the Seasonal Sorceress is overcome by sorrow at the sight.</p>
+        <p>Quickly, let's keep doing our Dailies so that the beast won't strike again!</p>
         <div class="small">by Lemoness, Beffymaroo, SabreCat, viirus, Apollo, and piyorii</div>
-        <div class="small">Art by AnnDeLune, Lemoness, and Beffymaroo</div>
-        <div class="small">Written by Lemoness</div>
-        <div class="small">Phobia Protection Extension by Alys</div>
-
+        <div class="media align-items-center">
+          <div class="media-body">
+            <h3>Sharing the Love: Guilds for Interpersonal Relationships</h3>
+            <p>There's a new <a href='https://habitica.wordpress.com/2018/02/19/sharing-the-love-guilds-for-interpersonal-relationships/' target='_blank'>Guild Spotlight on the blog</a> that highlights the Guilds that can help you as you work on building and maintaining your relationships with others! Check it out now to find Habitica's best places for support and help with your interpersonal connections.</p>
+            <div class="small">by Beffymaroo</div>
+          </div>
+          <div class="scene_tavern"></div>
+        </div>
+        <div class="media">
+          <div class="scene_coding mr-3"></div>
+          <div class="media-body">
+            <h3>Blog Post: Routines</h3>
+            <p>This month's <a href='https://habitica.wordpress.com/2018/02/21/routines/' target='_blank'>featured Wiki article</a> is about Routines! We hope that it will help you as you work on structuring your time and tasks. Be sure to check it out, and let us know what you think by reaching out on <a href='https://twitter.com/habitica' target='_blank'>Twitter</a>, <a href='http://blog.habitrpg.com' target='_blank'>Tumblr</a>, and <a href='https://facebook.com/habitica' target='_blank'>Facebook</a>.</p>
+            <div class="small">by Beffymaroo and the Wiki Wizards</div>
+          </div>
+        </div>
       </div>
       `,
     });
