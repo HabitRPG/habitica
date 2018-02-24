6e804339be74311ff4387e732786340fd6a25ad4
kartik adur <kartikadur@gmail.com>
2018-02-24 17:14:00 +0000

    add vue-test-utils pkg + unit test for column.vue

--- test/client/unit/specs/components/tasks/column.js
+++ test/client/unit/specs/components/tasks/column.js
@@ -1,48 +1,111 @@
 // VueJS produces following warnings if the corresponding imports are not used
 // [Vue warn]: You are using the runtime-only build of Vue where the template option is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
-import Vue from 'vue/dist/vue';
+// import Vue from 'vue/dist/vue';
+
+import { shallow } from '@vue/test-utils';
 
 import TaskColumn from 'client/components/tasks/column.vue';
 
+import Store from 'client/libs/store';
+
 // eslint-disable no-exclusive-tests
 
-describe('Task Column Component', () => {
-  let vm, Ctor, tasks;
+describe.only('Task Column', () => {
+  let wrapper, type;
 
-  describe('Passed Props', () => {
-    beforeEach(() => {
-      Ctor = Vue.extend(TaskColumn);
-    });
-
-    it('returns isUser prop as false by default', () => {
-      vm = new Ctor({propsData: {type: 'habit'}}).$mount();
-      expect(vm.isUser).to.eq(false);
-    });
-
-    it('returns isUser prop as passed by parent', () => {
-      vm = new Ctor({propsData: {type: 'habit', isUser: false}}).$mount();
-      expect(vm.isUser).to.eq(false);
-
-      vm = new Ctor({propsData: {type: 'habit', isUser: true}}).$mount();
-      expect(vm.isUser).to.eq(true);
-    });
-
-    afterEach(() => {
-      vm.$destroy();
+  beforeEach(() => {
+    type = 'habit';
+    wrapper = shallow(TaskColumn, {
+      propsData: {
+        type,
+      },
+      mocks: {
+        $t () {}, // vue-test-utils throws error if this is not mocked for i18n,
+      },
     });
   });
 
-  describe('Task Filtering', () => {
-    beforeEach(() => {
-      Ctor = Vue.extend(TaskColumn);
-      vm = new Ctor({
-        propsData: {
-          type: 'habit',
-        },
-      }).$mount();
+  it('returns a vue instance', () => {
+    expect(wrapper.isVueInstance()).to.be.true;
+  });
+
+  describe('Passed Properties', () => {
+    it('defaults isUser to false', () => {
+      expect(wrapper.vm.isUser).to.be.false;
     });
 
-    describe('by Tags', () => {
+    it('passes isUser to component instance', () => {
+      wrapper.setProps({ isUser: false });
+
+      expect(wrapper.vm.isUser).to.be.false;
+
+      wrapper.setProps({ isUser: true });
+
+      expect(wrapper.vm.isUser).to.be.true;
+    });
+  });
+
+  describe.only('Computed Properties', () => {
+    let habits, taskListOverride, store, getters, state;
+    beforeEach(() => {
+      type = 'habit';
+      habits = [
+        { id: 1 },
+        { id: 2 },
+      ];
+      taskListOverride = [
+        { id: 3 },
+        { id: 4 },
+      ];
+      getters = {
+        'tasks:getFilteredTaskList': (type, filter) => habits,
+      };
+      store = new Store({state, getters});
+    });
+
+    it('returns task list from props for group-plan', () => {
+      wrapper = shallow(TaskColumn, {
+        propsData: {
+          type,
+          taskListOverride,
+        },
+        mocks: {
+          $t () {}, // vue-test-utils throws error if this is not mocked for i18n,
+        },
+        store,
+      });
+
+      wrapper.vm.taskList.forEach((el, i) => {
+        expect(el).to.eq(taskListOverride[i]);
+      });
+    });
+
+    // it('returns task list from store for user', () => {
+    //   wrapper = shallow(TaskColumn, {
+    //     propsData: {
+    //       type,
+    //       taskListOverride,
+    //       isUser: true,
+    //     },
+    //     mocks: {
+    //       $t () {}, // vue-test-utils throws error if this is not mocked for i18n,
+    //     },
+    //     store,
+    //   });
+
+    //   wrapper.vm.taskList.forEach((el, i) => {
+    //     expect(el).to.eq(habits[i]);
+    //   });
+    //   // eslint-disable-next-line no-console
+    //   console.log(store);
+    //   // eslint-disable-next-line no-console
+    //   console.log(store._vm.$options.computed);
+    // });
+  });
+
+  describe('Task List', () => {
+    let tasks;
+    describe('Filter By Tags', () => {
       beforeEach(() => {
         tasks = [
           { tags: [3, 4] },
@@ -52,16 +115,16 @@ describe('Task Column Component', () => {
         ];
       });
 
-      it('returns all tasks when given no tags', () => {
-        let returnedTasks = vm.filterByTagList(tasks);
+      it('returns all tasks if no tag is given', () => {
+        let returnedTasks = wrapper.vm.filterByTagList(tasks);
         expect(returnedTasks).to.have.lengthOf(tasks.length);
         tasks.forEach((task, i) => {
           expect(returnedTasks[i]).to.eq(task);
         });
       });
 
-      it('returns all tasks with given single tag', () => {
-        let returnedTasks = vm.filterByTagList(tasks, [3]);
+      it('returns all tasks for given single tag', () => {
+        let returnedTasks = wrapper.vm.filterByTagList(tasks, [3]);
 
         expect(returnedTasks).to.have.lengthOf(3);
         expect(returnedTasks[0]).to.eq(tasks[0]);
@@ -69,15 +132,15 @@ describe('Task Column Component', () => {
         expect(returnedTasks[2]).to.eq(tasks[3]);
       });
 
-      it('returns all tasks with given multiple tags', () => {
-        let returnedTasks = vm.filterByTagList(tasks, [2, 3]);
+      it('returns all tasks for given multiple tags', () => {
+        let returnedTasks = wrapper.vm.filterByTagList(tasks, [2, 3]);
 
         expect(returnedTasks).to.have.lengthOf(1);
         expect(returnedTasks[0]).to.eq(tasks[1]);
       });
     });
 
-    describe('by Search Text', () => {
+    describe('Filter By Search Text', () => {
       beforeEach(() => {
         tasks = [
           {
@@ -111,42 +174,38 @@ describe('Task Column Component', () => {
         ];
       });
 
-      it('should return all tasks with empty search term', () => {
-        let returnedTasks = vm.filterBySearchText(tasks);
+      it('should return all tasks for empty search term', () => {
+        let returnedTasks = wrapper.vm.filterBySearchText(tasks);
         expect(returnedTasks).to.have.lengthOf(tasks.length);
         tasks.forEach((task, i) => {
           expect(returnedTasks[i]).to.eq(task);
         });
       });
 
-      it('should return tasks with search term in title /i', () => {
+      it('should return tasks for search term in title /i', () => {
         ['Title', 'TITLE', 'title', 'tItLe'].forEach((term) => {
-          expect(vm.filterBySearchText(tasks, term)[0]).to.eq(tasks[2]);
+          expect(wrapper.vm.filterBySearchText(tasks, term)[0]).to.eq(tasks[2]);
         });
       });
 
-      it('should return tasks with search term in note /i', () => {
+      it('should return tasks for search term in note /i', () => {
         ['Note', 'NOTE', 'note', 'nOtE'].forEach((term) => {
-          expect(vm.filterBySearchText(tasks, term)[0]).to.eq(tasks[3]);
+          expect(wrapper.vm.filterBySearchText(tasks, term)[0]).to.eq(tasks[3]);
         });
       });
 
-      it('should return tasks with search term in checklist title /i', () => {
+      it('should return tasks for search term in checklist title /i', () => {
         ['Check', 'CHECK', 'check', 'cHeCK'].forEach((term) => {
-          let returnedTasks = vm.filterBySearchText(tasks, term);
+          let returnedTasks = wrapper.vm.filterBySearchText(tasks, term);
 
           expect(returnedTasks[0]).to.eq(tasks[2]);
           expect(returnedTasks[1]).to.eq(tasks[3]);
         });
 
         ['Checkitem', 'CHECKITEM', 'checkitem', 'cHeCKiTEm'].forEach((term) => {
-          expect(vm.filterBySearchText(tasks, term)[0]).to.eq(tasks[3]);
+          expect(wrapper.vm.filterBySearchText(tasks, term)[0]).to.eq(tasks[3]);
         });
       });
-    });
-
-    afterEach(() => {
-      vm.$destroy();
     });
   });
 });
