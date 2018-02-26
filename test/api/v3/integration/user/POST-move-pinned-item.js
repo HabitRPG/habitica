874f1c8bd2fe31c3cc4c16a655f9a3d0d3658ae4
Kristofer Raske <kristofer.raske@wustl.edu>
2018-02-26 16:17:05 -0600

    Test failing still...
    This is worth a save. The api call grabs the seasonal items too, so we
    can't get away from using the common functions and calls here to get the
    actual list of items

--- test/api/v3/integration/user/POST-move-pinned-item.js
+++ test/api/v3/integration/user/POST-move-pinned-item.js
@@ -10,41 +10,61 @@ describe('POST /user/move-pinned-item/:path/move/to/:position', () => {
     user = await generateUser();
   });
 
-  // More tests in common code unit tests
-
-  it('adjusts the order of pinned items with no order mismatch', async () => {
+  it.only('adjusts the order of pinned items with no order mismatch', async () => {
     await user.update({
-      'pinnedItemsOrder':
-        ["hatchingPotions.Golden",
-         "cardTypes.greeting",
-         "armoire",
-         "gear.flat.weapon_warrior_1",
-         "gear.flat.head_warrior_1",
-         "cardTypes.thankyou",
-         "gear.flat.armor_warrior_1",
-         "food.Saddle",
-         "gear.flat.shield_warrior_1",
-         "potion"],
-    });
-
-    // Need to actually generate these items for the user, the new user will just have the base stuff and if they are different the pinnedItemsOrder will be overwritten
+      'pinnedItems': [
+        { type: 'armoire', path: 'armoire' },
+        { type: 'potion', path: 'potion' },
+        { type: 'marketGear', path: 'gear.flat.weapon_warrior_1' },
+        { type: 'marketGear', path: 'gear.flat.head_warrior_1' },
+        { type: 'marketGear', path: 'gear.flat.armor_warrior_1' },
+        { type: 'hatchingPotions', path: 'hatchingPotions.Golden' },
+        { type: 'marketGear', path: 'gear.flat.shield_warrior_1' },
+        { type: 'card', path: 'cardTypes.greeting' },
+        { type: 'potion', path: 'hatchingPotions.Golden' },
+        { type: 'card', path: 'cardTypes.thankyou' },
+        { type: 'food', path: 'food.Saddle' }
+      ],
+      'pinnedItemsOrder': [
+        'hatchingPotions.Golden',
+        'cardTypes.greeting',
+        'armoire',
+        'gear.flat.weapon_warrior_1',
+        'gear.flat.head_warrior_1',
+        'cardTypes.thankyou',
+        'gear.flat.armor_warrior_1',
+        'food.Saddle',
+        'gear.flat.shield_warrior_1',
+        'potion'
+      ],
+      });
 
     let res = await user.post('/user/move-pinned-item/armoire/move/to/5');
     await user.sync();
 
-    // Heck this is probably a good test. If mismatched overwrite items order first
-
-    expect(user.items.pets['Wolf-Base']).to.equal(5);
-    expect(user.items.eggs.Wolf).to.equal(0);
-    expect(user.items.hatchingPotions.Base).to.equal(0);
-
-    expect(res).to.eql({
-      message: t('messageHatched'),
-      data: JSON.parse(JSON.stringify(user.items)),
-    });
+    expect(user.pinnedItemsOrder[5]).to.equal('armoire');
+    expect(user.pinnedItemsOrder[2]).to.equal('gear.flat.weapon_warrior_1');
+    // The same as pinned items but now armoire is in position 5
+    expect(res).to.eql([
+      'hatchingPotions.Golden',
+      'cardTypes.greeting',
+      'gear.flat.weapon_warrior_1',
+      'gear.flat.head_warrior_1',
+      'cardTypes.thankyou',
+      'armoire',
+      'gear.flat.armor_warrior_1',
+      'food.Saddle',
+      'gear.flat.shield_warrior_1',
+      'potion'
+    ]);
   });
 
   it( 'adjusts the order of pinned items with order mismatch', async () => {
+
+  });
+
+  it( 'adjusts the order of pinned items with additional seasonal items', async () => {
+    // This is an important test but I am not sure how to do it
 
   });
 });
