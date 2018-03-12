aa0a8804214ac418ecb7d33f7925fa0ee9d58ca0
Kristofer Raske <kristofer.raske@wustl.edu>
2018-03-12 16:38:56 -0500

    Okay have the first test passing
    Need to clean up my linter problems though

--- test/api/v3/integration/user/POST-move-pinned-item.js
+++ test/api/v3/integration/user/POST-move-pinned-item.js
@@ -3,49 +3,68 @@ import {
   translate as t,
 } from '../../../../helpers/api-integration/v3';
 
+import getOfficialPinnedItems from '../../../../../website/common/script/libs/getOfficialPinnedItems.js';
+
 describe('POST /user/move-pinned-item/:path/move/to/:position', () => {
   let user;
+  let officialPinnedItems;
 
   beforeEach(async () => {
     user = await generateUser();
+    officialPinnedItems = getOfficialPinnedItems(user);
   });
 
   it.only('adjusts the order of pinned items with no order mismatch', async () => {
+
+    let testPinnedItems = [
+      { type: 'armoire', path: 'armoire' },
+      { type: 'potion', path: 'potion' },
+      { type: 'marketGear', path: 'gear.flat.weapon_warrior_1' },
+      { type: 'marketGear', path: 'gear.flat.head_warrior_1' },
+      { type: 'marketGear', path: 'gear.flat.armor_warrior_1' },
+      { type: 'hatchingPotions', path: 'hatchingPotions.Golden' },
+      { type: 'marketGear', path: 'gear.flat.shield_warrior_1' },
+      { type: 'card', path: 'cardTypes.greeting' },
+      { type: 'potion', path: 'hatchingPotions.Golden' },
+      { type: 'card', path: 'cardTypes.thankyou' },
+      { type: 'food', path: 'food.Saddle' }
+    ];
+
+    let testPinnedItemsOrder = [
+      'hatchingPotions.Golden',
+      'cardTypes.greeting',
+      'armoire',
+      'gear.flat.weapon_warrior_1',
+      'gear.flat.head_warrior_1',
+      'cardTypes.thankyou',
+      'gear.flat.armor_warrior_1',
+      'food.Saddle',
+      'gear.flat.shield_warrior_1',
+      'potion'
+    ];
+
+    let officialPinnedItemPaths = [];
+
+    // officialPinnedItems are returned in { type: ..., path:... } format
+    // but we just need the paths for testPinnedItemsOrder
+    if (officialPinnedItems.length > 0) {
+      officialPinnedItemPaths = officialPinnedItems.map(item => item.path);
+      // For this test put the seasonal items at the end where they can stay out of the way
+      testPinnedItemsOrder = testPinnedItemsOrder.concat(officialPinnedItemPaths);
+    }
+
     await user.update({
-      'pinnedItems': [
-        { type: 'armoire', path: 'armoire' },
-        { type: 'potion', path: 'potion' },
-        { type: 'marketGear', path: 'gear.flat.weapon_warrior_1' },
-        { type: 'marketGear', path: 'gear.flat.head_warrior_1' },
-        { type: 'marketGear', path: 'gear.flat.armor_warrior_1' },
-        { type: 'hatchingPotions', path: 'hatchingPotions.Golden' },
-        { type: 'marketGear', path: 'gear.flat.shield_warrior_1' },
-        { type: 'card', path: 'cardTypes.greeting' },
-        { type: 'potion', path: 'hatchingPotions.Golden' },
-        { type: 'card', path: 'cardTypes.thankyou' },
-        { type: 'food', path: 'food.Saddle' }
-      ],
-      'pinnedItemsOrder': [
-        'hatchingPotions.Golden',
-        'cardTypes.greeting',
-        'armoire',
-        'gear.flat.weapon_warrior_1',
-        'gear.flat.head_warrior_1',
-        'cardTypes.thankyou',
-        'gear.flat.armor_warrior_1',
-        'food.Saddle',
-        'gear.flat.shield_warrior_1',
-        'potion'
-      ],
-      });
+      'pinnedItems': testPinnedItems,
+      'pinnedItemsOrder': testPinnedItemsOrder,
+    });
 
     let res = await user.post('/user/move-pinned-item/armoire/move/to/5');
     await user.sync();
 
     expect(user.pinnedItemsOrder[5]).to.equal('armoire');
     expect(user.pinnedItemsOrder[2]).to.equal('gear.flat.weapon_warrior_1');
-    // The same as pinned items but now armoire is in position 5
-    expect(res).to.eql([
+
+    let expectedResponse = [
       'hatchingPotions.Golden',
       'cardTypes.greeting',
       'gear.flat.weapon_warrior_1',
@@ -56,15 +75,18 @@ describe('POST /user/move-pinned-item/:path/move/to/:position', () => {
       'food.Saddle',
       'gear.flat.shield_warrior_1',
       'potion'
-    ]);
+    ];
+    expectedResponse = expectedResponse.concat(officialPinnedItemPaths);
+
+    expect(res).to.eql(expectedResponse);
   });
 
   it( 'adjusts the order of pinned items with order mismatch', async () => {
 
-  });
-
-  it( 'adjusts the order of pinned items with additional seasonal items', async () => {
-    // This is an important test but I am not sure how to do it
+    // Shuffle all of them up and don't put the pinned items in...
 
   });
+
+  // TODO - this must be included in the other tests
+
 });
