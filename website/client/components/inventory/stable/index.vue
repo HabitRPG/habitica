42e0095bbd1385237312d2811e47794f5b1b1ec7
SabreCat <sabrecat@gmail.com>
2018-03-13 20:54:27 +0000

    feat(content): spring Hatching Potions

--- website/client/components/inventory/stable/index.vue
+++ website/client/components/inventory/stable/index.vue
@@ -904,7 +904,7 @@
         this.$store.dispatch('common:hatch', {egg: pet.eggKey, hatchingPotion: pet.potionKey});
 
         this.closeHatchPetDialog();
-        this.$root.$emit('hatchedPet::open', pet);
+        // this.$root.$emit('hatchedPet::open', pet);
       },
 
       onDragStart (ev, food) {
