8ff2a43bb00a790297ea5fddce3d25e8263a2eb5
Keith Holliday <keithrholliday@gmail.com>
2018-03-22 16:10:21 -0500

    Added apikey to cookie

--- website/client/libs/modform.js
+++ website/client/libs/modform.js
@@ -1,5 +1,21 @@
+// @TODO: I have abstracted this in another PR. Use that function when merged
+function getApiKey () {
+  let AUTH_SETTINGS = localStorage.getItem('habit-mobile-settings');
+
+  if (AUTH_SETTINGS) {
+    AUTH_SETTINGS = JSON.parse(AUTH_SETTINGS);
+
+    if (AUTH_SETTINGS.auth && AUTH_SETTINGS.auth.apiId && AUTH_SETTINGS.auth.apiToken) {
+      return AUTH_SETTINGS.auth.apiToken;
+    }
+  }
+}
+
 export function goToModForm (user) {
   if (!user) return;
+
+  const apiKey = getApiKey();
+  if (!apiKey) return;
 
   const tenMins = 10 * 60 * 1000;
   let dateTime;
@@ -13,6 +29,7 @@ export function goToModForm (user) {
     email,
     profileName: user.profile.name,
     uuid: user._id,
+    apiKey,
   };
 
   document.cookie = `habiticauserdata=${JSON.stringify(userData)};${expires};domain=.habitica.com;path=/`;
