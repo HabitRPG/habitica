c18284391eb339882abba91d3d84fe00aca7cb08
Alys <alice.harris@oldgods.net>
2018-03-24 19:54:27 +1000

    fix existing Hourglass and Gem Cap tests that were wrong
    The scenario originally used for these two tests was a six-month recurring
    subscription (you can tell that from the starting offset having a non-zero value).
    For recurring subscriptions, we do NOT want to increase the consecutive month
    benefits as soon as the sixth month starts because the user has already been
    given a full six months' benefits in advance and they might cancel the
    subscription before it renews later in the sixth month.
    Therefore we want to give the extra benefits at the beginning of the seventh
    month (ideally we'd give them mid-month in the sixth month when the renewal
    happens but we don't have support for tracking renewal dates).
    So, the two changed tests were actually not correct for the case
    where the offset started as non-zero.
    
    These tests are correct for one-month recurring subscriptions (when the offset
    is never set to anything above zero). The user isn't meant to get any consecutive
    month benefits until a multiple of 3 months has been reached.

--- test/api/v3/unit/libs/cron.test.js
+++ test/api/v3/unit/libs/cron.test.js
@@ -117,12 +117,11 @@ describe('cron', () => {
       expect(user.purchased.plan.consecutive.offset).to.equal(1);
     });
 
-    it('increments plan.consecutive.trinkets when user has reached a month that is a multiple of 3', () => {
+    it('increments plan.consecutive.trinkets when one-month-recurring subscription has reached a month that is a multiple of 3', () => {
       user.purchased.plan.consecutive.count = 5;
-      user.purchased.plan.consecutive.offset = 1;
       cron({user, tasksByType, daysMissed, analytics});
       expect(user.purchased.plan.consecutive.trinkets).to.equal(1);
-      expect(user.purchased.plan.consecutive.offset).to.equal(0);
+      expect(user.purchased.plan.consecutive.count).to.equal(6);
     });
 
     it('increments plan.consecutive.trinkets multiple times if user has been absent with continuous subscription', () => {
@@ -143,12 +142,11 @@ describe('cron', () => {
       expect(user.purchased.plan.consecutive.trinkets).to.equal(1);
     });
 
-    it('increments plan.consecutive.gemCapExtra when user has reached a month that is a multiple of 3', () => {
+    it('increments plan.consecutive.gemCapExtra when one-month-recurring subscription has reached a month that is a multiple of 3', () => {
       user.purchased.plan.consecutive.count = 5;
-      user.purchased.plan.consecutive.offset = 1;
       cron({user, tasksByType, daysMissed, analytics});
       expect(user.purchased.plan.consecutive.gemCapExtra).to.equal(5);
-      expect(user.purchased.plan.consecutive.offset).to.equal(0);
+      expect(user.purchased.plan.consecutive.count).to.equal(6);
     });
 
     it('increments plan.consecutive.gemCapExtra multiple times if user has been absent with continuous subscription', () => {
