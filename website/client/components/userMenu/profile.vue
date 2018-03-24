dcd680c2931f0fdae12f59b607016cbfbaa15134
Mark Kuba <tmkuba@users.noreply.github.com>
2018-03-24 09:15:40 -0700

    Fix/mana bar unselected class - fix: #10026 (#10126)
    * Update getClass() for users who have not yet selected a class
    
    * Added tests for members.getClass()
    
    * fix linter errors
    
    * Update test
    
    * Update import in test to point to correct module
    
    * use hasClass() getter where appropriate
    
    * Fix linter error

--- website/client/components/userMenu/profile.vue
+++ website/client/components/userMenu/profile.vue
@@ -235,7 +235,7 @@ div
                 li
                   strong {{$t('buffs')}}:
                   | {{user.stats.buffs[stat]}}
-      #allocation(v-if='user._id === userLoggedIn._id && user.flags.classSelected && !user.preferences.disableClasses')
+      #allocation(v-if='user._id === userLoggedIn._id && hasClass')
         .row.title-row
           .col-12.col-md-6
             h3(v-if='userLevel100Plus', v-once, v-html="$t('noMoreAllocate')")
@@ -732,6 +732,9 @@ export default {
     },
     startingPageOption () {
       return this.$store.state.profileOptions.startingPage;
+    },
+    hasClass () {
+      return this.$store.getters['members:hasClass'](this.userLoggedIn);
     },
   },
   watch: {
