6f1a551d7622c873ed152ed727ac2de05b14618b
SabreCat <sabrecat@gmail.com>
2018-03-30 01:12:38 +0000

    chore(news): Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'MARCH SUBSCRIBER ITEMS AND BEHIND THE SCENES BLOG';
+const LAST_ANNOUNCEMENT_TITLE = 'LAST CHANCE FOR SUBSCRIBER ITEMS, MAGIC POTIONS AVAILABLE INTO APRIL, AND COMMUNITY GUIDELINES UPDATES';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -32,25 +32,37 @@ api.getNews = {
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
           </div>
         </div>
-        <h2>3/22/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+        <h2>3/29/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
         <hr/>
         <div class="media align-items-center">
+          <div class="promo_rainbow_potions mr-3 mb-3"></div>
           <div class="media-body">
-            <h3>March Subscriber Items Revealed!</h3>
-            <p>The March Subscriber Item has been revealed: the Daring Dragonfly Set! You'll receive this set when you <a href='/user/settings/subscription' target='_blank'>subscribe</a> by March 31! If you're already an active subscriber, reload the site and then head to Inventory > Equipment to claim your gear!</p>
+            <div class="media align-items-center">
+              <div class="media-body">
+                <h3>Last Chance for Daring Dragonfly Set</h3>
+                <p>Reminder: this is the final day to <a href='/user/settings/subscription' target='_blank'>subscribe</a> and receive the Daring Dragonfly Set! Subscribing also lets you buy gems for gold. The longer your subscription, the more gems you get!</p>
+                <p>Thanks so much for your support! You help keep Habitica running.</p>
+                <div class="small mb-3">by Beffymaroo</div>
+              </div>
+              <div class="promo_mystery_201803"></div>
+            </div>
+            <h3>Extended Availability for Shimmer and Rainbow Hatching Potions</h3>
+            <p>Due to popular demand, we've extended the availability of the Rainbow and Shimmer Magic Hatching Potions until Spring Fling ends on April 30! Be sure to grab them from the <a href='/shops/market' target='_blank'>Market</a> before then! If they come back, it won't be until next year at the earliest, so don't delay!</p>
+            <div class="small mb-3">by Beffymaroo, Teto Forever, tricksy.fox, and SabreCat</div>
           </div>
-          <div class="promo_mystery_201803"></div>
         </div>
-        <p>Subscribers also receive the ability to buy Gems for Gold -- the longer you subscribe, the more Gems you can buy per month! There are other perks as well, such as longer access to uncompressed data and a cute Jackalope pet. Best of all, subscriptions let us keep Habitica running. Thank you very much for your support -- it means a lot to us.</p>
-        <div class="small mb-3">by Beffymaroo</div>
         <div class="media align-items-center">
-          <div class="scene_podcast mb-3 mr-3"></div>
           <div class="media-body">
-            <h3>Behind the Scenes: Staff Podcast Picks!</h3>
-            <p>Looking for new podcasts? The Habitica team loves podcasts, and in this special post we wanted to share some favorites that help us get things done, learn, and have fun! Come check them out in <a href='https://habitica.wordpress.com/2018/03/22/behind-the-scenes-habitica-staff-podcast-picks/' target='_blank'>this month's Behind the Scenes feature!</a></p>
-            <div class="small mb-3">by Beffymaroo and the rest of the Habitica team</div>
+            <h3>Community Guidelines Updates</h3>
+            <p>We've made some important updates to <a href='/static/community-guidelines' target='_blank'>the Community Guidelines</a> to make it easier to read, as well as to clarify some policies on subjects such as promotional Challenges, spamming, and more. <a href='/static/community-guidelines' target='_blank'>Be sure to read it</a> before you chat in the <a href='/groups/tavern' target='_blank'>Tavern</a>, in <a href='/groups/discovery' target='_blank'>Guilds</a>, or in your <a href='/party' target='_blank'>Party</a>!</p>
+            <p>Thanks for helping us keep Habitica's community happy and safe!</p>
+            <div class="small mb-3">by shanaqui and the marvelous moderators</div>
           </div>
+          <div class="community_guidelines"></div>
         </div>
+        <h3>Moderator Contact Form</h3>
+        <p>Encountering a community issue? There's a new, simple way to get in touch with the moderator team: <a href='http://contact.habitica.com' target='_blank'>the moderator contact form</a>! You can access it under the Help menu, under the Helpful Links in the <a href='/groups/tavern' target='_blank'>Tavern</a> sidebar, or on <a href='/static/contact' target='_blank'>the Contact Us page</a>. This is always the best and fastest way to contact a moderator if you need help. We hope that it will be useful!</p>
+        <div class="small mb-3">by TheHollidayInn, Lemoness, and Alys</p>
       </div>
       `,
     });
