d6b5152389bd57f8b23a28db55448a09307fe07f
fentie <brian.fenton@wheniwork.com>
2018-04-09 21:11:03 -0500

    Turning H2s into anchors to add hand cursor and to create addressable page fragments adding ref to target individual entries adding scroll handler to make sure expanded result is in view.

--- website/client/components/static/faq.vue
+++ website/client/components/static/faq.vue
@@ -4,7 +4,8 @@
       .col-12.col-md-6.offset-md-3
         h1 {{ $t('frequentlyAskedQuestions') }}
         .faq-question(v-for='(heading, index) in headings')
-          h2.accordion(@click='setActivePage(heading)') {{ $t(`faqQuestion${index}`) }}
+          h2.accordion(:ref='uniqueRef(index)')
+            a(v-on:click.stop.prevent="" href="#", :id='heading', @click='setActivePage(heading)') {{ $t(`faqQuestion${index}`) }}
           div(v-if='pageState[heading]', v-markdown="$t('webFaqAnswer' + index, replacements)")
         hr
         p(v-markdown="$t('webFaqStillNeedHelp')")
@@ -13,6 +14,11 @@
 <style lang='scss' scoped>
   .faq-question {
     margin-bottom: 1em;
+  }
+
+  .faq-question a {
+    text-decoration: none;
+    color: #4F2A93;
   }
 
   @media only screen and (max-width: 768px) {
@@ -25,6 +31,9 @@
 <script>
   // @TODO:  env.EMAILS.TECH_ASSISTANCE_EMAIL
   const TECH_ASSISTANCE_EMAIL = 'admin@habitica.com';
+  const NAVBAR_HEIGHT = 56;
+
+  import Vue from 'vue';
   import markdownDirective from 'client/directives/markdown';
 
   export default {
@@ -72,6 +81,19 @@
       setActivePage (page) {
         this.pageState[page] = !this.pageState[page];
       },
+      uniqueRef (index) {
+        return this.headings[index];
+      },
+    },
+    mounted () {
+      const hash = window.location.hash.replace('#', '');
+      if (hash && this.headings.includes(hash)) {
+        this.setActivePage(hash);
+      }
+      Vue.nextTick(() => {
+        if (!this.$refs[hash] || !this.$refs[hash][0]) return;
+        window.scroll(0, this.$refs[hash][0].getBoundingClientRect().top - NAVBAR_HEIGHT);
+      });
     },
   };
 </script>
