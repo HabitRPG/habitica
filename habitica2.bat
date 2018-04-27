c95fb7dd27109ee7f8881e2ddfb1cffe4d287a74
Shadi Moustafa <snasser2015@my.fit.edu>
2018-04-27 01:02:12 -0400

    remove habitica2.bat

--- habitica2.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-start cmd /k " cd C:\Program Files\MongoDB\Server\3.4\bin\ & mongod"
-
-start cmd /k " cd C:\habitica2\ & npm run client:dev"
-
-start cmd /k " cd C:\habitica2\ & npm start"
