bcf9670dbe08a38a47db7e22db75fe3d66095906
SabreCat <sabrecat@gmail.com>
2018-05-01 20:55:16 +0000

    feat(content): Armoire and backgrounds May 2018

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'LAST CHANCE FOR APRIL GOODIES!';
+const LAST_ANNOUNCEMENT_TITLE = 'NEW BACKGROUNDS, ARMOIRE ITEMS, AND OFFICIAL HABITICA CHALLENGES FOR MAY';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -32,28 +32,34 @@ api.getNews = {
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
           </div>
         </div>
-        <h2>4/30/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+        <h2>5/1/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
         <hr/>
-        <div class="media align-items-center">
-          <div class="promo_spring_fling_2018 mr-3"></div>
+        <div class="media">
           <div class="media-body">
-            <div class="media">
+            <h3>New Backgrounds and Armoire Items</h3>
+            <p>We’ve added three new backgrounds to the Background Shop! Now your avatar can tiptoe through a Terraced Rice Field, bask in the glory of the Champions' Colosseum, and look for fun new footwear in the Fantastical Shoe Store. Check them out under User Icon > Backgrounds!</p>
+            <p>Plus, there’s new gold-purchasable equipment in the Enchanted Armoire, including the Cobbler Set. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)</p>
+            <div class="small mb-3">by Katy133, confusus, TheMushroomKing, khdarkwolf, RandomGryffindor, ChimeraLiani, and Mewrose</div>
+            <div class="media align-items-center">
+              <div class="scene_todos"></div>
               <div class="media-body">
-                <h3>Last Chance for Spiffy Squirrel Set</h3>
-                <p>Reminder: this is the final day to <a href='/user/settings/subscription' target='_blank'>subscribe and receive the Spiffy Squirrel Set</a>! Subscribing also lets you buy gems for gold. The longer your subscription, the more gems you get!</p>
+                <h3>May 2018 Resolution Success Challenge and Take This Challenge</h3>
+                <p>The Habitica team has launched a special official Challenge series hosted in the <a href='/groups/guild/6e6a8bd3-9f5f-4351-9188-9f11fcd80a99' target='_blank'>Official New Year's Resolution Guild</a>. These Challenges are designed to help you build and maintain goals that are destined for success and then stick with them as the year progresses. For this month's Challenge, <a href='/challenges/2c524717-699e-4fd6-acb6-dd868bf537dd' target='_blank'>Review your Combat Tactics</a>, we're focusing on refining your strategy to help you stay motivated and keep moving forward as we're almost halfway through the year! It has a 15 gem prize, which will be awarded to five lucky winners on June 1st.</p>
               </div>
-              <div class="promo_mystery_201804"></div>
             </div>
-            <p>Thanks so much for your support! You help keep Habitica running.</p>
-            <div class="small mb-3">by Beffymaroo</div>
-            <h3>Last Chance for Spring Fling Hatching Potions, Equipment, Avatar Skins and Hair Colors, and Seasonal Shop Items</h3>
-            <p>Spring Fling is coming to a close, so be sure to grab any exciting items you've had your eye on! This includes the <a href='/shops/market' target='_blank'>Shimmer and Rainbow Hatching Potions in the Market</a>. If they come back, it won't be until next year at the earliest, so don't delay!</p>
-            <p>Pastel Skins and Shimmer Hair colors will also disappear when the Gala ends. If you purchase them in User > Avatar, you can use them year-round!</p>
-            <p>Be sure to also get your limited edition Spring Fling equipment in your Rewards Column! When it returns next year, it will be in the Seasonal Shop and will be available for gems rather than gold.</p>
-            <p>And lastly, it's a great time to stock up on items from the <a href='/shops/seasonal' target='_blank'>Seasonal Shop</a> before it closes. This includes past Spring Fling equipment, the Egg Hunt Quest, and Shiny Seeds!</p>
-            <div class="small mb=3">by Scarvia, Awesome kitty, usnbfs, Lemoness, Balduranne, PainterProphet, Vikte, Lalaitha, DialFForFunky, Gerald the Pixel, Beffymaroo and SabreCat</div>
           </div>
+          <div class="promo_armoire_backgrounds_201805 ml-3"></div>
         </div>
+        <p>Congratulations to the winners of the April Challenge: V-Starr, Carlos Víquez, lilliburlero, NowyChris, and PizzaMyHeart!</p>
+        <div class="media">
+          <div class="media-body">
+            <p>The next Take This Challenge has also launched, "<a href='/challenges/4ee6bb54-1c29-487b-a2de-07e6be24cccd' target='_blank'>Keep Calm and Carry On!</a>", with a focus on deep breathing to settle stressful emotions. Be sure to check it out to earn additional pieces of the Take This armor set!</p>
+            <p><a href='http://www.takethis.org/' target='_blank'>Take This</a> is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness.</p>
+          </div>
+          <div class="promo_take_this"></div>
+        </div>
+        <p>Congratulations to the winners of the last Take This Challenge, "Test Thy Courage!": grand prize winner Nietos, and runners-up Teslectrik, Mflute, Kolthar, lilyandrosemary, and thewandererrae! Plus, all participants in that Challenge have received a piece of the <a href='http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set' target='_blank'>Take This item set</a> if they didn't have all the pieces already. It is located in your Rewards column. Enjoy!</p>
+        <div class="small mb-3">by Doctor B, the Take This team, Lemoness, Beffymaroo, and SabreCat</div>
       </div>
       `,
     });
