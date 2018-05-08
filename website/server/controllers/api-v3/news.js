24cceb1c91f291c9d83c09135e3fbf7cd1c5e6f7
SabreCat <sabrecat@gmail.com>
2018-05-08 18:28:33 +0000

    feat(content): Cuddle Bundle

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'NEW BACKGROUNDS, ARMOIRE ITEMS, AND OFFICIAL HABITICA CHALLENGES FOR MAY';
+const LAST_ANNOUNCEMENT_TITLE = 'CUDDLE BUDDIES QUEST BUNDLE AND TIPS FOR PARTY MOTIVATION';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -26,40 +26,32 @@ api.getNews = {
     res.status(200).send({
       html: `
       <div class="bailey">
-        <div class="media">
-          <div class="align-self-center mr-3 ${baileyClass}"></div>
+        <div class="media align-items-center">
           <div class="media-body">
-            <h1 class="align-self-center">${res.t('newStuff')}</h1>
-          </div>
-        </div>
-        <h2>5/1/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
-        <hr/>
-        <div class="media">
-          <div class="media-body">
-            <h3>New Backgrounds and Armoire Items</h3>
-            <p>We’ve added three new backgrounds to the Background Shop! Now your avatar can tiptoe through a Terraced Rice Field, bask in the glory of the Champions' Colosseum, and look for fun new footwear in the Fantastical Shoe Store. Check them out under User Icon > Backgrounds!</p>
-            <p>Plus, there’s new gold-purchasable equipment in the Enchanted Armoire, including the Cobbler Set. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)</p>
-            <div class="small mb-3">by Katy133, confusus, TheMushroomKing, khdarkwolf, RandomGryffindor, ChimeraLiani, and Mewrose</div>
-            <div class="media align-items-center">
-              <div class="scene_todos"></div>
+            <div class="media">
+              <div class="align-self-center mr-3 ${baileyClass}"></div>
               <div class="media-body">
-                <h3>May 2018 Resolution Success Challenge and Take This Challenge</h3>
-                <p>The Habitica team has launched a special official Challenge series hosted in the <a href='/groups/guild/6e6a8bd3-9f5f-4351-9188-9f11fcd80a99' target='_blank'>Official New Year's Resolution Guild</a>. These Challenges are designed to help you build and maintain goals that are destined for success and then stick with them as the year progresses. For this month's Challenge, <a href='/challenges/2c524717-699e-4fd6-acb6-dd868bf537dd' target='_blank'>Review your Combat Tactics</a>, we're focusing on refining your strategy to help you stay motivated and keep moving forward as we're almost halfway through the year! It has a 15 gem prize, which will be awarded to five lucky winners on June 1st.</p>
+                <h1 class="align-self-center">${res.t('newStuff')}</h1>
               </div>
             </div>
+            <h2>5/8/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <hr/>
+            <h3>New Discounted Pet Quest Bundle: Cuddle Buddies!</h3>
+            <p>If you are looking to add some pets to your Habitica stable, you're in luck! From now until 31-May, you can purchase the Cuddle Buddies Pet Quest Bundle and receive the Bunny, Ferret, and Guinea Pig quests, all for only 7 Gems! That's a discount of 5 Gems from the price of purchasing them separately. Check it out in the <a href="/shops/quests" target="_blank">Quest Shop</a> today!</p>
+            <div class="small">by Beffymaroo and SabreCat</div>
+            <div class="small">Art by Willow the Witty, mewrose, Stefalupagus, Pandah, UncommonCriminal, Gully, Draayder, Teto Forever, PainterProphet, Beffymaroo, Faye, RBrinks, Pocketmole, and James Danger</div>
+            <div class="small mb-3">Writing by Lilith of Alfheim, Teto Forever, and Bartelmy</div>
           </div>
-          <div class="promo_armoire_backgrounds_201805 ml-3"></div>
+          <div class="promo_bundle_cuddleBuddies ml-3"></div>
         </div>
-        <p>Congratulations to the winners of the April Challenge: V-Starr, Carlos Víquez, lilliburlero, NowyChris, and PizzaMyHeart!</p>
-        <div class="media">
+        <div class="media align-items-center">
+          <div class="scene_casting_spells mr-3 mb-3"></div>
           <div class="media-body">
-            <p>The next Take This Challenge has also launched, "<a href='/challenges/4ee6bb54-1c29-487b-a2de-07e6be24cccd' target='_blank'>Keep Calm and Carry On!</a>", with a focus on deep breathing to settle stressful emotions. Be sure to check it out to earn additional pieces of the Take This armor set!</p>
-            <p><a href='http://www.takethis.org/' target='_blank'>Take This</a> is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness.</p>
+            <h3>Blog Post: Keeping Parties Motivated</h3>
+            <p>Did you catch our latest <a href="https://habitica.wordpress.com/2018/04/25/keeping-parties-motivated/" target="_blank">featured Wiki article</a>? It's about Keeping Parties Motivated! We hope that it will help you as you battle Habitica's bosses and your tasks with your friends. Be sure to check it out, and let us know what you think by reaching out on <a href="https://twitter.com/habitica" target="_blank">Twitter</a>, <a href="http://blog.habitrpg.com" target="_blank">Tumblr</a>, and <a href="https://facebook.com/habitica" target="_blank">Facebook</a>.</p>
+            <div class="small mb-3">by shanaqui and the Wiki Wizards</div>
           </div>
-          <div class="promo_take_this"></div>
         </div>
-        <p>Congratulations to the winners of the last Take This Challenge, "Test Thy Courage!": grand prize winner Nietos, and runners-up Teslectrik, Mflute, Kolthar, lilyandrosemary, and thewandererrae! Plus, all participants in that Challenge have received a piece of the <a href='http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set' target='_blank'>Take This item set</a> if they didn't have all the pieces already. It is located in your Rewards column. Enjoy!</p>
-        <div class="small mb-3">by Doctor B, the Take This team, Lemoness, Beffymaroo, and SabreCat</div>
       </div>
       `,
     });
