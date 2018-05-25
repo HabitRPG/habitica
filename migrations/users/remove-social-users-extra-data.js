12255e49fe67942ba8614126b555085302d4060a
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2018-05-25 17:21:08 +0200

    add migration to remove extra data from social profiles

---
+++ migrations/users/remove-social-users-extra-data.js
@@ -0,0 +1,109 @@
+const migrationName = 'remove-social-users-extra-data.js';
+const authorName = 'paglias'; // in case script author needs to know when their ...
+const authorUuid = 'ed4c688c-6652-4a92-9d03-a5a79844174a'; // ... own data is done
+
+/*
+ * Award this month's mystery items to subscribers
+ */
+const connectionString = 'mongodb://localhost:27017/habitrpg?auto_reconnect=true'; // FOR TEST DATABASE
+
+const monk = require('monk');
+const dbUsers = monk(connectionString).get('users', { castIds: false });
+
+function processUsers (lastId) {
+  // specify a query to limit the affected users (empty for all users):
+  let query = {
+    migration: {$ne: migrationName},
+    $or: [
+      { 'auth.facebook.id': { $exists: true } },
+      { 'auth.google.id': { $exists: true } },
+    ],
+  };
+
+  if (lastId) {
+    query._id = {
+      $gt: lastId,
+    };
+  }
+
+  dbUsers.find(query, {
+    sort: {_id: 1},
+    limit: 250,
+  })
+    .then(updateUsers)
+    .catch((err) => {
+      console.log(err);
+      return exiting(1, `ERROR! ${  err}`);
+    });
+}
+
+let progressCount = 1000;
+let count = 0;
+
+function updateUsers (users) {
+  if (!users || users.length === 0) {
+    console.warn('All appropriate users found and modified.');
+    displayData();
+    return;
+  }
+
+  let userPromises = users.map(updateUser);
+  let lastUser = users[users.length - 1];
+
+  return Promise.all(userPromises)
+    .then(() => {
+      processUsers(lastUser._id);
+    });
+}
+
+function updateUser (user) {
+  count++;
+
+  const isFacebook = user.auth.facebook && user.auth.facebook.id;
+  const isGoogle = user.auth.facebook && user.auth.facebook.id;
+
+  const update = { $set: {} };
+
+  if (isFacebook) {
+    update.$set['auth.facebook'] = {
+      id: user.auth.facebook.id,
+      emails: user.auth.facebook.emails,
+    };
+  }
+
+  if (isGoogle) {
+    update.$set['auth.google'] = {
+      id: user.auth.google.id,
+      emails: user.auth.google.emails,
+    };
+  }
+
+  dbUsers.update({
+    _id: user._id,
+  }, update);
+
+  if (count % progressCount === 0) console.warn(`${count  } ${  user._id}`);
+  if (user._id === authorUuid) console.warn(`${authorName  } processed`);
+}
+
+function displayData () {
+  console.warn(`\n${  count  } users processed\n`);
+  return exiting(0);
+}
+
+function exiting (code, msg) {
+  code = code || 0; // 0 = success
+  if (code && !msg) {
+    msg = 'ERROR!';
+  }
+  if (msg) {
+    if (code) {
+      console.error(msg);
+    } else      {
+      console.log(msg);
+    }
+  }
+  process.exit(code);
+}
+
+module.exports = processUsers;
