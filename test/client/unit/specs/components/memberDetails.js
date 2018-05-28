dc32b32bfdb723d36982ee2ac62a9b9a4ed49cde
haydenbetts <haydenbetts@gmail.com>
2018-05-28 12:14:55 -0700

    added unit test for flicker prevention style

---
+++ test/client/unit/specs/components/memberDetails.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import MemberDetailsComponent from 'client/components/memberDetails.vue';
+
+  describe('Members Details Component', () => {
+    let CTor;
+    let vm;
+
+    beforeEach(() => {
+      CTor = Vue.extend(MembersModalComponent);
+      vm = new CTor().$mount();
+    });
+
+    afterEach(() => {
+      vm.$destroy();
+    });
+
+    it.only('prevents flickering by setting a 1px margin-right on elements of class member-stats', () => {
+      expect(vm.find('.member-stats').element.style['margin-right']).to.equal('1');
+    });
+  });
