c4867f1e8e23b8d0fca9163da74f742670f4d4eb
Sabe Jones <sabrecat@gmail.com>
2018-05-29 22:16:30 +0000

    chore(i18n): update locales

--- migrations/migration-runner.js
+++ migrations/migration-runner.js
@@ -17,5 +17,5 @@ function setUpServer () {
 setUpServer();
 
 // Replace this with your migration
-const processUsers = require('./groups/migrate-chat.js');
+const processUsers = require('./users/mystery-items.js');
 processUsers();
