6927e23f2ece8e72472c6f53adefe8f0aaeda517
haydenbetts <haydenbetts@gmail.com>
2018-05-29 20:54:19 -0700

    rewrote margin test using computed style

--- test/client/unit/specs/components/memberDetails.js
+++ test/client/unit/specs/components/memberDetails.js
@@ -1,20 +1,23 @@
 import Vue from 'vue';
 import MemberDetailsComponent from 'client/components/memberDetails.vue';
 
-  describe('Members Details Component', () => {
-    let CTor;
-    let vm;
+describe('Members Details Component', () => {
+  let CTor;
+  let vm;
 
-    beforeEach(() => {
-      CTor = Vue.extend(MembersModalComponent);
-      vm = new CTor().$mount();
-    });
-
-    afterEach(() => {
-      vm.$destroy();
-    });
-
-    it('prevents flickering by setting a 1px margin-right on elements of class member-stats', () => {
-      expect(vm.find('.member-stats').element.style['margin-right']).to.equal('1');
-    });
+  beforeEach(() => {
+    CTor = Vue.extend(MemberDetailsComponent);
+    vm = new CTor().$mount();
   });
+
+  afterEach(() => {
+    vm.$destroy();
+  });
+
+  it('prevents flickering by setting a 1px margin-right on elements of class member-stats', () => {
+    const memberstats = vm.$el.querySelector('.member-stats');
+    const style = window.getComputedStyle(memberstats, null);
+    const marginRightProp = style.getPropertyValue('margin-right');
+    expect(marginRightProp).to.equal('1');
+  });
+});
