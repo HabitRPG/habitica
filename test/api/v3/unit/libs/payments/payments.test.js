301beea185a587f10f28ea0fc2431ec64eeac22e
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2018-06-01 15:29:22 +0200

    fix tests

--- test/api/v3/unit/libs/payments/payments.test.js
+++ test/api/v3/unit/libs/payments/payments.test.js
@@ -638,7 +638,7 @@ describe('payments/index', () => {
         await api.buyGems(data);
 
         const msg = `\`Hello recipient, sender has sent you 4 gems!\` ${data.gift.message}`;
-        expect(user.sendMessage).to.be.calledWith(recipient, { receiverMsg: msg, senderMsg: msg });
+        expect(user.sendMessage).to.be.calledWith(recipient, { receiverMsg: msg, senderMsg: msg, save: false });
       });
 
       it('sends a push notification if user did not gift to self', async () => {
@@ -667,7 +667,7 @@ describe('payments/index', () => {
           return `\`${messageContent}\``;
         });
 
-        expect(user.sendMessage).to.be.calledWith(recipient, { receiverMsg: recipientsMessageContent, senderMsg: sendersMessageContent });
+        expect(user.sendMessage).to.be.calledWith(recipient, { receiverMsg: recipientsMessageContent, senderMsg: sendersMessageContent, save: false });
       });
     });
   });
