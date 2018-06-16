f4243f1927c7d92e27d7ce876ea8370c4a19fd5c
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2018-06-16 16:08:51 +0200

    rename v3app to app

--- website/server/middlewares/appRoutes.js
+++ website/server/middlewares/appRoutes.js
@@ -8,29 +8,29 @@ import path from 'path';
 const API_V3_CONTROLLERS_PATH = path.join(__dirname, '/../controllers/api-v3/');
 const TOP_LEVEL_CONTROLLERS_PATH = path.join(__dirname, '/../controllers/top-level/');
 
-const v3app = express();
+const app = express();
 
 // re-set the view options because they are not inherited from the top level app
-v3app.set('view engine', 'pug');
-v3app.set('views', `${__dirname}/../../views`);
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/../../views`);
 
-v3app.use(expressValidator());
-v3app.use(analytics);
-v3app.use(setupBody);
+app.use(expressValidator());
+app.use(analytics);
+app.use(setupBody);
 
 const topLevelRouter = express.Router(); // eslint-disable-line new-cap
 
 routes.walkControllers(topLevelRouter, TOP_LEVEL_CONTROLLERS_PATH);
-v3app.use('/', topLevelRouter);
+app.use('/', topLevelRouter);
 
 const v3Router = express.Router(); // eslint-disable-line new-cap
 routes.walkControllers(v3Router, API_V3_CONTROLLERS_PATH);
-v3app.use('/api/v3', v3Router);
+app.use('/api/v3', v3Router);
 
 // API v4 proxies API v3
 // Can also disable or override with its own version v3 routes
 const v4Router = express.Router(); // eslint-disable-line new-cap
 routes.walkControllers(v4Router, API_V3_CONTROLLERS_PATH);
-v3app.use('/api/v4', v4Router);
+app.use('/api/v4', v4Router);
 
-module.exports = v3app;
+module.exports = app;
