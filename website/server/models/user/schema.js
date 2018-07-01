e87f9c1d2397bc0bcd097f9c42c1c6be5663ba15
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2018-07-01 17:46:45 +0200

    add back messages in user schema

--- website/server/models/user/schema.js
+++ website/server/models/user/schema.js
@@ -557,7 +557,12 @@ let schema = new Schema({
   notifications: [UserNotificationSchema],
   tags: [TagSchema],
 
-  inbox: { // messages are stored in the Inbox collection
+  inbox: {
+    // messages are stored in the Inbox collection, this path will be removed
+    // as soon as the migration has run and all the messages have been removed from here
+    messages: {type: Schema.Types.Mixed, default: () => {
+      return {};
+    }},
     newMessages: {type: Number, default: 0},
     blocks: {type: Array, default: () => []},
     optOut: {type: Boolean, default: false},
