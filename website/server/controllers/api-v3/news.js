ff82c37d5f7d84e21de4a63c2f5c7c54a633cfe0
Sabe Jones <sabrecat@gmail.com>
2018-07-19 14:00:53 -0500

    chore(news): Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'NEW PET QUEST: SEA SERPENT!';
+const LAST_ANNOUNCEMENT_TITLE = 'HABITICA COMIC-CON MEETUP AND WIKI SPOTLIGHT ON THE POMODORO TECHNIQUE';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,17 +30,25 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>7/17/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>7/19/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
         <div class="media align-items-center">
           <div class="media-body">
-            <p>A submersible expedition launching from the Diligent Docks turns to danger when a mighty Sea Serpent swims into the picture! Get the latest pet quest, <a href="/shops/quests" target="_blank">Danger in the Depths: Sea Serpent Strike!</a>, and earn some shimmering Sea Serpent pets by completing your real-life tasks.</p>
-            <div class="small">Art by Aries Faries, Witticaster, *~Seraphina~, Mara, and 1920-kun</div>
-            <div class="small mb-3">Writing by gwyllgi</div>
+            <h3>Habitica at San Diego Comic Con!</h3>
+            <p>Beffymaroo will be representing Habitica at San Diego Comic Con this year. If you’d like to meet her, along with other fellow Habiticans, join us at the Habitica SDCC Meetup! Beffymaroo will be handing out Habitica stickers, promo codes for the Unconventional Armor set, and other exciting special swag (quantities limited!).</p>
+            <p>You can find the meetup on Saturday, July 21, at the San Diego Bayfront Hilton lobby from 12:00-1:00 PM! Look for the purple Gryphon banner. Can’t wait to meet you :)</p>
           </div>
-          <div class="promo_seaserpent ml-3 mb-3"></div>
+          <div class="promo_unconventional_armor ml-3 mb-3"></div>
+        </div>
+        <div class="media align-items-center">
+          <div class="scene_pomodoro mr-3"></div>
+          <div class="media-body">
+            <h3>Wiki Spotlight: The Pomodoro Technique</h3>
+            <p>This month's <a href='https://habitica.wordpress.com/2018/07/18/pomodoro/' target='_blank'>featured Wiki article</a> is about the Pomodoro Technique! We hope that it will help you as you look for new productivity strategies. Be sure to check it out, and let us know what you think by reaching out on <a href='https://twitter.com/habitica' target='_blank'>Twitter</a>, <a href='http://blog.habitrpg.com' target='_blank'>Tumblr</a>, and <a href='https://facebook.com/habitica' target='_blank'>Facebook</a>.</p>
+            <div class="small mb-3">by shanaqui and the Wiki Wizards</div>
+          </div>
         </div>
       </div>
       `,
