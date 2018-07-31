c88b9b80b56339c3fd4531298921264b98dbcabc
Sabe Jones <sabrecat@gmail.com>
2018-07-31 14:08:56 -0500

    feat(event): Habitica Naming Day and Bailey announcements

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'NEW GROUP PLANS FEATURE: SHARED COMPLETION FOR TO-DOS';
+const LAST_ANNOUNCEMENT_TITLE = 'HABITICA NAMING DAY; LAST CHANCE FOR SUMMER SPLASH AND JULY SUBSCRIBER ITEMS';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,17 +30,38 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>7/26/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>7/31/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
         <div class="media align-items-center">
           <div class="media-body">
-            <p>We've added a new feature for To-Dos in Group Plans: Shared Completion! Now, you can set To-Dos on the Task Board so that the group task is completed when group members finish the task. A "Single" task is done when anyone finishes it (it's a race!), while a task set to "All" is completed when everyone assigned is done. You can find these options by editing To-Dos on your Task Board.</p>
-            <p>Many of you have been eager to see this feature implemented. We hope it'll help you better organize and motivate your Group for success!</p>
-            <div class="small mb-3">by SabreCat, TheHollidayInn, and paglias</div>
+            <h3>Habitica Naming Day and Purple Gryphon Rewards!</h3>
+            <p>Happy Habitica Naming day! In honor of the day when we changed the name of the app from HabitRPG to Habitica, we've given everyone an achievement, as well as some delicious cake for your pets and mounts.</p>
           </div>
-          <div class="scene_todos ml-3 mb-3"></div>
+          <div class="promo_naming_day_2018 ml-3 mb-3"></div>
+        </div>
+        <p>Speaking of pets and mounts, we've given all users Royal Purple Gryphon rewards! Depending on how many Naming Days you've celebrated with us, you've received Melior (a Purple Gryphon mount), his little sister Meliora (a Purple Gryphon pet), a Purple Gryphon Helm, or the Purple Gryphon Wing Cloak !</p>
+        <p>Thanks for being a Habitica user -- you all mean so much to us. We hope that you enjoy your presents!</p>
+        <div class="small mb-3">by Lemoness, Beffymaroo, and Baconsaur</div>
+        <div class="media align-items-center">
+          <div class="media-body">
+            <div class="media align-items-center">
+              <div class="promo_mystery_201807 mr-3"></div>
+              <div class="media-body">
+                <h3>Last Chance for Sea Serpent Set</h3>
+                <p>Reminder: this is the final day to <a href='/user/settings/subscription' target='_blank'>subscribe</a> and receive the Sea Serpent Set! Subscribing also lets you buy Gems for Gold. The longer your subscription, the more Gems you can get!</p>
+                <p>Thanks so much for your support! You help keep Habitica running.</p>
+                <div class="small mb-3">by Beffymaroo</div>
+              </div>
+            </div>
+            <h3>Last Chance for Glass and Aquatic Hatching Potions</h3>
+            <p>Reminder: this is the final day to <a href='/shops/market' target='_blank'>buy Glass and Aquatic Hatching Potions</a> If they come back, it won't be until next year at the earliest, so don't delay!</p>
+            <div class="small mb-3">by stefalupagus, Beffymaroo, Mako413, Willow The Witty, and SabreCat</div>
+            <h3>Last Chance for Summer Splash Goodies!</h3>
+            <p>A reminder that Summer Splash is ending as well! Be sure to grab your special class gear from your Rewards column and any items you've been eyeing in the <a href='/shops/seasonal' target='_blank'>Seasonal Shop</a>!</p>
+          </div>
+          <div class="promo_aquatic_glass_potions ml-3 mb-3"></div>
         </div>
       </div>
       `,
