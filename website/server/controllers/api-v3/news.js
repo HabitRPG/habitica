4ec5df170c1b3df34b63ff1f45964c5d95db2a10
Sabe Jones <sabrecat@gmail.com>
2018-08-02 13:38:51 -0500

    feat(content): Backgrounds, Armoire, minor sprite fixes

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'AUGUST 2018 RESOLUTION SUCCESS CHALLENGE AND NEW TAKE THIS CHALLENGE';
+const LAST_ANNOUNCEMENT_TITLE = 'AUGUST BACKGROUNDS AND ARMOIRE ITEMS, AND QUEST PARTICIPANTS VIEW!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,27 +30,22 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>8/1/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>8/2/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
         <div class="media align-items-center">
           <div class="media-body">
-            <p>The Habitica team has launched a special official Challenge series hosted in the <a href='/groups/guild/6e6a8bd3-9f5f-4351-9188-9f11fcd80a99' target='_blank'>Official New Year's Resolution Guild</a>. These Challenges are designed to help you build and maintain goals that are destined for success and then stick with them as the year progresses. For this month's Challenge, <a href='/challenges/c1c253c7-fd65-4227-8079-28b051dc1e0d' target='_blank'>Count Your Treasure</a>, we're focusing on the importance of rewards! It has a 15 Gem prize, which will be awarded to five lucky winners on September 3rd.</p>
+            <h3>New Backgrounds and Armoire Items!</h3>
+            <p>We’ve added three new backgrounds to the Background Shop! Now your avatar can fly over a Rocky Canyon, spar on the Training Grounds, and cross a charming Bridge. Check them out under User Icon > Backgrounds!</p>
+            <p>Plus, there’s new Gold-purchasable equipment in the Enchanted Armoire, including the Jeweled Archer Set. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)</p>
+            <div class="small mb-3">by Lalaitha, Kiwibot, Balduranne, Irrevenant, DialFforFunky, RandomGryffindor, Mewrose, and CitrineQuartzFox</div>
+            <h3>New! Quest Partipant List</h3>
+            <p>There's a new feature on your Party Page! If you're in a Quest, you can now view all Party members who have joined the Quest via the Participants link in the box where you can see your progress. Enjoy!</p>
+            <div class="small mb-3">by Alys</div>
           </div>
-          <div class="scene_rewards ml-3 mb-3"></div>
+          <div class="promo_armoire_backgrounds_201808 ml-3 mb-3"></div>
         </div>
-        <p>Congratulations to the winners of July's Challenge: Krilae, catliyon, cemelee, Rafael Moura, and Eduardo The Invincible!</p>
-        <div class="media align-items-center">
-          <div class="promo_take_this mr-3 mb-3"></div>
-          <div class="media-body">
-            <p>The next Take This Challenge has also launched, "<a href='/challenges/1044ec0c-4a85-48c5-9f36-d51c0c62c7d3' target='_blank'>Notice Me, Senpai!</a>", with a focus on seeking help when we're struggling. Be sure to check it out to earn additional pieces of the Take This armor set!</p>
-          </div>
-        </div>
-        <p>Long-time Take This Challenge participants may notice that recent Challenges have been reprises of early ones. Great news, though: next month, we'll begin debuting brand new mental health and self-care Challenges from the Take This team! Stay tuned!</p>
-        <p><a href='http://www.takethis.org/' target='_blank'>Take This</a> is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness.</p>
-        <p>Congratulations to the winners of the last Take This Challenge, "Multi-Player Co-Op Exercise!": grand prize winner Lex Talion, and runners-up wcampospro, KorJik_3, FortemFiducia, DanDee, and Xander Scott! Plus, all participants in that Challenge have received a piece of the <a href='http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set' target='_blank'>Take This item set</a> if they hadn't completed it already. It is located in your Rewards column. Enjoy!</p>
-        <div class="small mb-3">by Doctor B, the Take This team, Lemoness, Beffymaroo, and SabreCat</div>
       </div>
       `,
     });
