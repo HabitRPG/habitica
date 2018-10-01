dc0aa1902fbf2919ebaccf3db009fae34156a033
Phillip Thelen <viirus@pherth.net>
2018-10-01 15:58:47 +0200

    Re-add register call

--- website/server/controllers/api-v4/auth.js
+++ website/server/controllers/api-v4/auth.js
@@ -9,6 +9,7 @@ import {
 import * as passwordUtils from '../../libs/password';
 import { model as User } from '../../models/user';
 import {verifyUsername} from '../../libs/user/validation';
+import * as authLib from '../../libs/auth';
 
 const api = {};
 
@@ -102,6 +103,30 @@ api.verifyUsername = {
 */
 
 /* NOTE this route has also an API v3 version */
+
+/**
+ * @api {post} /api/v4/user/auth/local/register Register
+ * @apiDescription Register a new user with email, login name, and password or attach local auth to a social user
+ * @apiName UserRegisterLocal
+ * @apiGroup User
+ *
+ * @apiParam (Body) {String} username Login name of the new user. Must be 1-36 characters, containing only a-z, 0-9, hyphens (-), or underscores (_).
+ * @apiParam (Body) {String} email Email address of the new user
+ * @apiParam (Body) {String} password Password for the new user
+ * @apiParam (Body) {String} confirmPassword Password confirmation
+ *
+ * @apiSuccess {Object} data The user object, if local auth was just attached to a social user then only user.auth.local
+ */
+api.registerLocal = {
+  method: 'POST',
+  middlewares: [authWithHeaders({
+    optional: true,
+  })],
+  url: '/user/auth/local/register',
+  async handler (req, res) {
+    await authLib.registerLocal(req, res, { isV3: false });
+  },
+};
 
 /**
  * @api {post} /api/v4/user/auth/local/register Register
