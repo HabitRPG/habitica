d1633bd690215786c2acc7814d7eda970d8182ca
Sabe Jones <sabrecat@gmail.com>
2018-10-02 14:24:02 -0500

    fix(migration): use missing field, not migration tag, for query

--- migrations/users/generate-usernames.js
+++ migrations/users/generate-usernames.js
@@ -15,7 +15,6 @@ let dbUsers = monk(CONNECTION_STRING).get('users', { castIds: false });
 function processUsers (lastId) {
   // specify a query to limit the affected users (empty for all users):
   let query = {
-    migration: {$ne: migrationName},
     'auth.local.username': {$exists: false},
     'auth.timestamps.loggedin': {$gt: new Date('2018-04-01')}, // Initial coverage for users active within last 6 months
   };
