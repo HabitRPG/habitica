057f5fea8f930ccd466a0117dc12c56b97c75ec0
Sabe Jones <sabrecat@gmail.com>
2018-10-02 10:35:16 -0500

    refactor(migration): use batching and sendTxn

--- migrations/users/20181002_username_email.js
+++ migrations/users/20181002_username_email.js
@@ -1,58 +1,99 @@
+const MIGRATION_NAME = '20181002_username_email.js';
+let authorName = 'Sabe'; // in case script author needs to know when their ...
+let authorUuid = '7f14ed62-5408-4e1b-be83-ada62d504931'; // ... own data is done
+
+/*
+ * Send emails to eligible users announcing upcoming username changes
+ */
+
 import monk from 'monk';
 import nconf from 'nconf';
-import axios from 'axios';
-
+import { sendTxn } from '../website/server/libs/email';
 const CONNECTION_STRING = nconf.get('MIGRATION_CONNECT_STRING');
-const MIGRATION_NAME = '20181002_username_email';
-
-const BASE_URL = nconf.get('BASE_URL');
-const EMAIL_SERVER_URL = nconf.get('EMAIL_SERVER:url');
-const EMAIL_SERVER_USER = nconf.get('EMAIL_SERVER:authUser');
-const EMAIL_SERVER_PASSWORD = nconf.get('EMAIL_SERVER:authPassword');
-
 let dbUsers = monk(CONNECTION_STRING).get('users', { castIds: false });
 
-function sendEmails() {
-  dbUsers.find(
-    {migration: {$ne: MIGRATION_NAME}},
-    {fields: {_id: 1, auth: 1, profile: 1, preferences: 1}}
-  ).each((user, {close, pause, resume}) => {
-    pause();
-    dbUsers.update({_id: user._id}, {$set: {migration: MIGRATION_NAME}});
-    return axios.post(
-      `${EMAIL_SERVER_URL}/job`, {
-          type: 'email',
-          data: {
-            emailType: 'username-change',
-            to: [
-              {name: user.profile.name, email: user.auth.local.email}
-            ],
-            variables: [
-              {name: 'BASE_URL', content: BASE_URL},
-              {name: 'LOGIN_NAME', content: user.auth.local.username},
-              {name: 'UNSUB_EMAIL_TYPE_URL', content: '/user/settings/notifications?unsubFrom=importantAnnouncements'},
-            ],
-          },
-          options: {
-            priority: 'high',
-            attempts: 5,
-            backoff: {delay: 10 * 60 * 1000, type: 'fixed'},
-          },
-        },
-        {
-          auth: {
-            username: EMAIL_SERVER_USER,
-            password: EMAIL_SERVER_PASSWORD,
-          },
-        }
-    ).then(() => {
-      resume();
-    }).catch((error) => {
-      console.log(error);
+function processUsers (lastId) {
+  // specify a query to limit the affected users (empty for all users):
+  let query = {
+    migration: {$ne: MIGRATION_NAME},
+  };
+
+  if (lastId) {
+    query._id = {
+      $gt: lastId,
+    };
+  }
+
+  dbUsers.find(query, {
+    sort: {_id: 1},
+    limit: 250,
+    fields: [
+      '_id',
+      'auth',
+      'preferences',
+      'profile',
+    ], // specify fields we are interested in to limit retrieved data (empty if we're not reading data):
+  })
+    .then(updateUsers)
+    .catch((err) => {
+      console.log(err);
+      return exiting(1, `ERROR! ${  err}`);
     });
-  }).then(() => {
-    return process.exit(0);
-  });
 }
 
-module.exports = sendEmails;
+let progressCount = 1000;
+let count = 0;
+
+function updateUsers (users) {
+  if (!users || users.length === 0) {
+    console.warn('All appropriate users found and modified.');
+    displayData();
+    return;
+  }
+
+  let userPromises = users.map(updateUser);
+  let lastUser = users[users.length - 1];
+
+  return Promise.all(userPromises)
+    .then(() => {
+      processUsers(lastUser._id);
+    });
+}
+
+function updateUser (user) {
+  count++;
+
+  dbUsers.update({_id: user._id}, {$set: {migration: MIGRATION_NAME}});
+
+  sendTxn(
+    user,
+    'username-change',
+    [{name: 'UNSUB_EMAIL_TYPE_URL', content: '/user/settings/notifications?unsubFrom=importantAnnouncements'}],
+    [{name: 'LOGIN_NAME', content: user.auth.local.username}]
+  );
+
+  if (count % progressCount === 0) console.warn(`${count} ${user._id}`);
+  if (user._id === authorUuid) console.warn(`${authorName} processed`);
+}
+
+function displayData () {
+  console.warn(`\n${count} users processed\n`);
+  return exiting(0);
+}
+
+function exiting (code, msg) {
+  code = code || 0; // 0 = success
+  if (code && !msg) {
+    msg = 'ERROR!';
+  }
+  if (msg) {
+    if (code) {
+      console.error(msg);
+    } else      {
+      console.log(msg);
+    }
+  }
+  process.exit(code);
+}
+
+module.exports = processUsers;
