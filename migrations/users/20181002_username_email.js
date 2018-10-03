20ad488e5c14c61f4b1773a8361fb15ced48904a
Sabe Jones <sabrecat@gmail.com>
2018-10-03 19:44:31 +0000

    fix(migration): add daterange

--- migrations/users/20181002_username_email.js
+++ migrations/users/20181002_username_email.js
@@ -16,6 +16,7 @@ function processUsers (lastId) {
   // specify a query to limit the affected users (empty for all users):
   let query = {
     migration: {$ne: MIGRATION_NAME},
+    'auth.timestamps.loggedin': {$gt: new Date('2018-04-01')},
   };
 
   if (lastId) {
