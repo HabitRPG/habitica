d80dc0795d77b8489fc8e942fb7cff18ea6ece17
Phillip Thelen <viirus@pherth.net>
2018-10-19 12:37:13 +0200

    Add unit tests

--- test/api/unit/middlewares/auth.test.js
+++ test/api/unit/middlewares/auth.test.js
@@ -3,9 +3,12 @@ import {
   generateReq,
 } from '../../../helpers/api-unit.helper';
 import { authWithHeaders as authWithHeadersFactory } from '../../../../website/server/middlewares/auth';
+import nconf from 'nconf';
+import requireAgain from 'require-again';
 
 describe('auth middleware', () => {
   let res, req, user;
+  let pathToAuthMiddleware = '../../../../website/server/middlewares/auth';
 
   beforeEach(async () => {
     res = generateRes();
@@ -34,6 +37,41 @@ describe('auth middleware', () => {
         expect(userToJSON.preferences).to.exist;
 
         done();
+      });
+    });
+  });
+
+  describe('auth on test servers', () => {
+    it('allows all users on non-test servers', (done) => {
+      const authWithHeaders = authWithHeadersFactory({
+        userFieldsToExclude: [],
+      });
+
+      req.headers['x-api-user'] = user._id;
+      req.headers['x-api-key'] = user.apiToken;
+
+      authWithHeaders(req, res, (err) => {
+        if (err) return done(err);
+        done();
+      });
+    });
+    it('doesn\'t allow all users on test servers', async () => {
+      sandbox.stub(nconf, 'get').withArgs('IS_TEST_SERVER').returns('true');
+      let attachAuth = requireAgain(pathToAuthMiddleware);
+      const authWithHeaders = attachAuth.authWithHeaders({
+        userFieldsToExclude: [],
+      });
+
+      req.headers['x-api-user'] = user._id;
+      req.headers['x-api-key'] = user.apiToken;
+
+      await new Promise((resolve, reject) => {
+        authWithHeaders(req, res, (err) => {
+          if (err.httpCode === 401) {
+            resolve();
+          }
+          reject(err);
+        });
       });
     });
   });
