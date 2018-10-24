804fe1c6d564c594a6ea0e6b63745e535752742e
Sabe Jones <sabrecat@gmail.com>
2018-10-24 18:39:54 -0500

    fix(usernames): various z-index modals above Resting banner force reload after verify username add missing e-mail validation on frontpage let Yesterdaily modal float behind username modal

--- website/client/components/auth/authForm.vue
+++ website/client/components/auth/authForm.vue
@@ -156,6 +156,10 @@ export default {
         }
       });
     }, 500),
+    validateEmail (email) {
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
     // @TODO: Abstract hello in to action or lib
     async socialAuth (network) {
       try {
