59436a8bf7fe47c0d9ab97aa65c5e408c8309691
Sabe Jones <sabrecat@gmail.com>
2018-10-25 16:31:00 -0500

    feat(content): Mystery Items Oct 2018

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'VETERAN PETS AND NEW STAFF MEMBERS';
+const LAST_ANNOUNCEMENT_TITLE = 'OCTOBER SUBSCRIBER ITEMS REVEALED';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,25 +30,14 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>10/23/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>10/25/2018 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="promo_veteran_pets center-block"></div>
-        <h3>New Veteran Pet!</h3>
-        <p>Hello Habiticans! Everyone is doing really well with the transition to our new <a href='https://habitica.wordpress.com/2018/10/11/coming-soon-unique-usernames/' target='_blank'>unique username system</a>!</p>
-        <p>As a reward for confirming your username, you've been granted a veteran pet!  If you haven't confimed yet, your new pet will appear in your Stable as soon as you do. Which pet? That depends on how many major changes to Habitica you've been around for.</p>
-        <p>Enjoy, and thank you for being part of our community- it means a lot to us! <3</p>
-        <div class="small mb-3">by Beffymaroo, SabreCat, Apollo, Piyo, viirus, Paglias, and TheHollidayInn</div>
-        <div class="media align-items-center">
-          <div class="npc_apollo ml-3"></div>
-          <div class="media-body">
-            <h3>New Staff Members: Apollo and Piyo</h3>
-            <p>We're thrilled to announce that our long-time designers Apollo and Piyo will be coming onboard as staff members! They've done a lot of beautiful work on the site and the mobile apps and we couldn't be happier to welcome them aboard. Go congratulate them in <a href='/groups/tavern'>the Tavern</a>!</p>
-            <div class="small mb-3">by the Habitica Team</div>
-          </div>
-          <div class="npc_piyo mr-3"></div>
-        </div>
+        <div class="promo_mystery_201810 center-block"></div>
+        <p>October's Subscriber Items have been revealed: the <a href='/user/settings/subscription' target='_blank'>Dark Forest Item Set</a>! You only have seven days to receive the item set when you subscribe. If you're already an active subscriber, reload the site and then head to Inventory > Items to claim your gear!</p>
+        <p>Subscribers also receive the ability to buy Gems for Gold -- the longer you subscribe, the more Gems you can buy per month! There are other perks as well, such as longer access to uncompressed data and a cute Jackalope pet. Best of all, subscriptions let us keep Habitica running. Thank you very much for your support -- it means a lot to us.</p>
+        <div class="small mb-3">by Beffymaroo</div>
       </div>
       `,
     });
