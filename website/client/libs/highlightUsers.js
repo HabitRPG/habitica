7649e2daf6f986d5b01f07199c4937f84c95fea3
negue <eugen.bolz@gmail.com>
2019-01-07 22:18:01 +0100

    refactor highlight regex

--- website/client/libs/highlightUsers.js
+++ website/client/libs/highlightUsers.js
@@ -1,25 +1,16 @@
-
-import escapeRegExp from 'lodash/escapeRegExp';
-
 export function highlightUsers (text, userName, displayName) {
-  const escapedDisplayName = escapeRegExp(displayName);
-  const escapedUsername = escapeRegExp(userName);
-
-  const findCurrentUserRegex = `@(${escapedDisplayName}|${escapedUsername})(?:\\b)`;
-  const findAnyOtherUserRegex = '(?!\\b)@[\\w-]+(?:\\b)';
+  const findAnyMentionRegex = '@[\\w-]+(?:\\b)';
   const notInsideHtmlTagsRegex = '(?<!<\/?[^>]*|&[^;]*)';
-  const userRegex = new RegExp(`${notInsideHtmlTagsRegex}${findCurrentUserRegex}`, 'gi');
 
-  if (userRegex.test(text)) {
-    text = text.replace(userRegex, match => {
-      return `<span class="at-highlight at-text">${match}</span>`;
-    });
-  }
-
-  const atRegex = new RegExp(`${notInsideHtmlTagsRegex}${findAnyOtherUserRegex}`, 'gi');
+  const atRegex = new RegExp(`${notInsideHtmlTagsRegex}${findAnyMentionRegex}`, 'gi');
+  const currentUser = [`@${userName}`, `@${displayName}`];
 
   if (atRegex.test(text)) {
     text = text.replace(atRegex, match => {
+      if (currentUser.includes(match)) {
+        return `<span class="at-highlight at-text">${match}</span>`;
+      }
+
       return `<span class="at-text">${match}</span>`;
     });
   }
