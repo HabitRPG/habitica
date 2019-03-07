bc91dd81e90b8f086bb60be1b31f3bbcd7a1f2b4
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2019-03-07 16:58:38 +0100

    Revert "fix(amazon): make sure to use up to date data"
    This reverts commit 298a79b58d334c874d2d08726d60aac1eb261588.

--- website/client/components/payments/amazonButton.vue
+++ website/client/components/payments/amazonButton.vue
@@ -60,7 +60,7 @@ export default {
     this.buttonId = `AmazonPayButton-${uuid.v4()}`;
   },
   mounted () {
-    this.amazonPaymentsInit(this.amazonData);
+    this.amazonPaymentsInit(this.amazonData); // TOOD clone
     if (this.isAmazonReady) return this.setupAmazon();
 
     this.$store.watch(state => state.isAmazonReady, (isAmazonReady) => {
@@ -90,8 +90,6 @@ export default {
             this.$root.$emit('habitica::pay-with-amazon', this.amazonPayments);
           },
           authorization: () => {
-            this.amazonPaymentsInit(this.amazonData);
-
             window.amazon.Login.authorize({
               scope: 'payments:widget',
               popup: true,
