55f374cdf11c508bf865b751ecd14e5281c1156c
Alys <alice.harris@oldgods.net>
2019-03-16 14:51:48 +1000

    allow moderators/staff to always see Report button for a flagged message

--- website/client/components/chat/chatCard.vue
+++ website/client/components/chat/chatCard.vue
@@ -20,7 +20,7 @@ div
       .action.d-flex.align-items-center(v-if='!inbox', @click='copyAsTodo(msg)')
         .svg-icon(v-html="icons.copy")
         div {{$t('copyAsTodo')}}
-      .action.d-flex.align-items-center(v-if='(inbox || (user.flags.communityGuidelinesAccepted && msg.uuid !== "system")) && !isMessageReported', @click='report(msg)')
+      .action.d-flex.align-items-center(v-if='(inbox || (user.flags.communityGuidelinesAccepted && msg.uuid !== "system")) && (!isMessageReported || user.contributor.admin)', @click='report(msg)')
         .svg-icon(v-html="icons.report", v-once)
         div(v-once) {{$t('report')}}
       .action.d-flex.align-items-center(v-if='msg.uuid === user._id || inbox || user.contributor.admin', @click='remove()')
