e0291cf43261c3e07e23fb551714e695bd422ab1
Sabe Jones <sabrecat@gmail.com>
2019-03-19 19:52:59 -0500

    feat(event): Spring Fling 2019

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'CELEBRATE PI DAY WITH HABITICA!';
+const LAST_ANNOUNCEMENT_TITLE = 'SPRING FLING BEGINS! LIMITED EDITION EQUIPMENT, SEASONAL SHOP, AND EGG QUEST!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,14 +30,22 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>3/14/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>3/19/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="promo_pi_day center-block"></div>
-        <p>Hello Habiticans! In celebration of Pi Day on March 14, we've gifted everyone delicious slices of pie for you all to feed to your pets.</p>
-        <p>We've also given everyone a festive Pi Hat and Shield so you can celebrate in style. Enjoy them, and thanks for being a part of our community!</p>
-        <div class="small mb-3">by Beffymaroo and SabreCat</div>
+        <div class="promo_classes_spring2019 center-block"></div>
+        <h3>Limited Edition Class Outfits</h3>
+        <p>From now until April 30th, limited edition outfits are available in the Rewards column! Depending on your class, you can be a Cloud Rogue, Robin Healer, Amber Mage, or Orchid Warrior. You'd better get productive to earn enough Gold before your time runs out...</p>
+        <div class="small mb-3">by Eslyn, Aspiring Advocate, OuttaMyMind, Lt. Cabel, Beffymaroo and SabreCat</div>
+        <div class="promo_seasonalshop_spring center-block"></div>
+        <h3>Seasonal Shop Opens</h3>
+        <p>The <a href='/shops/seasonal'>Seasonal Shop</a> has opened! It's stocking springtime Seasonal Edition goodies at the moment, including past spring outfits. Everything there will be available to purchase during the Spring Fling event each year, but it's only open until April 30th, so be sure to stock up now, or you'll have to wait a year to buy these items again!</p>
+        <div class="small mb-3">by Vikte, Lalaitha, DialFForFunky, Gerald the Pixel, Scarvia, Awesome kitty, usnbfs, Lemoness, Balduranne, PainterProphet, Beffymaroo and SabreCat</div>
+        <div class="promo_egg_hunt center-block"></div>
+        <h3>Egg Quest Available</h3>
+        <p>The Egg Quest is also available again in the <a href='/shops/seasonal'>Seasonal Shop</a>! Strange eggs are appearing all over Habitica. Can you collect them all to earn some colorful Egg pets and mounts?</p>
+        <div class="small mb-3">by Megan, Beffymaroo, and Lemoness</div>
       </div>
       `,
     });
