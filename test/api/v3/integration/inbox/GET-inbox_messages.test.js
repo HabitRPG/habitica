f831708bb99f8d80ab9a5c95a8074e73daf2a3ed
negue <eugen.bolz@gmail.com>
2019-03-21 23:54:06 +0100

    paging for inbox

--- test/api/v3/integration/inbox/GET-inbox_messages.test.js
+++ test/api/v3/integration/inbox/GET-inbox_messages.test.js
@@ -45,4 +45,23 @@ describe('GET /inbox/messages', () => {
     expect(messages[2].text).to.equal('second');
     expect(messages[3].text).to.equal('first');
   });
+
+  it('returns no messages when using page-query ', async () => {
+    const promises = [];
+
+    for (let i = 0; i < 10; i++) {
+      promises.push(user.post('/members/send-private-message', {
+        toUserId: user.id,
+        message: 'fourth',
+      }));
+    }
+
+    await Promise.all(promises);
+
+    await user.sync();
+
+    const messages = await user.get('/inbox/messages?page=1');
+
+    expect(messages.length).to.equal(4);
+  });
 });
