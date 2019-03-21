928327e02a315801459ac8c4d8a0159d846f5395
Sabe Jones <sabrecat@gmail.com>
2019-03-21 16:34:19 -0500

    feat(content): Magic Hatching Potions March 2019

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'SPRING FLING BEGINS! LIMITED EDITION EQUIPMENT, SEASONAL SHOP, AND EGG QUEST!';
+const LAST_ANNOUNCEMENT_TITLE = 'CELESTIAL AND RAINBOW HATCHING POTIONS! PLUS USE CASE SPOTLIGHT ON MAKING ROUTINES';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,22 +30,20 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>3/19/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>3/21/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="promo_classes_spring2019 center-block"></div>
-        <h3>Limited Edition Class Outfits</h3>
-        <p>From now until April 30th, limited edition outfits are available in the Rewards column! Depending on your class, you can be a Cloud Rogue, Robin Healer, Amber Mage, or Orchid Warrior. You'd better get productive to earn enough Gold before your time runs out...</p>
-        <div class="small mb-3">by Eslyn, Aspiring Advocate, OuttaMyMind, Lt. Cabel, Beffymaroo and SabreCat</div>
-        <div class="promo_seasonalshop_spring center-block"></div>
-        <h3>Seasonal Shop Opens</h3>
-        <p>The <a href='/shops/seasonal'>Seasonal Shop</a> has opened! It's stocking springtime Seasonal Edition goodies at the moment, including past spring outfits. Everything there will be available to purchase during the Spring Fling event each year, but it's only open until April 30th, so be sure to stock up now, or you'll have to wait a year to buy these items again!</p>
-        <div class="small mb-3">by Vikte, Lalaitha, DialFForFunky, Gerald the Pixel, Scarvia, Awesome kitty, usnbfs, Lemoness, Balduranne, PainterProphet, Beffymaroo and SabreCat</div>
-        <div class="promo_egg_hunt center-block"></div>
-        <h3>Egg Quest Available</h3>
-        <p>The Egg Quest is also available again in the <a href='/shops/seasonal'>Seasonal Shop</a>! Strange eggs are appearing all over Habitica. Can you collect them all to earn some colorful Egg pets and mounts?</p>
-        <div class="small mb-3">by Megan, Beffymaroo, and Lemoness</div>
+        <div class="promo_celestial_rainbow_potions center-block"></div>
+        <h3>Celestial and Rainbow Hatching Potions</h3>
+        <p>There's a new pet breed in town! Check out the brand-new Celestial Potions and the return of Rainbow Potions to brighten your Spring avatar look from <a href='/shops/market'>the Market</a> and use them to hatch any standard pet egg. (Magic Hatching Potions do not work on Quest Pet eggs.) Magic Hatching Potion Pets aren't picky, so they'll happily eat any kind of food that you feed them!</p>
+        <p>After they're gone, it will be at least a year before the Celestial or Rainbow Hatching Potions are available again, so be sure to get them now!</p>
+        <div class="small mb-3">by Bonogo, Mara, Beffymaroo, and SabreCat</div>
+        <div class="scene_todos center-block"></div>
+        <h3>Use Case Spotlight: Routines</h3>
+        <p>This month's <a href='https://habitica.wordpress.com/2019/03/21/use-case-spotlight-setting-up-a-routine/' target='_blank'>Use Case Spotlight</a> is about Routines! It features a number of great suggestions submitted by Habiticans in the <a href='/groups/guild/1d3a10bf-60aa-4806-a38b-82d1084a59e6'>Use Case Spotlights Guild</a>. We hope it helps any of you who might be looking for new ideas to set up related tasks.</p>
+        <p>Plus, we're collecting user submissions for the next spotlight! How do you use Habitica to review and evaluate your tasks? We’ll be featuring player-submitted examples in Use Case Spotlights on the Habitica Blog next month, so post your suggestions in the Use Case Spotlight Guild now. We look forward to learning more about how you use Habitica to improve your life and get things done!</p>
+        <div class="small mb-3">by shanaqui</div>
       </div>
       `,
     });
