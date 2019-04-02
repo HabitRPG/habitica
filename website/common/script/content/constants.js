2419b219baa1df7ebb5bbc884836a5a181bd4a8e
Sabe Jones <sabrecat@gmail.com>
2019-04-02 07:53:37 -0500

    chore(event): end April Fool's and fix end date of Gala items

--- website/common/script/content/constants.js
+++ website/common/script/content/constants.js
@@ -35,7 +35,7 @@ export const EVENTS = {
   summer2018: { start: '2018-06-19', end: '2018-08-02' },
   fall2018:   { start: '2018-09-20', end: '2018-11-02' },
   winter2019: { start: '2018-12-19', end: '2019-02-02' },
-  spring2019: { start: '2019-03-19', end: '2019-04-02' },
+  spring2019: { start: '2019-03-19', end: '2019-05-02' },
 };
 
 export const SEASONAL_SETS = {
