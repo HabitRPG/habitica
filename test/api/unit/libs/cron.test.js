7c77d0afc416d5be422fc2f38cf6b7f57cbe3af3
Alys <alice.harris@oldgods.net>
2019-04-16 21:14:13 +1000

    add extra tests for repeat every X days since last completion

--- test/api/unit/libs/cron.test.js
+++ test/api/unit/libs/cron.test.js
@@ -8,7 +8,7 @@ import * as Tasks from '../../../../website/server/models/task';
 import common from '../../../../website/common';
 import analytics from '../../../../website/server/libs/analyticsService';
 
-// const scoreTask = common.ops.scoreTask;
+const scoreTask = common.ops.scoreTask;
 
 let pathToCronLib = '../../../../website/server/libs/cron';
 
@@ -933,6 +933,68 @@ describe('cron', () => {
       cron({user, tasksByType, daysMissed, analytics});
 
       expect(user.stats.hp).to.equal(48);
+    });
+
+    context('repeat every X days after completion is on', () => {
+      beforeEach(() => {
+        tasksByType.dailys[0].everyX = 3;
+        tasksByType.dailys[0].frequency = 'daily';
+        tasksByType.dailys[0].repeatAfterCompletion = true;
+        scoreTask({ user, task: tasksByType.dailys[0], direction: 'up', cron: false });
+      });
+
+      context('player uses Habitica every day', () => {
+        it('should be due 3+ days after completion but not before', () => {
+          // TODO Tell me if you'd prefer this split into four tests with one expect() in each. There'd still need to be multiple cron() calls in the second, third, and fourth tests to properly mimic the user logging in every day. -- Alys
+
+          daysMissed = 1;
+
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          // we subtract 5 minutes to ensure we haven't jumped more than a day from the starting time due to any small delays in the tests
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.false;
+
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.false;
+
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.true;
+
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.true;
+        });
+      });
+
+      context('player does not use Habitica every day', () => {
+        it('should not be due 2 days after completion when the intervening day is missed', () => {
+          daysMissed = 2;
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.false;
+        });
+
+        it('should be due 3 days after completion when one intervening day is missed', () => {
+          daysMissed = 1;
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.false;
+
+          daysMissed = 2;
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.true;
+        });
+
+        it('should be due 3 days after completion when all intervening days are missed', () => {
+          daysMissed = 3;
+          clock = sinon.useFakeTimers( moment().add(daysMissed, 'days').subtract(5, 'minutes').toDate() );
+          cron({user, tasksByType, daysMissed, analytics});
+          expect(tasksByType.dailys[0].isDue).to.be.true;
+        });
+      });
     });
   });
 
