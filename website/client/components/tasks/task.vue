1040f84113e81711a965af2fe9743b1e694c3a64
sayansur25 <s.syan20@gmail.com>
2019-05-07 01:33:28 +0530

    FIX:timeTillDue() function changed task.vue

--- website/client/components/tasks/task.vue
+++ website/client/components/tasks/task.vue
@@ -634,21 +634,9 @@ export default {
     },
     timeTillDue () {
       // this.task && is necessary to make sure the computed property updates correctly
-      if(this.user.preferences.dayStart){
-        const endOfToday = moment().endOf('day').add(this.user.preferences.dayStart,"hours");
-        const endOfDueDate = moment(this.task && this.task.date).endOf('day')
-        console.log(endOfToday)
-        console.log(endOfDueDate)
-        console.log(moment.duration(endOfDueDate.diff(endOfToday)))
+        const endOfToday = moment().endOf('day').subtract((24-this.user.preferences.dayStart),"hours");
+        const endOfDueDate = moment(this.task.date).endOf('day').subtract((24-this.user.preferences.dayStart),"hours");
         return moment.duration(endOfDueDate.diff(endOfToday));
-      }
-      else{
-        const endOfToday = moment().endOf('day');
-        const endOfDueDate = moment(this.task && this.task.date).endOf('day');
-
-        return moment.duration(endOfDueDate.diff(endOfToday));
-      }
-      
     },
     isDueOverdue () {
       return this.timeTillDue.asDays() <= 0;
