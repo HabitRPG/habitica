bda3fb5f4d15fc1009fab409dbf0a235baafb0e2
Jose Garay <garayj@oregonstate.edu>
2019-05-15 08:10:44 -0700

    if-else statement to be sure that the markdown library doesn't create issues with empty strings.

--- website/client/directives/markdown.js
+++ website/client/directives/markdown.js
@@ -3,8 +3,11 @@ import habiticaMarkdown from 'habitica-markdown';
 export default function markdown (el, {value, oldValue}) {
   if (value === oldValue) return;
 
-  if (value || value === '') {
+  if (value) {
     el.innerHTML = habiticaMarkdown.render(String(value));
+  } else {
+    el.innerHTML = '';
   }
+
   el.classList.add('markdown');
 }
