8c005aa05a33b83db3d47e39080224a66efe0ca1
Sabe Jones <sabrecat@gmail.com>
2019-05-23 13:40:06 -0500

    fix(shops): remove outdated Featured Item and Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'NEW MAGIC HATCHING POTION QUEST!';
+const LAST_ANNOUNCEMENT_TITLE = 'BLOG POSTS: WIKI AND USE CASE SPOTLIGHTS!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,13 +30,19 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>5/21/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>5/23/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <p>Great news, Habiticans! We're introducing a new type of Quest which will reward you and your party members with never-before-released Magic Hatching Potions. Get the first ever Magic Hatching Potion quest, Brazen Beetle Battle, and defeat the boss to earn bewitching Bronze Magic Hatching Potions by completing your real-life tasks. Check it out in the <a href='/shops/quests'>Quest Shop</a> today!</p>
-        <div class="small mb-3">by Beffymaroo, Hachiseiko, UncommonCriminal, redphoenix, and SabreCat</div>
-        <div class="promo_bronze_quest center-block"></div>
+        <div class="scene_rewards center-block"></div>
+        <h3>Blog Post: Enchanted Armoire</h3>
+        <p>This month's <a href='https://habitica.wordpress.com/2019/05/22/enchanted-armoire/' target='_blank'>featured Wiki article</a> is about the Enchanted Armoire! We hope that it will help you as you reward yourself for your accomplishments. Be sure to check it out, and let us know what you think by reaching out on <a href='https://twitter.com/habitica' target='_blank'>Twitter</a>, <a href='http://blog.habitrpg.com' target='_blank'>Tumblr</a>, and <a href='https://facebook.com/habitica' target='_blank'>Facebook</a>.</p>
+        <div class="small mb-3">by shanaqui and the Wiki Wizards</div>
+        <div class="scene_gold center-block"></div>
+        <h3>Use Case Spotlight: Invigorating the Endgame</h3>
+        <p>This month's <a href='https://habitica.wordpress.com/2019/05/23/use-case-spotlight-keeping-things-interesting-in-endgame/' target='_blank'>Use Case Spotlight</a> is about Keeping Things Interesting in Habitica's "Endgame"! It features a number of great suggestions submitted by Habiticans in the <a href='/groups/guild/1d3a10bf-60aa-4806-a38b-82d1084a59e6'>Use Case Spotlights Guild</a>. We hope it helps any of you who might be longtime Habiticans looking to keep things fresh.</p>
+        <p>Plus, we're collecting user submissions for the next spotlight! We want to hear your best tricks and strategies for playing the Warrior class to its full advantage. We’ll be featuring player-submitted tips in Use Case Spotlights on the Habitica Blog next month, so post your suggestions in the Use Case Spotlight Guild now. We look forward to learning more about how you use Habitica to improve your life and get things done!</p>
+        <div class="small mb-3">by shanaqui</div>
       </div>
       `,
     });
