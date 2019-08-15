4de8e9a3fde7e821b8f07779d0b005628e4fa4b5
Sabe Jones <sabrecat@gmail.com>
2019-08-15 09:47:52 -0500

    chore(news): Bailey Also adds target:_blank to some Tavern links

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'NEW MAGIC HATCHING POTION QUEST! AND SOMETHING SPECIAL ON THE WAY...';
+const LAST_ANNOUNCEMENT_TITLE = 'WORLDCON HABITICA MEETUP AND BLOG POST ON ROGUES';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,17 +30,18 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>8/13/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>8/15/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <h3>Something Special is on the Way!</h3>
-        <p>Have you been looking to add more Habitican magic to your IRL world? Something exciting is coming to help you do just that! Keep your eyes peeled for a special announcement from Bailey soon!</p>
-        <div class="small mb-3">by the Habitica Team</div>
-        <div class="quest_silver center-block"></div>
-        <h3>New Magic Hatching Potion Quest!</h3>
-        <p>Where there's Bronze, there must be Silver... right? <a href='/shops/quests'>Get the latest Magic Hatching Potion quest</a>, "The Silver Solution," and collect Silver Ingots and Runes to earn Silver Magic Hatching Potions by completing your real-life tasks!</p>
-        <div class="small mb-3">by Beffymaroo, Edge, gawrone, QuartzFox, starsystemic, WillowTheWitty, and SabreCat</div>
+        <div class="promo_unconventional_armor center-block"></div>
+        <h3>Habitica Meetup at Worldcon!</h3>
+        <p>shanaqui and Dewines will be representing Habitica at <a href='https://dublin2019.com/' target='_blank'>Worldcon</a>  this year. If you’d like to meet them along with other fellow Habiticans, join us at the Habitica Worldcon Meetup! They'll be handing out promo codes for the special Unconventional Armor set and Habitica stickers (note quantities may be limited). Look for shanaqui's Habitica t-shirt!</p>
+        <p>You can find the meetup on Saturday, August 17, at ground floor foyer of Convention Centre Dublin from 5:00-6:00 PM local time!  Can’t wait to meet you :)</p>
+        <div class="scene_casting_spells center-block"></div>
+        <h3>Blog Post: Rogue</h3>
+        <p>This month's <a href='https://habitica.wordpress.com/2019/08/14/rogue/' target='_blank'>featured Wiki article</a> is about the Rogue class! We hope that it will help you as you choose the best class for your Habitica play style. Be sure to check it out, and let us know what you think by reaching out on <a href='https://twitter.com/habitica' target='_blank'>Twitter</a>, <a href='http://blog.habitrpg.com' target='_blank'>Tumblr</a>, and <a href='https://facebook.com/habitica' target='_blank'>Facebook</a>.</p>
+        <div class="small mb-3">by shanaqui and the Wiki Wizards</div>
       </div>
       `,
     });
