0056163a178d30ffe90512016417888d05c8bed3
Sabe Jones <sabrecat@gmail.com>
2019-08-29 14:31:42 -0500

    chore(news): Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'SPECIAL TIME TRAVELERS QUEST!';
+const LAST_ANNOUNCEMENT_TITLE = 'SPECIAL TIME TRAVELERS QUEST AND LAST CHANCE FOR AUGUST ITEMS!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,7 +30,7 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>8/22/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>8/29/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
@@ -38,6 +38,17 @@ api.getNews = {
         <h3>Special Time Travelers' Pet Quest: Mysterious Mechanical Marvels!</h3>
         <p>Hello Habiticans! We've released a brand-new quest in the Time Travelers' shop! It will be available at the cost of one <a href='https://habitica.fandom.com/wiki/Mystic_Hourglass' target='_blank'>Mystic Hourglass</a>, and is not limited, so you can buy it anytime you like, and as many times as you like. Get "<a href='/shops/time'>Mysterious Mechanical Marvels</a>", and earn some futuristic Robot pets by completing your real-life tasks!</p>
         <div class="small mb-3">by Beffymaroo, Rev, artemie, McCoyly, FolleMente, elyons1, QuartzFox, and SabreCat</div>
+        <div class="promo_mystery_201908 center-block"></div>
+        <h3>Last Chance for Footloose Faun Set</h3>
+        <p>Reminder: this weekend is your final chance to <a href='/user/settings/subscription'>subscribe and receive the Footloose Faun Set</a>! Subscribing also lets you buy Gems for Gold. The longer your subscription, the more Gems you get!</p>
+        <p>Thanks so much for your support! You help keep Habitica running.</p>
+        <div class="small mb-3">by Beffymaroo</div>
+        <div class="promo_farm_friends_bundle center-block"></div>
+        <h3>Last Chance for Farm Friends Pet Quest Bundle</h3>
+        <p>Time is also running out for the discounted Farm Friends Pet Quest Bundle, featuring the Cow, Horse and Sheep quests all for seven Gems! Be sure to lasso it before it gallops out of the <a href='/shops/quests'>Quest Shop</a>!</p>
+        <div class="small">by Lemoness and SabreCat</div>
+        <div class="small">Art by UncommonCriminal, Soloana, fuzzytrees, Feralem Tau, Eevachu, Beffymaroo, JessicaChase, starsystemic, nonight, Misceo and Tocath</div>
+        <div class="small mb-3">Writing by schizelle, VikingRunner, Salambander, Leephon, and Lemoness</div>
       </div>
       `,
     });
