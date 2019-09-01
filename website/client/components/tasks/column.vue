6d76dfe15f59a2429c4cc0c51e35925935ef0c6c
negue <negue@users.noreply.github.com>
2019-09-01 14:50:48 +0200

    hide "clear completed todos" while no entries are listed (#11321)
    * hide "clear completed todos" while no entries are listed
    
    * move column background (negative margin)

--- website/client/components/tasks/column.vue
+++ website/client/components/tasks/column.vue
@@ -25,7 +25,7 @@
     )
     transition(name="quick-add-tip-slide")
       .quick-add-tip.small-text(v-show="quickAddFocused", v-html="$t('addMultipleTip', {taskType: $t(typeLabel)})")
-    clear-completed-todos(v-if="activeFilter.label === 'complete2' && isUser === true")
+    clear-completed-todos(v-if="activeFilter.label === 'complete2' && isUser === true && taskList.length > 0")
     .column-background(
       v-if="isUser === true",
       :class="{'initial-description': initialColumnDescription}",
@@ -207,6 +207,7 @@
     position: absolute;
     width: 100%;
     bottom: 32px;
+    margin-left: -8px;
 
     &.initial-description {
       top: 30%;
