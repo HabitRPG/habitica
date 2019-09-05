271fc720280e3a0250aef30bcd72d202f0eaf8a8
Sabe Jones <sabrecat@gmail.com>
2019-09-05 14:40:20 -0500

    chore(news): Kickstarter Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'HABITICA OFFICIAL CHALLENGES, NEW BACKGROUNDS, NEW ARMOIRE ITEMS';
+const LAST_ANNOUNCEMENT_TITLE = 'LAST CHANCE TO PLEDGE FOR HABITICA’S ENAMEL PIN KICKSTARTER!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,24 +30,14 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>9/3/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>9/5/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="scene_medal center-block"></div>
-        <h3>September 2019 Resolution Success Challenge and New Take This Challenge</h3>
-        <p>The Habitica team has launched a special official Challenge series hosted in the <a href='/groups/guild/6e6a8bd3-9f5f-4351-9188-9f11fcd80a99' target='_blank'>Official New Year's Resolution Guild</a>. These Challenges are designed to help you build and maintain goals that are destined for success and then stick with them as the year progresses. For this month's Challenge, <a href='/challenges/375de4a2-1050-4886-8eaf-1de25ed2ee02'>Celebrate Your Triumphs</a>, we're focusing on celebrating your positive outcomes so far! It has a 15 Gem prize, which will be awarded to five lucky winners on October 1st.</p>
-        <p>Congratulations to the winners of August's Challenge, @rcgwriter, @silverivy, @dontanticipate, @Namida2u, and @Frowldrees!</p>
-        <div class="promo_take_this center-block"></div>
-      <p>The next Take This Challenge has also launched, "<a href='/challenges/41b9de95-67aa-4609-a4b9-216c3c613018'>It's Dangerous to Go Alone!</a>", with a focus on strengthening social connections. Be sure to check it out to earn additional pieces of the Take This armor set!</p>
-        <p><a href='http://www.takethis.org/' target='_blank'>Take This</a> is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness.</p>
-        <p>Congratulations to the winners of the last Take This Challenge, "Enter Sandman!": grand prize winner Weatherwax (@Weatherwax1), and runners-up TacoDanger (@hb-3b58tnlhnzgixkj49), Mis (@Ellaiyn), @GodofTroll, @ellie_ivanova, and @agateAngel! Plus, all participants in that Challenge have received a piece of the <a href='http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set' target='_blank'>Take This item set</a> if they hadn't completed it already. It is located in your Rewards column. Enjoy!</p>
-        <div class="small mb-3">by Doctor B, the Take This team, Lemoness, Beffymaroo, and SabreCat</div>
-        <div class="promo_armoire_backgrounds_201909 center-block"></div>
-        <h3>September Backgrounds and Armoire Items</h3>
-        <p>We’ve added three new backgrounds to the Background Shop! Now your avatar can take in the warmth of an Autumn Flower Garden, brave the mysteries of an Ancient Tomb, and absorb knowledge in a Classroom. Check them out under User Icon > Backgrounds!</p>
-        <p>Plus, there’s new Gold-purchasable equipment in the Enchanted Armoire, including the Florid Fan, a Polished Pocketwatch, and a Resplendent Rapier. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)</p>
-        <div class="small mb-3">by Vikte, aspiring_advocate, Tigergurke, AnnDeLune, Katy133, ravenlune, QuartzFox, and GeraldThePixel.</div>
+        <img src="https://habitica-assets.s3.amazonaws.com/mobileApp/images/promo_kickstarter_last_chance.png" class="center-block mw-100 h-auto"></img>
+        <p>Hello Habiticans! A reminder that our <a href='https://www.kickstarter.com/projects/habitica/habitica-class-pins' target='_blank'>Kickstarter campaign</a> for Enamel Class Pins ends on September 10. If you've been thinking about pledging, now's the time!</p>
+        <p>We're fully funded and all our stretch goals have been met, so we've got nine exciting pins to choose from. Pin pledges will also receive animated in-game items as bonuses, plus free vinyl stickers of the pins they chose! Check it out today!</p>
+        <div class="small mb-3">by The Habitica Team</div>
       </div>
       `,
     });
