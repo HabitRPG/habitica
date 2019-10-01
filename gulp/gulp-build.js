86dbc23f3c51b77973b05029263408b6d538fb33
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2019-10-01 17:57:42 +0200

    fix server

--- gulp/gulp-build.js
+++ gulp/gulp-build.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import webpackProductionBuild from '../webpack/build';
 
 gulp.task('build:src', () => {
   return gulp.src('website/server/**/*.js')
@@ -16,18 +15,8 @@ gulp.task('build:common', () => {
 
 gulp.task('build:server', gulp.series('build:src', 'build:common', done => done()));
 
-// Client Production Build
-gulp.task('build:client', (done) => {
-  webpackProductionBuild((err, output) => {
-    if (err) return done(err);
-    console.log(output); // eslint-disable-line no-console
-    done();
-  });
-});
-
 gulp.task('build:prod', gulp.series(
   'build:server',
-  'build:client',
   'apidoc',
   done => done()
 ));
