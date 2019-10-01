3c7d3aafe55aa4dfa2115e2830030ad19c5bddf9
Sabe Jones <sabrecat@gmail.com>
2019-10-01 18:58:40 -0500

    feat(content): Armoire and BGs

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -3,7 +3,7 @@ import { authWithHeaders } from '../../middlewares/auth';
 let api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from controllers
-const LAST_ANNOUNCEMENT_TITLE = 'LAST CHANCE FOR SEPTEMBER ITEMS';
+const LAST_ANNOUNCEMENT_TITLE = 'NEW BACKGROUNDS, ARMOIRE ITEMS, AND OFFICIAL HABITICA CHALLENGES!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -30,20 +30,28 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>9/30/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>10/1/2019 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="promo_mystery_201909 center-block"></div>
-        <h3>Last Chance for Affable Acorn Set</h3>
-        <p>Reminder: this is the final day to <a href='/user/settings/subscription'>subscribe and receive the Affable Acorn Set</a>! Subscribing also lets you buy Gems for Gold. The longer your subscription, the more Gems you get!</p>
-        <p>Thanks so much for your support! You help keep Habitica running.</p>
-        <div class="small mb-3">by Beffymaroo</div>
-        <div class="promo_rocking_reptiles_bundle center-block"></div>
-        <h3>Last Chance for Rocking Reptiles Quest Bundle</h3>
-        <p>This is also the final day to buy the discounted Rocking Reptiles Pet Quest Bundle, featuring the Alligator, Snake, and Velociraptor quests all for seven gems! Be sure to get your claws on it before it slithers away from the <a href='/shops/quests'>Quest Shop</a>!</p>
-        <div class="small">Art by Gully, Willow The Witty, mfonda, UncommonCriminal, tabbytoes, EmeraldOx, LordDarkly, PainterProphet, Seraphina, Anna Glassman, Procyon, and Lilith of Alfheim</div>
-        <div class="small mb-3">Writing by Mike.Antonacci, lilackbar, Daniel The Bard, and felipena</div>
+        <div class="promo_armoire_backgrounds_201910 center-block"></div>
+        <h3>October Backgrounds and Armoire Items!</h3>
+        <p>We’ve added three new backgrounds to the Background Shop! Now your avatar can experiment in a Monster Maker's Workshop, ride in a magical Pumpkin Carriage, and journey across a Foggy Moor. Check them out under User Icon > Backgrounds!</p>
+        <p>Plus, there’s new gold-purchasable equipment in the Enchanted Armoire, including the Shadow Master Set. Better work hard on your real-life tasks to earn all the pieces! Enjoy :)</p>
+        <div class="small mb-3">by GeraldThePixel, QuartzFox, Tigergurke, Vikte, Daikagaru, and SabreCat</div>
+        <div class="scene_strength center-block"></div>
+        <h3>October 2019 Resolution Success Challenge and New Take This Challenge</h3>
+        <p>The Habitica team has launched a special official Challenge series hosted in the <a href='/groups/guild/6e6a8bd3-9f5f-4351-9188-9f11fcd80a99'>Official New Year's Resolution Guild</a>. These Challenges are designed to help you build and maintain goals that are destined for success and then stick with them as the year progresses. For this month's Challenge, <a href='/challenges/ef1f7844-a58a-48f8-aab0-0423ff67ffa2'>Staying Strong</a>, we're focusing on boosting your motivation heading into the final quarter of the year! It has a 15 Gem prize, which will be awarded to five lucky winners on November 1st.</p>
+        <p>Congratulations to the winners of the September Challenge: Inkblots, Betelgeuse_aOri, timohi, IceBlueMelody, and han-!</p>
+        <p>The next Take This Challenge has also launched, "<a href='/challenges/23f6f7b7-fcf5-46a2-b591-aae05d3e62fe'>Check Your HP!</a>", with a focus on tracking your mood over time. Be sure to check it out to earn additional pieces of the Take This armor set!</p>
+        <p><a href='http://www.takethis.org/' target='_blank'>Take This</a> is a nonprofit that seeks to inform the gamer community about mental health issues, to provide education about mental disorders and mental illness prevention, and to reduce the stigma of mental illness.</p>
+        <p>Congratulations to the winners of the last Take This Challenge, "It's Dangerous to Go Alone!": grand prize winner @Ukioye_Kana, and runners-up @TheReptilianCave, @Merilio, @DocBajillian, @Melodyheart, and @Melanchoii! Plus, all participants in that Challenge have received a piece of the <a href='http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set' target='_blank'>Take This item set</a> if they hadn't completed it already. It is located in your Rewards column.</p> Enjoy!
+        <div class="small mb-3">by Doctor B, the Take This team, Lemoness, Beffymaroo, and SabreCat</div>
+        <div class="promo_take_this center-block"></div>
+        <h3>Back-to-School Preparation Challenge Winners</h3>
+        <p>The winners of the Habitica Back-to-School Preparation Challenge have been selected! Congratulations to: gils__, talklesssmilemore, Nadoko, pigmaniac1941, and French1Fry !</p>
+        <p>Thank you to everyone who participated! We're excited to help you pursue your goals through the new school year and beyond!</p>
+        <div class="small mb-3">by Beffymaroo and SabreCat</div>
       </div>
       `,
     });
