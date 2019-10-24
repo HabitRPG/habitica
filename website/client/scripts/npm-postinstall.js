78946bf328fb6c8b63c47806d4f639144896f3c1
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2019-10-24 14:29:47 +0200

    fix(client scripts): add output and fix linting

--- website/client/scripts/npm-postinstall.js
+++ website/client/scripts/npm-postinstall.js
@@ -1,5 +1,9 @@
+/* eslint-disable import/no-commonjs */
+
 const { execSync } = require('child_process');
 
 if (process.env.NODE_ENV === 'production') {
-  execSync('npm run build');
+  execSync('npm run build', {
+    stdio: 'inherit',
+  });
 }
