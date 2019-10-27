8e9e34cd22c0af82b407e70c9335196331056cff
Adam Fitzgibbon <fitzgamer@hotmail.com>
2019-10-27 01:04:33 -0500

    update challenge member dropdown and api to display and search by username rather than display name

--- website/server/controllers/api-v3/members.js
+++ website/server/controllers/api-v3/members.js
@@ -330,7 +330,7 @@ function _getMembersForItem (type) {
       }
 
       if (req.query.search) {
-        query['profile.name'] = { $regex: req.query.search };
+        query['auth.local.username'] = { $regex: req.query.search };
       }
     } else if (type === 'group-members') {
       if (group.type === 'guild') {
@@ -389,7 +389,7 @@ function _getMembersForItem (type) {
       .lean()
       .exec();
 
-    // manually call toJSON with minimize: true so empty paths aren't returned
+      // manually call toJSON with minimize: true so empty paths aren't returned
     members.forEach(member => User.transformJSONUser(member, addComputedStats));
     res.respond(200, members);
   };
