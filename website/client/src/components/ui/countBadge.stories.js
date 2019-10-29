a54e932e3d49e85255724d23b6242538c68e5969
negue <eugen.bolz@gmail.com>
2019-10-29 23:16:59 +0100

    :tada: Working example of storybook and vue

---
+++ website/client/src/components/ui/countBadge.stories.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue';
+
+import CountBadge from './countBadge.vue';
+
+storiesOf('Count Badge', module)
+  .add('simple', () => ({
+    components: { CountBadge },
+    template: `
+      <div style="position: absolute; margin: 20px">
+        <count-badge :count="2" :show="true"></count-badge>
+      </div>
+    `,
+  }))
+  .add('bind count', () => ({
+    components: { CountBadge },
+    template: `
+      <div style="position: absolute; margin: 20px">
+        <count-badge :count="count" :show="true"></count-badge>
+      </div>
+    `,
+    data () {
+      return {
+        count: 3,
+      };
+    },
+  }))
+;
