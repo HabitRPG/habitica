1c719a8dbb409efc3f696d52f978f252293aad84
nikosmonaut <nikosmonaut@protonmail.ch>
2019-12-14 14:53:54 +0100

    Unlink group tags when a user leaves it

--- website/server/models/group.js
+++ website/server/models/group.js
@@ -1345,6 +1345,8 @@ schema.methods.leave = async function leaveGroup (user, keep = 'keep-all', keepC
     .map(task => this.unlinkTask(task, user, keep, false));
   await Promise.all(assignedTasksToRemoveUserFrom);
 
+  this.unlinkTags(user);
+
   // the user could be modified by calls to `unlinkTask` for challenge and group tasks
   // it has not been saved before to avoid multiple saves in parallel
   const promises = user.isModified() ? [user.save()] : [];
@@ -1405,6 +1407,15 @@ schema.methods.leave = async function leaveGroup (user, keep = 'keep-all', keepC
   promises.push(group.update(update).exec());
 
   return Promise.all(promises);
+};
+
+schema.methods.unlinkTags = function unlinkTags (user) {
+  const group = this;
+  user.tags.forEach(tag => {
+    if (tag.group && tag.group === group._id) {
+      tag.group = undefined;
+    }
+  });
 };
 
 /**
