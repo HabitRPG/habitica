2aecb9e21d0245f0c9fefaaeb7a8672b7d42a2de
Phillip Thelen <phillip@habitica.com>
2020-01-21 13:58:54 +0100

    temporarily log everything as error

--- website/server/libs/logger.js
+++ website/server/libs/logger.js
@@ -74,11 +74,7 @@ const loggerInterface = {
       const loggerArgs = [stack, errorData, ...otherArgs];
 
       // Treat 4xx errors that are handled as warnings, 5xx and uncaught errors as serious problems
-      if (!errorData || !errorData.isHandledError || errorData.httpCode >= 500) {
-        logger.error(...loggerArgs);
-      } else {
-        logger.warn(...loggerArgs);
-      }
+      logger.error(...loggerArgs);
     } else {
       logger.error(...args);
     }
