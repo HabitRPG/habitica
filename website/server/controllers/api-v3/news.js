cbac69ab11b7c1b9ba521b9d4da60c3a752e9f08
Sabe Jones <sabrecat@gmail.com>
2020-01-23 15:47:53 -0600

    chore(news): Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -4,7 +4,7 @@ const api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from
 // controllers
-const LAST_ANNOUNCEMENT_TITLE = 'NEW STEAMPUNK BACKGROUNDS FROM THE TIME TRAVELERS!';
+const LAST_ANNOUNCEMENT_TITLE = 'HABITICA BLOG POSTS! FAVORITE GUILDS AND USE CASE SPOTLIGHT';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -31,29 +31,43 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>1/21/2020 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>1/23/2020 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="d-flex justify-content-center mb-3">
-          <img src="https://habitica-assets.s3.amazonaws.com/mobileApp/images/background_steamworks_demo.gif" />
-          <img src="https://habitica-assets.s3.amazonaws.com/mobileApp/images/background_clocktower_demo.gif" />
-          <img src="https://habitica-assets.s3.amazonaws.com/mobileApp/images/background_airship_demo.gif" />
-        </div>
+        <div class="scene_beffymaroo center-block"></div>
+        <h3>Guild Spotlight: Beffymaroo's Favorites</h3>
         <p>
-          Hello Habiticans! We've released brand-new backgrounds in the Time Travelers' shop! Show
-          off your retrofuturistic outfits at the Clocktower, Steamworks, and even in an Airship!
-          These will be available at the cost of one <a
-          href='https://habitica.fandom.com/wiki/Mystic_Hourglass' target='_blank'>Mystic
-          Hourglass</a> each.
+          For this year's Guild Spotlight series, we're highlighting some favorites from Habitica's
+          staff, moderators, and maybe some high-level contributors!
         </p>
         <p>
-          Check out the <a href='/shops/time'>Time Travelers' shop</a> to find these cool
-          backgrounds, as well as past subscriber gear, the Robot pet quest, and even rare pets and
-          mounts!
+          This month we're kicking things off with some <a
+          href='https://habitica.wordpress.com/2020/01/23/guild-spotlight-our-favourites/' target=
+          '_blank'>picks from Beffymaroo!</a> If you want to curate your Habitica experience and
+          join active, positive Guilds, this is a great way to pick up some new ideas for Guilds
+          to join.
         </p>
-        <p>Thanks for supporting Habitica! We hope that you enjoy your new backgrounds.</p>
-        <div class="small mb-3">by gawrone, annetiger, SabreCat, and Viirus</div>
+        <div class="small mb-3">by Beffymaroo and shanaqui</div>
+        <div class="scene_rewards center-block"></div>
+        <h3>Use Case Spotlight: Custom Habitica Mechanics</h3>
+        <p>
+          This month's <a href=
+          'https://habitica.wordpress.com/2020/01/23/use-case-spotlight-custom-habitica-mechanics/'
+          target='_blank'>Use Case Spotlight</a> is about Custom Habitica Mechanics! It features a
+          number of great suggestions submitted by Habiticans in the <a href=
+          '/groups/guild/1d3a10bf-60aa-4806-a38b-82d1084a59e6'>Use Case Spotlights Guild</a>. We
+          hope it helps any of you who might be looking for ways to enhance your Habitica
+          experience.
+        </p>
+        <p>
+          Plus, we're collecting user submissions for the next spotlight! How do you maximize
+          participation in your quest party? We’ll be featuring player-submitted examples in Use
+          Case Spotlights on the Habitica Blog next month, so post your suggestions in the Use Case
+          Spotlight Guild now. We look forward to learning more about how you use Habitica to
+          improve your life and get things done!
+        </p>
+        <div class="small mb-3">by shanaqui</div>
       </div>
       `,
     });
