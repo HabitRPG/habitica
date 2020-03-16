013861e251b0f69439cf8c9c37d5a5a90f244b21
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-03-16 23:09:17 +0100

    add some unit and integration tests

--- test/api/v3/integration/user/auth/POST-user_auth_social.test.js
+++ test/api/v3/integration/user/auth/POST-user_auth_social.test.js
@@ -6,13 +6,14 @@ import {
   getProperty,
 } from '../../../../../helpers/api-integration/v3';
 
-describe('POST /user/auth/social', () => {
+describe.only('POST /user/auth/social', () => {
   let api;
   let user;
   const endpoint = '/user/auth/social';
   const randomAccessToken = '123456';
   const facebookId = 'facebookId';
   const googleId = 'googleId';
+  const appleId = 'appleId';
   let network = 'NoNetwork';
 
   beforeEach(async () => {
@@ -91,6 +92,62 @@ describe('POST /user/auth/social', () => {
   });
 
   describe('google', () => {
+    before(async () => {
+      const expectedResult = { id: googleId, displayName: 'a google user' };
+      sandbox.stub(passport._strategies.google, 'userProfile').yields(null, expectedResult);
+      network = 'google';
+    });
+
+    it('registers a new user', async () => {
+      const response = await api.post(endpoint, {
+        authResponse: { access_token: randomAccessToken }, // eslint-disable-line camelcase
+        network,
+      });
+
+      expect(response.apiToken).to.exist;
+      expect(response.id).to.exist;
+      expect(response.newUser).to.be.true;
+      await expect(getProperty('users', response.id, 'profile.name')).to.eventually.equal('a google user');
+    });
+
+    it('logs an existing user in', async () => {
+      const registerResponse = await api.post(endpoint, {
+        authResponse: { access_token: randomAccessToken }, // eslint-disable-line camelcase
+        network,
+      });
+
+      const response = await api.post(endpoint, {
+        authResponse: { access_token: randomAccessToken }, // eslint-disable-line camelcase
+        network,
+      });
+
+      expect(response.apiToken).to.eql(registerResponse.apiToken);
+      expect(response.id).to.eql(registerResponse.id);
+      expect(response.newUser).to.be.false;
+    });
+
+    it('add social auth to an existing user', async () => {
+      const response = await user.post(endpoint, {
+        authResponse: { access_token: randomAccessToken }, // eslint-disable-line camelcase
+        network,
+      });
+
+      expect(response.apiToken).to.exist;
+      expect(response.id).to.exist;
+      expect(response.newUser).to.be.false;
+    });
+
+    xit('enrolls a new user in an A/B test', async () => {
+      await api.post(endpoint, {
+        authResponse: { access_token: randomAccessToken }, // eslint-disable-line camelcase
+        network,
+      });
+
+      await expect(getProperty('users', user._id, '_ABtests')).to.eventually.be.a('object');
+    });
+  });
+
+  describe('apple', () => {
     before(async () => {
       const expectedResult = { id: googleId, displayName: 'a google user' };
       sandbox.stub(passport._strategies.google, 'userProfile').yields(null, expectedResult);
