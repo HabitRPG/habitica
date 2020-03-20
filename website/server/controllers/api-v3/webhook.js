f8aa756d524c250b21c4fee4427db316bb564460
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-03-20 23:26:21 +0100

    Disable Failing Webhooks (#11966)
    * todo comment
    
    * add failures field to webhooks and sanitize
    
    * implement logic
    
    * use update instead of save
    
    * specify timeout and maximum number of retries
    
    * add tests

--- website/server/controllers/api-v3/webhook.js
+++ website/server/controllers/api-v3/webhook.js
@@ -108,7 +108,7 @@ api.addWebhook = {
   url: '/user/webhook',
   async handler (req, res) {
     const { user } = res.locals;
-    const webhook = new Webhook(req.body);
+    const webhook = new Webhook(Webhook.sanitize(req.body));
 
     const existingWebhook = user.webhooks.find(hook => hook.id === webhook.id);
 
--- website/server/models/webhook.js
+++ website/server/models/webhook.js
@@ -61,6 +61,10 @@ export const schema = new Schema({
     }), shared.i18n.t('invalidUrl')],
   },
   enabled: { $type: Boolean, required: true, default: true },
+  // How many times this webhook has failed, disabled after 10
+  failures: { $type: Number, default: 0 },
+  // When the last failure happened, if older than 1 month the number of failures is reset
+  lastFailureAt: { $type: Date },
   options: {
     $type: Schema.Types.Mixed,
     required: true,
@@ -76,7 +80,7 @@ export const schema = new Schema({
 });
 
 schema.plugin(baseModel, {
-  noSet: ['_id'],
+  noSet: ['_id', 'failures', 'lastFailureAt'],
   timestamps: true,
   _id: false,
 });
