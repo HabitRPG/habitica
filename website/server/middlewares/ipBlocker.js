4a2fc18fd3398ff5fe93abe6ac4efc93472919f9
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-03-25 17:03:46 +0100

    start implementing an ip blocker

---
+++ website/server/middlewares/ipBlocker.js
@@ -0,0 +1,52 @@
+import nconf from 'nconf';
+import {
+  NotAuthorized,
+} from '../libs/errors';
+
+// TODO explain what it does and how it works
+// TODO prod only
+// TODO logging
+// TODO 401 or 403?
+
+const BLOCKED_IPS_RAW = nconf.get('BLOCKED_IPS');
+
+const blockedIps = BLOCKED_IPS_RAW
+  .split(',')
+  .map(blockedIp => blockedIp.trim());
+
+export default function ipBlocker (req, res, next) {
+  // If there are no IPs to block, skip the middleware
+  if (blockedIps.length === 0) return next();
+
+  // If x-forwarded-for is undefined we're not behind the prod proxy
+  // TODO necessary?
+  const originIpsRaw = req.header('x-forwarded-for');
+  if (!originIpsRaw) return next();
+
+  // Format xxx.xxx.xxx.xxx, xxx.xxx.xxx.xxx (comma separated ip)
+  const originIps = originIpsRaw
+    .split(',')
+    .map(originIp => originIp.trim());
+
+  // The last ip in the list is made by GCP load balancer
+  // See https://cloud.google.com/load-balancing/docs/https#target-proxies
+  // In particular:
+  // << A Google Cloud external HTTP(S) load balancer adds two IP addresses to the header:
+  // the IP address of the requesting client and the external IP address of the load balancer's
+  // forwarding rule, in that order.
+  // Therefore, the IP address that immediately precedes the Google Cloud load balancer's
+  // IP address is the IP address of the system that contacts the load balancer.
+  // The system might be a client, or it might be another proxy server, outside Google Cloud,
+  // that forwards requests on behalf of a client. >>
+  //
+  // So the IP to be check is the penultimate in the list
+  const targetIp = originIps[originIps.length - 2];
+  if (!targetIp) return next();
+
+  if (blockedIps.includes(targetIp)) {
+    // Not translated because no user is loaded at this point
+    throw new NotAuthorized('This ip is blocked.');
+  }
+
+  return next();
+}
