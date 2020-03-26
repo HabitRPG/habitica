57e58d62b62d4c9e09b3e3520a2776cbed84b829
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-03-26 16:10:40 +0100

    fix code, comment code and improve response

--- website/server/middlewares/ipBlocker.js
+++ website/server/middlewares/ipBlocker.js
@@ -1,7 +1,13 @@
 import nconf from 'nconf';
 import {
-  NotAuthorized,
+  Forbidden,
 } from '../libs/errors';
+import apiError from '../libs/apiError';
+
+// Middleware to block unwanted IP addresses
+
+// NOTE: it's meant to be used behind a proxy (for example a load balancer)
+// that uses the 'x-forwarded-for' header to forward the original IP addresses.
 
 // A list of comma separated IPs to block
 // It works fine as long as the list is short,
@@ -9,24 +15,26 @@ import {
 const BLOCKED_IPS_RAW = nconf.get('BLOCKED_IPS');
 
 const blockedIps = BLOCKED_IPS_RAW
-  .split(',')
-  .map(blockedIp => blockedIp.trim());
+  ? BLOCKED_IPS_RAW.split(',').map(blockedIp => blockedIp.trim())
+  : [];
 
 export default function ipBlocker (req, res, next) {
   // If there are no IPs to block, skip the middleware
   if (blockedIps.length === 0) return next();
 
-  // If x-forwarded-for is undefined we're not behind the prod proxy
-  // TODO necessary?
+  // If x-forwarded-for is undefined we're not behind the production proxy
   const originIpsRaw = req.header('x-forwarded-for');
   if (!originIpsRaw) return next();
 
-  // Format xxx.xxx.xxx.xxx, xxx.xxx.xxx.xxx (comma separated ip)
+  // Format xxx.xxx.xxx.xxx, xxx.xxx.xxx.xxx (comma separated list of ip)
   const originIps = originIpsRaw
     .split(',')
     .map(originIp => originIp.trim());
 
-  // The last ip in the list is made by GCP load balancer
+  // We try to match any of the origins IPs against the blocked IPs list.
+  //
+  // In case we're behind a Google Cloud Load Balancer the last ip
+  // in the list is added by the load balancer.
   // See https://cloud.google.com/load-balancing/docs/https#target-proxies
   // In particular:
   // << A Google Cloud external HTTP(S) load balancer adds two IP addresses to the header:
@@ -36,14 +44,12 @@ export default function ipBlocker (req, res, next) {
   // IP address is the IP address of the system that contacts the load balancer.
   // The system might be a client, or it might be another proxy server, outside Google Cloud,
   // that forwards requests on behalf of a client. >>
-  //
-  // So the IP to be check is the penultimate in the list
-  const targetIp = originIps[originIps.length - 2];
-  if (!targetIp) return next();
 
-  if (blockedIps.includes(targetIp)) {
+  const match = originIps.find(originIp => blockedIps.includes(originIp)) !== undefined;
+
+  if (match === true) {
     // Not translated because no user is loaded at this point
-    throw new NotAuthorized('This ip is blocked.');
+    throw new Forbidden(apiError('ipAddressBlocked'));
   }
 
   return next();
