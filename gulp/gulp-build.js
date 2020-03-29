6bef105cf60e07457a08cf7302fae3ecc9976e6d
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-03-29 16:26:35 +0200

    fix(gulp): tasks order

--- gulp/gulp-build.js
+++ gulp/gulp-build.js
@@ -1,17 +1,18 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 
-gulp.task('build:server', () => gulp.src('website/server/**/*.js')
+gulp.task('build:babel:server', () => gulp.src('website/server/**/*.js')
   .pipe(babel())
   .pipe(gulp.dest('website/transpiled-babel/')));
 
-gulp.task('build:common', () => gulp.src('website/common/script/**/*.js')
+gulp.task('build:babel:common', () => gulp.src('website/common/script/**/*.js')
   .pipe(babel())
   .pipe(gulp.dest('website/common/transpiled-babel/')));
 
-gulp.task('build:prod', gulp.parallel(
-  'build:server',
-  'build:common',
+gulp.task('build:babel', gulp.parallel('build:babel:server', 'build:babel:common', done => done()));
+
+gulp.task('build:prod', gulp.series(
+  'build:babel',
   'apidoc',
   'content:cache',
   done => done(),
