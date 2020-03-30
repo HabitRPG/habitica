6d26fbc5f2841755d49ee6e3f1d0c4d479263da9
Sabe Jones <sabrecat@gmail.com>
2020-03-30 16:12:03 -0500

    feat(content): April subscriber items

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -4,7 +4,7 @@ const api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from
 // controllers
-const LAST_ANNOUNCEMENT_TITLE = 'BLOG POSTS: GUILD SPOTLIGHT AND USE CASE SPOTLIGHT!';
+const LAST_ANNOUNCEMENT_TITLE = 'LAST CHANCE FOR MARCH SUBSCRIBER ITEMS AND HUG A BUG BUNDLE!';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -31,37 +31,27 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>3/24/2020 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>3/30/2020 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="scene_QuartzFox center-block"></div>
-        <h3>Guild Spotlight: QuartzFox's Favorites!</h3>
+        <div class="promo_mystery_202003 center-block"></div>
+        <h3>Last Chance for Barbed Battler Set</h3>
         <p>
-          There's a new <a
-          href='https://habitica.wordpress.com/2020/03/26/guild-spotlights-our-favourites-2/'
-          target='_blank'>Guild Spotlight on the blog</a> featuring the favorite Guilds of Habitica
-          contributor @QuartzFox! Check it out now to find Guilds that have helped her and which
-          you might appreciate as well!
+          Reminder: this is the last day to receive the Barbed Battler Set when you <a
+          href='/user/settings/subscription'>sign up for a new Habitica subscription</a>!
+          Subscribing also lets you buy Gems with Gold. The longer your subscription, the more Gems
+          you can get!
         </p>
-        <div class="small mb-3">by shanaqui and QuartzFox</div>
-        <div class="scene_tough_times center-block"></div>
-        <h3>Use Case Spotlight: Managing Conflicting Needs</h3>
+        <p>Thanks so much for your support! You help keep Habitica running.</p>
+        <div class="small mb-3">by Beffymaroo</div>
+        <div class="promo_hugabug_bundle center-block"></div>
+        <h3>Last Chance for Hug a Bug Pet Quest Bundle</h3>
         <p>
-          This month's <a href='https://habitica.wordpress.com/2020/03/26/use-case-spotlight-managing-conflicting-goals-and-needs/'
-          target='_blank'>Use Case Spotlight</a> is about Managing Conflicting Needs! It features a
-          number of great suggestions submitted by Habiticans in the <a
-          href='/groups/guild/1d3a10bf-60aa-4806-a38b-82d1084a59e6'>Use Case Spotlights Guild</a>.
-          We hope it helps any of you who might be looking to balance different task types in your life.
+          This is also the final day to buy the discounted Hug a Bug Pet Quest Bundle, featuring
+          the Snail, Beetle, and Butterfly quests, all for seven Gems! Be sure to take your bug-
+          catching net over to the <a href='/shops/quests'>Quest Shop</a> before it bugs out!
         </p>
-        <p>
-          Plus, we're collecting user submissions for the next spotlight! How do you use Habitica
-          to Manage Long-Term Conditions? We’ll be featuring player-submitted examples in Use Case
-          Spotlights on the Habitica Blog next month, so post your suggestions in the Use Case
-          Spotlight Guild now. We look forward to learning more about how you use Habitica to
-          improve your life and get things done! 
-        </p>
-        <div class="small mb-3">by shanaqui</div>
       </div>
       `,
     });
