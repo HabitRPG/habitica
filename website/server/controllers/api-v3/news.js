a2c91aae70052dd7392cdb0f4b5235666eb0b03e
Sabe Jones <sabrecat@gmail.com>
2020-04-02 16:09:36 -0500

    chore(news): Challenge and Blog Bailey

--- website/server/controllers/api-v3/news.js
+++ website/server/controllers/api-v3/news.js
@@ -4,7 +4,7 @@ const api = {};
 
 // @TODO export this const, cannot export it from here because only routes are exported from
 // controllers
-const LAST_ANNOUNCEMENT_TITLE = 'HAPPY APRIL FOOLS DAY! (ALSO SUBSCRIBER MYSTERY ITEMS)';
+const LAST_ANNOUNCEMENT_TITLE = 'NEW OFFICIAL CHALLENGES AND BEHIND THE SCENES BLOG POST';
 const worldDmg = { // @TODO
   bailey: false,
 };
@@ -31,54 +31,65 @@ api.getNews = {
           <div class="mr-3 ${baileyClass}"></div>
           <div class="media-body">
             <h1 class="align-self-center">${res.t('newStuff')}</h1>
-            <h2>4/1/2020 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
+            <h2>4/2/2020 - ${LAST_ANNOUNCEMENT_TITLE}</h2>
           </div>
         </div>
         <hr/>
-        <div class="promo_april_fools_2020 center-block"></div>
-        <h3>Dessert Pets!</h3>
+        <div class="scene_hat_guild center-block"></div>
+        <h3>April 2020 Resolution Success Challenge and New Take This Challenge</h3>
         <p>
-          The April Fool rushes into the Tavern, clearly out of breath, with a gigantic picnic
-          basket in tow. The heavenly smell of icing and chocolate wafts from the open basket into
-          the crowd.
+          The Habitica team has launched a special official Challenge series hosted in the <a
+          href='/groups/guild/6e6a8bd3-9f5f-4351-9188-9f11fcd80a99' target='_blank'>Official New
+          Year's Resolution Guild</a>. These Challenges are designed to help you build and maintain
+          goals that are destined for success and then stick with them as the year progresses. For
+          this month's Challenge, <a href='/challenges/178ba70c-b446-4fd6-913c-408f6ac40c96'>Gather
+          Your Party</a>, we're focusing on finding encouraging allies to help you gain
+          accountability for your goals!! It has a 15 Gem prize, which will be awarded to five
+          lucky winners on May 1st.
         </p>
         <p>
-          "HABITICANS!" he cries. "I have heard you are all in need of some good cheer and comfort,
-          and here I am! I hope you don't mind, in rather a turnaround from last year, I've brought
-          along some treats to brighten your day."
+          Congratulations to the winners of March's Challenge: @mymartianromance,
+          @BadWolfandTheStorm, @KatieIrene, @LastminuteKing, and @Janqt!
+        </p>
+        <div class="promo_take_this center-block"></div>
+        <p>
+          The next Take This Challenge has also launched, "<a
+          href='/challenges/a167f3d9-f32d-46b5-ba4b-5277357f2322'>This One Goes to 11!</a>", with
+          a focus on managing feelings of overwhelm. Be sure to check it out to earn additional
+          pieces of the Take This armor set!
         </p>
         <p>
-          "Looks like this year he's turned our pets into desserts!" says Piyo. She picks up her
-          new cinnamon bun companion gently. "Awww, they're so perfect and sweet!"
+          <a href='http://www.takethis.org/' target='_blank'>Take This</a> is a nonprofit that
+          seeks to inform the gamer community about mental health issues, to provide education
+          about mental disorders and mental illness prevention, and to reduce the stigma of mental
+          illness.
         </p>
         <p>
-          Equipping different pets will show different desserts. Have fun discovering them all!
+          Congratulations to the winners of the last Take This Challenge, "Gaining Inspiration
+          Points": grand prize winner @Susiturrikka, and runners-up @Kereenas-ff, @JohnMakiej,
+          @Martes625, @ScarletSlayer, and @bradders154276! Plus, all participants in that Challenge
+          have received a piece of the <a
+          href='http://habitica.wikia.com/wiki/Event_Item_Sequences#Take_This_Armor_Set'
+          target='_blank'>Take This item set</a> if they hadn't completed it already. It is located
+          in your Rewards column. Enjoy!
         </p>
-        <div class="npc_aprilFool center-block"></div>
-        <h3>Special April Fool's Social Media Challenge!</h3>
+        <div class="small mb-3">
+          by Doctor B, the Take This team, Lemoness, Beffymaroo, and SabreCat
+        </div>
+        <div class="scene_meditation center-block"></div>
+        <h3>Behind the Scenes: Mental Health Tips from the Team</h3>
         <p>
-          For even more fun, check out the <a
-          href='/challenges/c12641ae-16bc-428b-b949-4e4a6137a6aa'>official Challenge</a> posted
-          especially for today! Share your avatar featuring your new dessert pet on social media
-          between now and April 3, and you'll have a chance to win Gems and have your avatar
-          featured on the Habitica Blog!
-        </p>
-        <div class="small mb-3">by Beffymaroo, SabreCat, Piyo, and Viirus</div>
-        <div class="promo_mystery_202004 center-block"></div>
-        <h3>April Subscriber Items Revealed!</h3>
-        <p>
-          The April Subscriber Items have been revealed: the Majestic Monarch Item Set! <a
-          href='/user/settings/subscription'>Subscribe to Habitica</a> by April 30 to receive this
-          exciting set! If you're already an active subscriber, reload the site and then head to
-          Inventory > Items to claim your gear!
+          Hello Habiticans! In this edition of <a
+          href='https://habitica.wordpress.com/2020/04/02/behind-the-scenes-coping-in-a-crisis/'
+          target='_blank'>Behind the Scenes</a>, some of the Habitica staff and mods are sharing
+          some of the ways they're taking care of their mental health, physical well-being, and
+          work routines during the current crisis. We hope some of their tips are helpful for you
+          as well!
         </p>
         <p>
-          Subscribers also receive the ability to buy Gems with Gold -- the longer you subscribe,
-          the more Gems you can buy per month! There are other perks as well, such as longer access
-          to uncompressed data and a cute Jackalope pet. Best of all, subscriptions let us keep
-          Habitica running. Thank you very much for your support -- it means a lot to us.
+          Take care! 💜
         </p>
-        <div class="small mb-3">by Beffymaroo</div>
+        <div class="small mb-3">by shanaqui and the Habitica Team</div>
       </div>
       `,
     });
