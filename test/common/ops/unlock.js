20f4b2fabf5b35110580b4ead5278580e71efcfb
Bart Enkelaar <benkelaar@bol.com>
2020-04-22 22:48:04 +0200

    Issue 11050 - Fix last lint issue

--- test/common/ops/unlock.js
+++ test/common/ops/unlock.js
@@ -8,7 +8,7 @@ import {
   BadRequest,
 } from '../../../website/common/script/libs/errors';
 
-describe.only('shared.ops.unlock', () => {
+describe('shared.ops.unlock', () => {
   let user;
   const unlockPath = 'shirt.convict,shirt.cross,shirt.fire,shirt.horizon,shirt.ocean,shirt.purple,shirt.rainbow,shirt.redblue,shirt.thunder,shirt.tropical,shirt.zombie';
   const unlockGearSetPath = 'items.gear.owned.headAccessory_special_bearEars,items.gear.owned.headAccessory_special_cactusEars,items.gear.owned.headAccessory_special_foxEars,items.gear.owned.headAccessory_special_lionEars,items.gear.owned.headAccessory_special_pandaEars,items.gear.owned.headAccessory_special_pigEars,items.gear.owned.headAccessory_special_tigerEars,items.gear.owned.headAccessory_special_wolfEars';
