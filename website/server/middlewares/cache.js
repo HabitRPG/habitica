7f24e24f7ba48cd07373fe9c0e68fae8c85d1820
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-04-23 11:58:18 +0200

    wip: cache i18n responses

--- website/server/middlewares/cache.js
+++ website/server/middlewares/cache.js
@@ -1,4 +1,5 @@
 import onHeaders from 'on-headers';
+import packageInfo from '../../../package.json';
 
 export function disableCache (req, res, next) {
   res.set('Cache-Control', 'no-store');
@@ -9,6 +10,18 @@ export function disableCache (req, res, next) {
   onHeaders(res, function removeEtag () {
     this.removeHeader('ETag');
   });
+
+  return next();
+}
+
+export function cacheForAppVersion (req, res, next) {
+  const appVersion = packageInfo.version;
+
+  // If the app version is passed as a query parameter and matches the current package.json version
+  // Enable caching
+  if (req.query && req.query.appVersion === appVersion) {
+    
+  }
 
   return next();
 }
