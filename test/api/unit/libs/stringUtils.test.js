9f91775e78de419bfd267816cff2c3be1b5689c7
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-04-28 17:00:57 +0200

    fix(unit tests): re add code removed by mistake in stringUtils tests

--- test/api/unit/libs/stringUtils.test.js
+++ test/api/unit/libs/stringUtils.test.js
@@ -4,7 +4,7 @@ import bannedWords from '../../../../website/server/libs/bannedWords';
 describe('stringUtils', () => {
   describe('getMatchesByWordArray', () => {
     it('check all banned words are matched', async () => {
-      const message = bannedWords.join(',');
+      const message = bannedWords.join(',').replace(/\\/g, '');
       const matches = getMatchesByWordArray(message, bannedWords);
       expect(matches.length).to.equal(bannedWords.length);
     });
