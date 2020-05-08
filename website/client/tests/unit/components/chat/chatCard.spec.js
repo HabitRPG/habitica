57264c6a9c77562b2844219d141850a8d59fc400
Bart Enkelaar <benkelaar@bol.com>
2020-05-08 09:13:41 +0200

    fix(chat) - Improve unit test to only mount the wrapper once

--- website/client/tests/unit/components/chat/chatCard.spec.js
+++ website/client/tests/unit/components/chat/chatCard.spec.js
@@ -7,6 +7,10 @@ const localVue = createLocalVue();
 localVue.use(Store);
 
 describe('ChatCard', () => {
+  function createMessage (text) {
+    return { text, likes: {} };
+  }
+
   function createUser (username) {
     return {
       auth: { local: { username } },
@@ -16,9 +20,12 @@ describe('ChatCard', () => {
     };
   }
 
-  function mountWrapper (msg) {
-    return shallowMount(ChatCard, {
-      propsData: { msg },
+  const message = createMessage('test');
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(ChatCard, {
+      propsData: { msg: message },
       store: new Store({
         state: {
           user: { data: createUser('Tester') },
@@ -29,24 +36,22 @@ describe('ChatCard', () => {
       localVue,
       mocks: { $t: string => string },
     });
-  }
+  });
 
   it('shows the message text', () => {
-    const wrapper = mountWrapper({ text: 'test', likes: {} });
-
-    expect(wrapper.find('div.text').text()).to.equal('test');
+    expect(wrapper.find('div.text').text()).to.equal(message.text);
     expect(wrapper.find('div.mentioned-icon').exists()).to.be.false;
   });
 
   it('shows mention dot if user is mentioned', () => {
-    const wrapper = mountWrapper({ text: '@Tester', likes: {} });
+    wrapper.setProps({ msg: createMessage('@Tester') });
 
     expect(wrapper.find('div.mentioned-icon').exists()).to.be.true;
   });
 
   // Bug fixed by https://github.com/HabitRPG/habitica/pull/12177
   it('shows mention dot if user is mentioned after almostmention', () => {
-    const wrapper = mountWrapper({ text: 'thetester @Tester', likes: {} });
+    wrapper.setProps({ msg: createMessage('thetester @Tester') });
 
     expect(wrapper.find('div.mentioned-icon').exists()).to.be.true;
   });
