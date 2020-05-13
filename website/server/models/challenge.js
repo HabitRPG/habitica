96287bff6f9cec35fd65da51b478529a5226c3ed
Sabe Jones <sabrecat@gmail.com>
2020-05-13 15:27:31 -0500

    fix(lint): no-cycle

--- website/server/models/challenge.js
+++ website/server/models/challenge.js
@@ -12,7 +12,7 @@ import { removeFromArray } from '../libs/collectionManipulators';
 import shared from '../../common';
 import { sendTxn as txnEmail } from '../libs/email'; // eslint-disable-line import/no-cycle
 import { sendNotification as sendPushNotification } from '../libs/pushNotifications'; // eslint-disable-line import/no-cycle
-import { syncableAttrs, setNextDue } from '../libs/taskManager';
+import { syncableAttrs, setNextDue } from '../libs/taskManager'; // eslint-disable-line import/no-cycle
 
 const { Schema } = mongoose;
 
