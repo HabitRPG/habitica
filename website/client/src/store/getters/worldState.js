a89c436c400be7d862480b84ac0f4ba2c14714aa
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-05-25 16:45:41 +0200

    reduce code duplication

--- website/client/src/store/getters/worldState.js
+++ website/client/src/store/getters/worldState.js
@@ -1,29 +1,23 @@
-export function brokenSeasonalShop (store) {
+function getWorldDamage (store) {
   const worldState = store.state.worldState.data;
 
   return worldState
     && worldState.worldBoss
     && worldState.worldBoss.extra
-    && worldState.worldBoss.extra.worldDmg
-    && worldState.worldBoss.extra.worldDmg.seasonalShop;
+    && worldState.worldBoss.extra.worldDmg;
+}
+
+export function brokenSeasonalShop (store) {
+  const worldDmg = getWorldDamage(store);
+  return worldDmg && worldDmg.seasonalShop;
 }
 
 export function brokenMarket (store) {
-  const worldState = store.state.worldState.data;
-
-  return worldState
-    && worldState.worldBoss
-    && worldState.worldBoss.extra
-    && worldState.worldBoss.extra.worldDmg
-    && worldState.worldBoss.extra.worldDmg.market;
+  const worldDmg = getWorldDamage(store);
+  return worldDmg && worldDmg.market;
 }
 
 export function brokenQuests (store) {
-  const worldState = store.state.worldState.data;
-
-  return worldState
-    && worldState.worldBoss
-    && worldState.worldBoss.extra
-    && worldState.worldBoss.extra.worldDmg
-    && worldState.worldBoss.extra.worldDmg.quests;
+  const worldDmg = getWorldDamage(store);
+  return worldDmg && worldDmg.quests;
 }
