d7ee1ec4f4e59ea34e87e79553a234fe921e8fb9
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-08-10 18:38:24 +0200

    remove modals stack (#12423)

--- website/client/src/components/payments/sendGemsModal.vue
+++ website/client/src/components/payments/sendGemsModal.vue
@@ -296,7 +296,7 @@ export default {
       this.sendingInProgress = false;
     },
     close () {
-      this.$root.$emit('habitica::dismiss-modal', 'send-gems');
+      this.$root.$emit('bv::hide::modal', 'send-gems');
     },
   },
 };
