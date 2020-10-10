34a6533815ce83eac8d3b71f85cf22b44fb1db03
Bart Enkelaar <benkelaar@bol.com>
2020-10-10 22:15:08 +0200

    Make test setup more explicit

--- website/client/tests/unit/components/tasks/task.spec.js
+++ website/client/tests/unit/components/tasks/task.spec.js
@@ -59,35 +59,40 @@ describe('Task', () => {
 
     it('formats due date to today if due today', () => {
       const now = setClockTo('2019-09-17T17:57:00+02:00');
-      wrapper = makeWrapper({ date: now });
+      const date = moment(now);
+      wrapper = makeWrapper({ date });
 
       expect(wrapper.vm.formatDueDate()).to.equal('dueIn{"dueIn":"today"}');
     });
 
     it('formats due date to tomorrow if due tomorrow', () => {
-      const now = setClockTo('2012-06-12T14:17:28Z');
-      wrapper = makeWrapper({ date: now.add(1, 'day') });
+      setClockTo('2012-06-12T14:17:28Z');
+      const date = moment('2012-06-13T14:17:28Z');
+      wrapper = makeWrapper({ date });
 
       expect(wrapper.vm.formatDueDate()).to.equal('dueIn{"dueIn":"in a day"}');
     });
 
     it('formats due date to 5 days if due in 5 days', () => {
-      const now = setClockTo();
-      wrapper = makeWrapper({ date: now.add(5, 'days') });
+      setClockTo('2019-03-23T08:22:17Z');
+      const date = moment('2019-03-28T09:17:28Z');
+      wrapper = makeWrapper({ date });
 
       expect(wrapper.vm.formatDueDate()).to.equal('dueIn{"dueIn":"in 5 days"}');
     });
 
     it('formats due date to tomorrow if today but before dayStart', () => {
-      const now = setClockTo('2019-06-12T04:23:37+07:00');
-      wrapper = makeWrapper({ date: now.add(8, 'hours') }, { preferences: { dayStart: 7 } });
+      setClockTo('2019-06-12T04:23:37+07:00');
+      const date = moment('2019-06-12:18:27+07:00');
+      wrapper = makeWrapper({ date }, { preferences: { dayStart: 7 } });
 
       expect(wrapper.vm.formatDueDate()).to.equal('dueIn{"dueIn":"in a day"}');
     });
 
     it('formats due date to today if due tomorrow before dayStart', () => {
-      const now = setClockTo('2017-07-02T20:21:27Z');
-      wrapper = makeWrapper({ date: now.add(5, 'hours') }, { preferences: { dayStart: 5 } });
+      setClockTo('2017-07-02T20:21:27Z');
+      const date = moment('2017-07-03T01:30:02Z');
+      wrapper = makeWrapper({ date }, { preferences: { dayStart: 5 } });
 
       expect(wrapper.vm.formatDueDate()).to.equal('dueIn{"dueIn":"today"}');
     });
