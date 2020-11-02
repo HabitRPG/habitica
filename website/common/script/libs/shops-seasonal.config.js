31d0cb5a91912d189cd34d13e8de13170de5d1da
Sabe Jones <sabrecat@gmail.com>
2020-11-01 22:02:39 -0600

    chore(event): conclude and fix rotten potatoe

--- website/common/script/libs/shops-seasonal.config.js
+++ website/common/script/libs/shops-seasonal.config.js
@@ -1,25 +1,19 @@
-import { SEASONAL_SETS } from '../content/constants';
+// import { SEASONAL_SETS } from '../content/constants';
 
 export default {
-  opened: true,
+  opened: false,
 
-  currentSeason: 'Fall',
+  currentSeason: 'Closed',
 
   dateRange: { start: '2020-09-22', end: '2020-10-31' },
 
   availableSets: [
-    ...SEASONAL_SETS.fall,
   ],
 
   pinnedSets: {
-    healer: 'fall2020DeathsHeadMothHealerSet',
-    rogue: 'fall2020TwoHeadedRogueSet',
-    warrior: 'fall2020WraithWarriorSet',
-    wizard: 'fall2020ThirdEyeMageSet',
   },
 
   availableSpells: [
-    'spookySparkles',
   ],
 
   availableQuests: [
