ff72bc3c70e53375ee0e5dd3431cfe3a8f101f9f
negue <eugen.bolz@gmail.com>
2021-04-13 02:05:44 +0200

    missing import

--- website/common/script/libs/shops.js
+++ website/common/script/libs/shops.js
@@ -16,6 +16,7 @@ import seasonalShopConfig from './shops-seasonal.config';
 import featuredItems from '../content/shop-featuredItems';
 
 import getOfficialPinnedItems from './getOfficialPinnedItems';
+import { getClassName } from './getClassName';
 
 const shops = {};
 
