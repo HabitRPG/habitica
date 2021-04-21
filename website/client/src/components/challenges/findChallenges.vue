4d7f316af23a5b222c472560ebe95533ccee31a5
Lizapritchardg <lizap@umich.edu>
2021-04-21 10:33:24 -0400

    Revert "added membership filtering function to discover challenges"
    This reverts commit 4cd5a4cb4ea86ea2a764f1b980f606cea81d2459.

--- website/client/src/components/challenges/findChallenges.vue
+++ website/client/src/components/challenges/findChallenges.vue
@@ -171,23 +171,7 @@ export default {
   computed: {
     ...mapState({ user: 'user.data' }),
     filteredChallenges () {
-      const { filters } = this;
-      const { user } = this;
-
-      return this.challenges.filter(challenge => {
-        let isMember = true;
-
-        const filteringRole = filters.membership && filters.membership.length > 0;
-        if (filteringRole && filters.membership.indexOf('participating') !== -1) {
-          isMember = this.isMemberOfChallenge(user, challenge);
-        }
-
-        if (filteringRole && filters.membership.indexOf('not_participating') !== -1) {
-          isMember = !this.isMemberOfChallenge(user, challenge);
-        }
-
-        return isMember;
-      });
+      return this.challenges;
     },
   },
   mounted () {
