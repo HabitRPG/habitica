591c9bc98cbf6c048f808b7b1e6b14c21f8d55ee
Bart Enkelaar <benkelaar@gmail.com>
2021-04-29 22:18:58 +0200

    #12781 Add a unified component for small modals (#12987)
    * 12781 - Add common component for small modal windows
    
    * 12781 - Fix drop cap layout and hide footer
    
    * 12781 - Implement drop cap reached and won challenge modals
    
    * 12781 - Couple small fixes
    
    * 12781 - Remove added drop cap modal test convenience
    
    * Less assumptions in small-modal component
    
    * 12781 - Fix already migrated small modals
    
    * Figured out how to properly test Bootstrap modals
    
    * 12781 - Add unit test for success modal
    
    * 12781 - Some more steps towards integrating successModal.vue
    
    * 12781 - Use small modal mixin in success modal
    
    * 12781 - Optimize templating in successModal
    
    * 12781 - Revert AppFooter additions
    
    * 12781 - Add test case and remove unnecessary method

--- website/client/src/components/achievements/levelUp.vue
+++ website/client/src/components/achievements/levelUp.vue
@@ -1,167 +1,45 @@
 <template>
-  <b-modal
-    id="level-up"
-    size="sm"
-    :title="title"
-    ok-only
-    :ok-title="$t('onwards')"
-    :footer-class="{ greyed: displayRewardQuest }"
-  >
-    <section class="d-flex">
-      <span
-        class="star-group mirror"
-        v-html="icons.starGroup"
-      ></span>
+  <small-modal id="level-up" :title="title">
+    <starred>
       <avatar
         class="avatar"
         :member="user"
+        hide-class-badge
       />
-      <span
-        class="star-group"
-        v-html="icons.starGroup"
-      ></span>
-    </section>
+    </starred>
 
-    <p
-      v-once
-      class="text"
-    >
-      {{ $t('levelup') }}
-    </p>
+    <p class="text">{{ $t('levelup') }}</p>
 
-    <section
-      v-if="displayRewardQuest"
-      class="greyed"
-    >
-      <div
-        v-once
-        class="your-rewards d-flex"
-      >
-        <span
-          class="sparkles"
-          v-html="icons.sparkles"
-        ></span>
-        <span class="text">{{ $t('yourRewards') }}</span>
-        <span
-          class="sparkles mirror"
-          v-html="icons.sparkles"
-        ></span>
+    <template #greyed>
+      <div v-if="displayRewardQuest">
+        <your-rewards />
+        <div :class="questClass"></div>
       </div>
-
-      <div :class="questClass"></div>
-    </section>
-    <!-- @TODO: Keep this? .checkboxinput(type='checkbox', v-model=
-'user.preferences.suppressModals.levelUp', @change='changeLevelupSuppress()')
-label(style='display:inline-block') {{ $t('dontShowAgain') }}
-      -->
-  </b-modal>
+    </template>
+  </small-modal>
 </template>
 
 <style lang="scss">
-  @import '~@/assets/scss/colors.scss';
+  .avatar {
+    cursor: auto;
+  }
 
-  #level-up {
-    .modal-content {
-      border-radius: 8px;
-      box-shadow: 0 14px 28px 0 rgba($black, 0.24), 0 10px 10px 0 rgba($black, 0.28);
-    }
+  .text {
+    margin: 1rem;
+  }
 
-    @media (min-width: 576px) {
-      .modal-sm {
-        max-width: 330px;
-      }
-    }
-
-    header {
-      padding: 0;
-      border: none;
-
-      h5 {
-        margin: 31px auto 16px;
-        color: $purple-200;
-      }
-
-      button {
-        position: absolute;
-        right: 18px;
-        top: 12px;
-      }
-    }
-
-    footer {
-      padding: 0;
-      border: none;
-
-      button {
-        margin: 0 auto 32px auto;
-      }
-    }
-
-    .greyed {
-      background-color: $gray-700;
-    }
-
-    .modal-body {
-      padding: 0;
-    }
-
-    .mirror {
-      transform: scaleX(-1);
-    }
-
-    .star-group {
-      margin: auto;
-
-      svg {
-        height: 64px;
-        width: 40px;
-      }
-    }
-
-    .avatar {
-      cursor: auto;
-    }
-
-    .class-badge {
-      display: none !important;
-    }
-
-    .text {
-      margin: 25px 24px 24px;
-      min-height: auto;
-    }
-
-    .your-rewards {
-      margin: 0 auto;
-      width: fit-content;
-
-      .sparkles {
-        width: 32px;
-        margin-top: 12px;
-      }
-
-      .text {
-        font-weight: bold;
-        margin: 16px;
-        color: $gray-50;
-      }
-    }
-
-    section.greyed {
-      padding-bottom: 17px
-    }
-
-    .scroll {
-      margin: -11px auto 0;
-    }
+  .scroll {
+    margin: -11px auto 0;
   }
 </style>
 
 <script>
-import Avatar from '../avatar';
 import { mapState } from '@/libs/store';
-import starGroup from '@/assets/svg/star-group.svg';
-import sparkles from '@/assets/svg/sparkles-left.svg';
+
+import Avatar from '@/components/avatar';
+import smallModal from '@/components/ui/modal/smallModal';
+import starred from '@/components/ui/modal/starred.vue';
+import yourRewards from '@/components/ui/modal/your-rewards.vue';
 
 const levelQuests = {
   15: 'atom1',
@@ -173,14 +51,9 @@ const levelQuests = {
 export default {
   components: {
     Avatar,
-  },
-  data () {
-    return {
-      icons: Object.freeze({
-        starGroup,
-        sparkles,
-      }),
-    };
+    smallModal,
+    starred,
+    yourRewards,
   },
   computed: {
     ...mapState({ user: 'user.data' }),
@@ -192,12 +65,6 @@ export default {
     },
     questClass () {
       return `scroll inventory_quest_scroll_${levelQuests[this.user.stats.lvl]}`;
-    },
-  },
-  methods: {
-    changeLevelupSuppress () {
-      // @TODO: dispatch set({"preferences.suppressModals.levelUp":
-      // user.preferences.suppressModals.levelUp?true: false})
     },
   },
 };
