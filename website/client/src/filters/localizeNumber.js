0a85b37e5bcf9d9bab6b4e87257148476e1184ff
Sabe Jones <sabrecat@gmail.com>
2021-05-19 07:07:16 -0500

    fix(quests): don't break Rage if no options sent

--- website/client/src/filters/localizeNumber.js
+++ website/client/src/filters/localizeNumber.js
@@ -1,4 +1,4 @@
-export default function localizeNumber (valIn, lang, optIn) {
+export default function localizeNumber (valIn, lang, optIn = {}) {
   // Extra catch just incase non number
   const val = (typeof valIn === 'number') ? valIn : parseFloat(valIn);
 
