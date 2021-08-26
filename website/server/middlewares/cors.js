7870ffece84871b51b9a8fc921ad6da5c29d61aa
Nat Welch <nat@natwelch.com>
2021-08-26 19:12:11 -0400

    fix(cors): Allow all origins to support 3rd party tools (#13383)

--- website/server/middlewares/cors.js
+++ website/server/middlewares/cors.js
@@ -1,6 +1,6 @@
 export default function corsMiddleware (req, res, next) {
   res.set({
-    'Access-Control-Allow-Origin': req.header('origin') || '*',
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,HEAD,DELETE',
     'Access-Control-Allow-Headers': 'Authorization,Content-Type,Accept,Content-Encoding,X-Requested-With,x-api-user,x-api-key,x-client',
     // Expose rate limit headers to CORS requests
