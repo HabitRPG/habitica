a6e32d0822a8230356c68589f4530c1295928bcd
Sabe Jones <sabrecat@gmail.com>
2021-08-26 22:50:37 -0500

    Revert "chore(lint): disable breaking rules"
    This reverts commit 7207ca8cc4501df9b16c81246ace9b3c988c0c20.

--- website/client/tests/unit/.eslintrc.js
+++ website/client/tests/unit/.eslintrc.js
@@ -5,6 +5,5 @@ base.rules = base.rules || {};
 
 // TODO find a way to let eslint understand webpack aliases
 base.rules['import/no-unresolved'] = 'off';
-base.rules['import/extensions'] = 'off';
 
 module.exports = base;
