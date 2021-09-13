1f81e1971b2bee4a74058808d0791997edf4cc27
Sabe Jones <sabrecat@gmail.com>
2021-09-13 10:57:01 -0500

    fix(cron): remove now redundant logic

--- website/server/middlewares/cron.js
+++ website/server/middlewares/cron.js
@@ -126,19 +126,8 @@ async function cronAsync (req, res) {
 
     // Save user and tasks
     const toSave = [user.save()];
-    tasks.forEach(async task => {
+    tasks.forEach(task => {
       if (task.isModified()) toSave.push(task.save());
-      if (task.isModified() && task.group && task.group.taskId) {
-        const groupTask = await Tasks.Task.findOne({
-          _id: task.group.taskId,
-        }).exec();
-
-        if (groupTask) {
-          let delta = (0.9747 ** task.value) * -1;
-          if (groupTask.group.assignedUsers) delta /= groupTask.group.assignedUsers.length;
-          await groupTask.scoreChallengeTask(delta, 'down');
-        }
-      }
     });
     await Promise.all(toSave);
 
