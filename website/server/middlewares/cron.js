45fc2b62e37f6bd8048a79847ed6d47390a47492
Sabe Jones <sabrecat@gmail.com>
2021-09-13 10:48:05 -0500

    fix(cron): process team board tasks assigned to the user

--- website/server/middlewares/cron.js
+++ website/server/middlewares/cron.js
@@ -74,10 +74,19 @@ async function cronAsync (req, res) {
     }
 
     const tasks = await Tasks.Task.find({
-      userId: user._id,
-      $or: [ // Exclude completed todos
-        { type: 'todo', completed: false },
-        { type: { $in: ['habit', 'daily', 'reward'] } },
+      $and: [
+        {
+          $or: [
+            { userId: user._id },
+            { userId: { $exists: false }, 'group.assignedUsers': user._id },
+          ],
+        },
+        {
+          $or: [ // Exclude completed todos
+            { type: 'todo', completed: false },
+            { type: { $in: ['habit', 'daily', 'reward'] } },
+          ],
+        },
       ],
     }).exec();
 
