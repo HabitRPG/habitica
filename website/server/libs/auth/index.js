2db56db4fd9ceca10c952be7a4d39790ee7d1be6
Phillip Thelen <viirus@pherth.net>
2021-12-17 15:26:53 +0100

    fix error

--- website/server/libs/auth/index.js
+++ website/server/libs/auth/index.js
@@ -161,9 +161,9 @@ async function registerLocal (req, res, { isV3 = false }) {
   };
 
   if (existingUser) {
-    let networks = common.constants.SUPPORTED_SOCIAL_NETWORKS;
+    const networks = common.constants.SUPPORTED_SOCIAL_NETWORKS;
     // need to insert FB here to allow users who only have FB auth to connect local auth.
-    networks.insert({ key: 'facebook', name: 'Facebook' })
+    networks.push({ key: 'facebook', name: 'Facebook' });
     const hasSocialAuth = networks.find(network => {
       if (existingUser.auth.hasOwnProperty(network.key)) { // eslint-disable-line no-prototype-builtins, max-len
         return existingUser.auth[network.key].id;
