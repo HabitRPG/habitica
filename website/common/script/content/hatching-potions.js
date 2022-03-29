0783ccae242a03e3a234a3a5b5d7911e481cecf1
CuriousMagpie <eilatan@gmail.com>
2022-03-29 13:15:01 -0400

    2022 Spring Fling Potions

--- website/common/script/content/hatching-potions.js
+++ website/common/script/content/hatching-potions.js
@@ -70,13 +70,13 @@ const premium = {
     value: 2,
     text: t('hatchingPotionShimmer'),
     limited: true,
-    event: EVENTS.spring2020,
+    event: EVENTS.spring2022,
     _addlNotes: t('eventAvailabilityReturning', {
       availableDate: t('dateEndMarch'),
-      previousDate: t('marchYYYY', { year: 2018 }),
+      previousDate: t('marchYYYY', { year: 2020 }),
     }),
     canBuy () {
-      return moment().isBefore('2020-05-02');
+      return moment().isBefore(EVENTS.spring2022.end);
     },
   },
   Fairy: {
@@ -284,13 +284,13 @@ const premium = {
     value: 2,
     text: t('hatchingPotionCelestial'),
     limited: true,
-    event: EVENTS.spring2020,
+    event: EVENTS.spring2022,
     _addlNotes: t('eventAvailabilityReturning', {
       availableDate: t('dateEndMarch'),
-      previousDate: t('marchYYYY', { year: 2019 }),
+      previousDate: t('marchYYYY', { year: 2020 }),
     }),
     canBuy () {
-      return moment().isBefore('2020-05-02');
+      return moment().isBefore(EVENTS.spring2022.end);
     },
   },
   Sunshine: {
@@ -469,12 +469,13 @@ const premium = {
     value: 2,
     text: t('hatchingPotionPolkaDot'),
     limited: true,
-    event: EVENTS.spring2021,
-    _addlNotes: t('premiumPotionAddlNotes', {
-      date: t('dateEndMarch'),
+    event: EVENTS.spring2022,
+    _addlNotes: t('eventAvailabilityReturning', {
+      availableDate: t('dateEndMarch'),
+      previousDate: t('marchYYYY', { year: 2021 }),
     }),
     canBuy () {
-      return moment().isBefore(EVENTS.spring2021.end);
+      return moment().isBefore(EVENTS.spring2022.end);
     },
   },
   MossyStone: {
