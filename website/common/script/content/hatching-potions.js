7ba073e9f82a3a4ddba6fed3ed2d0f02a77e6aad
SabreCat <sabe@habitica.com>
2022-05-17 09:31:36 -0500

    fix(content): add and use event

--- website/common/script/content/hatching-potions.js
+++ website/common/script/content/hatching-potions.js
@@ -96,12 +96,13 @@ const premium = {
     value: 2,
     text: t('hatchingPotionFloral'),
     limited: true,
+    event: EVENTS.potions202205,
     _addlNotes: t('eventAvailabilityReturning', {
       availableDate: t('dateEndMay'),
       previousDate: t('mayYYYY', { year: 2021 }),
     }),
     canBuy () {
-      return moment().isBefore('2022-05-31');
+      return moment().isBefore(EVENTS.potions202205.end);
     },
   },
   Aquatic: {
@@ -296,12 +297,13 @@ const premium = {
     value: 2,
     text: t('hatchingPotionSunshine'),
     limited: true,
+    event: EVENTS.potions202205,
     _addlNotes: t('eventAvailabilityReturning', {
       availableDate: t('dateEndMay'),
       previousDate: t('mayYYYY', { year: 2020 }),
     }),
     canBuy () {
-      return moment().isBefore('2022-05-31');
+      return moment().isBefore(EVENTS.potions202205.end);
     },
   },
   Bronze: {
