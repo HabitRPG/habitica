09105b5b39ea997dedee81743a50fdeb6d80ac92
CuriousMagpie <eilatan@gmail.com>
2022-06-15 13:33:33 -0400

    Summer Splash 2022 Content

--- website/common/script/content/constants/events.js
+++ website/common/script/content/constants/events.js
@@ -10,10 +10,21 @@ const gemsPromo = {
 
 export const EVENTS = {
   noCurrentEvent: {
-    start: '2022-05-31T20:00-04:00',
-    end: '2022-06-30T20:00-05:00',
+    start: '2022-08-01T20:00-04:00',
+    end: '2022-09-30T20:00-05:00',
     season: 'normal',
     npcImageSuffix: '',
+  },
+  summer2022: {
+    start: '2022-06-21T08:00-04:00',
+    end: '2022-07-31T20:00-04:00',
+    season: 'summer',
+    npcImageSuffix: 'summer',
+    gear: true,
+  },
+  bundle202206: {
+    start:'2022-06-14T08:00-04:00',
+    end:'2022-06-30T20:00-04:00',
   },
   potions202205: {
     start: '2022-05-17T08:00-04:00',
