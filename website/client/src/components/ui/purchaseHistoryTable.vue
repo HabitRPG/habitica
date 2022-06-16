0374890d90ea7fca37742f38cf5f0497b51a5664
negue <eugen.bolz@gmail.com>
2022-06-16 17:19:52 +0200

    WIP - redesign of Gem / Hourglass History

--- website/client/src/components/ui/purchaseHistoryTable.vue
+++ website/client/src/components/ui/purchaseHistoryTable.vue
@@ -1,92 +1,218 @@
 <template>
-  <div class="row">
-    <div class="col-6">
-      <h1>{{ $t('gemTransactions') }}</h1>
-      <span v-if="gemTransactions.length === 0">{{ $t('noGemTransactions') }}</span>
-      <table class="table">
-        <tr
-          v-for="entry in gemTransactions"
-          :key="entry.createdAt"
+  <div>
+    <div class="clearfix">
+      <div class="mb-4 float-left">
+        <button
+          class="page-header btn-flat tab-button textCondensed"
+          :class="{'active': selectedTab === 'gems'}"
+          @click="selectTab('gems')"
         >
-          <td>
-            <span
-              v-b-tooltip.hover="entry.createdAt"
-            >{{ entry.createdAt | timeAgo }}</span>
-          </td>
-          <td class="amount-column">
-            <span
-              class="svg-icon inline icon-24"
-              aria-hidden="true"
-              v-html="icons.gem"
-            ></span>
-            <span
-              class="amount gems"
-              :class="entry.amount | addedDeducted"
-            >{{ entry.amount * 4 }}</span>
-          </td>
-          <td>
-            <span>{{ transactionTypeText(entry.transactionType) }}</span>
-          </td>
-          <td>
-            <span v-if="['gift_send', 'gift_receive'].includes(entry.transactionType)">
-              <user-link
-                :user-id="entry.reference"
-                :name="entry.referenceText"
-              />
-            </span>
-            <span v-else v-html="entryReferenceText(entry)"></span>
-          </td>
-          <td v-if="entry.currentAmount">
-            <span
-              class="svg-icon inline icon-24"
-              aria-hidden="true"
-              v-html="icons.gem"
-            ></span>
-            <span
-              class="amount gems"
-            >{{ entry.currentAmount * 4 }}</span>
-          </td>
-        </tr>
-      </table>
-    </div>
-    <div class="col-6">
-      <h1>{{ $t('hourglassTransactions') }}</h1>
-      <span v-if="hourglassTransactions.length === 0">{{ $t('noHourglassTransactions') }}</span>
-      <table class="table">
-        <tr
-          v-for="entry in hourglassTransactions"
-          :key="entry.createdAt"
+          {{ $t('gemTransactions') }}
+        </button>
+        <button
+          class="page-header btn-flat tab-button textCondensed"
+          :class="{'active': selectedTab === 'hourglass'}"
+          @click="selectTab('hourglass')"
         >
-          <td>
-            <span
-              v-b-tooltip.hover="entry.createdAt"
-            >{{ entry.createdAt | timeAgo }}</span>
-          </td>
-          <td class="amount-column">
-            <span
-              class="svg-icon inline icon-24"
-              aria-hidden="true"
-              v-html="icons.hourglass"
-            ></span>
-            <span
-              class="amount hourglasses"
-              :class="entry.amount | addedDeducted"
-            >{{ entry.amount }}</span>
-          </td>
-          <td>
-            <span>{{ transactionTypeText(entry.transactionType) }}</span>
-          </td>
-          <td>
-            <span v-html="entryReferenceText(entry)"></span>
-          </td>
-        </tr>
-      </table>
+          {{ $t('hourglassTransactions') }}
+        </button>
+      </div>
     </div>
+
+    <div class="row">
+      <div class="col-12" v-if="selectedTab === 'gems'">
+        <span v-if="gemTransactions.length === 0">
+          {{ $t('noGemTransactions') }}
+        </span>
+        <table class="table">
+          <tr>
+            <th v-once class="timestamp-column">
+              {{ $t('timestamp')}}
+            </th>
+            <th v-once class="amount-column">
+              {{ $t('amount')}}
+            </th>
+            <th v-once class="action-column">
+              {{ $t('action')}}
+            </th>
+            <th v-once class="note-column">
+              {{ $t('note')}}
+            </th>
+          </tr>
+          <tr
+            v-for="entry in gemTransactions"
+            :key="entry.createdAt"
+          >
+            <td>
+              <span
+                v-b-tooltip.hover="entry.createdAt"
+              >{{ entry.createdAt | timeAgo }}</span>
+            </td>
+            <td>
+              <div class="amount-with-icon" :id="entry.id">
+                <span
+                  class="svg-icon inline icon-16 my-1"
+                  aria-hidden="true"
+                  v-html="entry.amount < 0 ? icons.gemRed : icons.gem"
+                ></span>
+                <span
+                  class="amount gems"
+                  :class="entry.amount | addedDeducted"
+                >{{ entry.amount * 4 }}</span>
+              </div>
+
+              <b-popover
+                v-if="typeof entry.currentAmount !== 'undefined'"
+                ref="popover"
+                :target="entry.id"
+                triggers="hover focus click"
+                placement="bottom"
+              >
+                <div class="remaining-amount-popover-content">
+                  {{ $t('remainingBalance') }}:
+                  <span
+                    class="svg-icon inline icon-16 ml-1"
+                    aria-hidden="true"
+                    v-html="icons.gem"
+                  ></span>
+                  <span
+                    class="amount gems"
+                  >{{ entry.currentAmount * 4 }}</span>
+                </div>
+              </b-popover>
+            </td>
+            <td>
+              <span>{{ transactionTypeText(entry.transactionType) }}</span>
+            </td>
+            <td>
+              {{entry.transactionType}}
+              <span v-if="['gift_send', 'gift_receive'].includes(entry.transactionType)">
+                <router-link
+                  class="user-link"
+                  :to="{'name': 'userProfile', 'params': {'userId': entry.reference}}"
+                >
+                  @{{ entry.referenceText }}
+                </router-link>
+              </span>
+              <span v-else-if="['create_challenge'].includes(entry.transactionType)">
+                <router-link
+                    class="challenge-link"
+                    :to="{ name: 'challenge', params: { challengeId: entry.reference } }">
+                  <span
+                    v-markdown="entry.referenceText"
+                  ></span>
+                </router-link>
+              </span>
+              <span v-else v-html="entryReferenceText(entry)"></span>
+
+
+                ({{entry.reference}})
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="col-12" v-if="selectedTab === 'hourglass'">
+        <span v-if="hourglassTransactions.length === 0">
+          {{ $t('noHourglassTransactions') }}
+        </span>
+        <table class="table">
+          <tr>
+            <th v-once class="timestamp-column">
+              {{ $t('timestamp')}}
+            </th>
+            <th v-once class="amount-column">
+              {{ $t('amount')}}
+            </th>
+            <th v-once class="action-column">
+              {{ $t('action')}}
+            </th>
+            <th v-once class="note-column">
+              {{ $t('note')}}
+            </th>
+          </tr>
+          <tr
+            v-for="entry in hourglassTransactions"
+            :key="entry.createdAt"
+          >
+            <td>
+              <span
+                v-b-tooltip.hover="entry.createdAt"
+              >{{ entry.createdAt | timeAgo }}</span>
+            </td>
+            <td>
+              <div class="amount-with-icon" :id="entry.id">
+                <span
+                  class="svg-icon inline icon-16 my-1"
+                  aria-hidden="true"
+                  v-html="entry.amount < 0 ? icons.hourglassRed : icons.hourglass"
+                ></span>
+                <span
+                  class="amount hourglasses"
+                  :class="entry.amount | addedDeducted"
+                >{{ entry.amount }}</span>
+              </div>
+
+              <b-popover
+                v-if="typeof entry.currentAmount !== 'undefined'"
+                ref="popover"
+                :target="entry.id"
+                triggers="hover focus click"
+                placement="bottom"
+              >
+                <div class="remaining-amount-popover-content">
+                  {{ $t('remainingBalance') }}:
+                  <span
+                    class="svg-icon inline icon-16 ml-1"
+                    aria-hidden="true"
+                    v-html="icons.hourglass"
+                  ></span>
+                  <span
+                    class="amount gems"
+                  >{{ entry.currentAmount }}</span>
+                </div>
+              </b-popover>
+            </td>
+            <td>
+              <span>{{ transactionTypeText(entry.transactionType) }}</span>
+            </td>
+            <td>
+              <span v-html="entryReferenceText(entry)"></span>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style lang="scss">
   @import '~@/assets/scss/colors.scss';
+
+  .page-header.btn-flat {
+    background: transparent;
+  }
+
+  .tab-button {
+    height: 2rem;
+    font-size: 24px;
+    font-weight: bold;
+    font-stretch: condensed;
+    line-height: 1.33;
+    letter-spacing: normal;
+    color: $gray-10;
+
+    margin-right: 1.125rem;
+    padding-left: 0;
+    padding-right: 0;
+    padding-bottom: 2.5rem;
+
+    &.active, &:hover {
+      color: $purple-300;
+      box-shadow: 0px -0.25rem 0px $purple-300 inset;
+      outline: none;
+    }
+  }
 
   .amount-column {
     white-space: nowrap;
@@ -98,7 +224,6 @@
 
   .amount {
     font-weight: bold;
-    font-size: 1.1rem;
     margin-left: 4px;
   }
 
@@ -122,16 +247,63 @@
       color: $red-10;
     }
   }
+
+  .amount-with-icon {
+    display: inline-flex;
+  }
+
+  .remaining-amount-popover-content {
+    display: flex;
+    font-size: 12px;
+    line-height: 1.33;
+    color: $white;
+  }
+
+  th {
+    border-top: 0 !important;
+    padding: 0.25rem 0.5rem !important;
+    font-weight: bold;
+    line-height: 1.71;
+    color: $gray-50;
+  }
+
+  td {
+    padding-left: 0.5rem !important;
+    padding-right: 0.5rem !important;
+
+    line-height: 1.71;
+    color: $gray-50;
+  }
+
+  .timestamp-column, .action-column  {
+    width: 20%;
+  }
+
+  .amount-column {
+    width: 10%;
+  }
+
+  .note-column {
+    width: 50%;
+  }
+
+  .challenge-link, .user-link {
+    color: $blue-10 !important;
+  }
 </style>
 
 <script>
 import moment from 'moment';
 import svgGem from '@/assets/svg/gem.svg';
+import svgGemRed from '@/assets/svg/gem-red.svg';
 import svgHourglass from '@/assets/svg/hourglass.svg';
-import userLink from '@/components/userLink';
+import svgHourglassRed from '@/assets/svg/hourglass-red.svg';
+import markdownDirective from '@/directives/markdown';
 
 export default {
-  components: { userLink },
+  directives: {
+    markdown: markdownDirective,
+  },
   filters: {
     timeAgo (value) {
       return moment(value).fromNow();
@@ -162,11 +334,17 @@ export default {
     return {
       icons: Object.freeze({
         gem: svgGem,
+        gemRed: svgGemRed,
         hourglass: svgHourglass,
+        hourglassRed: svgHourglassRed,
       }),
+      selectedTab: 'gems',
     };
   },
   methods: {
+    selectTab (type) {
+      this.selectedTab = type;
+    },
     entryReferenceText (entry) {
       if (entry.reference === undefined && entry.referenceText === undefined) {
         return '';
