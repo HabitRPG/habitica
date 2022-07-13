24705989c338c4aa9156f3ab475e538ddc5e2541
CuriousMagpie <eilatan@gmail.com>
2022-07-13 15:07:41 -0400

    fix: remove unnecessary console.log statement

--- website/client/src/components/settings/subscriptionOptions.vue
+++ website/client/src/components/settings/subscriptionOptions.vue
@@ -144,8 +144,6 @@ export default {
     },
     subscriptionBlocksOrdered () {
       const subscriptions = filter(subscriptionBlocks, o => o.discount !== true);
-      console.log(this.gift.subscription); // remove this
-
       return sortBy(subscriptions, [o => o.months]);
     },
   },
