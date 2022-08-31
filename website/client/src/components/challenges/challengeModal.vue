4b2439141acf48b6723ec498068fe5da7caa8165
Tundrian <antoniosansotta@gmail.com>
2022-08-31 17:30:25 -0400

    Issue #14175 - Created a categoryOptions.js file in website/common/script/content to store list of category Options. Imported and added the list to website/client/src/components : challenges/challengeModal, challenges/sidebar, groups/groupFormModal, and groups/groupSidebar. This replaced the statically typed lists that previously existed. Tested the challengeModal and sidebar but unable to test the groups files due to credit card requirements. -@Tundrian

--- website/client/src/components/challenges/challengeModal.vue
+++ website/client/src/components/challenges/challengeModal.vue
@@ -280,6 +280,7 @@ import markdownDirective from '@/directives/markdown';
 import { userStateMixin } from '../../mixins/userState';
 
 import { TAVERN_ID, MIN_SHORTNAME_SIZE_FOR_CHALLENGES, MAX_SUMMARY_SIZE_FOR_CHALLENGES } from '@/../../common/script/constants';
+import CategoryOptions from '@/../../common/script/content/categoryOptions';
 
 export default {
   directives: {
@@ -288,64 +289,7 @@ export default {
   mixins: [userStateMixin],
   props: ['groupId'],
   data () {
-    const categoryOptions = [
-      {
-        label: 'habitica_official',
-        key: 'habitica_official',
-      },
-      {
-        label: 'academics',
-        key: 'academics',
-      },
-      {
-        label: 'advocacy_causes',
-        key: 'advocacy_causes',
-      },
-      {
-        label: 'creativity',
-        key: 'creativity',
-      },
-      {
-        label: 'entertainment',
-        key: 'entertainment',
-      },
-      {
-        label: 'finance',
-        key: 'finance',
-      },
-      {
-        label: 'health_fitness',
-        key: 'health_fitness',
-      },
-      {
-        label: 'hobbies_occupations',
-        key: 'hobbies_occupations',
-      },
-      {
-        label: 'location_based',
-        key: 'location_based',
-      },
-      {
-        label: 'mental_health',
-        key: 'mental_health',
-      },
-      {
-        label: 'getting_organized',
-        key: 'getting_organized',
-      },
-      {
-        label: 'self_improvement',
-        key: 'self_improvement',
-      },
-      {
-        label: 'spirituality',
-        key: 'spirituality',
-      },
-      {
-        label: 'time_management',
-        key: 'time_management',
-      },
-    ];
+    const categoryOptions = CategoryOptions;
     const hashedCategories = {};
     categoryOptions.forEach(category => {
       hashedCategories[category.key] = category.label;
--- website/client/src/components/groups/groupSidebar.vue
+++ website/client/src/components/groups/groupSidebar.vue
@@ -86,6 +86,7 @@
 import throttle from 'lodash/throttle';
 import FilterSidebar from '@/components/ui/filterSidebar';
 import FilterGroup from '@/components/ui/filterGroup';
+import CategoryOptions from '@/../../common/script/content/categoryOptions';
 
 // TODO use checkbox-component to add/remove entries to *Filters, but without the v-model binding
 
@@ -94,64 +95,7 @@ export default {
   data () {
     return {
       categoryFilters: [],
-      categoryOptions: [
-        {
-          label: 'habitica_official',
-          key: 'habitica_official',
-        },
-        {
-          label: 'academics',
-          key: 'academics',
-        },
-        {
-          label: 'advocacy_causes',
-          key: 'advocacy_causes',
-        },
-        {
-          label: 'creativity',
-          key: 'creativity',
-        },
-        {
-          label: 'entertainment',
-          key: 'entertainment',
-        },
-        {
-          label: 'finance',
-          key: 'finance',
-        },
-        {
-          label: 'health_fitness',
-          key: 'health_fitness',
-        },
-        {
-          label: 'hobbies_occupations',
-          key: 'hobbies_occupations',
-        },
-        {
-          label: 'location_based',
-          key: 'location_based',
-        },
-        {
-          label: 'mental_health',
-          key: 'mental_health',
-        },
-        {
-          label: 'getting_organized',
-          key: 'getting_organized',
-        },
-        {
-          label: 'recovery_support_groups',
-          key: 'recovery_support_groups',
-        },
-        {
-          label: 'spirituality',
-          key: 'spirituality',
-        },
-        {
-          label: 'time_management',
-          key: 'time_management',
-        },
-      ],
+      categoryOptions: CategoryOptions,
       roleFilters: [],
       roleOptions: [
         {
