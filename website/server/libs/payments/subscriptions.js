6ca6f86012a3c015aafccbef5b42f1104846e883
SabreCat <sabe@habitica.com>
2022-10-06 14:34:37 -0500

    fix(lint): import syntax

--- website/server/libs/payments/subscriptions.js
+++ website/server/libs/payments/subscriptions.js
@@ -11,7 +11,7 @@ import { // eslint-disable-line import/no-cycle
   model as Group,
   basicFields as basicGroupFields,
 } from '../../models/group';
-import model as User from '../../models/user';
+import { model as User } from '../../models/user';
 import {
   NotAuthorized,
   NotFound,
