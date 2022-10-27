a6968735b66df30bdbefca73f8018a1110eafaac
CuriousMagpie <eilatan@gmail.com>
2022-10-27 15:30:50 -0400

    chore(content): bone picking

--- website/common/script/content/constants/petCompleteSetAchievements.js
+++ website/common/script/content/constants/petCompleteSetAchievements.js
@@ -1,15 +1,12 @@
 // this achievement covers all pets of a specific type--classic & quest
-import { dropPets, questPets } from '../stable';
 
 const PET_SET_COMPLETE_ACHIEVEMENTS = [
   {
     boneToPick: {
       type: 'pet',
       color: 'Skeleton',
-      egg: dropPets && questPets,
       petAchievement: 'boneToPick',
       petNotificationType: 'ACHIEVEMENT_PET_SET_COMPLETE',
-      // need to do some sort of loop here (or outside the constant)
     },
   },
 ];
