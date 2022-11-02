9e98e56e9be1f7462bfedc1226f209cd97f93cd2
Phillip Thelen <phillip@habitica.com>
2022-11-02 03:07:23 +0100

    Allow sub upgrades/downgrades on iOS (#14303)
    * Allow sub upgrades/downgrades on iOS
    
    * fix check
    
    * fix(lint): line length
    
    * fix(typo): customER
    
    * fix tests
    
    * Implement correct handling for when subs are up/downgraded
    
    * fix lint errors
    
    * fix test
    
    Co-authored-by: SabreCat <sabe@habitica.com>
    Co-authored-by: Sabe Jones <sabrecat@gmail.com>

--- website/server/libs/payments/subscriptions.js
+++ website/server/libs/payments/subscriptions.js
@@ -75,7 +75,15 @@ async function prepareSubscriptionValues (data) {
     ? data.gift.subscription.key
     : data.sub.key];
   const autoRenews = data.autoRenews !== undefined ? data.autoRenews : true;
-  const months = Number(block.months);
+  const updatedFrom = data.updatedFrom
+    ? shared.content.subscriptionBlocks[data.updatedFrom.key]
+    : undefined;
+  let months;
+  if (updatedFrom && Number(updatedFrom.months) !== 1) {
+    months = Math.max(0, Number(block.months) - Number(updatedFrom.months));
+  } else {
+    months = Number(block.months);
+  }
   const today = new Date();
   let group;
   let groupId;
