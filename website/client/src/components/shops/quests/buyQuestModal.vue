96a919ed4b465c63011b5473d90a89afa84497ff
SabreCat <sabe@habitica.com>
2022-11-23 13:13:06 -0600

    fix(shops): quest countdowns too

--- website/client/src/components/shops/quests/buyQuestModal.vue
+++ website/client/src/components/shops/quests/buyQuestModal.vue
@@ -319,7 +319,6 @@ export default {
 
       isPinned: false,
       selectedAmountToBuy: 1,
-      endDate: seasonalShopConfig.dateRange.end,
     };
   },
   computed: {
@@ -342,6 +341,9 @@ export default {
       if (this.priceType === 'gold') return this.icons.gold;
       if (this.priceType === 'hourglasses') return this.icons.hourglass;
       return this.icons.gem;
+    },
+    endDate () {
+      return moment(this.item.event.end);
     },
   },
   watch: {
