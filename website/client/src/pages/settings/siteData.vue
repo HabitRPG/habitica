dec1a1159df17325c23718b929edf3244e033284
negue <eugen.bolz@gmail.com>
2023-03-14 21:00:52 +0100

    developer mode dummy row

--- website/client/src/pages/settings/siteData.vue
+++ website/client/src/pages/settings/siteData.vue
@@ -30,6 +30,7 @@
 
       <table class="table">
         <api-row />
+        <developer-mode-row />
         <tr>
           <td colspan="3">
           </td>
@@ -46,10 +47,15 @@ import UserIdRow from '@/pages/settings/siteDataRows/userIdRow.vue';
 import UserDataRow from '@/pages/settings/siteDataRows/userDataRow.vue';
 import ApiRow from '@/pages/settings/siteDataRows/apiRow.vue';
 import WebhooksRow from '@/pages/settings/siteDataRows/webhooksRow.vue';
+import DeveloperModeRow from '@/pages/settings/siteDataRows/developerModeRow.vue';
 
 export default {
   components: {
-    WebhooksRow, ApiRow, UserDataRow, UserIdRow,
+    DeveloperModeRow,
+    WebhooksRow,
+    ApiRow,
+    UserDataRow,
+    UserIdRow,
   },
   mounted () {
     this.$store.dispatch('common:setTitle', {
---
+++ website/client/src/pages/settings/siteDataRows/developerModeRow.vue
@@ -0,0 +1,33 @@
+<template>
+  <tr>
+    <td class="settings-label">
+      {{ $t("developerMode") }}
+    </td>
+    <td class="settings-value">
+      Todo
+    </td>
+    <td class="settings-button">
+    </td>
+  </tr>
+</template>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/colors.scss';
+
+</style>
+
+<script>
+import { mapState } from '@/libs/store';
+
+export default {
+  mixins: [],
+  computed: {
+    ...mapState({
+      user: 'user.data',
+    }),
+  },
+  methods: {
+
+  },
+};
+</script>
