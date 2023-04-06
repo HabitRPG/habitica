e0de7b41f79499073402600c292ee042a58b4e5f
Phillip Thelen <phillip@habitica.com>
2023-04-06 16:09:56 +0200

    addtest case for blessing spell

--- test/api/v4/user/POST-user_class_cast_spellId.test.js
+++ test/api/v4/user/POST-user_class_cast_spellId.test.js
@@ -238,6 +238,12 @@ describe('POST /user/class/cast/:spellId', () => {
     await groupLeader.sync();
     expect(groupLeader.stats.mp).to.be.equal(170);
 
+
+    await groupLeader.update({ 'stats.mp': 200, 'stats.class': 'healer', 'stats.lvl': 20 });
+    await groupLeader.post('/user/class/cast/healAll');
+    await groupLeader.sync();
+    expect(groupLeader.stats.mp).to.be.equal(175);
+
     await groupLeader.update({ 'stats.mp': 200, 'stats.class': 'wizard', 'stats.lvl': 20 });
     await groupLeader.post('/user/class/cast/mpheal');
     await groupLeader.sync();
