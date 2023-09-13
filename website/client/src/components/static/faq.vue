eedfa57e92a2a2174973877cc00a22232f5124af
CuriousMagpie <eilatan@gmail.com>
2023-09-13 16:12:54 -0400

    well, at least the questions are showing up

--- website/client/src/components/static/faq.vue
+++ website/client/src/components/static/faq.vue
@@ -1,67 +1,73 @@
 <template>
-  <div
-    class="container-fluid"
-    role="tablist"
-  >
-    <div class="row">
-      <div class="col-12 col-md-6 offset-md-3">
-        <h1
-          v-once
-          id="faq-heading"
-        >
-          {{ $t('frequentlyAskedQuestions') }}
-        </h1>
-        <div
-          v-for="(entry, index) in faq.questions"
-          :key="index"
-          class="faq-question"
-        >
-          <h2
+  <div class="top-container mx-auto">
+    <div class="main-text mr-4">
+      <div
+        class="title-details"
+        role="tablist"
+      >
+        <div class="row body-text">
+          <h1
             v-once
-            v-if="index === 0"
+            id="faq-heading"
           >
-            {{ $t('general') }}
-          </h2>
-          <h2
-            v-once
-            v-if="entry.heading === 'party-with-friends'"
-            id="parties"
+            {{ $t('frequentlyAskedQuestions') }}
+          </h1>
+          <div
+            v-for="(entry, index) in faq.questions"
+            :key="index"
+            class="body-text"
           >
-            {{ $t('parties') }}
-          </h2>
-          <h3
-            v-once
-            v-b-toggle="entry.heading"
-            role="tab"
-            variant="info"
-            @click="handleClick($event)"
-          >
-            {{ entry.question }}
-          </h3>
-          <b-collapse
-            :id="entry.heading"
-            :visible="isVisible(entry.heading)"
-            accordion="faq"
-            role="tabpanel"
-          >
-            <div
+            <h2
+              v-if="index === 0"
               v-once
-              v-markdown="entry.web"
-              class="card-body"
-            ></div>
-          </b-collapse>
+            >
+              {{ $t('general') }}
+            </h2>
+            <h2
+              v-if="entry.heading === 'party-with-friends'"
+              v-once
+              id="parties"
+            >
+              {{ $t('parties') }}
+            </h2>
+            <h3
+              v-once
+              v-b-toggle="entry.heading"
+              role="tab"
+              variant="info"
+              @click="handleClick($event)"
+            >
+              {{ entry.question }}
+            </h3>
+            <b-collapse
+              :id="entry.heading"
+              :visible="isVisible(entry.heading)"
+              accordion="faq"
+              role="tabpanel"
+            >
+              <div
+                v-once
+                v-markdown="entry.web"
+                class="card-body"
+              ></div>
+            </b-collapse>
+          </div>
+          <hr>
+          <p
+            v-once
+            v-markdown="stillNeedHelp"
+          ></p>
         </div>
-        <hr>
-        <p
-          v-once
-          v-markdown="stillNeedHelp"
-        ></p>
       </div>
     </div>
+
+  <!-- final div! -->
   </div>
 </template>
 
 <style lang='scss' scoped>
+  @import '~@/assets/scss/colors.scss';
+
   h2 {
     color: #34313a;
     border-bottom: 1px solid #e1e0e3;
@@ -99,11 +105,198 @@
     top: 2rem;
   }
 
-  @media only screen and (max-width: 768px) {
-    .container-fluid {
-      margin: auto;
+  .top-container {
+    width: 66.67%;
+    margin-top: 80px;
+    display: flex;
+
+    @media (max-width: 1024px) {
+      flex-wrap: wrap;
     }
   }
+
+  .main-text {
+    .body-text {
+      font-size: 1em;
+      color: $gray-10;
+      line-height: 1.71;
+    }
+
+    .headings {
+      font-size: 1.15em;
+      font-weight: 400;
+      line-height: 1.75;
+      color: $purple-200;
+    }
+  }
+
+  .sidebar {
+    height: fit-content;
+    width: 330px;
+    background-color: $gray-700;
+    border-radius: 16px;
+
+    h2 {
+      color: $gray-10;
+      font-family: Roboto;
+      font-size: 14px;
+      font-weight: bold;
+      line-height: 1.71;
+    }
+
+    .staff {
+      .staff-icon {
+        width: 10px;
+        margin-top: 5px;
+      }
+
+      .title {
+        height: 24px;
+        color: $purple-300;
+        font-weight: bold;
+        display: inline-block;
+        margin-bottom: 4px;
+      }
+    }
+
+    .tier-list {
+      list-style-type: none;
+      padding: 0;
+      width: 282px;
+      font-size: 1em !important;
+
+      li {
+        height: 40px;
+        border-radius: 4px;
+        border: solid 1px $gray-500;
+        text-align: center;
+        padding: 8px 0;
+        margin-bottom: 8px;
+        margin-right: 4px;
+        font-weight: bold;
+        line-height: 1.71;
+      }
+
+      .tier1 {
+        color: #c42870;
+        .svg-icon {
+          width: 11px;
+          margin-top: 5px;
+        }
+      }
+
+      .tier2 {
+        color: #b01515;
+        .svg-icon {
+          width: 11px;
+          margin-top: 5px;
+        }
+      }
+
+      .tier3 {
+        color: #d70e14;
+        .svg-icon {
+          width: 13px;
+          margin-top: 4px;
+        }
+      }
+
+      .tier4 {
+        color: #c24d00;
+        .svg-icon {
+          width: 13px;
+          margin-top: 4px;
+        }
+      }
+
+      .tier5 {
+        color: #9e650f;
+        .svg-icon {
+          width: 8px;
+          margin-top: 7px;
+        }
+      }
+
+      .tier6 {
+        color: #2b8363;
+        .svg-icon {
+          width: 8px;
+          margin-top: 7px;
+        }
+      }
+
+      .tier7 {
+        color: #167e87;
+        .svg-icon {
+          width: 12px;
+          margin-top: 4px;
+        }
+      }
+
+      .moderator {
+        color: #277eab;
+        .svg-icon {
+          width: 13px;
+          margin-top: 3px;
+        }
+      }
+
+      .staff {
+        color: #6133b4;
+        .svg-icon {
+          width: 10px;
+          margin-top: 7px;
+        }
+      }
+
+      .npc {
+        color: $black;
+      }
+    }
+
+    .gradient {
+      position: absolute;
+      width: 330px;
+      height: 100px;
+      margin: -1px 0 116px;
+      background-image: linear-gradient(to bottom, $gray-700 0%, rgba(249, 249, 249, 0) 100%);
+    }
+
+    .grassy-meadow-backdrop {
+      background-repeat: repeat-x;
+      width: 330px;
+      height: 246px;
+    }
+
+    .daniel_front {
+      height: 246px;
+      width: 330px;
+      background-repeat: no-repeat;
+      margin: 0 auto;
+    }
+
+    .pixel-border {
+      width: 330px;
+      height: 30px;
+      background-repeat: no-repeat;
+      position: absolute;
+      margin-top: -30px;
+    }
+
+    .question {
+      font-size: 1em;
+      font-weight: bold;
+      line-height: 1.71;
+      color: $gray-10;
+      margin-top: 24px;
+    }
+
+    .contact p {
+      font-size: 1em;
+      margin-bottom: 0px;
+    }
+  }
+
 </style>
 
 <script>
