1a5c4a72e6e98668bb7ba1cc457b9364af06e94c
Sabe Jones <sabrecat@gmail.com>
2023-10-13 19:45:03 -0500

    fix(test): permissions not contrib lol

--- test/api/v3/integration/challenges/POST-challenge_flag_clear.test.js
+++ test/api/v3/integration/challenges/POST-challenge_flag_clear.test.js
@@ -23,7 +23,7 @@ describe('POST /challenges/:challengeId/clearflags', () => {
     admin = groupLeader;
     [nonAdmin] = members;
 
-    await admin.update({ 'contributor.moderator': true });
+    await admin.update({ 'permissions.moderator': true });
 
     challenge = await generateChallenge(admin, group);
     await admin.post(`/challenges/${challenge._id}/flag`);
