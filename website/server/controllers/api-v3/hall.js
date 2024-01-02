4f753ed893272a037e8f235ab3940ce1eb44d460
SabreCat <sabe@habitica.com>
2024-01-02 16:52:39 -0600

    fix(lint): destructure assignment

--- website/server/controllers/api-v3/hall.js
+++ website/server/controllers/api-v3/hall.js
@@ -275,7 +275,7 @@ api.updateHero = {
     }
 
     if (updateData.purchased && updateData.purchased.plan) {
-      const plan = updateData.purchased.plan;
+      const { plan } = updateData.purchased;
       if (plan.gemsBought) {
         hero.purchased.plan.gemsBought = plan.gemsBought;
       }
