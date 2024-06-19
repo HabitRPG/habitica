8dbd3c3db15aac5c3defd294b6c6613cf80136da
Phillip Thelen <phillip@habitica.com>
2024-06-19 17:37:04 +0200

    fix canOwn test

--- test/common/ops/armoireCanOwn.js
+++ test/common/ops/armoireCanOwn.js
@@ -3,6 +3,7 @@ import armoireSet from '../../../website/common/script/content/gear/sets/armoire
 describe('armoireSet items', () => {
   it('checks if canOwn has the same id', () => {
     Object.keys(armoireSet).forEach(type => {
+      if (type === 'all') return;
       Object.keys(armoireSet[type]).forEach(itemKey => {
         const ownedKey = `${type}_armoire_${itemKey}`;
         expect(armoireSet[type][itemKey].canOwn({
