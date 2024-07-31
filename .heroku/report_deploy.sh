883bb6bac4407fda8fb3d09e5300eb269136d9d0
Phillip Thelen <phillip@habitica.com>
2024-07-31 17:37:02 +0200

    add fallback for when script is run outside of git repo

--- .heroku/report_deploy.sh
+++ .heroku/report_deploy.sh
@@ -1,19 +1,22 @@
-DEVELOPERS=$(git log -5 --pretty=format:'%an')
-PARTS=$(cut -d"." -f1 <<< $BASE_URL)
-SERVER_NAME=$(cut -d"/" -f3 <<< ${PARTS[0]})
-
-IFS=$'\n'
-DEVELOPER=""
-for dev in $DEVELOPERS
-do
-    if [ "$DEVELOPER" == "" ]; then
-        if [[ ${dev} != *"[bot]"* ]]; then
-            DEVELOPER=$dev
+DEVELOPER="someone"
+if git rev-parse --git-dir > /dev/null 2>&1; then
+    DEVELOPERS=$(git log -5 --pretty=format:'%an')
+    IFS=$'\n'
+    DEVELOPER=""
+    for dev in $DEVELOPERS
+    do
+        if [ "$DEVELOPER" == "someone" ]; then
+            if [[ ${dev} != *"[bot]"* ]]; then
+                DEVELOPER=$dev
+                continue
+            fi
             continue
         fi
-        continue
-    fi
-done
+    done
+fi
+
+PARTS=$(cut -d"." -f1 <<< $BASE_URL)
+SERVER_NAME=$(cut -d"/" -f3 <<< ${PARTS[0]})
 
 SERVER_NAME=":$SERVER_EMOJI: $SERVER_NAME"
 
