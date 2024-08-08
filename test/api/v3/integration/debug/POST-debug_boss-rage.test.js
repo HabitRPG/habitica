15042318a72fb16f5ff4eb1d8dca386e584c3a7c
CuriousMagpie <eilatan@gmail.com>
2024-08-08 16:04:08 -0400

    adding contexts to test cases

--- test/api/v3/integration/debug/POST-debug_boss-rage.test.js
+++ test/api/v3/integration/debug/POST-debug_boss-rage.test.js
@@ -4,11 +4,9 @@ import {
   createAndPopulateGroup,
 } from '../../../../helpers/api-integration/v3';
 
-describe('POST /debug/boss-rage', () => {
+describe.only('POST /debug/boss-rage', () => {
   const PET_QUEST = 'trex_undead';
   let user;
-  let invitedUser;
-  let party;
   let questingGroup;
   let leader;
   let partyMembers;
@@ -16,17 +14,6 @@ describe('POST /debug/boss-rage', () => {
 
   beforeEach(async () => {
     user = await generateUser();
-    const { group, groupLeader, invitees } = await createAndPopulateGroup({
-      groupDetails: {
-        name: 'Test Party',
-        type: 'party',
-      },
-      members: 2,
-      invites: 1,
-    });
-    party = group;
-    user = groupLeader;
-    [invitedUser] = invitees;
   });
 
   beforeEach(() => {
@@ -48,17 +35,60 @@ describe('POST /debug/boss-rage', () => {
       });
   });
 
-  it('adds user to party', async () => {
-    await invitedUser.post(`/groups/${party._id}/join`);
-  });
+  context('user joins a party', () => {
+    let user; 
+    let invitedUser;
+    let party;
 
-  it('joins a quest from an invitation', async () => {
-    await leader.post(`/groups/${questingGroup._id}/quests/invite/${PET_QUEST}`);
-    await partyMembers[0].post(`/groups/${questingGroup._id}/quests/accept`);
+    beforeEach(async () =>{
+      const { group, groupLeader, invitees } = await createAndPopulateGroup({
+        groupDetails: {
+          name: 'Test Party',
+          type: 'party',
+        },
+        members: 2,
+        invites: 1,
+      });
+      party = group;
+      user = groupLeader;
+      [invitedUser] = invitees;
+    });
 
-    await Promise.all([partyMembers[0].sync(), questingGroup.sync()]);
-    expect(leader.party.quest.RSVPNeeded).to.equal(false);
-    expect(questingGroup.quest.members[partyMembers[0]._id]).to.equal(true);
+    it('adds user to party', async () => {
+      await invitedUser.post(`/groups/${party._id}/join`);
+    });
+
+    it('errors if user is not on a quest', async () => {
+      await expect(user.post('/debug/quest-progress'))
+        .to.eventually.be.rejected.and.to.deep.equal({
+          code: 400,
+          error: 'BadRequest',
+          message: 'User is not on a valid quest.',
+        });
+    });
+
+    it('joins a quest from an invitation', async () => {
+      await leader.post(`/groups/${questingGroup._id}/quests/invite/${PET_QUEST}`);
+      await partyMembers[0].post(`/groups/${questingGroup._id}/quests/accept`);
+
+      await Promise.all([partyMembers[0].sync(), questingGroup.sync()]);
+      expect(leader.party.quest.RSVPNeeded).to.equal(false);
+      expect(questingGroup.quest.members[partyMembers[0]._id]).to.equal(true);
+    }),
+  }); // why does this curly boi and parentheses throw an unexpected token error? Everything seems to match up...
+
+  context('successfully force starting a quest', () => {
+    it('allows quest leader to force start quest', async () => {
+      const questLeader = partyMembers[0];
+      await questLeader.updateOne({ [`items.quests.${PET_QUEST}`]: 1 });
+      await questLeader.post(`/groups/${questingGroup._id}/quests/invite/${PET_QUEST}`);
+
+      await questLeader.post(`/groups/${questingGroup._id}/quests/force-start`);
+
+      await questingGroup.sync();
+
+      expect(questingGroup.quest.active).to.eql(true);
+    });
   });
 
   it('increases boss rage to 50', async () => {
