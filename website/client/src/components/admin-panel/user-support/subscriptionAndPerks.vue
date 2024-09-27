ef8ff0ea9eebcbd682a34fd7f52722b92fdfae16
Phillip Thelen <phillip@habitica.com>
2024-09-27 14:14:44 +0200

    show date for hourglass bonus if it was received

--- website/client/src/components/admin-panel/user-support/subscriptionAndPerks.vue
+++ website/client/src/components/admin-panel/user-support/subscriptionAndPerks.vue
@@ -119,6 +119,25 @@
         </div>
         <div class="form-group row">
           <label class="col-sm-3 col-form-label">
+            Received hourglass bonus:
+          </label>
+          <div class="col-sm-9">
+            <div class="input-group">
+              <input
+                v-model="hero.purchased.plan.cumulativeCount"
+                class="form-control"
+                type="text"
+              >
+              <div class="input-group-append">
+                <strong class="input-group-text">
+                  {{ dateFormat(hero.purchased.plan.cumulativeCount) }}
+                </strong>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">
             Next Mystic Hourglass:
           </label>
           <strong class="col-sm-9 col-form-label">{{ nextHourglassDate }}</strong>
