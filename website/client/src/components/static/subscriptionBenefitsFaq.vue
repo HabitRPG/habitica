289345781fa64532556bae4e8aa2a395c0846327
Kalista Payne <sabrecat@gmail.com>
2024-10-15 10:29:46 -0500

    fix(faq): more h4 and p, less ul li

--- website/client/src/components/static/subscriptionBenefitsFaq.vue
+++ website/client/src/components/static/subscriptionBenefitsFaq.vue
@@ -12,41 +12,29 @@
         <p>{{ $t('contentFaqPara1') }}</p>
       </div>
       <h3>{{ $t('subscriptionHeading0') }}</h3>
+      <p>{{ $t('subscriptionDetail00') }}</p>
       <ul>
-        <li>{{ $t('subscriptionDetail00') }}
-          <ul>
-            <li>{{ $t('subscriptionDetail000') }}</li>
-            <li>{{ $t('subscriptionDetail001') }}</li>
-            <li>{{ $t('subscriptionDetail002') }}</li>
-            <li>{{ $t('subscriptionDetail003') }}</li>
-          </ul>
-        </li>
+        <li>{{ $t('subscriptionDetail000') }}</li>
+        <li>{{ $t('subscriptionDetail001') }}</li>
+        <li>{{ $t('subscriptionDetail002') }}</li>
+        <li>{{ $t('subscriptionDetail003') }}</li>
       </ul>
+      <p>{{ $t('subscriptionDetail01') }}</p>
       <ul>
-        <li>{{ $t('subscriptionDetail01') }}
-          <ul>
-            <li>{{ $t('subscriptionDetail010') }}</li>
-            <li>{{ $t('subscriptionDetail011') }}</li>
-            <li>{{ $t('subscriptionDetail012') }}</li>
-          </ul>
-        </li>
+        <li>{{ $t('subscriptionDetail010') }}</li>
+        <li>{{ $t('subscriptionDetail011') }}</li>
+        <li>{{ $t('subscriptionDetail012') }}</li>
       </ul>
       <h3>{{ $t('subscriptionHeading1') }}</h3>
+      <p>{{ $t('subscriptionDetail10') }}</p>
       <ul>
-        <li>{{ $t('subscriptionDetail10') }}
-          <ul>
-            <li>{{ $t('subscriptionDetail100') }}</li>
-            <li>{{ $t('subscriptionDetail101') }}</li>
-            <li>{{ $t('subscriptionDetail102') }}</li>
-          </ul>
-        </li>
+        <li>{{ $t('subscriptionDetail100') }}</li>
+        <li>{{ $t('subscriptionDetail101') }}</li>
+        <li>{{ $t('subscriptionDetail102') }}</li>
       </ul>
+      <p>{{ $t('subscriptionDetail11') }}</p>
       <ul>
-        <li>{{ $t('subscriptionDetail11') }}
-          <ul>
-            <li>{{ $t('subscriptionDetail110') }}</li>
-          </ul>
-        </li>
+        <li>{{ $t('subscriptionDetail110') }}</li>
       </ul>
       <h3>{{ $t('subscriptionHeading2') }}</h3>
       <ul>
@@ -66,79 +54,30 @@
         <li>{{ $t('subscriptionDetail33') }}</li>
       </ul>
       <h3>{{ $t('commonQuestions') }}</h3>
+      <h4>{{ $t('subscriptionDetail40')}}</h4>
+      <p>{{ $t('subscriptionDetail400')}}</p>
+      <h4>{{ $t('subscriptionDetail41')}}</h4>
+      <p>{{ $t('subscriptionDetail410')}}</p>
+      <h4>{{ $t('subscriptionDetail42')}}</h4>
+      <p>{{ $t('subscriptionDetail420')}}</p>
+      <h4>{{ $t('subscriptionDetail43')}}</h4>
+      <p>{{ $t('subscriptionDetail430')}}</p>
+      <h4>{{ $t('subscriptionDetail44')}}</h4>
+      <p>{{ $t('subscriptionDetail440')}}</p>
       <ul>
-        <li>{{ $t('subscriptionDetail40')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail400')}}</li>
-          </ul>
-        </li>
+        <li>{{ $t('subscriptionDetail4400', { initialNumber: 25, roundedNumber: 26 }) }}</li>
+        <li>{{ $t('subscriptionDetail4400', { initialNumber: 35, roundedNumber: 36 }) }}</li>
+        <li>{{ $t('subscriptionDetail4400', { initialNumber: 45, roundedNumber: 46 }) }}</li>
       </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail41')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail410')}}</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail42')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail420')}}</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail43')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail430')}}</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail44')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail440')}}
-              <ul>
-                <li>{{ $t('subscriptionDetail4400', { initialNumber: 25, roundedNumber: 26 }) }}
-                </li>
-                <li>{{ $t('subscriptionDetail4400', { initialNumber: 35, roundedNumber: 36 }) }}
-                </li>
-                <li>{{ $t('subscriptionDetail4400', { initialNumber: 45, roundedNumber: 46 }) }}
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail45')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail450')}}</li>
-            <li>{{ $t('subscriptionDetail451')}}</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail46')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail460')}}</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail47')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail470')}}</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>{{ $t('subscriptionDetail48')}}
-          <ul>
-            <li>{{ $t('subscriptionDetail480')}}</li>
-          </ul>
-        </li>
-      </ul>
+      <h4>{{ $t('subscriptionDetail45')}}</h4>
+      <p>{{ $t('subscriptionDetail450')}}</p>
+      <p>{{ $t('subscriptionDetail451')}}</p>
+      <h4>{{ $t('subscriptionDetail46')}}</h4>
+      <p>{{ $t('subscriptionDetail460')}}</p>
+      <h4>{{ $t('subscriptionDetail47')}}</h4>
+      <p>{{ $t('subscriptionDetail470')}}</p>
+      <h4>{{ $t('subscriptionDetail48')}}</h4>
+      <p>{{ $t('subscriptionDetail480')}}</p>
       <p
         v-html="$t('subscriptionPara2',
                    { mailto: '<a href=mailto:admin@habitica.com>admin@habitica.com</a>'}
