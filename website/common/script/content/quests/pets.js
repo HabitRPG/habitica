e1b9ca4500c2f513ee651dba71daa9f9cd312bc2
CuriousMagpie <eilatan@gmail.com>
2025-02-12 18:51:50 -0500

    fix: fix string

--- website/common/script/content/quests/pets.js
+++ website/common/script/content/quests/pets.js
@@ -1053,7 +1053,7 @@ const QUEST_PETS = {
         }, {
           type: 'eggs',
           key: 'Otter',
-          text: t('quesOtterDropOtterEgg'),
+          text: t('questOtterDropOtterEgg'),
         },
       ],
       gp: 90,
