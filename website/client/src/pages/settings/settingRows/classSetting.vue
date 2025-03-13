e26819d41cdd826fa8cf890b669fd2b57cebf38e
Kalista Payne <sabrecat@gmail.com>
2025-03-13 15:01:33 -0500

    fix(style): vertically align elements in your-balance

--- website/client/src/pages/settings/settingRows/classSetting.vue
+++ website/client/src/pages/settings/settingRows/classSetting.vue
@@ -66,7 +66,7 @@
           <your-balance
             :amount-needed="amountNeeded"
             currency-needed="gems"
-            class="d-flex"
+            class="d-flex align-items-center"
           />
         </div>
       </td>
