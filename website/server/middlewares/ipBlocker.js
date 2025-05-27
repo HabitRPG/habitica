b95cc653b6fcbbddf5bf34f2074fae900f36a362
Phillip Thelen <phillip@habitica.com>
2025-05-27 17:58:54 +0200

    Read IP blocks from database

--- website/server/middlewares/ipBlocker.js
+++ website/server/middlewares/ipBlocker.js
@@ -3,6 +3,7 @@ import {
   Forbidden,
 } from '../libs/errors';
 import { apiError } from '../libs/apiError';
+import { model as Blocker } from '../models/blocker';
 
 // Middleware to block unwanted IP addresses
 
@@ -22,10 +23,28 @@ const blockedIps = BLOCKED_IPS_RAW
     .filter(blockedIp => Boolean(blockedIp))
   : [];
 
+Blocker.watchBlockers({
+  type: 'ipaddress',
+  area: 'full',
+}, {
+  initial: true,
+}).on('change', async change => {
+  const { operation, blocker } = change;
+  if (operation === 'add') {
+    if (blocker.value && !blockedIps.includes(blocker.value)) {
+      blockedIps.push(blocker.value);
+    }
+  } else if (operation === 'delete') {
+    const index = blockedIps.indexOf(blocker.value);
+    if (index !== -1) {
+      blockedIps.splice(index, 1);
+    }
+  }
+});
+
 export default function ipBlocker (req, res, next) {
   // If there are no IPs to block, skip the middleware
   if (blockedIps.length === 0) return next();
-
   // Is the client IP, req.ip, blocked?
   const match = blockedIps.find(blockedIp => blockedIp === req.ip) !== undefined;
 
