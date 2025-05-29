c3fd1fdd66b025a2fdd3f25c620334d3aaa55430
Phillip Thelen <phillip@habitica.com>
2025-05-29 12:32:14 +0200

    correctly reset local data after creating blocker

--- website/client/src/components/blocker/index.vue
+++ website/client/src/components/blocker/index.vue
@@ -144,6 +144,12 @@ export default {
     async createBlocker (blocker) {
       await this.$store.dispatch('blockers:createBlocker', { blocker });
       this.showCreateForm = false;
+      this.newBlocker = {
+        type: '',
+        area: '',
+        value: '',
+        reason: '',
+      };
       this.loadBlockers();
     },
 
