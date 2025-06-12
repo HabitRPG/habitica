bf16cf797dbc447e13c5b85aa5df43d0d88661a4
Phillip Thelen <phillip@habitica.com>
2025-06-12 16:43:58 +0200

    layout tweaks

--- website/client/src/components/static/features.vue
+++ website/client/src/components/static/features.vue
@@ -10,7 +10,7 @@
         <img :src="makeUrl('features_taskboard.png')" class="img-fluid">
         <h2>{{ $t('marketing1Lead1Title') }}</h2>
         <div class="row justify-content-md-center">
-          <p class="col col-lg-8 col-xl-6 margin-auto">{{ $t('marketing1Lead1') }}</p>
+          <p class="col col-lg-8 col-xl-6 margin-auto description">{{ $t('marketing1Lead1') }}</p>
         </div>
       </div>
     </div>
@@ -18,12 +18,12 @@
       <div class="col-md-6 mb-5 mb-md-0">
         <img :src="makeUrl('features_gear.png')">
         <h2>{{ $t('marketing1Lead2Title') }}</h2>
-        <p>{{ $t('marketing1Lead2') }}</p>
+        <p class="description">{{ $t('marketing1Lead2') }}</p>
       </div>
       <div class="col-md-6 mb-5 mb-md-0">
         <img :src="makeUrl('features_items.png')">
         <h2>{{ $t('marketing1Lead3Title') }}</h2>
-        <p>{{ $t('marketing1Lead3') }}</p>
+        <p class="description">{{ $t('marketing1Lead3') }}</p>
       </div>
     </div>
     <hr>
@@ -32,18 +32,22 @@
         <h1>{{ $t('marketing2Header') }}</h1>
       </div>
     </div>
+    <div class="row mb-5">
+      <div class="col-12">
+        <h2>{{ $t('marketing2Lead1Title') }}</h2>
+        <p class="description">{{ $t('marketing2Lead1') }}</p>
+      </div>
+    </div>
     <div class="row">
       <div class="col-md-6 mb-5 mb-md-0">
-        <h2>{{ $t('marketing2Lead1Title') }}</h2>
-        <p>{{ $t('marketing2Lead1') }}</p>
         <img :src="makeUrl('features_monsters.png')">
         <h2>{{ $t('marketing2Lead2Title') }}</h2>
-        <p v-markdown="$t('marketing2Lead2')"></p>
+        <p class="description" v-markdown="$t('marketing2Lead2')"></p>
       </div>
       <div class="col-md-6 mb-5 mb-md-0">
         <img :src="makeUrl('features_challenges.png')">
         <h2>{{ $t('marketing2Lead3Title') }}</h2>
-        <p>{{ $t('marketing2Lead3') }}</p>
+        <p class="description">{{ $t('marketing2Lead3') }}</p>
       </div>
     </div>
     <hr>
@@ -56,12 +60,12 @@
       <div class="col-md-6 mb-5 mb-md-0">
         <img :src="makeUrl('features_mobile.png')">
         <h2>{{ $t('footerMobile') }}</h2>
-        <p v-markdown="$t('marketing3Lead1')"></p>
+        <p class="description" v-markdown="$t('marketing3Lead1')"></p>
       </div>
       <div class="col-md-6 mb-5 mb-md-0">
         <img :src="makeUrl('features_opensource.png')">
         <h2>{{ $t('marketing3Lead2Title') }}</h2>
-        <p v-markdown="$t('marketing3Lead2')"></p>
+        <p class="description" v-markdown="$t('marketing3Lead2')"></p>
       </div>
     </div>
     <hr>
@@ -76,7 +80,7 @@
           <img src="@/assets/images/marketing/education.png">
           <div class="media-body">
             <h2>{{ $t('marketing4Lead1Title') }}</h2>
-            <p>{{ $t('marketing4Lead1') }}</p>
+            <p class="description">{{ $t('marketing4Lead1') }}</p>
           </div>
         </div>
       </div>
@@ -85,31 +89,23 @@
           <img src="@/assets/images/marketing/wellness.png">
           <div class="media-body">
             <h2>{{ $t('marketing4Lead2Title') }}</h2>
-            <p>{{ $t('marketing4Lead2') }}</p>
+            <p class="description">{{ $t('marketing4Lead2') }}</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="row">
+    <div class="row mt-5">
       <div class="col-md-6 offset-md-3 mb-5 mb-md-0">
-        <h2>{{ $t('marketing4Lead3Title') }}</h2>
+        <h1>{{ $t('marketing4Lead3Title') }}</h1>
         <img :src="makeUrl('features_gamify.png')">
         <div class="cta-container">
-          <h3>{{ $t('marketing4Lead3-1') }}</h3>
+          <h2>{{ $t('marketing4Lead3-1') }}</h2>
           <button
             class="btn btn-lg btn-primary"
             @click="playButtonClick()"
           >
             {{ $t('playButton') }}
           </button>
-        </div>
-        <div>
-          <h3>{{ $t('marketing4Lead3-2') }}</h3>
-          <a
-            class="btn btn-lg btn-primary"
-            href="/static/plans"
-            target="_blank"
-          >{{ $t('contactUs') }}</a>
         </div>
       </div>
     </div>
@@ -133,6 +129,20 @@
   .cta-container {
     text-align: center;
     margin: 2em auto;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  .description {
+    max-width: 600px;
+    margin: auto;
+    font-size: 1.1rem;
   }
 </style>
 
