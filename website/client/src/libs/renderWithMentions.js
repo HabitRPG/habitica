1f90aa9d4f2187a59a85de63e1e41916004ad22c
Hafiz <hafizbhamidi@gmail.com>
2025-08-10 21:02:48 -0500

    Mention highlighting to only highlight w/username mentions

--- website/client/src/libs/renderWithMentions.js
+++ website/client/src/libs/renderWithMentions.js
@@ -3,5 +3,14 @@ import habiticaMarkdown from 'habitica-markdown/withMentions';
 export default function renderWithMentions (text, user) {
   if (!text) return null;
   const env = { userName: user.auth.local.username, displayName: user.profile.name };
-  return habiticaMarkdown.render(String(text), env);
+  let html = habiticaMarkdown.render(String(text), env);
+  if (user.profile.name && user.profile.name !== user.auth.local.username) {
+    const escapedDisplayName = user.profile.name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const displayNameRegex = new RegExp(
+      `(<span class="at-text) at-highlight(">@${escapedDisplayName}</span>)`,
+      'gi',
+    );
+    html = html.replace(displayNameRegex, '$1$2');
+  }
+  return html;
 }
