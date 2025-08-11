df030d50e31a685ed8efeb11d702fcd689fbecfb
Hafiz <hafizbhamidi@gmail.com>
2025-08-10 21:16:46 -0500

    Update G1G1 Notification
    - Updated text styling for title & description
    - Updated button styling
    - Updated close button

--- website/client/src/components/header/notifications/g1g1.vue
+++ website/client/src/components/header/notifications/g1g1.vue
@@ -25,24 +25,32 @@
         {{ $t('sendGift') }}
       </div>
     </div>
-    <div
-      class="notification-remove"
-      @click.stop="remove()"
-    >
-      <div
-        v-once
-        class="svg-icon"
-        v-html="icons.close"
-      ></div>
-    </div>
+    <close-x
+      @close="remove()"
+    />
   </div>
 </template>
 
 <style lang='scss' scoped>
   @import '@/assets/scss/colors.scss';
 
-  small, strong {
+  small {
     color: $white;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 24px;
+    letter-spacing: 0;
+  }
+
+  strong {
+    color: $white;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 24px;
   }
 
   .notification {
@@ -55,31 +63,22 @@
     cursor: pointer;
   }
 
-  .notification-remove {
-    position: absolute;
-    width: 18px;
-    height: 18px;
-    padding: 4px;
-    right: 24px;
-    top: 24px;
-
-    .svg-icon {
-      width: 10px;
-      height: 10px;
-    }
-  }
 
   .btn-secondary {
     min-width: 5.75rem;
     width: auto;
     max-width: calc(100% - 2rem);
-    min-height: 1.5rem;
+    min-height: 32px;
     padding: 0.25rem 0.75rem;
     border-radius: 2px;
     border-color: $white;
     box-shadow: 0 2px 2px 0 rgba(26, 24, 29, 0.16), 0 1px 4px 0 rgba(26, 24, 29, 0.12);
-    font-size: 12px;
-    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 24px;
+    color: $gray-50;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -87,17 +86,13 @@
 </style>
 
 <script>
-import closeIcon from '@/assets/svg/close-teal.svg?raw';
+import closeX from '@/components/ui/closeX';
 
 export default {
-  props: ['notification', 'eventKey'],
-  data () {
-    return {
-      icons: Object.freeze({
-        close: closeIcon,
-      }),
-    };
+  components: {
+    closeX,
   },
+  props: ['notification', 'eventKey'],
   methods: {
     remove () {
       if (this.eventKey) {
