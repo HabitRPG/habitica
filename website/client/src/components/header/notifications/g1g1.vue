2a4a9566ea2739a0d1a25963b1f4c5f74f403fd6
Hafiz <hafizbhamidi@gmail.com>
2025-08-11 10:18:05 -0500

    Add updated G1G1 notification SVGs

--- website/client/src/components/header/notifications/g1g1.vue
+++ website/client/src/components/header/notifications/g1g1.vue
@@ -1,30 +1,42 @@
 <template>
   <div
-    class="notification d-flex flex-column justify-content-center text-center"
+    class="notification d-flex justify-content-center align-items-center"
   >
-    <strong
-      v-once
-      class="mx-auto mb-2"
+    <img
+      src="@/assets/images/gifts_start.svg"
+      class="gift-start"
+      alt=""
     >
-      {{ $t('g1g1') }}
-    </strong>
-    <small
-      v-once
-      class="mx-4 mb-3"
-    >
-      {{ $t('g1g1Details') }}
-    </small>
-    <div
-      class="btn-secondary mx-auto d-flex"
-      @click="showSelectUser()"
-    >
-      <div
+    <div class="content-wrapper d-flex flex-column justify-content-center text-center">
+      <strong
         v-once
-        class="m-auto"
+        class="mx-auto mb-2"
       >
-        {{ $t('sendGift') }}
+        {{ $t('g1g1') }}
+      </strong>
+      <small
+        v-once
+        class="mx-4 mb-3"
+      >
+        {{ $t('g1g1Details') }}
+      </small>
+      <div
+        class="btn-secondary mx-auto d-flex"
+        @click="showSelectUser()"
+      >
+        <div
+          v-once
+          class="m-auto"
+        >
+          {{ $t('sendGift') }}
+        </div>
       </div>
     </div>
+    <img
+      src="@/assets/images/gifts_end.svg"
+      class="gift-end"
+      alt=""
+    >
     <close-x
       @close="remove()"
     />
@@ -54,13 +66,39 @@
   }
 
   .notification {
-    background-image: url('@/assets/images/g1g1-notif.png');
+    background-image: url('@/assets/images/gifts_bg.svg');
+    background-size: cover;
+    background-position: center;
     height: 10rem;
-    padding: 3rem;
+    padding: 0;
     position: relative;
     overflow: hidden;
     white-space: normal;
     cursor: pointer;
+  }
+
+  .content-wrapper {
+    flex: 1;
+    padding: 2rem;
+    z-index: 1;
+  }
+
+  .gift-start {
+    align-self: stretch;
+    height: 100%;
+    width: auto;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .gift-end {
+    align-self: stretch;
+    height: 100%;
+    width: auto;
+    position: absolute;
+    right: 0;
+    top: 0;
   }
 
   .btn-secondary {
