a91e18b7a387c646d13db8fcbd32287d4c8c7560
Hafiz <hafizbhamidi@gmail.com>
2025-08-19 16:48:01 -0500

    find mentions that match the current user's username (case-insensitive)

--- website/client/src/libs/renderWithMentions.js
+++ website/client/src/libs/renderWithMentions.js
@@ -1,7 +1,18 @@
 import habiticaMarkdown from 'habitica-markdown/withMentions';
+import escapeRegExp from 'lodash/escapeRegExp';
 
 export default function renderWithMentions (text, user) {
   if (!text) return null;
   const env = { userName: user.auth.local.username };
-  return habiticaMarkdown.render(String(text), env);
+  let html = habiticaMarkdown.render(String(text), env);
+  
+  if (user.auth.local.username) {
+    const username = escapeRegExp(user.auth.local.username);
+    const regex = new RegExp(`(<span class="at-text">@)(${username})(</span>)`, 'gi');
+    html = html.replace(regex, (match, p1, p2, p3) => {
+      return `${p1.replace('at-text', 'at-text at-highlight')}${p2}${p3}`;
+    });
+  }
+  
+  return html;
 }
