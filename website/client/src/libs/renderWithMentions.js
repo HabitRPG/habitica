aa24cafa1ec2b1c9f5a346f882f10e2b878baacd
Hafiz <hafizbhamidi@gmail.com>
2025-08-19 16:51:19 -0500

    fix lint errors

--- website/client/src/libs/renderWithMentions.js
+++ website/client/src/libs/renderWithMentions.js
@@ -5,14 +5,12 @@ export default function renderWithMentions (text, user) {
   if (!text) return null;
   const env = { userName: user.auth.local.username };
   let html = habiticaMarkdown.render(String(text), env);
-  
+
   if (user.auth.local.username) {
     const username = escapeRegExp(user.auth.local.username);
     const regex = new RegExp(`(<span class="at-text">@)(${username})(</span>)`, 'gi');
-    html = html.replace(regex, (match, p1, p2, p3) => {
-      return `${p1.replace('at-text', 'at-text at-highlight')}${p2}${p3}`;
-    });
+    html = html.replace(regex, (match, p1, p2, p3) => `${p1.replace('at-text', 'at-text at-highlight')}${p2}${p3}`);
   }
-  
+
   return html;
 }
