ef66387c45c7b55f9e20312240291a8d74d6b3c3
Phillip Thelen <phillip@habitica.com>
2025-08-19 17:06:20 +0200

    fix sub state display

--- website/client/src/components/admin/admin-panel/user-support/subscriptionAndPerks.vue
+++ website/client/src/components/admin/admin-panel/user-support/subscriptionAndPerks.vue
@@ -16,13 +16,13 @@
         >
           Subscription, Monthly Perks
           <span
-            v-if="hero.purchased.plan.customerId && !hero.purchased.plan.dateTerminated"
+            v-if="isSubscribed && !isCancelled"
             class="text-success float-right"
           >
             Active
           </span>
           <span
-            v-else-if="isSubscribed"
+            v-else-if="isSubscribed && isCancelled"
             class="text-success float-right"
           >
             Active until {{ dateFormat(hero.purchased.plan.dateTerminated) }}
@@ -793,11 +793,19 @@ export default {
       return date ? moment(date).format('MM/DD/YYYY') : '---';
     },
     isSubscribed () {
-      return this.hero.purchased.plan && this.hero.purchased.plan.customerId && (
+      return this.hero.purchased.plan
+        && this.hero.purchased.plan.customerId
+        && this.hero.purchased.plan.customerId !== ''
+        && (
         this.hero.purchased.plan.dateTerminated === null
         || moment(this.hero.purchased.plan.dateTerminated).isAfter(moment())
       );
     },
+    isCancelled () {
+      return this.hero.purchased.plan
+        && this.hero.purchased.plan.dateTerminated
+        && this.hero.purchased.plan.dateTerminated !== ''
+    }
   },
 };
 </script>
