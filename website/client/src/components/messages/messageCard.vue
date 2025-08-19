e32c727b3aef1c1abb5c08f58a21d26a69705a58
Hafiz <hafizbhamidi@gmail.com>
2025-08-19 16:15:04 -0500

    Revert display name mention, strictly only username
    Mentions work w/username only (works w/case insensitive as well)

--- website/client/src/components/messages/messageCard.vue
+++ website/client/src/components/messages/messageCard.vue
@@ -459,15 +459,8 @@ export default {
       }
       const { user } = this;
       const { username } = user.auth.local;
-      const displayName = user.profile?.name;
-      if (username) {
-        const usernamePattern = `@${escapeRegExp(username)}(\\b)`;
-        message.highlight = new RegExp(usernamePattern, 'i').test(message.text);
-      }
-      if (!message.highlight && displayName && displayName !== username) {
-        const displayNamePattern = `@${escapeRegExp(displayName)}(\\b)`;
-        message.highlight = new RegExp(displayNamePattern, 'i').test(message.text);
-      }
+      const pattern = `@${escapeRegExp(username)}(\\b)`;
+      message.highlight = new RegExp(pattern, 'i').test(message.text);
       return message.highlight;
     },
     flagCountDescription () {
--- website/client/src/libs/renderWithMentions.js
+++ website/client/src/libs/renderWithMentions.js
@@ -2,9 +2,6 @@ import habiticaMarkdown from 'habitica-markdown/withMentions';
 
 export default function renderWithMentions (text, user) {
   if (!text) return null;
-  const env = { 
-    userName: user.auth.local.username,
-    displayName: user.profile?.name,
-  };
+  const env = { userName: user.auth.local.username };
   return habiticaMarkdown.render(String(text), env);
 }
