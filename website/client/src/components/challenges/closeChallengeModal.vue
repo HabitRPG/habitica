639f2ec2ff898542fdc1b47baeacdde026f74560
Hafiz <hafizbhamidi@gmail.com>
2025-08-21 13:22:15 -0500

    Add font details (and fix text color) for button disabled state

--- website/client/src/components/challenges/closeChallengeModal.vue
+++ website/client/src/components/challenges/closeChallengeModal.vue
@@ -285,13 +285,21 @@
 
       &:disabled {
         cursor: default;
-        color: #FFFFFF;
+        color: $gray-200;
         opacity: 1;
         background-color: transparent;
         border: 2px solid transparent;
         box-shadow:
           0 1px 3px 0 rgba(#1A1B1D, 0.12),
           0 1px 2px 0 rgba(#1A1B1D, 0.24);
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 24px;
+
+        .gem-icon {
+          color: $gray-200;
+        }
       }
 
       .gem-icon {
