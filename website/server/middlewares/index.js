c331679f583084477ddaf4c0c9036157e47edebe
Kalista Payne <kalista@habitica.com>
2025-11-25 09:27:05 -0600

    feat(security): implement CSP

--- website/server/middlewares/index.js
+++ website/server/middlewares/index.js
@@ -68,7 +68,19 @@ export default function attachMiddlewares (app, server) {
   // See https://helmetjs.github.io/ for the list of headers enabled by default
   app.use(helmet({
     // New middlewares added by default in Helmet 4 are disabled
-    contentSecurityPolicy: false, // @TODO implement
+    contentSecurityPolicy: {
+      directives: {
+        imgSrc: null,
+        scriptSrc: [
+          '\'self\'',
+          'cloudfront.loggly.com',
+          'js.stripe.com',
+          'm.stripe.network',
+          'static-na.payments-amazon.com',
+        ],
+        upgradeInsecureRequests: IS_PROD ? [] : null,
+      },
+    },
     expectCt: false,
     permittedCrossDomainPolicies: false,
     referrerPolicy: false,
