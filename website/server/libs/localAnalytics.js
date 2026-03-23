0e3e80bd086a493e76e2787ed3587e914e5f2a10
Phillip Thelen <phillip@habitica.com>
2026-03-23 17:41:07 +0100

    track registration events

---
+++ website/server/libs/localAnalytics.js
@@ -0,0 +1,50 @@
+import nconf from 'nconf';
+import { RegistrationEventModel } from '../models/analytics/registrationEvent';
+import { SubscriptionEventModel } from '../models/analytics/subscriptionEvent';
+
+const LOCAL_ANALYTICS = nconf.get('LOCAL_ANALYTICS');
+
+function getAuthenticationMethod (user) {
+  if (user.auth.local) return 'local';
+  if (user.auth.google) return 'google';
+  if (user.auth.facebook) return 'facebook';
+  if (user.auth.apple) return 'apple';
+  return 'unknown';
+}
+
+export function trackRegistrationEvent (eventData) {
+  if (!LOCAL_ANALYTICS) return null;
+
+  const { user, ipAddress } = eventData;
+
+  const registrationEvent = new RegistrationEventModel({
+    userId: user._id,
+    ipAddress,
+    authenticationMethod: getAuthenticationMethod(user),
+    platform: user.registeredThrough,
+  });
+  return registrationEvent.save();
+}
+
+export function trackSubscriptionEvent (eventData) {
+  if (!LOCAL_ANALYTICS) return null;
+
+  const {
+    eventType,
+    user,
+    paymentMethod,
+    customerId,
+    planId,
+    cancellationReason,
+  } = eventData;
+
+  const subscriptionEvent = new SubscriptionEventModel({
+    userId: user._id,
+    eventType,
+    paymentMethod,
+    customerId,
+    planId,
+    cancellationReason,
+  });
+  return subscriptionEvent.save();
+}
