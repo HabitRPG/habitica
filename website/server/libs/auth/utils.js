e3b18da7fe3386b8fa9c89eaeab689788bf9aa44
Kalista Payne <kalista@habitica.com>
2026-04-01 17:35:34 -0500

    fix(auth): still wrong place argh

--- website/server/libs/auth/utils.js
+++ website/server/libs/auth/utils.js
@@ -35,7 +35,6 @@ export function loginRes (user, req, res) {
     apiToken: user.apiToken,
     newUser: user.newUser || false,
     username: user.auth.local.username,
-    email: user.auth.local.email,
   };
   return res.respond(200, responseData);
 }
