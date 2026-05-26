68a83e363d03fdd6fd22acd509010d59119474cc
Kalista Payne <kalista@habitica.com>
2026-05-26 15:51:52 -0500

    fix(lint): various

--- website/client/src/router/index.js
+++ website/client/src/router/index.js
@@ -260,9 +260,12 @@ const router = new VueRouter({
     // Only used to handle some redirects
     // See router.beforeEach
     { path: '/static/tavern-and-guilds', redirect: '/static/faq/tavern-and-guilds' },
-    { path: '/apidoc', component: NotFoundPage, beforeEnter() {
-      window.location.href = 'https://apidoc.habitica.com';
-    }},
+    {
+      path: '/apidoc',
+      component: NotFoundPage, beforeEnter() {
+        window.location.href = 'https://apidoc.habitica.com';
+      },
+    },
     { path: '/redirect/:redirect', name: 'redirect' },
     { path: '*', redirect: { name: 'notFound' } },
   ],
