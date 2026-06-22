a151e6b6fef99c315e8c43b0f0f4c598bc656cbb
Kalista Payne <kalista@habitica.com>
2026-06-22 17:42:48 -0500

    feat(events): ad hoc event via env

--- website/server/libs/worldState.js
+++ website/server/libs/worldState.js
@@ -1,9 +1,31 @@
 import moment from 'moment';
+import nconf from 'nconf';
 import { // eslint-disable-line import/no-cycle
   model as Group,
   TAVERN_ID as tavernId,
 } from '../models/group';
 import common from '../../common';
+import { gemsPromo } from '../../common/script/content/constants/events';
+
+const AD_HOC_EVENT_BEGIN = nconf.get('AD_HOC_EVENT_BEGIN');
+const AD_HOC_EVENT_END = nconf.get('AD_HOC_EVENT_END');
+const AD_HOC_EVENT_GEMS = nconf.get('AD_HOC_EVENT_GEMS');
+const AD_HOC_EVENT_KEY = nconf.get('AD_HOC_EVENT_KEY');
+
+function adHocEvent () {
+  if (!moment().isBetween(AD_HOC_EVENT_BEGIN, AD_HOC_EVENT_END)) {
+    return null;
+  }
+  const event = {
+    start: AD_HOC_EVENT_BEGIN,
+    end: AD_HOC_EVENT_END,
+    event: AD_HOC_EVENT_KEY,    
+  };
+  if (AD_HOC_EVENT_GEMS === 'true') {
+    event.gemsPromo = gemsPromo;
+  }
+  return event;
+}
 
 export async function getWorldBoss () {
   const tavern = await Group.findById(tavernId)
@@ -31,6 +53,10 @@ export function getCurrentEvent () {
 export function getCurrentEventList () {
   const now = moment();
   const currentEvents = common.content.getRepeatingEventsOnDate(now);
+  const extraEvent = adHocEvent();
+  if (extraEvent) {
+    currentEvents.push(extraEvent);
+  }
   currentEvents.push(common.schedule.getCurrentGalaEvent());
   return currentEvents;
 }
