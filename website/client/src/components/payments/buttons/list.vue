153548d288032543ed39bacc35fb3f5f98c72029
Kalista Payne <kalista@habitica.com>
2026-07-13 16:21:15 -0500

    Revert "fix(payments): button fixes"
    This reverts commit 52d255b2642226f9d7132d8da4b92a131990e037.

--- website/client/src/components/payments/buttons/list.vue
+++ website/client/src/components/payments/buttons/list.vue
@@ -9,8 +9,8 @@
       @click="stripeFn()"
     >
       <div
-        class="svg-icon icon-16 color"
-        :class="{'mr-2 white': !disabled, 'gray-200': disabled}"
+        v-once
+        class="svg-icon color credit-card-icon"
         v-html="icons.creditCardIcon"
       ></div>
       {{ $t('card') }}
@@ -40,10 +40,6 @@
     width: 296px;
     justify-content: center;
 
-    .svg-icon {
-      transition: none;
-    }
-
     .payment-item {
       margin-bottom: 12px;
       display: flex;
@@ -53,8 +49,13 @@
         justify-content: center;
         align-items: center;
 
+        .credit-card-icon {
+          width: 21.3px;
+          height: 16px;
+          margin-right: 8.7px;
+        }
+
         &.paypal-checkout {
-          height: 32px;
           background: #009cde;
 
           img {
