cea3ae1f6dfe61e817dbe0f3a864dc9506d20b04
Phillip Thelen <phillip@habitica.com>
2026-08-11 12:51:05 +0200

    update passport

--- website/server/controllers/top-level/auth.js
+++ website/server/controllers/top-level/auth.js
@@ -26,7 +26,7 @@ api.logout = {
   method: 'GET',
   url: '/logout-server',
   async handler (req, res) {
-    if (req.logout) req.logout(); // passportjs method
+    if (req.logout) await req.logout();
     req.session = null;
 
     const redirectUrl = req.query.redirectToLogin === 'true' ? '/login' : '/';
